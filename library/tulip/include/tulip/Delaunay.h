d5394411d1c2a5f5089cee8b03e1bff36bccf4f5
David Auber <david.auber@labri.fr>
2005-06-02 22:09:39 +0000

    Add header files for Delaunay triangulation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@42 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip/include/tulip/Delaunay.h
@@ -0,0 +1,48 @@
+#ifndef TLP_GEO_DELAUNAY_H
+#define TLP_GEO_DELAUNAY_H
+#include <vector>
+#include <tulip/Coord.h>
+#include <tulip/Node.h>
+
+class SuperGraph;
+class LayoutProxy;
+template<typename TYPE>
+class  MutableContainer;
+
+namespace tlp {
+  /**
+   * \addtogroup basic
+   */ 
+  /*@{*/
+    /**
+     * \brief functions for Delaunay Triangulations
+     *
+     * Used to triangulate a set of points in the plane
+     *
+     * Author : <a href="mailto:auber@tulip-software.org>David Auber/Daniel Archambault</A>
+     * Adapted from an implementation of Sjaak Priester from Amsterdam (see source for details)
+     * Version 0.0.1 02/06/2005
+     */
+  /**
+   * Compute the delaunay triangulation and return the set of edges of the triangulation
+   * in the vector edges.  Edges are defined using a pair of indexes into the original set
+   * of points.
+   */
+  void delaunayTriangulation (const std::vector<Coord> &points, 
+			      std::vector<std::pair<unsigned int, unsigned int> > &edges);
+  /**
+   * Compute the delaunay triangulation and return the set of triangles of the triangulation
+   * in the vector triangles.  Triangles are defined using a triple indexes into the original 
+   * set of points.
+   */
+  void delaunayTriangulation (const std::vector<Coord> &points, 
+			      std::vector<tlp::Array<unsigned int, 3> > &triangles);
+  /**
+   * TO BE IMPLEMENTED  
+   */
+  SuperGraph *delaunayTriangulation (SuperGraph *inputGraph, LayoutProxy *inputCoord, 
+				     MutableContainer<node> &nodeMapping);
+
+    /*@}*/
+}
+#endif
