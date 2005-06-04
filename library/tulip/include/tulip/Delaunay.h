c95a27d5e2b373d1499b3aa325d23232b9bb1729
Daniel Archambault <daniel.archambault@labri.fr>
2005-06-04 23:41:03 +0000

    Delaunay implementation uses triangle by Jonathan Richard Shewchuk.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@50 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/Delaunay.h
+++ library/tulip/include/tulip/Delaunay.h
@@ -20,28 +20,36 @@ namespace tlp {
      * Used to triangulate a set of points in the plane
      *
      * Author : <a href="mailto:auber@tulip-software.org>David Auber/Daniel Archambault</A>
-     * Adapted from an implementation of Sjaak Priester from Amsterdam (see source for details)
+     * 
      * Version 0.0.1 02/06/2005
      */
   /**
-   * Compute the delaunay triangulation and return the set of edges of the triangulation
-   * in the vector edges.  Edges are defined using a pair of indexes into the original set
+   * Compute the delaunay triangulation and return the set of edges of the 
+   * triangulation in the vector edges.  Edges are defined using a pair of 
+   * indexes into the original set
    * of points.
    */
   void delaunayTriangulation (const std::vector<Coord> &points, 
 			      std::vector<std::pair<unsigned int, unsigned int> > &edges);
   /**
-   * Compute the delaunay triangulation and return the set of triangles of the triangulation
-   * in the vector triangles.  Triangles are defined using a triple indexes into the original 
+   * Compute the delaunay triangulation and return the set of triangles of the 
+   * triangulation in the vector triangles.  Triangles are defined using a 
+   * triple indexes into the original 
    * set of points.
    */
   void delaunayTriangulation (const std::vector<Coord> &points, 
-			      std::vector<tlp::Array<unsigned int, 3> > &triangles);
+		std::vector<tlp::Array<unsigned int, 3> > &triangles);
+
   /**
-   * TO BE IMPLEMENTED  
+   * Compute the delaunay triangulation and return the set of edges in the
+   * vector edge and triangles of the triangulation in the vector triangles.  
+   * Triangles are defined using a triple indexes into the original 
+   * set of points.
    */
-  SuperGraph *delaunayTriangulation (SuperGraph *inputGraph, LayoutProxy *inputCoord, 
-				     MutableContainer<node> &nodeMapping);
+  
+  void delaunayTriangulation (const std::vector<Coord> &points,
+     std::vector<std::pair<unsigned int, unsigned int> > &edges,
+     std::vector< tlp::Array<unsigned int, 3> > &triangles);
 
     /*@}*/
 }
--- library/tulip/src/Delaunay.cpp
+++ library/tulip/src/Delaunay.cpp
@@ -2,71 +2,142 @@
 #include <map>
 #include <iostream>
 #include <tulip/Delaunay.h>
-#include <tulip/DelaunayImpl.h>
-
+#define ANSI_DECLARATORS //ansi style declarations for triangle.h
+#include "thirdparty/triangle/triangle.h"
 
 using namespace std;
-using namespace delaunayImpl;
+using namespace triangle_1_5;
 
 //==============================================================
-namespace {
-void triangulationImpl (const std::vector<Coord> &points,
-			vertexSet &pointSet,
-			map<vertex, unsigned int> &vertexToIndex,
-			triangleSet &triangles,
-			Delaunay &triangulation)
- {
+//a function to initialize memory before a triangle call
+//triangleInput is the "triangle" algorithm input structure while
+//triangleOutput is the output structure
+inline void initTriangle (triangulateio &triangleInput,
+			  triangulateio &triangleOutput) {
+  triangleInput.pointmarkerlist = NULL;
+  triangleInput.numberofpointattributes = 0;
+  triangleInput.numberoftriangleattributes = 0;
+  triangleOutput.pointlist = NULL;
+  triangleOutput.pointmarkerlist = NULL;
+  triangleOutput.numberofpointattributes = 0;
+  triangleOutput.pointattributelist = NULL; 
+  triangleOutput.trianglelist = NULL;
+  triangleOutput.triangleattributelist = NULL;
+  triangleOutput.trianglearealist = NULL;
+  triangleOutput.neighborlist = NULL;
+  triangleOutput.segmentlist = NULL;
+  triangleOutput.segmentmarkerlist = NULL;
+  triangleOutput.holelist = NULL;
+  triangleOutput.regionlist = NULL;
+  triangleOutput.edgelist = NULL;
+  triangleOutput.edgemarkerlist = NULL;
+  triangleOutput.normlist = NULL;
+}//end initTriangle
+
+//a function to clean up memory after a triangle call
+inline void cleanupTriangle (triangulateio &triangleOutput) {
+  if (triangleOutput.pointlist != NULL)
+    free (triangleOutput.pointlist);
+  if (triangleOutput.pointattributelist != NULL) 
+    free (triangleOutput.pointattributelist);
+  if (triangleOutput.pointmarkerlist != NULL) 
+    free (triangleOutput.pointmarkerlist);
+  if (triangleOutput.trianglelist != NULL)
+    free (triangleOutput.trianglelist);
+  if (triangleOutput.triangleattributelist != NULL)
+    free (triangleOutput.triangleattributelist);
+  if (triangleOutput.trianglearealist != NULL)
+    free (triangleOutput.trianglearealist);
+  if (triangleOutput.neighborlist != NULL)
+    free (triangleOutput.neighborlist);
+  if (triangleOutput.segmentlist != NULL)
+    free (triangleOutput.segmentlist);
+  if (triangleOutput.segmentmarkerlist != NULL)
+    free (triangleOutput.segmentmarkerlist);
+  if (triangleOutput.holelist != NULL)
+    free (triangleOutput.holelist);
+  if (triangleOutput.regionlist != NULL)
+    free (triangleOutput.regionlist);
+  if (triangleOutput.edgelist != NULL)
+    free (triangleOutput.edgelist);
+  if (triangleOutput.edgemarkerlist != NULL)
+    free (triangleOutput.edgemarkerlist);
+  if (triangleOutput.normlist != NULL)
+    free (triangleOutput.normlist);
+}//end cleanupTriangle
+
+//A function to compute the triangulation using triangle.  The
+//solution is written into triangleOutput.  The two booleans indicate
+//if the edge list or the triangle list should be computed.
+void runTriangle (const vector<Coord> &points, bool genEdges,
+		  vector<pair<unsigned int, unsigned int> >&edgeList,
+		  bool genTriangles,
+		  vector< tlp::Array<unsigned int, 3> > &triangleList) {
+  //set up the input for triangle
+  REAL pointlist [2*points.size()];
+  unsigned int pointCounter = 0;
   vector<Coord>::const_iterator it = points.begin();
-  unsigned int index = 0;
-  for (; it != points.end(); ++it, ++index) {
-    vertex vertex_tmp(it->getX(), it->getY());
-    pointSet.insert(vertex_tmp);
-    vertexToIndex[vertex_tmp] = index;
+  for (; it != points.end(); ++it) {
+    pointlist[pointCounter++] = (*it).getX();
+    pointlist[pointCounter++] = (*it).getY();
   }//end for
-  triangulation.Triangulate(pointSet, triangles);
-}
-}
-//==============================================================
-void tlp::delaunayTriangulation (const std::vector<Coord> &points, 
-			   std::vector<std::pair<unsigned int, unsigned int> > &edges) {
-  vertexSet pointSet;
-  map<vertex, unsigned int> vertexToIndex;
-  triangleSet triangles;
-  Delaunay triangulation;
-  triangulationImpl (points, pointSet, vertexToIndex, triangles, triangulation);
-  edgeSet edge_set;
-  triangulation.TrianglesToEdges(triangles, edge_set);
-  set<edge>::const_iterator ite = edge_set.begin();
-  for (; ite != edge_set.end(); ++ite) {
-    pair<unsigned int, unsigned int> curEdge;
-    curEdge.first = vertexToIndex[*(ite->m_pV0)];    
-    curEdge.second = vertexToIndex[*(ite->m_pV1)];
-    edges.push_back(curEdge);
-  }//end for
-}
-//==============================================================
-void tlp::delaunayTriangulation (const std::vector<Coord> &points, 
-			    std::vector< tlp::Array<unsigned int, 3> > &triangles) {
-  vertexSet pointSet;
-  map<vertex, unsigned int> vertexToIndex;
-  triangleSet triangles_set;
-  Delaunay triangulation;
-  triangulationImpl (points, pointSet, vertexToIndex, triangles_set, triangulation);
-  triangleSet::const_iterator it= triangles_set.begin();
-  triangles.resize(triangles_set.size());
-  unsigned int ii;
-  for (ii=0; it != triangles_set.end() ; ++it, ++ii) {
-    triangle tmp = *it;
-    tlp::Array<unsigned int, 3> curTriangle;
-    for (int i = 0; i<3; ++i) {
-      curTriangle[i] = vertexToIndex[*tmp.GetVertex(i)];
+  
+  //flags for triangle 'z' option allows 0 indexing of arrays and
+  //the Q flag allows for quiet operation of triangle.
+  //P, N, E, suppresses text file output
+  //then make the triangle call
+  triangulateio triangleInput, triangleOutput;
+  initTriangle (triangleInput, triangleOutput);
+  triangleInput.numberofpoints = points.size();
+  triangleInput.pointlist = pointlist;
+  string triangleInputFlags ("zQPB");
+  if (genEdges) triangleInputFlags += string ("e");
+    triangulate (const_cast<char *> (triangleInputFlags.c_str()), 
+		 &triangleInput, &triangleOutput, NULL);
+  
+  //generate the triangle list if required
+  if (genTriangles) {
+    for (int i = 0; i < triangleOutput.numberoftriangles; ++i) {
+      tlp::Array<unsigned int, 3> curTriangle;
+      curTriangle[0] = triangleOutput.trianglelist[3*i];
+      curTriangle[1] = triangleOutput.trianglelist[3*i + 1];
+      curTriangle[2] = triangleOutput.trianglelist[3*i + 2];
+      triangleList.push_back (curTriangle);
     }//end for
-    triangles[ii] = curTriangle;
-  }//end for
+  }//end if
+  
+  //generate the edgelist if required
+  if (genEdges) {
+    for (int i = 0; i < triangleOutput.numberofedges; ++i) {
+      pair<unsigned int, unsigned int> curEdge;
+      curEdge.first = triangleOutput.edgelist[2*i];
+      curEdge.second = triangleOutput.edgelist[2*i + 1];
+      edgeList.push_back (curEdge);
+    }//end for i
+  }//end if
+  
+  //clean up memory allocated by triangle
+  cleanupTriangle (triangleOutput);
+}
+
+//==============================================================
+void tlp::delaunayTriangulation (const std::vector<Coord> &points, 
+				 vector<pair<unsigned int, unsigned int> > &edges) {
+  if (points.size() < 3) return; //cannot triangulate less than three points
+  vector< tlp::Array<unsigned int, 3> > triangles;
+  runTriangle (points, true, edges, false, triangles);
 }
 //==============================================================
-SuperGraph *tlp::delaunayTriangulation (SuperGraph *inputGraph, LayoutProxy *inputCoord, 
-				   MutableContainer<node> &nodeMapping){
-  cerr << __PRETTY_FUNCTION__ << " : NOT IMPLEMENTED." << endl;
-  return 0;
+void tlp::delaunayTriangulation (const std::vector<Coord> &points, 
+		       vector< tlp::Array<unsigned int, 3> > &triangles) {
+  if (points.size() < 3) return; //cannot triangulate less than three points
+  vector<pair<unsigned int, unsigned int> > edges;
+  runTriangle (points, false, edges, true, triangles);
+}
+//==============================================================
+void tlp::delaunayTriangulation (const std::vector<Coord> &points,
+		       vector<pair<unsigned int, unsigned int> > &edges,
+		       vector< tlp::Array<unsigned int, 3> > &triangles) {
+  if (points.size() < 3) return; //cannot triangulate less than three points
+  runTriangle (points, true, edges, true, triangles);
 }
