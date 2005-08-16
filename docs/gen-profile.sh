020b37817da4fe39e6b4c6893685baeeb01d0620
Sophie Bardet <bardet@mycompany.com>
2005-08-16 09:35:55 +0000

    update
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@113 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- docs/gen-profile.sh
+++ docs/gen-profile.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+QTDOCDIR=""
+for A in `find /usr -name "assistant.html"`
+do
+	QTDOCDIR=`dirname $A` 
+done
+
+if [ -z "$QTDOCDIR" ]
+then
+	QTDOCDIR=$QTDIR/doc/html 
+fi
 
 echo "<assistantconfig version=\"3.2.0\"><profile>" > profile.adp
 echo "<property name=\"name\">Help</property>" >> profile.adp
@@ -6,10 +16,13 @@ echo "<property name=\"title\">Help Tulip software</property>" >> profile.adp
 echo "<property name=\"applicationicon\">common/logo-icon.png</property>" >> profile.adp
 echo "<property name=\"startpage\">userHandbook/index.html</property>" >> profile.adp 
 echo "<property name=\"aboutmenutext\">About Manuals</property>" >> profile.adp
-echo "<property name=\"abouturl\">www.tulip-software.org</property>" >> profile.adp
-echo "<property name=\"assistantdocs\">common</property> </profile>" >> profile.adp
+#echo "<property name=\"abouturl\"></property>" >> profile.adp
+echo "<property name=\"assistantdocs\">$QTDOCDIR</property> </profile>" >> profile.adp
 
 cat developerHandbook/profile.dcf >> profile.adp
 cat userHandbook/profile.dcf >> profile.adp
 
+echo "<dcf ref=\"doxygen/tulip-lib/html/index.html\" title=\"Tulip Library\">" >> profile.adp
+echo "</dcf>" >> profile.adp
 echo "</assistantconfig>" >> profile.adp
+
