b1d4c262a883a03aea1039123193921a29cd874e
Patrick Mary <mary@labri.fr>
2006-01-20 09:28:23 +0000

    old clustering plugins externalization
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@232 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ externalplugins/rectangleclustering/ArityRecClustering.h
@@ -0,0 +1,51 @@
+//-*-c++-*-
+#ifndef _ARITYRECCLUSTERING_H
+#define _ARITYRECCLUSTERING_H
+
+#include <tulip/TulipPlugin.h>
+/// ArityRecClustering.h - a tree clustering algorithm.
+/** This plugin implement the statistical tree clustering algorithm 
+ *  first published as :
+ *
+ *  D. Auber and M. Delest,
+ *  "A clustering algorithm for huge trees",
+ *  "Advances in Applied Mathematics",
+ *  "To appear (Accepted 2002)",
+ *  pages "1--14"
+ *  publisher "Academic press"
+ *
+ *  <b> HISTORY </b>
+ *
+ *  - 01/01/2002 Verson 0.0.1: Initial release
+ *
+ *  \note This version is the one which use only the arity to make clustering.
+ *
+ *  \author David Auber University Bordeaux I France, Email : auber@tulip-software.com
+ *
+ *  <b> LICENCE </b>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by  
+ *  the Free Software Foundation; either version 2 of the License, or     
+ *  (at your option) any later version.
+ *
+ */
+class ArityRecClustering:public Clustering { 
+public:
+  ArityRecClustering(ClusterContext);
+  ~ArityRecClustering();
+  bool run();
+  bool check(std::string &);
+  void reset();
+
+private:
+  MetricProxy *arityM;
+  MetricProxy *leafM;
+  MetricProxy *nodeM;
+  int mResult;
+  double inter;
+  bool DfsClustering (node ,SelectionProxy *,SelectionProxy *);
+  void getRecurseChild(node,SelectionProxy *,SelectionProxy *);
+};
+
+#endif
---
+++ externalplugins/rectangleclustering/SegmentRecClustering.h
@@ -0,0 +1,53 @@
+//-*-c++-*-
+#ifndef _SEGMENTRECCLUSTERING_H
+#define _SEGMENTRECCLUSTERING_H
+
+#include <tulip/TulipPlugin.h>
+/// SegmentRecClustering.h - A tree clustering algorithm.
+/** This plugin implement the statistical tree clustering algorithm 
+ *  first published as :
+ *
+ *  D. Auber and M. Delest,
+ *  "A clustering algorithm for huge trees",
+ *  "Advances in Applied Mathematics",
+ *  "To appear (Accepted 2002)",
+ *  pages "1--14"
+ *  publisher "Academic press"
+ *
+ *  <b> HISTORY </b>
+ *
+ *  - 01/01/2002 Verson 0.0.1: Initial release
+ *
+ *  
+ *  \note This version is the one which use only maximum segment length to make clustering.
+ *
+ *  
+ *  \author David Auber University Bordeaux I France, Email : auber@tulip-software.com
+ *
+ *  <b> LICENCE </b>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by  
+ *  the Free Software Foundation; either version 2 of the License, or     
+ *  (at your option) any later version.
+ *
+ */
+class SegmentRecClustering:public Clustering { 
+public:
+  SegmentRecClustering(ClusterContext);
+  ~SegmentRecClustering();
+  bool run();
+  bool check(std::string &);
+  void reset();
+
+private:
+  bool DfsClustering (node ,SelectionProxy *,SelectionProxy *);
+  void getRecurseChild(node,SelectionProxy *,SelectionProxy *);
+  MetricProxy *segmentM;
+  MetricProxy *leafM;
+  MetricProxy *nodeM;
+  int mResult;
+  double inter;
+};
+
+#endif
