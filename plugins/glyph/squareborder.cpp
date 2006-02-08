64c45c9fd488f5b2607d1ec711771b70cf19cfd5
David Auber <david.auber@labri.fr>
2006-02-08 12:47:37 +0000

    indentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@267 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/glyph/squareborder.cpp
+++ plugins/glyph/squareborder.cpp
@@ -28,26 +28,26 @@ using namespace std;
 //====================================================================
 class SquareBorder: public Glyph {
 public:
-    SquareBorder(GlyphContext* gc = NULL);
-    virtual ~SquareBorder();
-    virtual string getName() {
-        return string("SquareBorder");
-    }
-    virtual void  draw(node n);
-    virtual Coord getAnchor(const Coord& vector) const;
-
+  SquareBorder(GlyphContext* gc = NULL);
+  virtual ~SquareBorder();
+  virtual string getName() {
+    return string("SquareBorder");
+  }
+  virtual void  draw(node n);
+  virtual Coord getAnchor(const Coord& vector) const;
+  
 protected:
-    GLuint LList;
-    bool   listOk;
-    void   drawSquare();
-
+  GLuint LList;
+  bool   listOk;
+  void   drawSquare();
+  
 private:
-    static float 	   borderProportion;
-    const static float BORDER_COLOR[3];
-    const static float MAX_BORDER_COEF;
-    SuperGraph*        tree;
-    float 			   calculateDepth(node n);
-    float 			   calculateBorderSize(node n);
+  static float 	   borderProportion;
+  const static float BORDER_COLOR[3];
+  const static float MAX_BORDER_COEF;
+  SuperGraph*        tree;
+  float calculateDepth(node n);
+  float calculateBorderSize(node n);
 };
 
 //====================================================================
@@ -67,168 +67,158 @@ SquareBorder::SquareBorder(GlyphContext* gc):Glyph(gc),listOk(false){
 
 //====================================================================
 SquareBorder::~SquareBorder() {
-    if (listOk)
-        if (glIsList(LList))
-            glDeleteLists(LList, 1);
+  if (listOk)
+    if (glIsList(LList))
+      glDeleteLists(LList, 1);
 }
-
 //====================================================================
 void SquareBorder::draw(node n) {
-    tree = (*superGraph);
-    borderProportion  = calculateBorderSize(n);
-    Color borderColor = glGraph->elementColor->getNodeValue(n);
-    setMaterial(borderColor);
-    string texFile = glGraph->elementTexture->getNodeValue(n);
+  tree = (*superGraph);
+  borderProportion  = calculateBorderSize(n);
+  Color borderColor = glGraph->elementColor->getNodeValue(n);
+  setMaterial(borderColor);
+  string texFile = glGraph->elementTexture->getNodeValue(n);
     
-    if (texFile != "") {
-        if (glGraph->activateTexture(texFile))
-            setMaterial(Color(255, 255, 255, 0));
-    }
-
-    LList = glGenLists(1);
-    glNewList(LList, GL_COMPILE); 
-    drawSquare();
-    glEndList();
-    glCallList(LList);
+  if (texFile != "") {
+    if (glGraph->activateTexture(texFile))
+      setMaterial(Color(255, 255, 255, 0));
+  }
+  
+  drawSquare();
 }
 
 //====================================================================
 Coord SquareBorder::getAnchor(const Coord& vector) const {
-    Coord v(vector);
-    float x, y, z, fMax;
-    v.get(x, y, z);
-    v.setZ(0.0f);
-    fMax = max(fabsf(x), fabsf(y));
-    if (fMax > 0.0f)
-        return v * (0.5f / fMax);
-    else
-        return v;
+  Coord v(vector);
+  float x, y, z, fMax;
+  v.get(x, y, z);
+  v.setZ(0.0f);
+  fMax = max(fabsf(x), fabsf(y));
+  if (fMax > 0.0f)
+    return v * (0.5f / fMax);
+  else
+    return v;
 }
 
 //====================================================================
 void SquareBorder::drawSquare() {
-    float borderCalc = borderProportion;     
-    float relatifBorderCalc = 0.5f * borderCalc;
-    glPushAttrib(GL_LIGHTING_BIT);
+  float borderCalc = borderProportion;     
+  float relatifBorderCalc = 0.5f * borderCalc;
+  glPushAttrib(GL_LIGHTING_BIT);
 
-    glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, BORDER_COLOR);
+  glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE, BORDER_COLOR);
 
-    glBegin(GL_TRIANGLE_STRIP);
+  glBegin(GL_TRIANGLE_STRIP);
 
-    glNormal3f(0.0f, 0.0f, 1.0f);
+  glNormal3f(0.0f, 0.0f, 1.0f);
 
-    // front face 
-    glVertex2f(0.5f, 0.5f);                             
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(0.5f, -0.5f);                            
+  // front face 
+  glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(0.5f, -0.5f);                            
 
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(0.5f, -0.5f);                            
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(0.5f, -0.5f);                            
 
-    glVertex2f(0.5f, -0.5f);                            
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(0.5f, -0.5f);                            
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(-0.5f, -0.5f);                           
 
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-0.5f, -0.5f);                           
 
-    glVertex2f(-0.5f, -0.5f);                           
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-0.5f, 0.5f);                            
+  glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-0.5f, 0.5f);                            
 
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(-0.5f, 0.5f);                            
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(-0.5f, 0.5f);                            
 
-    glVertex2f(-0.5f, 0.5f);                            
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(-0.5f, 0.5f);                            
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(0.5f, 0.5f);                             
 
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(0.5f, 0.5f);                             
 
-    glNormal3f(0.0f, 0.0f, -1.0f);
+  glNormal3f(0.0f, 0.0f, -1.0f);
 
-    // back face  
-    // -------------------------------------------------------
-    glVertex2f(0.5f, 0.5f);                             
-    glVertex2f(0.5f, -0.5f);                            
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  // back face  
+  // -------------------------------------------------------
+  glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(0.5f, -0.5f);                            
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
 
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(0.5f, -0.5f);                            
-    // ---------------------------------------------------------
-    glVertex2f(0.5f, -0.5f);                            
-    glVertex2f(-0.5f, -0.5f);                           
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(0.5f, -0.5f);                            
+  // ---------------------------------------------------------
+  glVertex2f(0.5f, -0.5f);                            
+  glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
 
-    glVertex2f(-0.5f, -0.5f);                           
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    // ---------------------------------------------------------
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-0.5f, -0.5f);                           
-    glVertex2f(-0.5f, 0.5f);                             
+  glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  // ---------------------------------------------------------
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-0.5f, -0.5f);                           
+  glVertex2f(-0.5f, 0.5f);                             
 
-    glVertex2f(-0.5f, 0.5f);                            
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    // ---------------------------------------------------------
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(-0.5f, 0.5f);                            
-    glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(-0.5f, 0.5f);                            
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  // ---------------------------------------------------------
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(-0.5f, 0.5f);                            
+  glVertex2f(0.5f, 0.5f);                             
 
-    glVertex2f(0.5f, 0.5f);                             
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(0.5f, 0.5f);                             
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
 
-    glEnd();
+  glEnd();
 
-    // ---------------------------------------------------------    
-    glPopAttrib();
+  // ---------------------------------------------------------    
+  glPopAttrib();
 
-    glBegin(GL_QUADS);
+  glBegin(GL_QUADS);
+  glNormal3f(0.0f, 0.0f, -1.0f);
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
 
-
-    glNormal3f(0.0f, 0.0f, -1.0f);
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-
-    glNormal3f(0.0f, 0.0f, 1.0f);
-    glVertex2f(relatifBorderCalc, relatifBorderCalc);   
-    glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
-    glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
-    glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
-
-    glEnd();
+  glNormal3f(0.0f, 0.0f, 1.0f);
+  glVertex2f(relatifBorderCalc, relatifBorderCalc);   
+  glVertex2f(-relatifBorderCalc, relatifBorderCalc);  
+  glVertex2f(-relatifBorderCalc, -relatifBorderCalc); 
+  glVertex2f(relatifBorderCalc, -relatifBorderCalc);  
+  glEnd();
 }
-
 //====================================================================
 float SquareBorder::calculateDepth(node n) {
-    int             profondeur   = 0;
-    Iterator<node>* itNode       = tree->getInNodes(n);
-    float           currentIndeg = tree->indeg(n);
-    while (currentIndeg != 0) {
-        node currentNode = itNode->next();
-        currentIndeg     = tree->indeg(currentNode);
-        itNode           = tree->getInNodes(currentNode);
-        profondeur++;
-    }
-    delete itNode;
-    return profondeur;
+  int             profondeur   = 0;
+  Iterator<node>* itNode       = tree->getInNodes(n);
+  float           currentIndeg = tree->indeg(n);
+  while (currentIndeg != 0) {
+    node currentNode = itNode->next();
+    currentIndeg     = tree->indeg(currentNode);
+    itNode           = tree->getInNodes(currentNode);
+    profondeur++;
+  }
+  delete itNode;
+  return profondeur;
 }
-
 //====================================================================
 float SquareBorder::calculateBorderSize(node n) {
-    float profondeur = calculateDepth(n);
-    float nbNoeuds   = tree->numberOfNodes();
-    if (nbNoeuds == 0)
-        return 0;
-    return (profondeur / nbNoeuds) * MAX_BORDER_COEF;
+  float profondeur = calculateDepth(n);
+  float nbNoeuds   = tree->numberOfNodes();
+  if (nbNoeuds == 0)
+    return 0;
+  return (profondeur / nbNoeuds) * MAX_BORDER_COEF;
 }
