f0caf3c79431e1d55720885f666d18b1c5774409
David Auber <david.auber@labri.fr>
2006-02-08 12:39:23 +0000

    Change name for internal variable in order to prevent name conflict
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@264 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/ForEach.h
+++ library/tulip/include/tulip/ForEach.h
@@ -3,33 +3,33 @@
 
 namespace tlp {
   template<typename TYPE>
-struct _TLP_IT {
-  _TLP_IT(TYPE &n, Iterator<TYPE> *it ) : it(it), n(n) {
+  struct _TLP_IT {
+    _TLP_IT(TYPE &_n, Iterator<TYPE> *_it ) : _it(_it), _n(_n) {
+    }
+    Iterator<TYPE> *_it;
+    TYPE &_n;
+  };
+
+  template <typename TYPE>
+  inline void * _tlp_get_it(TYPE &n, Iterator<TYPE> *_it) {
+    return (void *)new _TLP_IT<TYPE>(n, _it);
   }
-  Iterator<TYPE> *it;
-  TYPE &n;
-};
 
-template <typename TYPE>
-inline void * _tlp_get_it(TYPE &n, Iterator<TYPE> *it) {
-  return (void *)new _TLP_IT<TYPE>(n, it);
-}
-
-template<typename TYPE>
-inline bool _tlp_if_test(TYPE &n, void *_it) {
-  _TLP_IT<TYPE> &cit = *(_TLP_IT<TYPE>*)_it;
-  assert(cit.it !=0);
-  if(cit.it->hasNext()) {
-    cit.n = cit.it->next(); 
-    return true;
-  } 
-  else {
-    delete cit.it;
-    cit.it = 0;
-    return false;
+  template<typename TYPE>
+  inline bool _tlp_if_test(TYPE &n, void *_it) {
+    _TLP_IT<TYPE> &_cit = *(_TLP_IT<TYPE>*)_it;
+    assert(_cit._it !=0);
+    if(_cit._it->hasNext()) {
+      _cit._n = _cit._it->next(); 
+      return true;
+    } 
+    else {
+      delete _cit._it;
+      _cit._it = 0;
+      return false;
+    }
   }
 }
 
-}
 #define forEach(A, B) \
-  for(void *it=tlp::_tlp_get_it(A, B);tlp::_tlp_if_test(A,it);)
+  for(void *_it_foreach = tlp::_tlp_get_it(A, B); tlp::_tlp_if_test(A, _it_foreach);)
