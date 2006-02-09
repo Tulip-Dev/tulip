63c92ee8caccbae615647b61adec2a5dcf383cc3
David Auber <david.auber@labri.fr>
2006-02-09 13:50:47 +0000

    Add support of node size and order nodes according to a dfs
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@280 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/layout/Circular.cpp
+++ plugins/layout/Circular.cpp
@@ -1,20 +1,25 @@
 /**
- Author: David Auber
- Email : auber@labri.fr
- Last modification : 20/08/2001
+ Author: David Auber / Daniel Archambault
+ Email : auber@labri.fr, archam@cs.ubc.ca
+ Last modification : 25/11/2004
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by  
  the Free Software Foundation; either version 2 of the License, or     
  (at your option) any later version.
 */
 
+/**
+ * implementation of Circular (which is bubble circular).  For nodes, history, 
+ * author and licence please see the header file Circular.h
+ */
+
 #include <math.h>
+#include <tulip/ForEach.h>
 #include "Circular.h"
 
-LAYOUTPLUGIN(Circular,"Circular","David Auber","06/12/1999","Ok","0","1");
+LAYOUTPLUGIN(Circular,"Circular","David Auber/ Daniel Archambault","25/11/2004","Ok","0","1");
 
 using namespace std;
-
 namespace {
   const char * paramHelp[] = {
     // nodeSize
@@ -28,95 +33,119 @@ namespace {
   };
 }
 
-
 Circular::Circular(const PropertyContext &context):Layout(context){
   addParameter<SizesProxy>("nodeSize",paramHelp[0],"viewSize");
 }
 
-Circular::~Circular() {}
-
-/*
-void radius Circular::computRadius() {
-  Iterator<node> *it=superGraph->getNodes(n);
-  while (it->hasNext())  {
-    node n = itN->next();
-    Coord tmp = nodeSize->getNodeValue(n);
-    tmp[2] = 0; //2D Drawing we don't need z coordinate.
-    radius.set(n, tmp.norm());
-  } delete it;
-}
-*/
-/*
-double Circular::computeCirconf() {
-  double circonf = 0;
-  Iterator<node> *it=superGraph->getNodes(n);
-  while (it->hasNext())  {
-    node n = itN->next();
-    circonf += radius.get(n.id);
-  } delete it;
-  return circonf;
-}
-*/
-//=========================================================
-bool Circular::run() {
-  //  if ( dataSet==0 || !dataSet->get("nodeSize",nodeSize)) {
-    if (superGraph->existProperty("viewSize"))
-      SizesProxy *nodeSize = superGraph->getProperty<SizesProxy>("viewSize");    
-    else {
-      SizesProxy *nodeSize = superGraph->getProperty<SizesProxy>("viewSize");  
-      nodeSize->setAllNodeValue(Size(1.0,1.0,1.0));
+namespace {
+  //============================================================================
+  void dfsRecCall(SuperGraph *graph, vector<node> &vec, MutableContainer<bool> &nodeVisited, node n) {
+    nodeVisited.set(n.id, true);
+    vec.push_back(n);
+    node dest;
+    forEach(dest, graph->getInOutNodes(n)) {
+      if (!nodeVisited.get(dest.id)) {
+	dfsRecCall(graph, vec, nodeVisited, dest);
+      }
     }
-    //  }
+  }
+  //============================================================================
+  void buildDfsOrdering(SuperGraph *graph, vector<node> &vec) {
+    MutableContainer<bool> nodeVisited;
+    nodeVisited.setAll(false);
+    node n;
+    forEach(n, graph->getNodes()) {
+      if (!nodeVisited.get(n.id)) {
+	dfsRecCall(graph, vec, nodeVisited, n);
+      }
+    }
+  }
+  //============================================================================
+}
+//this inline function computes the radius size given a size
+inline double computeRadius (const Size &s) {
+  return sqrt (s[0]*s[0]/4.0 + s[1]*s[1]/4.0);
+}//end computeRad
 
-  //compute node radius
-    //  double circonf = computeCirconf();
-  //compute alpha
-  //compute rho
-  //place nodes
-  
+bool Circular::run() {
+  SizesProxy *nodeSizes;
 
+  if ( dataSet==0 || !dataSet->get("nodeSize", nodeSizes)) {
+    if (superGraph->existProperty("viewSize"))
+      nodeSizes = superGraph->getProperty<SizesProxy>("viewSize");    
+    else {
+      nodeSizes = superGraph->getProperty<SizesProxy>("viewSize");  
+      nodeSizes->setAllNodeValue(Size(1.0,1.0,1.0));
+    }
+  }
 
   Size resultSize;
-  double gamma=0;
-  int i;
-  double rayon;
   double sizeTm;
 
-  layoutProxy->setAllEdgeValue(vector<Coord>(0));
-
-  if (superGraph->numberOfNodes()<2) {
-    rayon=0;
-    resultSize.set(1,1,1);
+  //compute the sum of radii and the max radius of the graph
+  double sumOfRad = 0;
+  double maxRad = 0;
+  node maxRadNode;
+  node itn;
+  forEach(itn, superGraph->getNodes()) {
+    double rad = computeRadius(nodeSizes->getNodeValue(itn));
+    sumOfRad += rad;
+    if (maxRad < rad) {
+      maxRad = rad;
+      maxRadNode = itn;
+    }//end if
   }
+
+  //with two nodes, lay them on a line max rad appart
+  if (superGraph->numberOfNodes() <= 2) {
+    //set the (max 2) nodes maxRad appart
+    double xcoord = maxRad/2.0;
+    node itn;
+    forEach(itn, superGraph->getNodes()) {
+      layoutProxy->setNodeValue (itn, Coord (xcoord, 0, 0));
+      xcoord *= -1;
+    }
+  }//end if
+  
+  //with more nodes, things get a little more complicated...
   else {
-    //compute circonference
-    Iterator<node> *itN=superGraph->getNodes();
-    while(itN->hasNext()) {
-      node itn=itN->next();
-      layoutProxy->setNodeValue(itn,Coord(rayon*cos(gamma*i)+rayon*2,rayon*sin(gamma*i)+rayon*2,0));
-      ++i;
-    } delete itN;
+    //this is the current angle
+    double gamma = 0;
 
+    //if the ratio of maxRad/sumOfRad > .5, we need to ajust angles
+    bool angleAdjust = false;
+    if (maxRad/sumOfRad > 0.5) {
+      sumOfRad -= maxRad;
+      angleAdjust = true;
+    }//end if
 
+    vector<node> dfsOrdering;
+    buildDfsOrdering(superGraph, dfsOrdering);
+    vector<node>::const_iterator it = dfsOrdering.begin();
+    for(; it!=dfsOrdering.end(); ++it) {
+      node itn = *it;
+      //get the node and its size
+      Size curNodeSize = nodeSizes->getNodeValue(itn);
+      
+      //compute the radius to ensure non overlap.  If adjustment to 
+      //ensure no angle greater than pi done, detect it.
+      double nodeRad = computeRadius(curNodeSize);
+      double halfAngle = 
+	(nodeRad/sumOfRad)*((angleAdjust) ? M_PI/2.0 : M_PI);
+      double rayon = nodeRad/sin(halfAngle);
+      //if this node was the node that took up more than half the circle,
+      //we complet the adjustment to make sure that it does not.
+      if (angleAdjust && (maxRadNode == itn)) {
+	halfAngle = M_PI/2.0;
+	rayon = nodeRad;
+      }//end if
 
-    rayon=100;
-    gamma=2*M_PI/superGraph->numberOfNodes();
-    double x1,y1,x2,y2;
-    i=0;x1=rayon*cos(gamma*i)+rayon*2;y1=rayon*sin(gamma*i)+rayon*2,0;i=1;
-    x2=rayon*cos(gamma*i)+rayon*2;y2=rayon*sin(gamma*i)+rayon*2,0;
-    sizeTm=sqrt( (x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
-    sizeTm=sizeTm/2;
-    resultSize.set(sizeTm,sizeTm,sizeTm);
-  }
-  superGraph->getLocalProperty<SizesProxy>("viewSize")->setAllNodeValue(resultSize);
-  resultSize.set(sizeTm/16,sizeTm/16,sizeTm/4);
-  superGraph->getLocalProperty<SizesProxy>("viewSize")->setAllEdgeValue(resultSize);
-  i=0;
-  Iterator<node> *itN=superGraph->getNodes();
-  while(itN->hasNext()) {
-    node itn=itN->next();
-    layoutProxy->setNodeValue(itn,Coord(rayon*cos(gamma*i)+rayon*2,rayon*sin(gamma*i)+rayon*2,0));
-    ++i;
-  } delete itN;
+      //compute the position of the node.
+      gamma += halfAngle;
+      layoutProxy->setNodeValue(itn, Coord(rayon*cos(gamma),
+					   rayon*sin(gamma), 0));
+      gamma += halfAngle;
+    }//end while
+  }//end else
   return true;
-}
+} //end run
--- plugins/layout/Circular.h
+++ plugins/layout/Circular.h
@@ -1,31 +1,37 @@
 //-*-c++-*-
-#ifndef _CIRCULARLAYOUT_H
-#define _CIRCULARLAYOUT_H
+#ifndef _BUBBLE_CIRCULARLAYOUT_H
+#define _BUBBLE_CIRCULARLAYOUT_H
 
 #include <tulip/TulipPlugin.h>
 
 /** \addtogroup layout */
 /*@{*/
-/// Circular.h - An implementation of a circular layout.
+/// Circular.h - An implementation of a circurlar layout.
 /** 
- * This plugin is an implementation of a circular layout
- * All nodes are placed arbitrary on circle. 
+ * This plugin is an implementation of a circular layout that takes
+ * node size into account.  It uses a varient of the BubbleTree algorithm
+ * developed by S. Grivet, D. Auber, J.P. Domenger, and G. Melancon.
  *
- * <b>HISTORY</b>
+ * HISTORY:
  *
- * - 01/12/99 Verson 0.0.1: Initial release
+ * 25/11/2004 version 0.0.2: size and order
+ * 01/12/1999 Version 0.0.1: Initial release
+ * 
  *
+ * NOTES:
  *
- * \note This works on general graphs.
+ * This work on general graphs.
  * Let n be the number of nodes, the algorithm complexity is in O(n).
  *
+ * AUTHORS:
  *
- * \author David Auber University Bordeaux I France: Email:auber@tulip-software.org
+ * David Auber University Bordeaux I France: Email:auber@tulip-software.org
+ * Daniel Archambault The University of British Columbia:
+ * Email:  archam@cs.ubc.ca
  */
 class Circular:public Layout { 
 public:
   Circular(const PropertyContext &);
-  ~Circular();
   bool run();
 };
 /*@}*/
