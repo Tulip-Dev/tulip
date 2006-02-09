68e923d906ab29c8535886c0837bf78d318a2632
David Auber <david.auber@labri.fr>
2006-02-09 10:26:33 +0000

    Fix memory leak, Find new bug(unsolvable at the moment): Fix : never use return or break inside a forEach block
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@279 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/ForEach.h
+++ library/tulip/include/tulip/ForEach.h
@@ -1,3 +1,16 @@
+//-*-c++-*-
+/*
+ Author: David Auber
+ Email : auber@labri.fr
+ Last modification : 10/01/2006
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by  
+ the Free Software Foundation; either version 2 of the License, or     
+ (at your option) any later version.
+*/
+#ifndef Tulip_FOREACH_H
+#define Tulip_FOREACH_H
+
 #include <assert.h>
 #include <tulip/Iterator.h>
 
@@ -5,6 +18,9 @@ namespace tlp {
   template<typename TYPE>
   struct _TLP_IT {
     _TLP_IT(TYPE &_n, Iterator<TYPE> *_it ) : _it(_it), _n(_n) {
+    }
+    ~_TLP_IT() {
+      delete _it;
     }
     Iterator<TYPE> *_it;
     TYPE &_n;
@@ -17,19 +33,24 @@ namespace tlp {
 
   template<typename TYPE>
   inline bool _tlp_if_test(TYPE &n, void *_it) {
-    _TLP_IT<TYPE> &_cit = *(_TLP_IT<TYPE>*)_it;
-    assert(_cit._it !=0);
-    if(_cit._it->hasNext()) {
-      _cit._n = _cit._it->next(); 
+    assert(((_TLP_IT<TYPE>*)_it)->_it !=0);
+    if(((_TLP_IT<TYPE>*)_it)->_it->hasNext()) {
+      ((_TLP_IT<TYPE>*)_it)->_n = ((_TLP_IT<TYPE>*)_it)->_it->next(); 
       return true;
     } 
     else {
-      delete _cit._it;
-      _cit._it = 0;
+      delete (_TLP_IT<TYPE>*)_it;
       return false;
     }
   }
 }
 
+
+/**
+ * Warning, do not use break or return inside a for each block;
+ * it causes a memory leak.
+ */
 #define forEach(A, B) \
   for(void *_it_foreach = tlp::_tlp_get_it(A, B); tlp::_tlp_if_test(A, _it_foreach);)
+
+#endif
