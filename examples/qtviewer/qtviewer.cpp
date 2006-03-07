3307cda8979baf92a7d9a27d1777e8da1710fd66
David Auber <david.auber@labri.fr>
2006-03-07 10:19:31 +0000

    A first example that shows how to use tulip and qt in order to display a simple graph.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@299 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ examples/qtviewer/qtviewer.cpp
@@ -0,0 +1,53 @@
+#include <qapplication.h>
+#include <tulip/TlpTools.h>
+#include <tulip/GlGraphWidget.h>
+#include <tulip/MouseObject.h>
+
+using namespace std;
+
+/*******************************************************************/
+void importGraph(const string &filename, GlGraph *render) {
+  DataSet dataSet;
+  dataSet.set("filename", filename);
+  SuperGraph *newSuperGraph=tlp::importGraph("tlp", dataSet, NULL);
+  if (newSuperGraph!=0) {
+    render->setSuperGraph(newSuperGraph);
+    
+    DataSet glGraphData;
+    if (dataSet.get<DataSet>("displaying", glGraphData))
+      render->setParameters(glGraphData);
+  }
+  
+}
+/*******************************************************************/
+int main(int argc,char ** argv ){
+  
+  QApplication MainApp(argc,argv);
+  
+  if (argc<2) {
+    cerr << "usage :" << endl;
+    cerr << "\t " << argv[0] << " <filename>" << endl;
+    cerr << "file must be in tlp format" << endl;
+    exit(1);
+  }
+
+  /****************************************************/
+  tlp::initTulipLib();
+  tlp::loadPlugins();   // library side plugins
+  GlGraph::loadPlugins(); //Glyoh plugins
+  /****************************************************/
+  GlGraphWidget MainWin;
+  
+  MainApp.setMainWidget(&MainWin);
+  MainWin.resize(640,480);
+  MainWin.show();
+
+  if( argv[1][0] !='g' || argv[1][1] !='r' || argv[1][2] !='i' || argv[1][3] !='d'){
+    importGraph(argv[1], &MainWin);
+  }
+
+  MainWin.centerScene();
+  MouseGraphNavigate m;
+  MainWin.setMouse(&m);
+  return MainApp.exec();
+}
