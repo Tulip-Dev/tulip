14c5f744ac9426553b35b0bab663e1b9927fbf14
David Auber <david.auber@labri.fr>
2006-03-23 12:25:28 +0000

    General improvement
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@326 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- tutorial/tutorial-001/tutorial.cpp
+++ tutorial/tutorial-001/tutorial.cpp
@@ -2,32 +2,42 @@
 #include <tulip/TlpTools.h>
 
 /**
+ *
  * Tutorial 001
  *
  * Create a graph 
- * add node and edges
+ * add three nodes and three edges
  * remove an edge and a node
+ * print the result on the standard output
+ * 
  */
 
 using namespace std;
 
 int main() {
   //create an empty graph
-  SuperGraph *graph=tlp::newSuperGraph();
+  SuperGraph *graph = tlp::newSuperGraph();
+
   //add three nodes
-  node n1=graph->addNode();
-  node n2=graph->addNode();
-  node n3=graph->addNode();
+  node n1 = graph->addNode();
+  node n2 = graph->addNode();
+  node n3 = graph->addNode();
+
   //add three edges
-  edge e=graph->addEdge(n2,n3);
-  graph->addEdge(n1,n2);
-  graph->addEdge(n3,n1);
+  edge e1 = graph->addEdge(n2,n3);
+  edge e2 = graph->addEdge(n1,n2);
+  edge e3 = graph->addEdge(n3,n1);
+
   //remove an edge
-  graph->delEdge(e);
+  graph->delEdge(e1);
+
   //remove a node
   graph->delNode(n2);
-  //print the result
+
+  //print the result on the standard output
   cout << graph << flush ;
+
+  //delete the graph
   delete graph;
   return EXIT_SUCCESS;
 }
--- tutorial/tutorial-002/tutorial.cpp
+++ tutorial/tutorial-002/tutorial.cpp
@@ -6,6 +6,7 @@
  *
  * Create a graph 
  * display all the structure using iterators
+ *
  */
 
 using namespace std;
@@ -24,74 +25,82 @@ int main() {
   //create an empty graph
   SuperGraph *graph=tlp::newSuperGraph();
 
-  Iterator<node> *itNodes=graph->getNodes();
-  while(itNodes->hasNext()) {
-    node n=itNodes->next();
-    cout << "node: " <<  n.id << endl;
-  }delete itNodes;
-
   //build the graph
   buildGraph(graph);
-
+  
+  //===========================
   //Iterate all nodes and display the structure
-  itNodes=graph->getNodes();
+  Iterator<node> *itNodes = graph->getNodes();
   while(itNodes->hasNext()) {
-    node n=itNodes->next();
+    node n = itNodes->next();
     cout << "node: " <<  n.id << endl;
     cout << " degree: " << graph->deg(n) << endl;
     cout << " in-degree: " << graph->indeg(n) << endl;
     cout << " out-degree: " << graph->outdeg(n) << endl;
-    cout << " ancestors: {";
+
+    //===========================
     //iterate all ancestors of a node
+    cout << " ancestors: {";
     Iterator<node> *itN=graph->getInNodes(n);
     while(itN->hasNext()) {
       cout << itN->next().id;
       if (itN->hasNext()) cout << ",";
-    } delete itN;
+    } delete itN; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
-    cout << " successors: {";
+
+    //===========================
     //iterate all successors of a node
-    itN=graph->getOutNodes(n);
+    cout << " successors: {";
+    itN = graph->getOutNodes(n);
     while (itN->hasNext()) {
       cout << itN->next().id;
       if (itN->hasNext()) cout << ",";
-    } delete itN;
+    } delete itN; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
-    cout << " neighborhood: {";
+
+    //===========================
     //iterate the neighborhood of a node
-    itN=graph->getInOutNodes(n);
+    cout << " neighborhood: {";
+    itN = graph->getInOutNodes(n);
     while(itN->hasNext()) {
       cout << itN->next().id;
       if (itN->hasNext()) cout << ",";
-    } delete itN;
+    } delete itN; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
-    cout << " incoming edges: {";
+
+    //===========================
     //iterate the incoming edges
+    cout << " incoming edges: {";
     Iterator<edge> *itE=graph->getInEdges(n);
     while(itE->hasNext()) {
       cout << itE->next().id;
       if (itE->hasNext()) cout << ",";
-    } delete itE;
+    } delete itE; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
     cout << " outcoming edges: {";
-    //iterate the out comming edges
-    itE=graph->getOutEdges(n);
+
+    //===========================
+    //iterate the outcomming edges
+    itE = graph->getOutEdges(n);
     while(itE->hasNext()) {
       cout << itE->next().id;
       if (itE->hasNext()) cout << ",";
-    } delete itE;
+    } delete itE; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
-    cout << " adjacent edges: {";
+
+    //===========================
     //iterate the adjacent edges
-    itE=graph->getInOutEdges(n);
+    cout << " adjacent edges: {";
+    itE = graph->getInOutEdges(n);
     while(itE->hasNext()) {
       cout << itE->next().id;
       if (itE->hasNext()) cout << ",";
-    } delete itE;
+    } delete itE; //!!!Warning : do not forget to delete iterators (memory leak)
     cout << "}" << endl;
 
-  }delete itNodes;
+  } delete itNodes; //!!!Warning : do not forget to delete iterators (memory leak)
 
+  //===========================
   //Iterate all edges
   Iterator<edge> *itEdges=graph->getEdges();
   while(itEdges->hasNext()) {
@@ -100,8 +109,8 @@ int main() {
     cout << " source: " << graph->source(e).id;
     cout << " target: " << graph->target(e).id;
     cout << endl;
-  }delete itEdges;
+  } delete itEdges; //!!!Warning : do not forget to delete iterators (memory leak)
 
-  delete graph;
+  delete graph; //delete the entire graph
   return EXIT_SUCCESS;
 }
