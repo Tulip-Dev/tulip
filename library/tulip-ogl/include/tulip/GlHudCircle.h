f8dcef0afb742d7c4aa147d38dff2f86f1ac0be0
Daniel Archambault <daniel.archambault@labri.fr>
2006-09-07 23:54:16 +0000

    commit for picking augmented displays
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@468 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/GlHudCircle.h
+++ library/tulip-ogl/include/tulip/GlHudCircle.h
@@ -27,8 +27,11 @@ class GlHudCircle : public GlHud {
  protected:
   Coord center; /**< The center position of the circle */
   Color color; /**< The color of the circle */
+  Color fillColor; /** The fill color of the circle */
   int radius; /**< The radius of the circle */
   int segments; /**< The number of segments of the circle. Higher values imply a smoother circle */
+  bool filled; /** If true, the circle is filled*/
+  bool outline; /** If true, the circle has an outline*/
 
   vector<Coord> points; /**< The control points of the circle*/
 
@@ -47,7 +50,9 @@ class GlHudCircle : public GlHud {
   /**
    * Constructor
    */
-  GlHudCircle(const Coord &center, const Color &color, int radius, int segments);
+  GlHudCircle(const Coord &center, const Color &color,
+	      int radius, int segments, const Color &fillColor = Color (255,255,255,255),
+	      bool filled = false, bool outline = true);
 
   /**
    * Destructor
@@ -68,6 +73,21 @@ class GlHudCircle : public GlHud {
    * Accessor in reading to the color of the circle
    */
   virtual Color getColor();
+
+  /**
+   * Accessor in reading the fill color of the circle
+   */
+  virtual Color getFillColor();
+
+  /**
+   * Accessor for getting if outline is shown
+   */
+  virtual bool getOutline();
+
+  /**
+   * Accessor for getting if the cicled is filled
+   */
+  virtual bool getFilled();
 
   /**
    * Accessor in reading to the radius of the circle
@@ -98,6 +118,21 @@ class GlHudCircle : public GlHud {
    * Accessor in writing to the number of segments
    */
   virtual void setSegments(int segments);
+
+  /**
+   * Accessor to set fill color of the circle
+   */
+  virtual void setFillColor(const Color &newFillColor);
+
+  /**
+   * Accessor to set the outline
+   */
+  virtual void setOutline(bool newOutline);
+
+  /**
+   * Accessor to set if the circle is filled
+   */
+  virtual void setFilled(bool newFilled);
 };
 }
 
