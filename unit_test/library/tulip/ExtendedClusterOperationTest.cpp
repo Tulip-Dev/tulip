facf6d846f686b235bde1ee53c0cc54b16355ba1
David Auber <david.auber@labri.fr>
2006-10-16 21:22:42 +0000

    Activate test for open meta node
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@576 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/library/tulip/ExtendedClusterOperationTest.cpp
+++ unit_test/library/tulip/ExtendedClusterOperationTest.cpp
@@ -13,6 +13,7 @@ using namespace tlp;
 CPPUNIT_TEST_SUITE_REGISTRATION( ExtendedClusterOperationTest );
 //==========================================================
 void ExtendedClusterOperationTest::setUp() {
+  //  cerr << __PRETTY_FUNCTION__ << endl;
   graph = tlp::newGraph();
   for (unsigned int i=0; i<5; ++i) {
     nodes.push_back(graph->addNode());
@@ -22,12 +23,15 @@ void ExtendedClusterOperationTest::setUp() {
   edges.push_back(graph->addEdge(nodes[1], nodes[3]));
   edges.push_back(graph->addEdge(nodes[1], nodes[4]));
   edges.push_back(graph->addEdge(nodes[2], nodes[3]));
+
   quotient = tlp::newCloneSubGraph(graph);
+
   group.insert(nodes[0]);
   group.insert(nodes[1]);
 }
 //==========================================================
 void ExtendedClusterOperationTest::tearDown() {
+  //  cerr << __PRETTY_FUNCTION__ << endl;
   delete graph;
   nodes.clear();
   edges.clear();
@@ -35,6 +39,7 @@ void ExtendedClusterOperationTest::tearDown() {
 }
 //==========================================================
 void ExtendedClusterOperationTest::testCreateMetaNode() {
+  cerr << __PRETTY_FUNCTION__ << endl;
   meta = createMetaNode(quotient, group);
   CPPUNIT_ASSERT_EQUAL(false, quotient->isElement(nodes[0]));
   CPPUNIT_ASSERT_EQUAL(false, quotient->isElement(nodes[1]));
@@ -42,6 +47,8 @@ void ExtendedClusterOperationTest::testCreateMetaNode() {
   CPPUNIT_ASSERT_EQUAL(true, quotient->existEdge(meta, nodes[2]).isValid());
   CPPUNIT_ASSERT_EQUAL(true, quotient->existEdge(meta, nodes[3]).isValid());
   CPPUNIT_ASSERT_EQUAL(4u, quotient->numberOfEdges());
+  CPPUNIT_ASSERT_EQUAL(6u, graph->numberOfNodes());
+
   GraphProperty *clusterInfo = quotient->getProperty<GraphProperty>("viewMetaGraph");
   Graph *cluster = clusterInfo->getNodeValue(meta);
   CPPUNIT_ASSERT(cluster!=0);
@@ -75,15 +82,25 @@ void ExtendedClusterOperationTest::testAddNodeToMetaNode() {
 */
 //==========================================================
 void ExtendedClusterOperationTest::testOpenMetaNode() {
-  /*
-    meta = createMetaNode(quotient, group);
-  extractNodeFromMetaNode(quotient, nodes[0], meta);
-  addNodeToMetaNode(quotient, nodes[0], meta);
+
+  meta = createMetaNode(quotient, group);
   openMetaNode(quotient, meta);
-  CPPUNIT_ASSERT(!quotient->isElement(meta));
+  
+  CPPUNIT_ASSERT_EQUAL(false, quotient->isElement(meta));
+  CPPUNIT_ASSERT_EQUAL(false, graph->isElement(meta));
+
+  CPPUNIT_ASSERT_EQUAL( 5u, graph->numberOfNodes());
+  CPPUNIT_ASSERT_EQUAL( 5u, graph->numberOfEdges());
+  
+  CPPUNIT_ASSERT_EQUAL( 5u, quotient->numberOfNodes());
+  CPPUNIT_ASSERT_EQUAL( 5u, quotient->numberOfEdges());
+  
   CPPUNIT_ASSERT(quotient->existEdge(nodes[0], nodes[1]).isValid());
   CPPUNIT_ASSERT(quotient->existEdge(nodes[0], nodes[2]).isValid());
-  */
+  CPPUNIT_ASSERT(quotient->existEdge(nodes[1], nodes[3]).isValid());
+  CPPUNIT_ASSERT(quotient->existEdge(nodes[1], nodes[4]).isValid());
+  CPPUNIT_ASSERT(quotient->existEdge(nodes[2], nodes[3]).isValid());
+
 }
 //==========================================================
 CppUnit::Test * ExtendedClusterOperationTest::suite() {
