07976fbe2be6198d4b56ee28a7fa8c3caf4a3818
Patrick Mary <mary@labri.fr>
2007-01-16 14:18:07 +0000

    allow to choose inout/in/out degree
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@706 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/metric/ArityMetric.cpp
+++ plugins/metric/ArityMetric.cpp
@@ -5,13 +5,48 @@ using namespace tlp;
 
 DOUBLEPLUGINOFGROUP(ArityMetric,"Degree","David Auber","04/10/2001","Stable","0","1","Graph");
 
+namespace {
+  const char * paramHelp[] = {
+    //Degree type
+    HTML_HELP_OPEN()				 \
+    HTML_HELP_DEF( "type", "String Collection" ) \
+    HTML_HELP_DEF( "default", "InOut" )	 \
+    HTML_HELP_BODY() \
+    "This parameter enables to choose the type of degree to use"	\
+    HTML_HELP_CLOSE(),
+  };
+}
+#define DEGREE_TYPE "Type"
+#define DEGREE_TYPES "InOut;In;Out;"
+#define INOUT 0
+#define IN 1
+#define OUT 2
+//==============================================================================
 ArityMetric::ArityMetric(const PropertyContext &context):DoubleAlgorithm(context) {
+   addParameter<StringCollection>(DEGREE_TYPE, paramHelp[0], DEGREE_TYPES);
 }
 //==================================================================
 bool ArityMetric::run() {
+  StringCollection degreeTypes(DEGREE_TYPES);
+  degreeTypes.setCurrent(0);
+  if (dataSet!=0) {
+    dataSet->get(DEGREE_TYPE, degreeTypes);
+  }
   node n;
-  forEach(n, graph->getNodes())
-    doubleResult->setNodeValue(n, graph->deg(n));
+  switch(degreeTypes.getCurrent()) {
+  case INOUT:
+    forEach(n, graph->getNodes())
+      doubleResult->setNodeValue(n, graph->deg(n));
+    break;
+  case IN:
+    forEach(n, graph->getNodes())
+      doubleResult->setNodeValue(n, graph->indeg(n));
+    break;
+  case OUT:
+    forEach(n, graph->getNodes())
+      doubleResult->setNodeValue(n, graph->outdeg(n));
+    break;
+  }
   doubleResult->setAllEdgeValue(0);
   return true;
 }
