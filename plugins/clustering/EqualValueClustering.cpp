bd6aeb78f3549c48208ee41d8a3355aa79872e2c
Patrick Mary <mary@labri.fr>
2007-02-14 09:12:27 +0000

    improvement for double property + name of subgraphs
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@763 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/clustering/EqualValueClustering.cpp
+++ plugins/clustering/EqualValueClustering.cpp
@@ -30,53 +30,101 @@ EqualValueClustering::EqualValueClustering(AlgorithmContext context):Algorithm(c
   addParameter<PropertyInterface*>("Property", paramHelp[0], "viewMetric");
 }
 //================================================================================
+namespace stdext {
+  template<>
+  struct hash<double> {
+    size_t operator()(const double s) const { return (size_t) s; }
+  };
+};
+//===============================================================================
 bool EqualValueClustering::run() {
   string tmp1,tmp2;
-  PropertyInterface *metric=0;
+  PropertyInterface *property=0;
   if (dataSet!=0) 
-    dataSet->get("Property", metric);  
-  if (metric == 0)
-    metric = graph->getProperty("viewMetric");
-  
-  stdext::hash_map<string, Graph *> partitions;
+    dataSet->get("Property", property);  
+  if (property == 0)
+    property = graph->getProperty("viewMetric");
+
   StableIterator<node> itN(graph->getNodes());
-  int i = 0, step = 0, maxSteps = graph->numberOfNodes();
-  pluginProgress->setComment("Partitioning nodes...");
-  while (itN.hasNext()) {
-    Graph *sg;
-    node n = itN.next();
-    string tmp=metric->getNodeStringValue(n);
-    if (partitions.find(tmp)==partitions.end()) {
-      char str[100];
-      sprintf(str, "c_%06i", i++);
-      sg = graph->addSubGraph();
-      sg->setAttribute("name",string(str));
-      partitions[tmp]=sg;
-    } else
-      sg = partitions[tmp];
-    sg->addNode(n);
-    step++;
-    pluginProgress->progress(step, maxSteps);
-    if (pluginProgress->state() !=TLP_CONTINUE)
-      return pluginProgress->state()!= TLP_CANCEL;
-  }
-
   StableIterator<edge> itE(graph->getEdges());
-  step = 0;
-  maxSteps = graph->numberOfEdges();
-  pluginProgress->setComment("Partitioning edges...");
-  while(itE.hasNext()) {
-    edge ite = itE.next();
-    string tmp = metric->getNodeStringValue(graph->source(ite));
-    if (tmp == metric->getNodeStringValue(graph->target(ite))) {
-      partitions[tmp]->addEdge(ite);
-    }
-    step++;
-    pluginProgress->progress(step, maxSteps);
-    if (pluginProgress->state() !=TLP_CONTINUE)
-      return pluginProgress->state()!= TLP_CANCEL;    
-  }
+  int step = 0, maxSteps = graph->numberOfNodes();
+  pluginProgress->setComment("Partitioning nodes...");
 
+  // try to work with double value if it's a DoubleProperty
+  if (typeid(*property) == typeid(DoubleProperty)) {
+    stdext::hash_map<double, Graph *> partitions;
+    DoubleProperty *metric = (DoubleProperty *) property;
+    while (itN.hasNext()) {
+      Graph *sg;
+      node n = itN.next();
+      double tmp = metric->getNodeValue(n);
+      if (partitions.find(tmp) == partitions.end()) {
+	sg = graph->addSubGraph();
+	stringstream sstr;
+	sstr << "value = " << tmp;
+	sg->setAttribute("name", sstr.str());
+	partitions[tmp]=sg;
+      } else
+	sg = partitions[tmp];
+      sg->addNode(n);
+      if ((++step % (maxSteps/100)) == 0) {
+	pluginProgress->progress(step, maxSteps);
+	if (pluginProgress->state() !=TLP_CONTINUE)
+	  return pluginProgress->state()!= TLP_CANCEL;
+      }
+    }
+
+    step = 0;
+    maxSteps = graph->numberOfEdges();
+    pluginProgress->setComment("Partitioning edges...");
+    while(itE.hasNext()) {
+      edge ite = itE.next();
+      double tmp = metric->getNodeValue(graph->source(ite));
+      if (tmp == metric->getNodeValue(graph->target(ite))) {
+	partitions[tmp]->addEdge(ite);
+      }
+      if ((++step % (maxSteps/100)) == 0) {
+	pluginProgress->progress(step, maxSteps);
+	if (pluginProgress->state() !=TLP_CONTINUE)
+	  return pluginProgress->state()!= TLP_CANCEL;
+      }
+    }    
+  } else {
+    stdext::hash_map<string, Graph *> partitions;
+    while (itN.hasNext()) {
+      Graph *sg;
+      node n = itN.next();
+      string tmp=property->getNodeStringValue(n);
+      if (partitions.find(tmp)==partitions.end()) {
+	sg = graph->addSubGraph();
+	sg->setAttribute("name", string("value = ") + tmp);
+	partitions[tmp]=sg;
+      } else
+	sg = partitions[tmp];
+      sg->addNode(n);
+      if ((++step % (maxSteps/100)) == 0) {
+	pluginProgress->progress(step, maxSteps);
+	if (pluginProgress->state() !=TLP_CONTINUE)
+	  return pluginProgress->state()!= TLP_CANCEL;
+      }
+    }
+
+    step = 0;
+    maxSteps = graph->numberOfEdges();
+    pluginProgress->setComment("Partitioning edges...");
+    while(itE.hasNext()) {
+      edge ite = itE.next();
+      string tmp = property->getNodeStringValue(graph->source(ite));
+      if (tmp == property->getNodeStringValue(graph->target(ite))) {
+	partitions[tmp]->addEdge(ite);
+      }
+      if ((++step % (maxSteps/100)) == 0) {
+	pluginProgress->progress(step, maxSteps);
+	if (pluginProgress->state() !=TLP_CONTINUE)
+	  return pluginProgress->state()!= TLP_CANCEL;
+      }
+    }
+  }
   return true;
 }
 //================================================================================
