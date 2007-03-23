ca3ef53fa3f7b5b1ef0de7bd845159a6c000e81b
Patrick Mary <mary@labri.fr>
2007-03-23 14:34:22 +0000

    automatic stamping
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@817 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- newheader.sh
+++ newheader.sh
@@ -1,7 +1,5 @@
 FILES=$(find . -name CVS -prune -o -name "*.h" -print -o -name "*.cpp" -print -o -name "*.cxx" -print)
 
-HEADER=$(cat ./header.txt)
-
 for FILE in $FILES
 do
   echo "new header for $FILE"
@@ -10,7 +8,8 @@ do
   mv tmpfile $FILE
   # output header
   echo '//-*-c++-*-' > tmpfile
-  echo "$HEADER" >> tmpfile
+  pattern=$(echo 's/dd\/mm\/yyyy/'`date '+%d\/%m\/%Y/'`)
+  sed $pattern ./header.txt >> tmpfile
   first=$(awk 'NR == 1 {print $1; exit}' $FILE)
   if [ "$first" = "//-*-c++-*-" ]; then
     second=$(awk 'NR == 2 {print $1; exit}' $FILE)
