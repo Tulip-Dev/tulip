8b3918208f475bb0e27dd8bf8bb085b659742eff
Patrick Mary <mary@labri.fr>
2007-05-04 11:01:35 +0000

    selectSpanningForest function added
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@858 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/selection/SpanningTreeSelection.cpp
+++ plugins/selection/SpanningTreeSelection.cpp
@@ -2,6 +2,7 @@
 #include "SpanningTreeSelection.h"
 #include <tulip/BooleanProperty.h>
 #include <tulip/MethodFactory.h>
+#include <tulip/GraphTools.h>
 
 BOOLEANPLUGIN(SpanningTreeSelection,"Spanning Forest","David Auber","01/12/1999","Alpha","1.0");
 
@@ -17,79 +18,17 @@ SpanningTreeSelection::~SpanningTreeSelection() {}
 ///Calcul l'arbre couvrant
 bool SpanningTreeSelection::run()
 {
-  list<node> fifo;
-
-  BooleanProperty *nodeFlag=graph->getLocalProperty<BooleanProperty>("viewSelectionNodeFlag");
   if (graph->existProperty("viewSelection")) {
     BooleanProperty *viewSelection=graph->getProperty<BooleanProperty>("viewSelection");
     Iterator<node> *itN=graph->getNodes();
     for (;itN->hasNext();) { 
       node itn=itN->next();
       if (viewSelection->getNodeValue(itn)==true) {
-	fifo.push_back(itn);
-	nodeFlag->setNodeValue(itn,true);
+	booleanResult->setNodeValue(itn, true);
       }
     } delete itN;
   }
 
-  
-  booleanResult->setAllEdgeValue(true);
-  booleanResult->setAllNodeValue(true);
-
-  bool ok=true;
-  node tmp1;
-  while (ok) {
-    while (!fifo.empty()) {
-      tmp1=fifo.front();
-      fifo.pop_front();
-      Iterator<edge> *itE=graph->getOutEdges(tmp1);
-      for(;itE->hasNext();) {
-	edge adjit=itE->next();
-	if (!nodeFlag->getNodeValue(graph->target(adjit)))	{
-	  nodeFlag->setNodeValue(graph->target(adjit),true);	    
-	  fifo.push_back(graph->target(adjit));
-	}
-	else
-	  booleanResult->setEdgeValue(adjit,false);
-      } delete itE;
-    }
-    ok=false;
-    bool degZ=false;
-    node goodNode;
-    Iterator<node> *itN=graph->getNodes();
-    for(;itN->hasNext();) {
-      node itn=itN->next();
-      if (!nodeFlag->getNodeValue(itn)) {
-	if (!ok) {
-	  goodNode=itn;
-	  ok=true;
-	}
-	if (graph->indeg(itn)==0) {
-	  fifo.push_back(itn);
-	  nodeFlag->setNodeValue(itn,true);
-	  degZ=true;
-	}
-	if (!degZ) {
-	  if (graph->indeg(itn)<graph->indeg(goodNode))
-	    goodNode=itn;
-	  else {
-	    if (graph->indeg(itn)==graph->indeg(goodNode))
-	      if (graph->outdeg(itn)>graph->outdeg(goodNode))
-		goodNode=itn;
-	  }
-	}
-      }
-    } delete itN;
-    if (ok && (!degZ)) {
-      fifo.push_back(goodNode);
-      nodeFlag->setNodeValue(goodNode,true);
-    }
-  }
-  graph->delLocalProperty("viewSelectionNodeFlag");
+  selectSpanningForest(graph, booleanResult); 
   return true;
 }
-
-
-
-
-
