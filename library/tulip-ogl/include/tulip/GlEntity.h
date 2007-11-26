1601ed9a5bd4f310963672aa1c78cd5de1db7ab1
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2007-11-26 15:49:57 +0000

    - first version of GlEntity
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@968 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-ogl/include/tulip/GlEntity.h
@@ -0,0 +1,38 @@
+//-*-c++-*-
+/**
+ Author: Morgan Mathiaut
+ Email : mathiaut@labri.fr
+ Last modification : 23/10/2007
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by  
+ the Free Software Foundation; either version 2 of the License, or     
+ (at your option) any later version.
+*/
+#ifndef Tulip_GLENTITY_H
+#define Tulip_GLENTITY_H
+
+#include <GL/gl.h>
+#include <utility>
+
+#include <tulip/tulipconf.h>
+
+#include <tulip/BoundingBox.h>
+
+namespace tlp {
+
+  class GlSceneVisitor;
+  class GlLayout;
+
+  class TLP_GL_SCOPE GlEntity {
+  
+  public:
+
+    virtual ~GlEntity() {}
+
+    virtual void acceptVisitor(GlSceneVisitor *visitor)=0;
+
+  };
+  
+}
+
+#endif // Tulip_GLENTITY_H
