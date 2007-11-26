948a3e11ba7f1cb3438e192b0fbdd6b362a55002
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2007-11-26 15:57:18 +0000

    first version
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@969 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-ogl/include/tulip/GlComplexeEntity.h
@@ -0,0 +1,35 @@
+//-*-c++-*-
+/**
+ Author: Morgan Mathiaut
+ Email : mathiaut@labri.fr
+ Last modification : 23/10/2007
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by  
+ the Free Software Foundation; either version 2 of the License, or     
+ (at your option) any later version.
+*/
+#ifndef Tulip_GLCOMPLEXEENTITY_H
+#define Tulip_GLCOMPLEXEENTITY_H
+
+#include <vector>
+
+#include "tulip/GlEntity.h"
+#include "tulip/GlSceneVisitor.h"
+#include "tulip/Camera.h"
+#include "tulip/GlGraphInputData.h"
+
+namespace tlp {
+
+  class TLP_GL_SCOPE GlComplexeEntity : public GlEntity {
+
+  public:
+    
+    virtual BoundingBox getBoundingBox(GlGraphInputData* data) = 0;
+
+    virtual void draw(float lod,GlGraphInputData* data,Camera* camera) = 0;
+
+  };
+  
+}
+
+#endif // Tulip_GLCOMPLEXEENTITY_H
---
+++ library/tulip-ogl/include/tulip/GlSimpleEntity.h
@@ -0,0 +1,46 @@
+//-*-c++-*-
+/**
+ Author: Morgan Mathiaut
+ Email : mathiaut@labri.fr
+ Last modification : 23/10/2007
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by  
+ the Free Software Foundation; either version 2 of the License, or     
+ (at your option) any later version.
+*/
+#ifndef Tulip_GLSIMPLEENTITY_H
+#define Tulip_GLSIMPLEENTITY_H
+
+#include "GlEntity.h"
+#include "GlSceneVisitor.h"
+
+namespace tlp {
+
+  class TLP_GL_SCOPE GlSimpleEntity : public GlEntity {
+
+  public:
+
+    GlSimpleEntity():visible(true) {}
+
+    virtual void draw(float lod) = 0;
+    
+    virtual void acceptVisitor(GlSceneVisitor *visitor) {
+      visitor->visit(this);
+    }
+
+    virtual void setVisible(bool visible) {this->visible=visible;}
+    virtual bool isVisible() {return visible;}
+
+    virtual BoundingBox getBoundingBox() {return boundingBox;}
+
+  protected:
+    
+    bool visible;
+    
+    BoundingBox boundingBox;
+
+  };
+  
+}
+
+#endif // Tulip_GLSIMPLEENTITY_H
