c338290981489f20cb9276ad150bb9856863433c
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2007-11-27 10:27:51 +0000

    add openGl init in Camera for new version of 3D rendering
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_0_0_dev@974 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/Camera.h
+++ library/tulip-ogl/include/tulip/Camera.h
@@ -6,25 +6,73 @@
 #endif
 
 #include <tulip/Coord.h>
+#include <tulip/Vector.h>
+#include <tulip/Matrix.h>
 
 namespace tlp {
 
-class TLP_GL_SCOPE Camera {
- public:
-  Coord center,eyes,up;
-  double zoomFactor;
-  double sceneRadius;
-  Camera(Coord center=Coord(0,0,0),Coord eyes=Coord(0,0,10), Coord up=Coord(0,-1,0), double zoomFactor=0.5, double sceneRadius=10);
-  /// This moves the camera forward or backward depending on the speed
-  void move(float speed);
-  ///  This strafes the camera left and right depending on the speed (-/+)
-  void strafeLeftRight(float speed);
-  ///  This strafes the camera up and down depending on the speed (-/+)
-  void strafeUpDown(float speed);
-  /// This rotates the camera's eyes around the center depending on the values passed in.
-  void rotate(float angle, float x, float y, float z);
+  class GlScene;
 
-};
+  class TLP_GL_SCOPE Camera {
+  public:
+    
+    Camera(GlScene* scene,Coord center=Coord(0,0,0),Coord eyes=Coord(0,0,10), Coord up=Coord(0,-1,0), double zoomFactor=0.5, double sceneRadius=10);
+    Camera(GlScene* scene,bool d3,bool reversed=false);
+
+    void setScene(GlScene *scene);
+
+    /// This moves the camera forward or backward depending on the speed
+    void move(float speed);
+    ///  This strafes the camera left and right depending on the speed (-/+)
+    void strafeLeftRight(float speed);
+    ///  This strafes the camera up and down depending on the speed (-/+)
+    void strafeUpDown(float speed);
+    /// This rotates the camera's eyes around the center depending on the values passed in.
+    void rotate(float angle, float x, float y, float z);
+
+    bool is3D() {return d3;}
+
+    Vector<int, 4> getViewport();
+    
+    void initGl();
+    //void initGlParametters();
+    void initLight();
+    void initProjection(bool reset=true);
+    void initModelView();
+
+    double getSceneRadius() {return sceneRadius;}
+    double getZoomFactor() {return zoomFactor;}
+    Coord getEyes() {return eyes;}
+    Coord getCenter() {return center;}
+    Coord getUp() {return up;}
+
+    void setSceneRadius(double sceneRadius) {this->sceneRadius=sceneRadius;matrixCoherent=false;}
+    void setZoomFactor(double zoomFactor) {this->zoomFactor=zoomFactor;matrixCoherent=false;}
+    void setEyes(const Coord& eyes) {this->eyes=eyes;matrixCoherent=false;}
+    void setCenter(const Coord& center) {this->center=center;matrixCoherent=false;}
+    void setUp(const Coord& up) {this->up=up;matrixCoherent=false;}
+
+    Coord screenTo3DWorld(const Coord &point);
+    Coord worldTo2DScreen(const Coord &obj);
+    
+    GlScene* scene;
+
+  private:
+
+    bool matrixCoherent;
+    
+    Coord center,eyes,up;
+    double zoomFactor;
+    double sceneRadius;
+    
+    Matrix<float, 4> modelviewMatrix;
+    Matrix<float, 4> projectionMatrix;
+    Matrix<float, 4> transformMatrix;
+
+    bool d3;
+    bool reversed;
+    
+  };
 
 }
 
