0504b2c99c222c69b8eb5636912c92a9773a7035
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2008-03-10 10:14:47 +0000

    add plugins manager
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@1113 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ software/tulip/include/TulipPluginLoader.h
@@ -0,0 +1,55 @@
+#ifndef _Tulip_TULIPPLUGINLOADER_H_
+#define _Tulip_TULIPPLUGINLOADER_H_
+
+#include <vector>
+
+#include <PluginLoaderWithInfo.h>
+
+#include "AppStartUp.h"
+
+namespace tlp {
+
+  //==============================================================
+  struct TulipPluginLoader:public PluginLoaderWithInfo {
+    AppStartUp *appStartUp;
+    int progress;
+
+    virtual void start(const std::string &path,const std::string &type) {
+      appStartUp->setProgress(0);
+      progress=0;
+      PluginLoaderWithInfo::start(path,type);
+    }
+
+    virtual void numberOfFiles(int nbFile) {
+      appStartUp->setTotalSteps(nbFile);
+      qApp->processEvents();
+    }
+
+    virtual void loaded(const std::string &name,
+			const std::string &author,
+			const std::string &date, 
+			const std::string &info,
+			const std::string &release,
+			const std::string &version,
+			const std::list <Dependency> &deps)
+    {
+      progress++;
+      appStartUp->setLabel(name);
+      appStartUp->setProgress(progress);
+      qApp->processEvents();
+
+      PluginLoaderWithInfo::loaded(name,author,date,info,release,version,deps);
+    }
+
+    virtual void aborted(const std::string &filename,const  std::string &errormsg) {
+      progress++;
+      appStartUp->setLabel("Error");
+      appStartUp->setProgress(progress);
+      qApp->processEvents();
+
+      PluginLoaderWithInfo::aborted(filename,errormsg);
+    }
+  };
+  
+}
+#endif
