9c2edba9d6e170cbd1a73ac63d3dcc2885c1f5e3
Patrick Mary <mary@labri.fr>
2008-04-07 06:59:57 +0000

    cleanup for 3.0
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@1187 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- docs/gen-profile.sh
+++ docs/gen-profile.sh
@@ -1,35 +1,11 @@
 #!/bin/bash
 
-if [ -d "$QTDIR/doc/html" ]
-then
-    QTDOCDIR=$QTDIR/doc/html;
-elif [ -d "/usr/share/doc/qt-devel-3.3.4/html" ]
-then
-    QTDOCDIR=/usr/share/doc/qt-devel-3.3.4/html
-elif [ -n "$QTDOC" ]
-then
-	QTDOCDIR=$QTDOC 
-else
-    echo 
-    echo "**************************************************"
-    echo "  WARNING : "
-    echo "  We can not locate the documentations of Qt, "
-    echo "  please set the environment variable QTDOC to "
-    echo "  specify it. Refer the directory where you can"
-    echo "  find \"assistant.html\" "
-    echo "**************************************************"
-    echo
-    QTDOCDIR=""
-fi
-:
 echo "<assistantconfig version=\"3.2.0\"><profile>" > profile.adp
 echo "<property name=\"name\">Help</property>" >> profile.adp
 echo "<property name=\"title\">Help Tulip software</property>" >> profile.adp
 echo "<property name=\"applicationicon\">common/logo-icon.png</property>" >> profile.adp
 echo "<property name=\"startpage\">userHandbook/html/index.html</property>" >> profile.adp 
-echo "<property name=\"aboutmenutext\">About Manuals</property>" >> profile.adp
-#echo "<property name=\"abouturl\"></property>" >> profile.adp
-echo "<property name=\"assistantdocs\">$QTDOCDIR</property> </profile>" >> profile.adp
+echo "<property name=\"aboutmenutext\">About Manuals</property> </profile>" >> profile.adp
 if test -f userHandbook/profile.dcf ; then
 	cat userHandbook/profile.dcf >> profile.adp ;
 fi;
