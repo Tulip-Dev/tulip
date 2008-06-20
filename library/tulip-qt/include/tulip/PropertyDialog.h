b7a23698740e514f487f6686a2f06b4ce8d83755
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2008-06-20 14:59:13 +0000

    Tulip first version
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/Tulive@1280 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-qt/include/tulip/PropertyDialog.h
@@ -0,0 +1,57 @@
+#ifndef PROPERTYDIALOG_H
+#define PROPERTYDIALOG_H
+#include <string>
+
+#ifdef  _WIN32
+// compilation pb workaround
+#include <windows.h>
+#endif
+#include <tulip/Graph.h>
+#include <tulip/Observable.h>
+
+#include "PropertyDialogData.h"
+
+class QListWidgetItem;
+class QStringList;
+class QScrollBar;
+
+namespace tlp {
+  class GlMainWidget;
+  class PropertyInterface;
+
+  class PropertyDialog : public QWidget, public Ui::PropertyDialogData { 
+    Q_OBJECT;
+    
+  public:
+    PropertyDialog(QWidget *parent = 0);
+    ~PropertyDialog();
+    void setGraph(Graph *);
+    
+  private:
+    Graph *graph;
+    //GlMainWidget *glWidget;
+    PropertyInterface *editedProperty;
+    std::string editedPropertyName;
+    bool _filterSelection;
+    void updateNbElements();
+    
+  public slots:  
+    ///Manages observers change notification.
+    void update();
+    void selectNode(node);
+    void selectEdge(edge);
+    void changePropertyName(QListWidgetItem *, QListWidgetItem *);
+    void newProperty();
+    void removeProperty();
+    void cloneProperty();
+    void toStringProperty();
+    void filterSelection(bool);
+    void setAllValue();
+    
+  signals:
+    void tulipAllNodePropertyChanged(Graph *, const QString &property, const QString &value);
+    void tulipAllEdgePropertyChanged(Graph *, const QString &property, const QString &value);
+  };
+
+}
+#endif // PROPERTYDIALOG_H
