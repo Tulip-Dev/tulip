c79c422107e200d1d84e86afb8029658a74c666d
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2008-12-17 13:16:20 +0000

    - bug fix in GlCircle, write color accessor to overload GlPolygon functions
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@1577 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/GlCircle.h
+++ library/tulip-ogl/include/tulip/GlCircle.h
@@ -6,8 +6,8 @@
  Last modification : 31/10/2007
 
  This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by  
- the Free Software Foundation; either version 2 of the License, or     
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 */
 
@@ -20,7 +20,7 @@
 #include "tulip/GlPolygon.h"
 
 namespace tlp {
-  
+
   /** \brief General class used to render circles as GlEntity.
    *
    * This class is a generic class to render circles as GlEntity.
@@ -28,20 +28,43 @@ namespace tlp {
   class TLP_GL_SCOPE GlCircle : public GlPolygon {
   public:
     /**
-     * Constructor 
+     * Constructor
      */
-    GlCircle(const Coord &center = Coord(0,0,0), 
-		float radius = 1., 
+    GlCircle(const Coord &center = Coord(0,0,0),
+		float radius = 1.,
 		const Color &outlineColor = Color(255, 0 , 0 , 255),
 		const Color &fillColor = Color (0, 0, 255,255),
-		bool filled = false, 
+		bool filled = false,
 		bool outlined = true,
 		float startAngle = 0.0,
 		unsigned int segments = 10);
+
     /**
      * Accessor in writing to the center, the radius and the startAngle of the circle
      */
     void set(const Coord &center, float radius, float startAngle);
+
+    /**
+     * return the fill color of the circle (i parameters is not use)
+     * This function is write to overload the GlPolygon function
+     */
+    virtual const Color& fcolor(const unsigned int i) const {return fcolor(0);}
+    /**
+     * return the fill color of the circle (i parameters is not use)
+     * This function is write to overload the GlPolygon function
+     */
+    virtual Color& fcolor(const unsigned int i) {return fcolor(0);}
+
+    /**
+     * return the outline color of the circle (i parameters is not use)
+     * This function is write to overload the GlPolygon function
+     */
+    virtual const Color& ocolor(const unsigned int i) const {return ocolor(0);}
+    /**
+     * return the outline color of the circle (i parameters is not use)
+     * This function is write to overload the GlPolygon function
+     */
+    virtual Color& ocolor(const unsigned int i) {return ocolor(0);}
 
     /**
      * Function to export data in XML
