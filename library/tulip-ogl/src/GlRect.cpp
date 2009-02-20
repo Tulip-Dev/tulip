d7ae33a7b1c10af9224911fb165704c2ea9572ec
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2009-02-20 08:26:04 +0000

    - bug fix in normal
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@1739 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlRect.cpp
+++ library/tulip-ogl/src/GlRect.cpp
@@ -1,5 +1,7 @@
 #include "tulip/GlRect.h"
 
+#include "tulip/GlTools.h"
+
 using namespace std;
 
 namespace tlp {
@@ -88,7 +90,37 @@ namespace tlp {
   }
   //=====================================================
   void GlRect::draw(float lod,Camera *camera) {
-    GlPolygon::draw(lod,camera);
+    glDisable(GL_CULL_FACE);
+    Coord normal=points[1]-points[0];
+    normal^=(points[3]-points[0]);
+    normal/=normal.norm();
+    if (filled){
+      glBegin(GL_QUADS);
+      glNormal3fv((float*)&normal);
+      setMaterial(fillColors[0]);
+      glVertex3fv((float *)&points[0]);
+      setMaterial(fillColors[1]);
+      glVertex3fv((float *)&points[1]);
+      setMaterial(fillColors[2]);
+      glVertex3fv((float *)&points[2]);
+      setMaterial(fillColors[3]);
+      glVertex3fv((float *)&points[3]);
+      glEnd();
+    }
+    if (outlined) {
+      if(outlineSize!=1)
+        glLineWidth(outlineSize);
+      glBegin(GL_LINE_LOOP);
+      setMaterial(outlineColors[0]);
+      glVertex3fv((float *)&points[0]);
+      setMaterial(outlineColors[1]);
+      glVertex3fv((float *)&points[1]);
+      setMaterial(outlineColors[2]);
+      glVertex3fv((float *)&points[2]);
+      setMaterial(outlineColors[3]);
+      glVertex3fv((float *)&points[3]);
+      glEnd();
+    }
   }
   //===========================================================
   void GlRect::getXML(xmlNodePtr rootNode) {
