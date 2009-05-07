3e82101bcf5fb2f7ed254edbdb49be15a3317424
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2009-05-07 12:41:34 +0000

    add TLP_Gl_SCOPE
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@1860 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/Curves.h
+++ library/tulip-ogl/include/tulip/Curves.h
@@ -12,19 +12,19 @@
 namespace tlp {
   template<typename T, unsigned int N>
     class Matrix;
-  void curveVisibility(const Coord &startPoint,const std::vector<Coord> &bends, const Coord &endPoint,
+  TLP_GL_SCOPE void curveVisibility(const Coord &startPoint,const std::vector<Coord> &bends, const Coord &endPoint,
 		       const Size &size, bool &polygon, bool &line, const Matrix<float,4> &projectionMatrix, const Matrix<float,4> &modelviewMatrix, const tlp::Vector<int, 4>&viewportArray);
   //====================================================================
   //return vertices, update startN and endN to prevent bad edge drawing
-  std::vector<Coord> computeCleanVertices(const std::vector<Coord> &bends,
+  TLP_GL_SCOPE std::vector<Coord> computeCleanVertices(const std::vector<Coord> &bends,
 					  const Coord &startPoint, const Coord &endPoint,
 					  Coord &startN, Coord &endN);
-  void polyLine(const std::vector<Coord> &,/* polyline vertices */
+  TLP_GL_SCOPE void polyLine(const std::vector<Coord> &,/* polyline vertices */
 		const Color &,             /* start color */
 		const Color &);            /* end color */
-  void polyLine(const std::vector<Coord> &, /* polyline vertices */
+  TLP_GL_SCOPE void polyLine(const std::vector<Coord> &, /* polyline vertices */
 		const std::vector<Color> &);/* colors at polyline verts */
-  void polyQuad(const std::vector<Coord> &, /* polyline vertces */
+  TLP_GL_SCOPE void polyQuad(const std::vector<Coord> &, /* polyline vertces */
 		const Color &,            /* start color */
 		const Color &,            /* end color */
 		const float,              /* start size */
@@ -32,18 +32,18 @@ namespace tlp {
 		const Coord &,            /* normal to the begin of the curve */
 		const Coord &,            /* normal to the end curve */
 		const std::string &textureName="");			/* textureName */
-  void polyQuad(const std::vector<Coord> &, /* polyline vertces */
+  TLP_GL_SCOPE void polyQuad(const std::vector<Coord> &, /* polyline vertces */
 		const std::vector<Color> &, /* colors at polyline verts */
 		const std::vector<float> &, /* size at polyline verts */
 		const Coord &,            /* nomal to the begin of the curve */
 		const Coord &,            /* nomal to the end curve */
 		const std::string &textureName="");			/* textureName */
-  void polyCylinder(const std::vector<Coord> &,    /* polyline vertces */
+  TLP_GL_SCOPE void polyCylinder(const std::vector<Coord> &,    /* polyline vertces */
 		    const std::vector<Color> &,    /* colors at polyline verts */
 		    const std::vector<float> &,    /* size at polyline verts */
 		    const Coord &,            /* nomal to the begin of the curve */
 		    const Coord &);           /* nomal to the end curve */
-  void polyCylinder(const std::vector<Coord> &,    /* polyline vertces */
+  TLP_GL_SCOPE void polyCylinder(const std::vector<Coord> &,    /* polyline vertces */
 		    const Color &,            /* start color */
 		    const Color &,            /* end color */
 		    const float,              /* start size */
@@ -51,45 +51,45 @@ namespace tlp {
 		    const Coord &,            /* nomal to the begin of the curve */
 		    const Coord &);           /* nomal to the end curve */
   //====================================================================
-  void bezierCylinder(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void bezierCylinder(const std::vector<Coord> &,  /* polyline vertces */
 		      const Color &,          /* start color */
 		      const Color &,          /* end color */
 		      const float,            /* start size */
 		      const float,            /* end size */
 		      const Coord &,          /* nomal to the begin of the curve */
 		      const Coord &);         /* nomal to the end curve */
-  void bezierQuad(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void bezierQuad(const std::vector<Coord> &,  /* polyline vertces */
 		  const Color &,          /* start color */
 		  const Color &,          /* end color */
 		  const float,            /* start size */
 		  const float,            /* end size */
 		  const Coord &,          /* nomal to the begin of the curve */
 		  const Coord &);         /* nomal to the end curve */
-  void bezierLine(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void bezierLine(const std::vector<Coord> &,  /* polyline vertces */
 		  const Color &,          /* start color */
 		  const Color &);          /* end color */
   //====================================================================
-  void splineCylinder(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void splineCylinder(const std::vector<Coord> &,  /* polyline vertces */
 		      const Color &,          /* start color */
 		      const Color &,          /* end color */
 		      const float,            /* start size */
 		      const float,            /* end size */
 		      const Coord &,          /* nomal to the begin of the curve */
 		      const Coord &);         /* nomal to the end curve */
-  void splineQuad(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void splineQuad(const std::vector<Coord> &,  /* polyline vertces */
 		  const Color &,          /* start color */
 		  const Color &,          /* end color */
 		  const float,            /* start size */
 		  const float,            /* end size */
 		  const Coord &,          /* nomal to the begin of the curve */
 		  const Coord &);         /* nomal to the end curve */
-  void splineLine(const std::vector<Coord> &,  /* polyline vertces */
+  TLP_GL_SCOPE void splineLine(const std::vector<Coord> &,  /* polyline vertces */
 		  const Color &,          /* start color */
 		  const Color &);          /* end color */
   //====================================================================
-  std::vector<Color> getColors(const std::vector<Coord> &line, const Color &c1, const Color &c2);
-  std::vector<float> getSizes(const std::vector<Coord> &line, float s1, float s2);
-  GLfloat* buildCurvePoints (const std::vector<Coord> &vertices,
+  TLP_GL_SCOPE std::vector<Color> getColors(const std::vector<Coord> &line, const Color &c1, const Color &c2);
+  TLP_GL_SCOPE std::vector<float> getSizes(const std::vector<Coord> &line, float s1, float s2);
+  TLP_GL_SCOPE GLfloat* buildCurvePoints (const std::vector<Coord> &vertices,
       const std::vector<float> &sizes,
       const Coord &startN,
       const Coord &endN,
