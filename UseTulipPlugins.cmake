a3defb3658f0420357d52c690a87a0d15caf6ae9
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2009-07-24 14:15:12 +0000

    - bug fix on cmake to compile on windows
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_cmake@1991 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- UseTulipPlugins.cmake
+++ UseTulipPlugins.cmake
@@ -1,21 +1,35 @@
-FIND_PACKAGE( Qt4 REQUIRED )
-INCLUDE( ${QT_USE_FILE} )
-
-INCLUDE("${TulipSharePath}UseTulip.cmake")
+## -----------------------------------------------------------------------------------------------
+## ***********************************************************************************************
+## -----------------------------------------------------------------------------------------------
+##
+## Options for cmake (use -D):
+## PLUGINS_DIR=path_to_install_plugins (if no set : tulip install dir)
+## SERVER_INSTALL=1 activate server style installation
+## PLUGINS_SERVER_EXT=i386 extension use with server style installation
+##
+## -----------------------------------------------------------------------------------------------
+## ***********************************************************************************************
+## -----------------------------------------------------------------------------------------------
 
 cmake_minimum_required(VERSION 2.6)
+
+FIND_PACKAGE( Qt4 REQUIRED )
+INCLUDE( ${QT_USE_FILE} )
+FIND_PACKAGE(OpenGL)
+
+INCLUDE("${TulipSharePath}UseTulip.cmake")
 
 ## -----------------------------------------------------------------------------------------------
 ## tulip-config 
 ## -----------------------------------------------------------------------------------------------
-execute_process(COMMAND tulip-config --cxxflags
-                OUTPUT_VARIABLE TulipCxxFlags OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND tulip-config --version
-                OUTPUT_VARIABLE TulipVersion OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND tulip-config --pluginpath
-                OUTPUT_VARIABLE PluginsInstallationDir OUTPUT_STRIP_TRAILING_WHITESPACE)
-execute_process(COMMAND tulip-config --pluginextension
-                OUTPUT_VARIABLE PluginsExtension OUTPUT_STRIP_TRAILING_WHITESPACE)
+EXEC_PROGRAM(sh ARGS "tulip-config --cxxflags" OUTPUT_VARIABLE TulipCxxFlags)
+EXEC_PROGRAM(sh ARGS "tulip-config --version" OUTPUT_VARIABLE TulipVersion)
+EXEC_PROGRAM(sh ARGS "tulip-config --cmakepluginpath" OUTPUT_VARIABLE PluginsInstallationDir)
+EXEC_PROGRAM(sh ARGS "tulip-config --pluginextension" OUTPUT_VARIABLE PluginsExtension)
+EXEC_PROGRAM(sh ARGS "tulip-config --cmakelib_tulip" OUTPUT_VARIABLE LibTulip)
+EXEC_PROGRAM(sh ARGS "tulip-config --cmakelib_ogl" OUTPUT_VARIABLE LibTulipOgl)
+EXEC_PROGRAM(sh ARGS "tulip-config --cmakelib_qt" OUTPUT_VARIABLE LibTulipQt)
+
 
 ## -----------------------------------------------------------------------------------------------
 ## debug mode
@@ -58,6 +72,14 @@ QT4_ADD_RESOURCES(RCC_SRCS ${RCC_PRE_SRCS})
 ## Compilation 
 ## -----------------------------------------------------------------------------------------------
 ADD_LIBRARY(${PluginName} SHARED ${UI_SRCS} ${MOC_SRCS} ${RCC_SRCS} ${LIB_SRCS})
+TARGET_LINK_LIBRARIES(${PluginName} ${OPENGL_glu_LIBRARY})
+TARGET_LINK_LIBRARIES(${PluginName} ${OPENGL_gl_LIBRARY})
+TARGET_LINK_LIBRARIES(${PluginName} ${QT_QTCORE_LIBRARY})
+TARGET_LINK_LIBRARIES(${PluginName} ${QT_QTGUI_LIBRARY})
+TARGET_LINK_LIBRARIES(${PluginName} ${QT_QTOPENGL_LIBRARY})
+TARGET_LINK_LIBRARIES(${PluginName} ${LibTulip})
+TARGET_LINK_LIBRARIES(${PluginName} ${LibTulipOgl})
+TARGET_LINK_LIBRARIES(${PluginName} ${LibTulipQt})
 
 ## -----------------------------------------------------------------------------------------------
 ## Install
