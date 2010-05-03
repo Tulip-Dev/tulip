47ab3b3ccf5676c0add9027b20ccf5843292dae6
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2010-05-03 06:10:59 +0000

    - ui file missing on svn - remove some comments
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2470 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/OpenGlConfigManager.h
+++ library/tulip-ogl/include/tulip/OpenGlConfigManager.h
@@ -1,13 +1,3 @@
-//-*-c++-*-
-/**
- Author: Morgan Mathiaut
- Email : mathiaut@labri.fr
- Last modification : 17/2/2009
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-*/
 #ifndef Tulip_OPENGLCONFIGMANAGER_H
 #define Tulip_OPENGLCONFIGMANAGER_H
 
