7986a1360a8b9f8556a85d44c83e71431f19bb70
David Auber <david.auber@labri.fr>
2010-05-23 06:23:38 +0000

    Add some usefull function developped for Quadtree need to Refactor Quatree to use them now
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2541 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/Rectangle.h
+++ library/tulip/include/tulip/Rectangle.h
@@ -1,40 +1,98 @@
 //@TLPGEOLICENCE#
-#ifndef TLP_GEO_RECTANGLE_H
-#define TLP_GEO_RECTANGLE_H
+#ifndef TLP_RECTANGLE_H
+#define TLP_RECTANGLE_H
 #include <tulip/Vector.h>
 namespace tlp {
 
-  /**
+    /**
    * \addtogroup basic
    */ 
-  /*@{*/
+    /*@{*/
     /**
      * \brief class for rectangle
      *
-     * Enables to both create and manipulate a rectangle
+     * Enables to both create and manipulate a 2D Axis Aligned Rectangle
      *
-     * Author : <a href="mailto:auber@tulip-software.org>David Auber</A>
-     * Version 0.0.1 24/01/2003 
+     * Author : <a href="www.tulip-software.org>Tulip team</a>
      */
     template<typename Obj>
     struct Rectangle : public Array<Vector<Obj,2>,2> {
-      /**
-       * Returns true if r intersect "this".
+        /**
+          * Create a new invalid rectangle
+          */
+        Rectangle() {
+            (*this)[0].fill(1);
+            (*this)[1].fill(-1);
+        }
+       /**
+       * create a new Rectangle
+       * Validity is checked in debug mode
        */
-      bool intersect(const Rectangle &r) const {
-	return (*this)[0][0]<r[1][0] && (*this)[1][0]>r[0][0] && (*this)[0][1]<r[1][1] && (*this)[1][1]>r[0][1];
-      }
-      /**
-       * Translate "this" by vector v
+        Rectangle(const Vector<Obj,2> &min, const Vector<Obj,2> &max) {
+            (*this)[0] = min;
+            (*this)[1] = max;
+            assert(isValid());
+        }
+        /**
+       * @return true if r intersect "this".
        */
-      void translate(const tlp::Vector<Obj,2> &v) {
-	(*this)[0]+=v;
-	(*this)[1]+=v;
-      }
+	bool intersect(const Rectangle &r) const {
+            assert(r.isValid() && this->isValid());
+	    
+            if ((*this)[0][0] > r[1][0])  return false;
+            if ((*this)[1][0] < r[0][0])  return false;
+            if ((*this)[0][1] > r[1][1])  return false;
+            if ((*this)[1][1] < r[0][1])  return false;
+
+            return true;
+	}
+	/**
+       * @return the true if there is an intersection else false, the intersection parameter is used to stored the Rectangle pf intersection (if it exists). 
+       */
+	bool intersect(const Rectangle &r, Rectangle &intersection) const {
+            assert(r.isValid() && this->isValid());
+            if (!this->intersect(r)) return false;
+
+            intersection[0][0] = std::max((*this)[0][0] , r[0][0]);
+            intersection[1][0] = std::min((*this)[1][0] , r[1][0]);
+            intersection[0][1] = std::max((*this)[0][1] , r[0][1]);
+            intersection[1][1] = std::min((*this)[1][1] , r[1][1]);
+
+            return true;
+	}
+	/**
+	* @return true if the Rectangle is well define [0] min corner, [1] max corner.
+	*/
+	bool isValid() {
+            return (*this)[0][0] <= (*this)[1][0] && (*this)[0][1] <= (*this)[1][1];
+	}
+        /**
+        * Return true if point is stricly inside the AARectangle
+        */
+        bool isInside(const Vector<Obj, 2> &p) const {
+            if (p[0] > (*this)[1][0]) return false;
+            if (p[0] < (*this)[0][0]) return false;
+            if (p[1] > (*this)[1][1]) return false;
+            if (p[1] < (*this)[0][1]) return false;
+            return true;
+        }
+        /**
+        * @return true if r is inside or equal to the AARectangle
+        */
+        bool isInside(const Rectangle &r) const{
+            if ((*this)[0] == r[0] && (*this)[1] == r[1]) return true;
+            if (this->isInside(r[0]) && this->isInside(r[1])) return true;
+            return false;
+        }
+
+        /**
+        * Translate "this" by vector v
+        */
+        void translate(const tlp::Vector<Obj,2> &v) {
+            (*this)[0] += v;
+            (*this)[1] += v;
+        }
     };
     /*@}*/
-
 }
-//#include "cxx/Rectangle.cxx"
 #endif
-
