fb6807b27e3abc46f03cf0504f4e8a5af4998556
Antoine Lambert <antoine.lambert33@gmail.com>
2010-05-25 15:30:23 +0000

    - add the "Smooth and efficient zooming and panning" implementation to Tulip trunk
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2563 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-ogl/include/tulip/GlSceneZoomAndPan.h
@@ -0,0 +1,105 @@
+/*
+ * GlSceneZoomAndPan.h
+ *
+ *  Created on: 20 oct. 2009
+ *      Author: antoine
+ */
+
+#ifndef GLSCENEZOOMANDPAN_H_
+#define GLSCENEZOOMANDPAN_H_
+
+#include <tulip/GlScene.h>
+
+namespace tlp {
+
+/**
+ * \brief A class which encapsulate a Tulip OpenGL scene animation
+ *  This class aims to encapsulate a Tulip OpenGL scene animation.
+ *  Derive it if you want to add extra animations to the Tulip OpenGL scene while a Zoom and Pan is performed
+ */
+class AdditionalGlSceneAnimation {
+
+public :
+
+	/**
+	 * Method to set the total number of animation steps. No need to call it because the GlSceneZoomAndPan class do that task.
+	 *
+	 */
+	void setNbAnimationSteps(int nbAnimationSteps) {this->nbAnimationSteps = nbAnimationSteps;}
+
+	/**
+	 * Pure virtual method called at each step of the Zoom and Pan animation.
+	 *
+	 * Implement it in your derived class to perform any extra animation you want on the Tulip OpenGL scene.
+	 */
+	virtual void animationStep(int animationStep) = 0;
+
+protected :
+
+	int nbAnimationSteps;
+};
+
+/** \brief A convenient class to perform Zoom and Pan animation on Tulip OpenGL scene
+ *
+ *  This class allow to perform a smooth and efficient zooming and panning on Tulip OpenGL scene.
+ *  The algorithm used to perform this task is the one published in : Jarke J. van Wijk and Wim A.A. Nuij, "Smooth and efficient zooming and panning"
+ *  For more details, the paper can be downloaded at the following url : www.win.tue.nl/~vanwijk/zoompan.pdf
+ *  Even if this class contains the whole Zoom and Pan implementation, it is not aimed to be used directly because its role is only to compute new camera parameters.
+ *  Use the derived class QtGlSceneZoomAndPanAnimator in the tulip-qt library instead to perform the animation.
+ */
+class GlSceneZoomAndPan  {
+
+public :
+
+	/**
+	 * GlSceneZoomAndPan constructor
+	 *
+	 * \param glScene the Tulip OpenGL scene on which to perform zooming and panning
+	 * \param boundingBox the bounding box in scene coordinates on which the Tulip OpenGL scene has to be zoomed and panned.
+	 * 					  At the end of the animation, the viewport will be zoomed and centered on the content of that bounding box.
+	 * \param nbAnimationSteps the number of steps to perform during the animation
+	 * \param optimalPath if true zooming and panning will be combined at each step of the animation, if false the scene will be zoomed out/in, panned then zoomed in/out
+	 * \param p zoom/pan trade-off parameter, adjust it according to your needs
+	 */
+	GlSceneZoomAndPan(GlScene *glScene, const BoundingBox &boundingBox, const int nbAnimationSteps = 50, const bool optimalPath = true, const double p = sqrt(1.6));
+
+	/**
+	 * Method to add an additional scene animation while zooming and panning
+	 *
+	 * \param additionalAnimation The animation to add
+	 */
+	void setAdditionalGlSceneAnimation(AdditionalGlSceneAnimation *additionalAnimation);
+
+	/**
+	 * Method which return the number of animation steps
+	 */
+	int getNbAnimationsStep() const {return nbAnimationSteps;}
+
+	/**
+	 * Method to set the number of animation steps
+	 */
+	void setNbAnimationSteps(const int nbAnimationSteps) {this->nbAnimationSteps = nbAnimationSteps;}
+
+	/**
+	 * Method which performs the zoom and pan animation. Its role is to compute new camera parameters at step animationStep.
+	 * The scene is not redrawn with this method, you have to call the draw method on the associated GlScene object
+	 */
+	void zoomAndPanAnimationStep(int animationStep);
+
+protected :
+
+	Camera *camera;
+	Vector<int, 4> viewport;
+	int nbAnimationSteps;
+	bool optimalPath;
+	double p;
+	Coord camCenterStart, camCenterEnd;
+	double w0, w1, u0, u1, b0, b1, r0, r1, S, sA, sB, wm;
+	AdditionalGlSceneAnimation *additionalAnimation;
+	bool doZoomAndPan;
+
+};
+
+}
+
+#endif /* GLSCENEZOOMANDPAN_H_ */
---
+++ library/tulip-ogl/src/GlSceneZoomAndPan.cpp
@@ -0,0 +1,115 @@
+/*
+ * GlSceneZoomAndPan.cpp
+ *
+ *  Created on: 20 oct. 2009
+ *      Author: antoine
+ */
+
+#include "tulip/GlSceneZoomAndPan.h"
+#include <algorithm>
+
+using namespace std;
+
+namespace tlp {
+
+GlSceneZoomAndPan::GlSceneZoomAndPan(GlScene *glScene, const BoundingBox &boundingBox, const int nbAnimationSteps, const bool optimalPath, const double p) :
+	nbAnimationSteps(nbAnimationSteps), optimalPath(optimalPath), p(p), additionalAnimation(NULL) {
+
+	camera = glScene->getLayer("Main")->getCamera();
+	viewport = glScene->getViewport();
+
+	camCenterStart = camera->getCenter();
+	camCenterEnd = Coord(boundingBox.center());
+
+	Coord blScene(camera->screenTo3DWorld(Coord(0, 0, 0)));
+	Coord trScene(camera->screenTo3DWorld(Coord(viewport[2], viewport[3], 0)));
+
+	w0 = min(abs(trScene.getX() - blScene.getX()), abs(trScene.getY() - blScene.getY()));
+	w1 = max(boundingBox[1][0] - boundingBox[0][0], boundingBox[1][1] - boundingBox[0][1]);
+
+	u0 = 0;
+	u1 = camCenterStart.dist(camCenterEnd);
+
+	if (u1 < 1e-5) u1 = 0;
+
+	if (optimalPath) {
+		if (u0 != u1) {
+			b0 = (w1 * w1 - w0 * w0 + p * p * p * p * u1 * u1) / (2 * w0 * p * p * u1);
+			b1 = (w1 * w1 - w0 * w0 - p * p * p * p * u1 * u1) / (2 * w1 * p * p * u1);
+			r0 = log(-b0 + sqrt(b0 * b0 + 1));
+			r1 = log(-b1 + sqrt(b1 * b1 + 1));
+
+			S = (r1 - r0) / p;
+		} else {
+			S = abs(log(w1 / w0)) / p;
+		}
+	} else {
+		wm = max(w0, max(w1, p * p * (u1 - u0) / 2));
+		sA = log(wm / w0) / p;
+		sB = sA + p * (u1 - u0) / wm;
+		S = sB + log(wm / w1) / p;
+	}
+
+	if (abs(w0 - w1) > 1e-3 || u1 > 0) {
+		doZoomAndPan = true;
+	} else {
+		doZoomAndPan = false;
+	}
+}
+
+void GlSceneZoomAndPan::setAdditionalGlSceneAnimation(AdditionalGlSceneAnimation *additionalAnimation) {
+	this->additionalAnimation = additionalAnimation;
+	if (additionalAnimation != NULL) {
+		additionalAnimation->setNbAnimationSteps(nbAnimationSteps);
+	}
+}
+
+void GlSceneZoomAndPan::zoomAndPanAnimationStep(int animationStep) {
+	if (doZoomAndPan) {
+		double t = (double) animationStep / (double) nbAnimationSteps;
+		double s = t * S;
+		double u = 0, w = 0, f = 0;
+		if (optimalPath) {
+			if (u0 != u1) {
+				u = w0 / (p * p) * cosh(r0) * tanh(p * s + r0) - w0 / (p * p) * sinh(r0) + u0;
+				w = w0 * cosh(r0) / cosh(p * s + r0);
+				f = u / u1;
+			} else {
+				double k = (w1 < w0) ? -1 : 1;
+				w = w0 * exp(k * p * s);
+				f = 0;
+			}
+		} else {
+			if (s >= 0 && s < sA) {
+				u = u0;
+				w = w0 * exp(p * s);
+			} else if (s >= sA && s < sB) {
+				u = wm * (s - sA) / p + u0;
+				w = wm;
+			} else {
+				u = u1;
+				w = wm * exp(p * (sB - s));
+			}
+			if (u0 != u1) {
+				f = u / u1;
+			} else {
+				f = 0;
+			}
+		}
+		camera->setCenter(camCenterStart + (camCenterEnd - camCenterStart) *f);
+		camera->setEyes(Coord(0, 0, camera->getSceneRadius()));
+		camera->setEyes(camera->getEyes() + camera->getCenter());
+		camera->setUp(Coord(0, 1., 0));
+
+		Coord bbScreenFirst = camera->worldTo2DScreen(camera->getCenter() - Coord(w/2, w/2, 0));
+		Coord bbScreenSecond = camera->worldTo2DScreen(camera->getCenter() + Coord(w/2, w/2, 0));
+		float bbWidthScreen = bbScreenSecond.getX() - bbScreenFirst.getX();
+		double newZoomFactor = min(viewport[2], viewport[3]) / bbWidthScreen;
+		camera->setZoomFactor(camera->getZoomFactor() * newZoomFactor);
+	}
+	if (additionalAnimation != NULL) {
+		additionalAnimation->animationStep(animationStep);
+	}
+}
+
+}
