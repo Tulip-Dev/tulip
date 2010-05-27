dea4590ee7a57647133cfad4d9c0e0357bc45ecf
Patrick Mary <mary@labri.fr>
2010-05-27 13:21:13 +0000

    fix for sf bug #3007947
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2585 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/tulip.sh
+++ software/tulip/src/tulip.sh
@@ -4,10 +4,10 @@ THISDIR=$(dirname $0)
 
 case "`uname -s`" in
     *darwin* | *Darwin* | *DARWIN*)
-	export DYLD_LIBRARY_PATH=${THISDIR}/../Frameworks:${THISDIR}/../lib/tlp
+	export DYLD_LIBRARY_PATH=${THISDIR}/../Frameworks:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/view:${DYLD_LIBRARY_PATH}
 	;;
     *Linux*|*GNU/kFreeBSD*)
-	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp
+	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/view:${LD_LIBRARY_PATH}
 	;;	
 esac
 
