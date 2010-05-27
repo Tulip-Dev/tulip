96f18ae127310f7e9b6ba352a3fc2edeed83718d
David Auber <david.auber@labri.fr>
2010-05-27 10:34:36 +0000

    add some documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2583 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/AcyclicTest.h
+++ library/tulip/include/tulip/AcyclicTest.h
@@ -44,7 +44,18 @@ public:
    *  The result is cached (ie. the next call with the same graph is done in O(1) time)
    */
   static bool isAcyclic(const Graph *graph);
+  /**
+    * Make the graph acyclic, by reversing edge direction (feedback arc set problem).
+    * If there is self loops a new node is added with two edges that points to it.
+    *
+    */
   static void makeAcyclic(Graph* graph, std::vector<edge> &reversed, std::vector<tlp::SelfLoops> &selfLoops);
+  /**
+    * Return true if the graph is acyclic else false;
+    * if the graph is not acyclic use obstructionEdges variable to store all edges
+    * that create cycle
+    *
+    */
   static bool acyclicTest(const Graph *, std::vector<edge> *obstructionEdges = 0);
 
 private:
