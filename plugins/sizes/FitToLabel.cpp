33541aa1d672fc36b66b2914fcc99cfb74f41ea5
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2010-06-01 15:50:22 +0000

    Fixed FitToLabel to use each node specific font and size. Also added parameters to use different other properties than the default ones
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2630 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/sizes/FitToLabel.cpp
+++ plugins/sizes/FitToLabel.cpp
@@ -16,7 +16,7 @@
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
 
-*/
+ */
 
 #include <tulip/TextRenderer.h>
 #include <tulip/TlpTools.h>
@@ -27,48 +27,84 @@
 using namespace std;
 using namespace tlp;
 
+namespace {
+//file::filepath
+static const char * paramHelp[] = { HTML_HELP_OPEN()
+HTML_HELP_DEF( "type", "StringProperty" )
+HTML_HELP_BODY()
+"The graph property containing the displayed nodes labels."
+HTML_HELP_CLOSE(),
+
+HTML_HELP_OPEN()
+HTML_HELP_DEF( "type", "StringProperty" )
+HTML_HELP_BODY()
+"The graph property containing the font used to display the label."
+HTML_HELP_CLOSE(),
+
+HTML_HELP_OPEN()
+HTML_HELP_DEF( "type", "IntegerProperty" )
+HTML_HELP_BODY()
+"The graph property containing the fonts sizes."
+HTML_HELP_CLOSE(), };
+}
+
 /** \addtogroup size */
 /*@{*/
 /// FitToLabel.cpp - Compute size of elements according to the size of the displayed label.
 /**
- *
- *  AUTHORS : 
- *  Sebastien Carceles,
- *  Pascal Niotout,
- *  Sophie Bardet,
- *  Julien Mercadal, 
- *  Bertrand Ng Sing Kwong
+ *  version 1.0.0 : Tulip Team, Complete rewrite
+ *  initial version  : Sebastien Carceles, Pascal Niotout, Sophie Bardet, Julien Mercadal, Bertrand Ng Sing Kwong
  *
  *  \author Maintainer : David Auber University Bordeaux I France: Email:auber@tulip-software.org
  */
-class FitToLabel: public SizeAlgorithm { 
+class FitToLabel: public SizeAlgorithm {
 public:
   //==============================================Algorithm======
-  FitToLabel(const PropertyContext &context):SizeAlgorithm(context){}
+  FitToLabel(const PropertyContext &context) :
+    SizeAlgorithm(context) {
+    addParameter<StringProperty> ("prop::label", paramHelp[0], "viewLabel");
+    addParameter<StringProperty> ("prop::font", paramHelp[1], "viewFont");
+    addParameter<IntegerProperty> ("prop::fontsize", paramHelp[2], "viewFontSize");
+  }
   //====================================================
-  ~FitToLabel(){}
+  ~FitToLabel() {
+  }
   //====================================================
   bool run() {
+    Observable::holdObservers();
+
+    StringProperty *labels(graph->getProperty<StringProperty> ("viewLabel"));
+    StringProperty *fonts(graph->getProperty<StringProperty> ("viewFont"));
+    IntegerProperty *fontSizes(graph->getProperty<IntegerProperty> ("viewFontSize"));
+
+    if (dataSet != 0) {
+      dataSet->get("prop::label", labels);
+      dataSet->get("prop::font", fonts);
+      dataSet->get("prop::fontsize", fontSizes);
+    }
+
     TextRenderer textRender;
     textRender.setMode(TLP_POLYGON);
-    textRender.setContext(tlp::TulipLibDir + "tlp/bitmaps/font.ttf", 12, 255, 255, 255); // valeur par défault pour l'affichage
-    sizeResult->setAllNodeValue(Size(18,18,1));
-    StringProperty *entryLabel = graph->getProperty<StringProperty>("viewLabel");
+    sizeResult->setAllNodeValue(Size(18, 18, 1));
     node n;
     forEach(n, graph->getNodes()) {
-      const string &str = entryLabel->getNodeValue(n);
+      textRender.setContext(fonts->getNodeValue(n), fontSizes->getNodeValue(n), 255, 255, 255);
+      const string &str = labels->getNodeValue(n);
       if (str != "") {
-	float w_max, h, w;
-	w_max = 256.0;
-	textRender.setString(str, VERBATIM);
-	textRender.getBoundingBox(w_max, h, w);
-	sizeResult->setNodeValue(n, Size(int(w), int(h), 1));
+        float w_max, h, w;
+        w_max = 256.0;
+        textRender.setString(str, VERBATIM);
+        textRender.getBoundingBox(w_max, h, w);
+        sizeResult->setNodeValue(n, Size(int(w), int(h), 1));
       }
     }
-    sizeResult->setAllEdgeValue(Size(1,1,8));
+    sizeResult->setAllEdgeValue(Size(1, 1, 8));
+
+    Observable::unholdObservers();
     return true;
   }
   //====================================================
 };
 /*@}*/
-SIZEPLUGIN(FitToLabel,"Fit to label","Carceles,Niotout,Bardet,Mercadal,Ng Sing Kwong","25/01/2006","","1.0");
+SIZEPLUGIN(FitToLabel,"Fit to label","Tulip Team","25/01/2006","","1.0")
+;
