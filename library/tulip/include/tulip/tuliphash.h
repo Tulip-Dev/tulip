e65826444ec0d7423fa1a1252979801c24de9f18
Patrick Mary <mary@labri.fr>
2010-06-03 20:11:10 +0000

    compilation fix for GCC < 4.3
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2649 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/tuliphash.h
+++ library/tulip/include/tulip/tuliphash.h
@@ -1,7 +1,8 @@
 #ifndef TLP_HASH_H
 #define TLP_HASH_H
-#include "tulip/tulipconf.h"
+
 #if  (__GNUC__ < 4 || __GNUC_MINOR__ < 1)
+#  include <tulip/tulipconf.h>
 #  if (__GNUC__ < 3)
 #    include <hash_map>
 #    include <hash_set>
@@ -25,9 +26,9 @@ namespace stdext {
   };
   template<>
     struct hash<double> {
-    size_t operator()(const double s) const { return (size_t) s; }
+      size_t operator()(const double s) const { return (size_t) s; }
+    };
 }
-  };
 #else
 #  include <tr1/unordered_map>
 #  include <tr1/unordered_set>
