73f8c95919960544927cdc44e3ec395669b25eda
Jonathan Dubois <jonathan.dubois@labri.fr>
2010-06-03 15:54:09 +0000

    Add new properties widget.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2644 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/PropertyDialog.h
+++ library/tulip-qt/include/tulip/PropertyDialog.h
@@ -11,51 +11,52 @@
 
 #include "PropertyDialogData.h"
 
-class QListWidgetItem;
 class QStringList;
 class QScrollBar;
 
 namespace tlp {
-  class GlMainWidget;
-  class PropertyInterface;
+class GlMainWidget;
+class PropertyInterface;
 
-  class TLP_QT_SCOPE PropertyDialog : public QWidget, public Ui::PropertyDialogData {
-    Q_OBJECT;
+class TLP_QT_SCOPE PropertyDialog: public QWidget, public Ui::PropertyDialogData {
+Q_OBJECT
+  ;
 
-  public:
-    PropertyDialog(QWidget *parent = 0);
-    ~PropertyDialog();
-    void setGraph(Graph *);
+public:
+  PropertyDialog(QWidget *parent = 0);
+  ~PropertyDialog();
+  void setGraph(Graph *);
 
-  private:
-    Graph *graph;
-    //GlMainWidget *glWidget;
-    PropertyInterface *editedProperty;
-    std::string editedPropertyName;
-    bool _filterSelection;
-    void updateNbElements();
+private:
+  Graph *graph;
+  //GlMainWidget *glWidget;
+  PropertyInterface *editedProperty;
+  std::string editedPropertyName;
+  bool _filterSelection;
+  void updateNbElements();
 
-  public slots:
-    void populatePropertiesList();
-    ///Manages observers change notification.
-    void update();
-    void selectNode(node);
-    void selectEdge(edge);
-    void changePropertyName(QListWidgetItem *, QListWidgetItem *);
-    void newProperty();
-    void removeProperty();
-    void cloneProperty();
-    void toStringProperty();
-    void filterSelection(bool);
-    void setAllValue();
-    void importCSVData();
-
-  signals:
-    void tulipAllNodePropertyChanged(Graph *, const QString &property, const QString &value);
-    void tulipAllEdgePropertyChanged(Graph *, const QString &property, const QString &value);
-    void newPropertySignal(Graph *,const std::string &);
-    void removePropertySignal(Graph *,const std::string &);
-  };
+public slots:
+  ///Manages observers change notification.
+  void update();
+  void selectNode(node);
+  void selectEdge(edge);
+  void newProperty();
+  void removeProperty();
+  void cloneProperty();
+  void toStringProperty();
+  void filterSelection(bool);
+  void setAllValue();
+  void importCSVData();
+private slots:
+  void propertySelectionChanged();
+  void propertyTypeFilterChanged();
+  void nameFilterChanged(const QString& text);
+signals:
+  void tulipAllNodePropertyChanged(Graph *, const QString &property, const QString &value);
+  void tulipAllEdgePropertyChanged(Graph *, const QString &property, const QString &value);
+  void newPropertySignal(Graph *, const std::string &);
+  void removePropertySignal(Graph *, const std::string &);
+};
 
 }
 #endif // PROPERTYDIALOG_H
