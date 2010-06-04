e349a0ca2d096205f8ac1d0ed673ba61211f1221
Patrick Mary <mary@labri.fr>
2010-06-04 10:50:16 +0000

    fix for Qt > 4.5 + define QT_MINOR_REL in cmake moc options
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2659 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/GlMainWidgetItem.h
+++ library/tulip-qt/include/tulip/GlMainWidgetItem.h
@@ -29,6 +29,9 @@ namespace tlp {
 class TLP_QT_SCOPE GlMainWidgetItem : public QObject, public QGraphicsItem {
 
   Q_OBJECT
+#if QT_MINOR_REL > 5
+  Q_INTERFACES(QGraphicsItem)
+#endif
 
 public :
 
