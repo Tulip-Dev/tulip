90a0fb20f111fa431e80caf9177d37450f82dd41
Jonathan Dubois <jonathan.dubois@labri.fr>
2010-06-11 12:48:02 +0000

    Fix bounding box computation for GlRect. Remove unused member position and color variables.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2719 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlRect.cpp
+++ library/tulip-ogl/src/GlRect.cpp
@@ -4,31 +4,32 @@ using namespace std;
 
 namespace tlp {
 
-  GlRect::GlRect(const Coord &topLeftPos, const Coord &bottomRightPos, const Color &topLeftCol, const Color &bottomRightCol, bool filled, bool outlined):
-    GlPolygon(4u,4u, 4u, filled, outlined) {
+  GlRect::GlRect(const Coord &topLeftPos, const Coord &bottomRightPos, const Color &topLeftCol, const Color &bottomRightCol, bool filled,
+      bool outlined) :
+    GlPolygon(4u, 4u, 4u, filled, outlined) {
     point(0) = topLeftPos;
     point(1) = topLeftPos;
     point(1)[0] = bottomRightPos[0];
-    point(2) = bottomRightPos ;
+    point(2) = bottomRightPos;
     point(3) = topLeftPos;
     point(3)[1] = bottomRightPos[1];
 
-    boundingBox.expand(topLeftPos);
-    boundingBox.expand(bottomRightPos);
+    computeBoundingBox();
 
-    setFillColor(0,topLeftCol);
-    setFillColor(1,topLeftCol);
-    setFillColor(2,bottomRightCol);
-    setFillColor(3,bottomRightCol);
+    setFillColor(0, topLeftCol);
+    setFillColor(1, topLeftCol);
+    setFillColor(2, bottomRightCol);
+    setFillColor(3, bottomRightCol);
 
   }
-  GlRect::GlRect(bool filled, bool outlined):GlPolygon(4u,4u,4u,filled,outlined) {
+  GlRect::GlRect(bool filled, bool outlined) :
+    GlPolygon(4u, 4u, 4u, filled, outlined) {
   }
   //=====================================================
-  GlRect::~GlRect()  {
+  GlRect::~GlRect() {
   }
   //=====================================================
-  Coord GlRect::getCenter()  {
+  Coord GlRect::getCenter() {
     return (point(0) + point(2)) / 2.f;
   }
   //=====================================================
@@ -52,9 +53,7 @@ namespace tlp {
     point(0) = _topLeftPos;
     point(1)[1] = _topLeftPos[1];
     point(3)[0] = _topLeftPos[0];
-    boundingBox = BoundingBox();
-    boundingBox.expand(point(0));
-    boundingBox.expand(point(2));
+    computeBoundingBox();
 
     clearGenerated();
   }
@@ -63,9 +62,7 @@ namespace tlp {
     point(2) = _bottomRightPos;
     point(1)[0] = _bottomRightPos[0];
     point(3)[1] = _bottomRightPos[1];
-    boundingBox = BoundingBox();
-    boundingBox.expand(point(0));
-    boundingBox.expand(point(2));
+    computeBoundingBox();
 
     clearGenerated();
   }
@@ -89,45 +86,48 @@ namespace tlp {
     double maxX = std::max(point(0).getX(), point(2).getX());
     double minY = std::min(point(0).getY(), point(2).getY());
     double maxY = std::max(point(0).getY(), point(2).getY());
-    return ((x >= minX) &&
-	    (x <= maxX) &&
-	    (y >= minY) &&
-	    (y <= maxY));
+    return ((x >= minX) && (x <= maxX) && (y >= minY) && (y <= maxY));
   }
   //=====================================================
-  void GlRect::draw(float lod,Camera *camera) {
-    GlPolygon::draw(lod,camera);
+  void GlRect::draw(float lod, Camera *camera) {
+    GlPolygon::draw(lod, camera);
   }
   //===========================================================
   void GlRect::getXML(xmlNodePtr rootNode) {
-    xmlNodePtr dataNode=NULL;
+    xmlNodePtr dataNode = NULL;
 
     GlXMLTools::createProperty(rootNode, "type", "GlRect");
 
     GlPolygon::getXMLOnlyData(rootNode);
 
-    GlXMLTools::getDataNode(rootNode,dataNode);
+    GlXMLTools::getDataNode(rootNode, dataNode);
 
     /*GlXMLTools::getXML(dataNode,"topLeftPos",topLeftPos);
-    GlXMLTools::getXML(dataNode,"bottomRightPos",bottomRightPos);
-    GlXMLTools::getXML(dataNode,"topLeftCol",topLeftCol);
-    GlXMLTools::getXML(dataNode,"bottomRightCol",bottomRightCol);*/
+     GlXMLTools::getXML(dataNode,"bottomRightPos",bottomRightPos);
+     GlXMLTools::getXML(dataNode,"topLeftCol",topLeftCol);
+     GlXMLTools::getXML(dataNode,"bottomRightCol",bottomRightCol);*/
 
   }
   //============================================================
   void GlRect::setWithXML(xmlNodePtr rootNode) {
-    xmlNodePtr dataNode=NULL;
+    xmlNodePtr dataNode = NULL;
 
     GlPolygon::setWithXML(rootNode);
 
-    GlXMLTools::getDataNode(rootNode,dataNode);
+    GlXMLTools::getDataNode(rootNode, dataNode);
 
     // Parse Data
     /*if(dataNode) {
-      GlXMLTools::setWithXML(dataNode,"topLeftPos",topLeftPos);
-      GlXMLTools::setWithXML(dataNode,"bottomRightPos",bottomRightPos);
-      GlXMLTools::setWithXML(dataNode,"topLeftCol",topLeftCol);
-      GlXMLTools::setWithXML(dataNode,"bottomRightCol",bottomRightCol);
-    }*/
+     GlXMLTools::setWithXML(dataNode,"topLeftPos",topLeftPos);
+     GlXMLTools::setWithXML(dataNode,"bottomRightPos",bottomRightPos);
+     GlXMLTools::setWithXML(dataNode,"topLeftCol",topLeftCol);
+     GlXMLTools::setWithXML(dataNode,"bottomRightCol",bottomRightCol);
+     }*/
+  }
+  void GlRect::computeBoundingBox() {
+    boundingBox = BoundingBox();
+    boundingBox.expand(point(0));
+    boundingBox.expand(point(2));
   }
 }
+
