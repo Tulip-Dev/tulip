7ce3282efe003e2ffa714ae0f730989a466f75c7
Patrick Mary <mary@labri.fr>
2010-06-15 12:17:47 +0000

    move Embedder plugin into tulip_plugins svn
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2737 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/layout/EmbedderDijkstra.h
+++
@@ -1,9 +0,0 @@
-#ifndef EMBEDDERDIJKSTRA_H
-#define EMBEDDERDIJKSTRA_H
-
-#include "EmbedderDefs.h"
-
-void dijkstra(int vertex, vtx_data * graph, int n, DistType * dist);
-void bfs(int vertex, vtx_data * graph, int n, DistType * dist) ;
-
-#endif
