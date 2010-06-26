f39c73d38a93014994e72f42002059d86c416d48
Patrick Mary <mary@labri.fr>
2010-06-26 15:23:40 +0000

    add LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest in common header
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@2834 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tlprender/src/PNGWriter.cpp
+++ software/tlprender/src/PNGWriter.cpp
@@ -3,6 +3,7 @@
  * This file is part of Tulip (www.tulip-software.org)
  *
  * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
  *
  * Tulip is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License
