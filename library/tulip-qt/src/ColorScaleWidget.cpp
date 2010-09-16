f5f73534c8ad7a5c13b3aaef62b5488716406e0a
Patrick Mary <mary@labri.fr>
2010-09-16 15:15:38 +0000

    tulip/ColorScaleWidget.h instead of ColorScaleWidget.h
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3030 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/ColorScaleWidget.cpp
+++ library/tulip-qt/src/ColorScaleWidget.cpp
@@ -10,7 +10,7 @@
  *  the Free Software Foundation; either version 2 of the License, or     
  *  (at your option) any later version.
  */
-#include "ColorScaleWidget.h"
+#include "tulip/ColorScaleWidget.h"
 #include <tulip/ColorScale.h>
 #include <QtGui/QPaintEvent>
 using namespace tlp;
