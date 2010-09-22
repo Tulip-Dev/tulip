3da094e1f186fd0bb1ae20365641d1337eb4bfe3
Patrick Mary <mary@labri.fr>
2010-09-22 08:37:13 +0000

    fix for sf bug #3072450: add lib/tlp/interactors in LD_LIBRARY_PATH and DYLD_LIBRARY_PATH
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3083 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/tulip.sh
+++ software/tulip/src/tulip.sh
@@ -4,10 +4,10 @@ THISDIR=$(dirname $0)
 
 case "`uname -s`" in
     *darwin* | *Darwin* | *DARWIN*)
-	export DYLD_LIBRARY_PATH=${THISDIR}/../Frameworks:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/view:${DYLD_LIBRARY_PATH}
+	export DYLD_LIBRARY_PATH=${THISDIR}/../Frameworks:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/interactors:${THISDIR}/../lib/tlp/view:${DYLD_LIBRARY_PATH}
 	;;
     *Linux*|*GNU/kFreeBSD*)
-	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/view:${LD_LIBRARY_PATH}
+	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/interactors:${THISDIR}/../lib/tlp/view:${LD_LIBRARY_PATH}
 	;;	
 esac
 
