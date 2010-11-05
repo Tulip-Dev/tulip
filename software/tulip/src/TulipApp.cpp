5e151e116f40a8ce6925f3536a86a4f1458ca5ec
Patrick Mary <mary@labri.fr>
2010-11-05 07:07:25 +0000

    fix for sf bug #3103005: disable export menu when there is no current graph
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3217 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/TulipApp.cpp
+++ software/tulip/src/TulipApp.cpp
@@ -149,6 +149,7 @@ void TulipApp::enableElements(bool enabled) {
   fileSaveAsAction->setEnabled(enabled);
   filePrintAction->setEnabled(enabled);
   mouseActionGroup->setEnabled(enabled);
+  exportGraphMenu.menuAction()->setEnabled(enabled);
 }
 //**********************************************************************
 ///Destructor of viewGl
@@ -522,6 +523,8 @@ bool TulipApp::doFileSave(Controller *controllerToSave,string plugin, string fil
   DataSet controllerData;
   Graph *graph;
   controllerToSave->getData(&graph,&controllerData);
+  if (graph == NULL)
+    return false;
   controller.set<DataSet>(controllerToControllerName[controllerToSave],controllerData);
   dataSet.set<DataSet>("controller",controller);
 
