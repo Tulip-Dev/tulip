3713dd1f3a7cdc0b2e7960241219e1df4f1189fe
Antoine Lambert <antoine.lambert33@gmail.com>
2010-11-10 09:32:24 +0000

    - update back buffer copy behaviour : render in back buffer first then copy its content - if Qt >= 4.6 and extension GL_EXT_framebuffer_blit is present, back buffer content is saved in a framebuffer object (via QGLFramebufferObject::blitFramebuffer) - otherwise, use a simple copy to memory of the back buffer content - this way, polygon anti aliasing is preserved (really appreciable for labels rendered in 3d)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3256 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/GlMainWidget.h
+++ library/tulip-qt/include/tulip/GlMainWidget.h
@@ -25,7 +25,7 @@
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation; either version 2 of the License, or
  (at your option) any later version.
-*/
+ */
 
 #ifndef Tulip_GLMAINWIDGET_H
 #define Tulip_GLMAINWIDGET_H
@@ -56,211 +56,212 @@ class GlCompositeHierarchyManager;
 /** \addtogroup Tulip_Widgets */
 /*@{*/
 class TLP_QT_SIMPLE_SCOPE GlMainWidget : public QGLWidget {
-  Q_OBJECT;
+	Q_OBJECT;
 
- public:
-   /**
-    * Construct the GlMainWidget
-    */
-  GlMainWidget(QWidget *parent,AbstractView *view=NULL);
-  ~GlMainWidget();
+public:
+	/**
+	 * Construct the GlMainWidget
+	 */
+	GlMainWidget(QWidget *parent,AbstractView *view=NULL);
+	~GlMainWidget();
 
-  /**
-   * Set graph and data of GlMainWidget
-   */
-  void setData(Graph *graph,DataSet dataSet);
-  /**
-   * Get data of GlMainWidget
-   */
-  DataSet getData();
-  /**
-   * Set the graph
-   */
-  void setGraph(Graph *graph);
-  /**
-   * GetGraph of GlMainWidget
-   */
-  Graph *getGraph();
+	/**
+	 * Set graph and data of GlMainWidget
+	 */
+	void setData(Graph *graph,DataSet dataSet);
+	/**
+	 * Get data of GlMainWidget
+	 */
+	DataSet getData();
+	/**
+	 * Set the graph
+	 */
+	void setGraph(Graph *graph);
+	/**
+	 * GetGraph of GlMainWidget
+	 */
+	Graph *getGraph();
 
-  /**
-   * Get the view who use this GlMainWidget
-   */
-  View *getView() {return view;}
+	/**
+	 * Get the view who use this GlMainWidget
+	 */
+	View *getView() {return view;}
 
-  /**
-   * Get the scene of this glMainWidget
-   */
-  tlp::GlScene* getScene(){return &scene;}
+	/**
+	 * Get the scene of this glMainWidget
+	 */
+	tlp::GlScene* getScene(){return &scene;}
 
-  /**
-   * add a foregroundEntity to this GlMainWidget
-   */
-  void addForegroundEntity(ForegroundEntity *entity) {foregroundEntity.push_back(entity);}
+	/**
+	 * add a foregroundEntity to this GlMainWidget
+	 */
+	void addForegroundEntity(ForegroundEntity *entity) {foregroundEntity.push_back(entity);}
 
-  /**************************************
-   * inherited methods overloading
-   **************************************/
-  /** \brief select nodes and edges in a region of the screen
-   *
-   *  select all nodes and edges lying in the area of the screen of given width and height,
-   *  and with its upper-left corner at (x,y)
-   *  \param sNode filled by the method with the nodes found in the region
-   *  \param sEdge filled by the method with the edges found in the region
-   */
-  void doSelect(const int x, const int y,
-		const int width, const int height,
-		std::vector<tlp::node> &sNode, std::vector<tlp::edge> &sEdge,
-		tlp::GlLayer* layer=NULL);
-  /** \brief select a node or edge at a point
-   *  select either a node or edge at point (x,y)
-   *  \param type tells what has been found: NODE, EDGE
-   *  \return true if something has been found, false otherwise
-   */
-  bool doSelect(const int x, const int y,
-		tlp::ElementType &type,
-		tlp::node &,tlp::edge &,
-		tlp::GlLayer* layer=NULL);
+	/**************************************
+	 * inherited methods overloading
+	 **************************************/
+	/** \brief select nodes and edges in a region of the screen
+	 *
+	 *  select all nodes and edges lying in the area of the screen of given width and height,
+	 *  and with its upper-left corner at (x,y)
+	 *  \param sNode filled by the method with the nodes found in the region
+	 *  \param sEdge filled by the method with the edges found in the region
+	 */
+	void doSelect(const int x, const int y,
+			const int width, const int height,
+			std::vector<tlp::node> &sNode, std::vector<tlp::edge> &sEdge,
+			tlp::GlLayer* layer=NULL);
+	/** \brief select a node or edge at a point
+	 *  select either a node or edge at point (x,y)
+	 *  \param type tells what has been found: NODE, EDGE
+	 *  \return true if something has been found, false otherwise
+	 */
+	bool doSelect(const int x, const int y,
+			tlp::ElementType &type,
+			tlp::node &,tlp::edge &,
+			tlp::GlLayer* layer=NULL);
 
-  /**
-   *  Take a snapshot of the Widget
-   *  \return an array of dimension width*height*3 char (8bits per color RGB).
-   *   The pointer has to be freed after (with free, not delete)
-   **/
-  unsigned char* getImage();
-  // EPS output of a GlGraph
-  bool outputEPS(int size, int doSort, const char *filename);
-  // SVG output of a GlGraph
-  bool outputSVG(int size, const char* filename);
+	/**
+	 *  Take a snapshot of the Widget
+	 *  \return an array of dimension width*height*3 char (8bits per color RGB).
+	 *   The pointer has to be freed after (with free, not delete)
+	 **/
+	unsigned char* getImage();
+	// EPS output of a GlGraph
+	bool outputEPS(int size, int doSort, const char *filename);
+	// SVG output of a GlGraph
+	bool outputSVG(int size, const char* filename);
 
-  /**
-   * Compute real texture size with given height and width
-   */
-  static void getTextureRealSize(int width, int height, int &textureRealWidth, int &textureRealHeight);
+	/**
+	 * Compute real texture size with given height and width
+	 */
+	static void getTextureRealSize(int width, int height, int &textureRealWidth, int &textureRealHeight);
 
-  /**
-   *
-   */
-  void getTextureShift(int width, int height,float &xTextureShift, float &yTextureShift);
+	/**
+	 *
+	 */
+	void getTextureShift(int width, int height,float &xTextureShift, float &yTextureShift);
 
-  /**
-   * Take a snapshot of the Widget and put it in an OpenGl texture
-   */
-  QGLFramebufferObject *createTexture(const std::string &textureName,int width, int height);
-  /**
-   * Take a snapshot of the Widget and put it in a picture
-   */
-  void createPicture(const std::string &pictureName,int width, int height,bool center=true);
+	/**
+	 * Take a snapshot of the Widget and put it in an OpenGl texture
+	 */
+	QGLFramebufferObject *createTexture(const std::string &textureName,int width, int height);
+	/**
+	 * Take a snapshot of the Widget and put it in a picture
+	 */
+	void createPicture(const std::string &pictureName,int width, int height,bool center=true);
 
-  /**
-   * Function to do picking on entities.  It just calls
-   * selectEntities on the GlScene instance.
-   */
-  bool selectGlEntities(const int x, const int y,
+	/**
+	 * Function to do picking on entities.  It just calls
+	 * selectEntities on the GlScene instance.
+	 */
+	bool selectGlEntities(const int x, const int y,
 			const int width, const int height,
 			std::vector<tlp::GlEntity *>
-			&pickedEntities,
-			tlp::GlLayer* layer=NULL);
-  /**
-   * Function to do picking on entities.  It just calls
-   * selectEntities on the GlScene instance with a small window of
-   * twelve pixels.
-   */
-  bool selectGlEntities(const int x, const int y,
+	&pickedEntities,
+	tlp::GlLayer* layer=NULL);
+	/**
+	 * Function to do picking on entities.  It just calls
+	 * selectEntities on the GlScene instance with a small window of
+	 * twelve pixels.
+	 */
+	bool selectGlEntities(const int x, const int y,
 			std::vector<tlp::GlEntity *>
-			&pickedEntities,
-			tlp::GlLayer* layer=NULL);
+	&pickedEntities,
+	tlp::GlLayer* layer=NULL);
 
-  /**
-   * Grab the image of this widget
-   */
-  virtual QImage grabFrameBuffer(bool withAlpha = false);
+	/**
+	 * Grab the image of this widget
+	 */
+	virtual QImage grabFrameBuffer(bool withAlpha = false);
 
-  /**
-   * set if the label is rendered in this widget
-   */
-  void setViewLabel(bool viewLabel) {scene.setViewLabel(viewLabel);}
+	/**
+	 * set if the label is rendered in this widget
+	 */
+	void setViewLabel(bool viewLabel) {scene.setViewLabel(viewLabel);}
 
-  /**
-   * Extend makeCurrent function of QGLWidget to inform TextureManager and DisplayListManager of context changement
-   */
-  virtual void makeCurrent();
+	/**
+	 * Extend makeCurrent function of QGLWidget to inform TextureManager and DisplayListManager of context changement
+	 */
+	virtual void makeCurrent();
 
-  /**
-   * Resize openGL view
-   */
-  void  resizeGL(int w, int h);
+	/**
+	 * Resize openGL view
+	 */
+	void  resizeGL(int w, int h);
 
-  /**
-   * Compute interactors before drawing
-   */
-  void  computeInteractors();
+	/**
+	 * Compute interactors before drawing
+	 */
+	void  computeInteractors();
 
-  /**
-   * Draw interactors
-   */
-  void  drawInteractors();
+	/**
+	 * Draw interactors
+	 */
+	void  drawInteractors();
 
-  /**
-   * Draw foreground entities
-   */
-  void drawForegroundEntities();
+	/**
+	 * Draw foreground entities
+	 */
+	void drawForegroundEntities();
 
 	void useHulls(bool hasHulls);
 	bool hasHulls() const;
-	
+
 private:
 
-  void  updateGL();
-  void  glInit();
-  void  glDraw();
-  void  paintGL();
-  void  setupOpenGlContext();
-  void  initializeGL();
-  bool createRenderingStore(int width, int height);
+	void  updateGL();
+	void  glInit();
+	void  glDraw();
+	void  paintGL();
+	void  setupOpenGlContext();
+	void  initializeGL();
+	void createRenderingStore(int width, int height);
 
-  tlp::GlScene scene;
-  QRegion _visibleArea;
-  AbstractView *view;
-  int widthStored;
-  int heightStored;
-  char *renderingStore;
-  bool frameBufferStored;
-  std::vector<ForegroundEntity *> foregroundEntity;
+	tlp::GlScene scene;
+	QRegion _visibleArea;
+	AbstractView *view;
+	int widthStored;
+	int heightStored;
+	char *renderingStore;
+	bool frameBufferStored;
+	std::vector<ForegroundEntity *> foregroundEntity;
 	GlCompositeHierarchyManager* manager;
 	bool _hasHulls;
-  QGLFramebufferObject *glFrameBuf;
-	
-public slots:
-  /**
-   * Draw the graph, the augmented dispaly and the interactors
-   */
-  void draw(bool graphChanged=true);
-  /**
-   * That function is given for optimisation purpose. If the hardware enable it,
-   * it enables to redraw only the Augmented display and the interactors and not the graph
-   * it is really usefull for interactors such as zoom box etc..
-   * Warning, if you change the graph or the porperties of element (Colors, size, etc...)
-   * applying that fonction will not display the change, in that case, use the draw function.
-   */
-  void redraw();
+	bool useFramebufferObject;
+	QGLFramebufferObject *glFrameBuf;
 
-  void closeEvent(QCloseEvent *e);
+public slots:
+/**
+ * Draw the graph, the augmented dispaly and the interactors
+ */
+void draw(bool graphChanged=true);
+/**
+ * That function is given for optimisation purpose. If the hardware enable it,
+ * it enables to redraw only the Augmented display and the interactors and not the graph
+ * it is really usefull for interactors such as zoom box etc..
+ * Warning, if you change the graph or the porperties of element (Colors, size, etc...)
+ * applying that fonction will not display the change, in that case, use the draw function.
+ */
+void redraw();
+
+void closeEvent(QCloseEvent *e);
 
 protected slots:
-  void paintEvent( QPaintEvent* );
-  void viewDrawnSlot(GlMainWidget *glWidget,bool graphChanged) {emit graphRedrawn(glWidget,graphChanged);}
+void paintEvent( QPaintEvent* );
+void viewDrawnSlot(GlMainWidget *glWidget,bool graphChanged) {emit graphRedrawn(glWidget,graphChanged);}
 
 signals:
-  void closing(GlMainWidget *, QCloseEvent *);
-  void graphRedrawn(GlMainWidget *glWidget,bool graphChanged);
-  void viewRedrawn(GlMainWidget *glWidget);
-  void viewDrawn(GlMainWidget *glWidget,bool graphChanged);
+void closing(GlMainWidget *, QCloseEvent *);
+void graphRedrawn(GlMainWidget *glWidget,bool graphChanged);
+void viewRedrawn(GlMainWidget *glWidget);
+void viewDrawn(GlMainWidget *glWidget,bool graphChanged);
 
 public :
-	static QGLWidget* getFirstQGLWidget();
+static QGLWidget* getFirstQGLWidget();
 
 private :
-  static QGLWidget *firstQGLWidget;
+static QGLWidget *firstQGLWidget;
 
 };
 
