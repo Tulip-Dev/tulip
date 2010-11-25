86d88fed1fda09859252550d227129a97d6b4df8
Patrick Mary <mary@labri.fr>
2010-11-25 10:01:23 +0000

    final fix for sf sf bug #3114471: output of a string must add \ before \ or " chars
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3346 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/src/Types.cpp
+++ library/tulip/src/Types.cpp
@@ -614,7 +614,14 @@ string StringType::defaultValue() {
 }
 
 void StringType::write(ostream& os, const RealType & v ) {
-  os << '"' << v.c_str() << '"';
+  os << '"';
+  for(char* str = (char *) v.c_str(); *str; ++str) {
+    char c = *str;
+    if (c == '\\' || c == '"')
+      os << '\\';
+    os << c;
+  }
+  os << '"';
 }
 
 string StringType::toString( const RealType & v ) {
