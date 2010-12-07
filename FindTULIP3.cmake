d19f93ac3cfa706335349c52ed81211197f50743
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2010-12-07 15:02:54 +0000

    Fixed FindTULIP3 in many ways. System path for libraries are not defined anymore, this script will also try to use the same tulip directory to find all lib files.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3425 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- FindTULIP3.cmake
+++ FindTULIP3.cmake
@@ -1,111 +1,110 @@
 #===========================================================================
 #
-#===========================================================================
-#
-#    Description: Find the Tulip libraries and include dir
+#    Description: Find directories used to build projects using Tulip framework
 #    Filename: FindTULIP.cmake
-#    Authors: Damien Genet
+#    Authors: Tulip team
 #
 #===========================================================================
+# Following variables should be added to the CMake build variables:
 #
-#    Description:
+# TULIP_DIR: Root directory for Tulip installation
+# TULIP_INCLUDE_DIR: Directory containing include files for Tulip
+# TULIP_PLUGINS_PATH: installation path for plugins
+# TULIP_LIBRARY: Absolute path of libtulip.
+# TULIP_QT4_LIBRARY: Absolute path of libtulip-qt
+# TULIP_OGL_LIBRARY: Absolute path of libtulip-ogl
+# TULIP_PLUGINS_MANAGER_LIBRARY: Absolute path of libtulip-pluginsmanager
+# TULIP_LIBS: Convenience variable containing all the previously mentioned Tulip libraries
+# TULIP_VERSION: Tulip release version number: A.B
+# TULIP_PLUGIN_VERSION: Tulip plugins release version number: A.B.x
 #
-#===========================================================================
-#
-# TULIP_INCLUDE_DIR  - Directories to include to use tulip
-# TULIP_LIBRARY      - Files to link against to use tulip
-# TULIP_FOUND        - When false, don't try to use tulip
-# TULIP_DIR          - (optional) Suggested installation directory to search
-# TULIP_VERSION		 - The version of Tulip
-# TULIP_PLUGIN_VERSION- The version of Tulip
-#
-# TULIP_DIR can be used to make it simpler to find the various include
-# directories and compiled libraries when not installed in the
-# usual/well-known directories (e.g. because you made an in tree-source
-# compilation or because you installed it in an "unusual" directory).
-# Just set TULIP_DIR it to your specific installation directory
-#
-FIND_PATH(TULIP_INCLUDE_DIR tulip/TulipRelease.h
-  PATHS ${TULIP_DIR}/include
-        $ENV{TULIP_DIR}/include
-	/usr/include
-	/usr/local/include
-	/opt/local/include
-	)
+# User can specify any of this directory to force using a specific installation.
+# If TULIP_DIR is given, it will be used as the root path to find all other libraries. CMake WILL NOT look in other directories if TULIP_DIR is given
+# Keep in mind that FindTulip will try to use libraries and include files coming from the same Tulip blundle, it will not fail if some of the libraries are not found (since you may want to only link against libtulip eg)
+# FindTulip will only set previously unset variables. User can specify any variable mentioned before manually.
 
-IF(WIN32)
-SET(TULIP_PATHS
-  ${TULIP_DIR}/bin
-  $ENV{TULIP_DIR}/bin
-  )
-ELSE(WIN32)
-SET(TULIP_PATHS
-  ${TULIP_DIR}/lib
-  $ENV{TULIP_DIR}/lib
-  /usr/lib
-  /usr/local/lib
-  /opt/local/lib
-  )
-ENDIF(WIN32)
+# Include and root directories
+IF(TULIP_DIR AND NOT TULIP_INCLUDE_DIR)
+  FIND_PATH(TULIP_INCLUDE_DIR tulip/TulipRelease.h
+            PATHS ${TULIP_DIR}/include
+            NO_DEFAULT_PATH)
+ELSEIF(NOT TULIP_DIR AND NOT TULIP_INCLUDE_DIR)
+  FIND_PATH(TULIP_INCLUDE_DIR tulip/TulipRelease.h)
+ENDIF()
 
-
+# Tulip version
 IF(NOT TULIP_VERSION AND TULIP_INCLUDE_DIR)
-file(STRINGS ${TULIP_INCLUDE_DIR}/tulip/TulipRelease.h TEMPVAR REGEX "[0-9]*\\.[0-9]*" NO_HEX_CONVERSION)
-string( REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*" TULIP_VERSION ${TEMPVAR} )
-ENDIF(NOT TULIP_VERSION AND TULIP_INCLUDE_DIR)
+FILE(STRINGS ${TULIP_INCLUDE_DIR}/tulip/TulipRelease.h
+     TEMPVAR
+     REGEX "[0-9]*\\.[0-9]*"
+     NO_HEX_CONVERSION)
 
+STRING(REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*"
+       TULIP_VERSION
+       ${TEMPVAR})
+ENDIF()
+
+# Plugin version
 IF(NOT TULIP_PLUGIN_VERSION AND TULIP_VERSION)
-string( REGEX MATCH "[0-9]*\\.[0-9]*" TULIP_PLUGIN_VERSION ${TULIP_VERSION} )
-ENDIF(NOT TULIP_PLUGIN_VERSION AND TULIP_VERSION)
+STRING(REGEX MATCH "[0-9]*\\.[0-9]*"
+       TULIP_PLUGIN_VERSION
+       ${TULIP_VERSION})
+ENDIF()
 
-IF(NOT TULIP_PLUGINS_PATH AND TULIP_INCLUDE_DIR)
-set(TULIP_PLUGINS_PATH ${TULIP_INCLUDE_DIR}/../lib/tlp)
-ENDIF(NOT TULIP_PLUGINS_PATH AND TULIP_INCLUDE_DIR)
+# No effect if TULIP_DIR was already given
+IF(TULIP_INCLUDE_DIR)
+  SET(TULIP_DIR ${TULIP_INCLUDE_DIR}/..)
+ENDIF()
 
-FIND_LIBRARY(TULIP_LIBRARY "tulip-${TULIP_PLUGIN_VERSION}"
-  PATHS ${TULIP_PATHS}
-  )
+# From now on, TULIP_DIR must be set
+IF(TULIP_DIR)
+  IF(NOT TULIP_LIBRARY)
+    FIND_LIBRARY(TULIP_LIBRARY "tulip-${TULIP_PLUGIN_VERSION}"
+                 PATHS ${TULIP_DIR}/lib
+                 NO_DEFAULT_PATH)
+  ENDIF()
+  IF(NOT TULIP_OGL_LIBRARY)
+    FIND_LIBRARY(TULIP_OGL_LIBRARY "tulip-ogl-${TULIP_PLUGIN_VERSION}"
+                 PATHS ${TULIP_DIR}/lib
+                 NO_DEFAULT_PATH)
+  ENDIF()
+  IF(NOT TULIP_QT4_LIBRARY)
+    FIND_LIBRARY(TULIP_QT4_LIBRARY "tulip-qt4-${TULIP_PLUGIN_VERSION}"
+                 PATHS ${TULIP_DIR}/lib
+                 NO_DEFAULT_PATH)
+  ENDIF()
+  IF(NOT TULIP_PLUGINS_MANAGER_LIBRARY)
+    FIND_LIBRARY(TULIP_PLUGINS_MANAGER_LIBRARY "tulip-pluginsmanager-${TULIP_PLUGIN_VERSION}"
+                 PATHS ${TULIP_DIR}/lib
+                 NO_DEFAULT_PATH)
+  ENDIF()
 
-FIND_LIBRARY(TULIP_QT4_LIBRARY "tulip-qt4-${TULIP_PLUGIN_VERSION}"
-  PATHS ${TULIP_PATHS}
-  )
+  IF(NOT TULIP_PLUGINS_PATH)
+    SET(TULIP_PLUGINS_PATH ${TULIP_DIR}/lib/tlp)
+  ENDIF()
+ENDIF()
 
-FIND_LIBRARY(TULIP_OGL_LIBRARY "tulip-ogl-${TULIP_PLUGIN_VERSION}"
-  PATHS ${TULIP_PATHS}
-  )
+IF(TULIP_INCLUDE_DIR AND TULIP_DIR AND TULIP_LIBRARY AND TULIP_PLUGIN_VERSION AND TULIP_VERSION AND TULIP_PLUGINS_PATH)
+  SET(TULIP_FOUND "YES")
+  MARK_AS_ADVANCED(TULIP_DIR)
+  MARK_AS_ADVANCED(TULIP_INCLUDE_DIR)
+  MARK_AS_ADVANCED(TULIP_LIBRARY)
+  MARK_AS_ADVANCED(TULIP_PLUGIN_VERSION)
+  MARK_AS_ADVANCED(TULIP_VERSION)
+  MARK_AS_ADVANCED(TULIP_PLUGINS_PATH)
+  IF(TULIP_OGL_LIBRARY)
+    MARK_AS_ADVANCED(TULIP_OGL_LIBRARY)
+    IF(TULIP_QT4_LIBRARY)
+      MARK_AS_ADVANCED(TULIP_QT4_LIBRARY)
+      IF(TULIP_PLUGINS_MANAGER_LIBRARY)
+        MARK_AS_ADVANCED(TULIP_PLUGINS_MANAGER_LIBRARY)
+      ENDIF()
+    ENDIF()
+  ENDIF()
+ENDIF()
 
-FIND_LIBRARY(TULIP_PLUGINS_MANAGER_LIBRARY "tulip-pluginsmanager-${TULIP_PLUGIN_VERSION}"
-  PATHS ${TULIP_PATHS}
-  )
-
-SET(TULIP_LIBS ${TULIP_LIBRARY}
-	       ${TULIP_QT4_LIBRARY}
-	       ${TULIP_OGL_LIBRARY}
-	       ${TULIP_PLUGINS_MANAGER_LIBRARY}
-)
-
-IF( TULIP_INCLUDE_DIR )
-  IF( TULIP_LIBRARY )
-    IF( TULIP_QT4_LIBRARY )
-      IF( TULIP_OGL_LIBRARY )
-        IF(TULIP_PLUGINS_MANAGER_LIBRARY)
-  	  SET( TULIP_FOUND "YES" )
-	  MARK_AS_ADVANCED( TULIP_DIR )	
-	  MARK_AS_ADVANCED( TULIP_INCLUDE_DIR )
-	  MARK_AS_ADVANCED( TULIP_LIBRARY )
-	  MARK_AS_ADVANCED( TULIP_OGL_LIBRARY )
-	  MARK_AS_ADVANCED( TULIP_QT4_LIBRARY )	  
-	  MARK_AS_ADVANCED( TULIP_PLUGINS_MANAGER_LIBRARY )
-	  MARK_AS_ADVANCED( TULIP_PLUGIN_VERSION )
-	  MARK_AS_ADVANCED( TULIP_VERSION )
-	  MARK_AS_ADVANCED( TULIP_PLUGINS_PATH )	  	  	  
-		ENDIF( TULIP_PLUGINS_MANAGER_LIBRARY )
-      ENDIF( TULIP_OGL_LIBRARY )
-    ENDIF( TULIP_QT4_LIBRARY )
-  ENDIF( TULIP_LIBRARY )
-ENDIF( TULIP_INCLUDE_DIR )
-
-IF( NOT TULIP_FOUND )
+IF(NOT TULIP_FOUND)
+  SET(TULIP_DIR "" CACHE PATH "Root of tulip install tree.")
   MESSAGE("tulip installation was not found. Please provide TULIP_DIR:")
   MESSAGE("  - through the GUI when working with ccmake, ")
   MESSAGE("  - as a command line argument when working with cmake e.g. ")
@@ -115,9 +114,7 @@ IF( NOT TULIP_FOUND )
   MESSAGE("    Providing TULIP_DIR (as above described) is probably the")
   MESSAGE("    simplest solution unless you have a really customized/odd")
   MESSAGE("    tulip installation...")
-  SET( TULIP_DIR "" CACHE PATH "Root of tulip install tree." )
-ENDIF( NOT TULIP_FOUND )
-
+ENDIF(NOT TULIP_FOUND)
 
 MACRO (TULIP_QT4_WRAP_UI outfiles )
   QT4_EXTRACT_OPTIONS(ui_files ui_options ${ARGN})
@@ -169,7 +166,7 @@ MACRO (TULIP_WRAP_QT_PLUGINS_IN_INCLUDE_TULIP outfiles )
     STRING(TOLOWER ${outfile} outfile_lower)
     SET(outfile_lower ${CMAKE_CURRENT_BINARY_DIR}/../include/tulip/${outfile_lower}.h)
     ADD_CUSTOM_COMMAND(OUTPUT ${outfile_lower}
-	COMMAND ${CMAKE_COMMAND} ARGS -E copy ${infile} ${outfile_lower}      
+    COMMAND ${CMAKE_COMMAND} ARGS -E copy ${infile} ${outfile_lower}
       MAIN_DEPENDENCY ${infile})
     SET(${outfiles} ${${outfiles}} ${outfile_lower})
   ENDFOREACH (it)
@@ -185,4 +182,4 @@ MACRO (TULIP_QT4_WRAP_CPP outfiles )
     QT4_CREATE_MOC_COMMAND(${it} ${outfile} "${moc_flags}" "${moc_options}")
     SET(${outfiles} ${${outfiles}} ${outfile})
   ENDFOREACH(it)
-ENDMACRO (TULIP_QT4_WRAP_CPP)	
+ENDMACRO (TULIP_QT4_WRAP_CPP)
