7eaf5ff3a28656caae0392c6e556eaad99944bdf
David Auber <david.auber@labri.fr>
2010-12-08 14:28:23 +0000

    indentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3439 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/glyph/window.cpp
+++ plugins/glyph/window.cpp
@@ -51,64 +51,64 @@ private:
 GLYPHPLUGIN(Window, "2D - Window", "David Auber", "28/05/2010", "Window with a title bar", "1.0", 17);
 
 Window::Window(GlyphContext* context): 
-	Glyph(context), 
-	_rectColor(205, 205, 205, 255), 
-	_textColor(205, 205, 205, 255), 
-	_titleBarSize(0.1), 
-	_borderWidth(0.02), 
-	_border(texture),
-	_center(texture),
-	_titleRec(gradient)
-	{
-	
-	const float textheight = 0.05;
-	//Coord textCenter(-0.5 + _borderWidth, 0.5 - textheight- _borderWidth , 0.0);
-	//_title = new GlLabel(textCenter, Coord(1. - (2.f * _borderWidth), _titleBarSize, 0.0), _textColor, true);
-	
-	Coord v[10];
-	v[0].set(-0.5, 0.5, 0);
-	v[1].set( 0.5, 0.5, 0);
-	v[2].set( 0.5,-0.5, 0);
-	v[3].set(-0.5,-0.5, 0);
-	v[4].set(-0.5 + _borderWidth, 0.5 - _borderWidth, 0);
-	v[5].set( 0.5 - _borderWidth, 0.5 - _borderWidth, 0);
-	v[6].set( 0.5 - _borderWidth,-0.5 + _borderWidth, 0);
-	v[7].set(-0.5 + _borderWidth,-0.5 + _borderWidth, 0);
-	v[8].set( 0.5 - _borderWidth, 0.5 - _borderWidth - textheight *2.f, 0);
-	v[9].set(-0.5 + _borderWidth, 0.5 - _borderWidth - textheight *2.f, 0);
+  Glyph(context), 
+  _rectColor(205, 205, 205, 255), 
+  _textColor(205, 205, 205, 255), 
+  _titleBarSize(0.1), 
+  _borderWidth(0.02), 
+  _border(texture),
+  _center(texture),
+  _titleRec(gradient)
+{
+  
+  const float textheight = 0.05;
+  //Coord textCenter(-0.5 + _borderWidth, 0.5 - textheight- _borderWidth , 0.0);
+  //_title = new GlLabel(textCenter, Coord(1. - (2.f * _borderWidth), _titleBarSize, 0.0), _textColor, true);
+  
+  Coord v[10];
+  v[0].set(-0.5, 0.5, 0);
+  v[1].set( 0.5, 0.5, 0);
+  v[2].set( 0.5,-0.5, 0);
+  v[3].set(-0.5,-0.5, 0);
+  v[4].set(-0.5 + _borderWidth, 0.5 - _borderWidth, 0);
+  v[5].set( 0.5 - _borderWidth, 0.5 - _borderWidth, 0);
+  v[6].set( 0.5 - _borderWidth,-0.5 + _borderWidth, 0);
+  v[7].set(-0.5 + _borderWidth,-0.5 + _borderWidth, 0);
+  v[8].set( 0.5 - _borderWidth, 0.5 - _borderWidth - textheight *2.f, 0);
+  v[9].set(-0.5 + _borderWidth, 0.5 - _borderWidth - textheight *2.f, 0);
 
-	_bb.expand(v[8]);
-	_bb.expand(v[9]);
-	_bb.expand(v[6]);
-	_bb.expand(v[7]);
+  _bb.expand(v[8]);
+  _bb.expand(v[9]);
+  _bb.expand(v[6]);
+  _bb.expand(v[7]);
 	
-	 _border.addQuadEdge(v[0], v[4], _textColor);
-	 _border.addQuadEdge(v[1], v[5], _textColor);
-	 _border.addQuadEdge(v[2], v[6], _textColor);
-	 _border.addQuadEdge(v[3], v[7], _textColor);
-	 _border.addQuadEdge(v[0], v[4], _textColor);
+  _border.addQuadEdge(v[0], v[4], _textColor);
+  _border.addQuadEdge(v[1], v[5], _textColor);
+  _border.addQuadEdge(v[2], v[6], _textColor);
+  _border.addQuadEdge(v[3], v[7], _textColor);
+  _border.addQuadEdge(v[0], v[4], _textColor);
 	 
-	 _titleRec.addQuadEdge(v[4], v[9], _rectColor);
-	 _titleRec.addQuadEdge(v[5], v[8], _rectColor);
+  _titleRec.addQuadEdge(v[4], v[9], _rectColor);
+  _titleRec.addQuadEdge(v[5], v[8], _rectColor);
 	 
-	_textbb.expand(v[4]);
-	_textbb.expand(v[9]);
-	_textbb.expand(v[5]);
-	_textbb.expand(v[8]);
+  _textbb.expand(v[4]);
+  _textbb.expand(v[9]);
+  _textbb.expand(v[5]);
+  _textbb.expand(v[8]);
 	 
-	 _center.addQuadEdge(v[4], v[7], _rectColor);
-	 _center.addQuadEdge(v[5], v[6], _rectColor);
+  _center.addQuadEdge(v[4], v[7], _rectColor);
+  _center.addQuadEdge(v[5], v[6], _rectColor);
  
-	}
-
+}
+//=====================================================
 void Window::getIncludeBoundingBox(BoundingBox &boundingBox,node) {
   boundingBox = _bb;
 }
-
+//=====================================================
 void Window::getTextBoundingBox(BoundingBox &boundingBox,node) {
   boundingBox = _textbb;
 }
-
+//=====================================================
 void Window::draw(node n, float lod) {
 	ColorProperty* color = glGraphInputData->elementColor;
 	ColorProperty* colorBorder = glGraphInputData->elementBorderColor;
@@ -117,21 +117,13 @@ void Window::draw(node n, float lod) {
 	_border.setColor(colorBorder->getNodeValue(n));	
 	_center.setColor(_rectColor);	
 	_titleRec.setColor(_rectColor);	
-//	_title->setText(prop->getNodeValue(n));
-//        _title->setColor(glGraphInputData->elementLabelColor->getNodeValue(n));
+
 	_center.draw(lod, NULL);
 	 GlTextureManager::getInst().activateTexture(gradient);
 	_titleRec.draw(lod, NULL);
 	GlTextureManager::getInst().activateTexture(texture);
 	_border.draw(lod, NULL);
-/*
- *        if (lod > 20) {
-            _title->draw(lod, NULL);
-	}
-*/
-  
 }
-
 //=====================================================
 Coord Window::getAnchor(const Coord &vector) const {
 	Coord v(vector);
