2c5d8031390542485bfef36ce3c65a8b55986edd
David Auber <david.auber@labri.fr>
2011-01-05 21:35:26 +0000

    add parameters in the constructor to enable some optimization create a documentation, there is no documentation for iterators !!!
    add a specific function to restart the iterations (we can do it here)
    
    
    
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3592 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/StableIterator.h
+++ library/tulip/include/tulip/StableIterator.h
@@ -23,29 +23,73 @@
 #include <vector>
 
 namespace tlp {
-
-///Interface of StableIterator,
-template<class itType> struct StableIterator : public Iterator<itType> {
-  StableIterator(Iterator<itType> *itIn) {
-    for (;itIn->hasNext();) {
-      cloneIt.push_back(itIn->next());
-    }delete itIn;
-    itStl=cloneIt.begin();
-  }
-  ~StableIterator(){};
-  itType next() {
-    itType tmp=*itStl;
-    ++itStl;
-    return tmp;
-  }
-  bool hasNext() {
-    return (itStl!=cloneIt.end());
-  };
-
-  protected :
-  std::vector<itType> cloneIt;
-  typename std::vector<itType>::iterator itStl;
+    /**
+     * \addtogroup iterators
+     */
+    /*@{*/
+/**
+  *\class StableIterator
+  *\brief That class enables to store elements of an iterator and to iterate on a copy.
+  *
+  * That iterator store all elements accessible by the input iterator into an internal data
+  * structure (created at the construction) and then only use that structure for the iteration.
+  * Order of iteration is the same. If one knows the number of elements that will be iterated
+  * one could give give it in paremeters to speed up the duplication process.
+  *
+  * \warning By default StableIterator manage the destruction of the iterator given in parameter, (ie,
+  * delete will be call on the input iterator).
+  *
+  * That class is really useful when one need to modify the graph during an iteration. For
+  * instance the following code remove all nodes that match the function myfunc().
+  * Using standard iterators for that operations is not possible since we modify the graph.
+  *
+  * \code
+  * StableIterator<node> it(graph->getNodes());
+  * while(it.hasNext()) {
+  *  node n = it.next();
+  *  if (myfunc(n))
+  *     graph->delNode(n);
+  * }
+  * \endcode
+  *
+  * \see stableForEach
+  */
+template<class itType>
+struct StableIterator : public Iterator<itType> {
+    //=============================
+    StableIterator(Iterator<itType> *itIn, size_t nbElements = 0, bool deleteIterator = true) {
+        cloneIt.reserve(nbElements);
+        for (;itIn->hasNext();) {
+            cloneIt.push_back(itIn->next());
+        }
+        if (deleteIterator)
+            delete itIn;
+        itStl = cloneIt.begin();
+    }
+    //=============================
+    ~StableIterator(){};
+    //=============================
+    itType next() {
+        itType tmp(*itStl);
+        ++itStl;
+        return tmp;
+    }
+    //=============================
+    bool hasNext() {
+        return (itStl != cloneIt.end());
+    };
+    //=============================
+    /**
+    * Enables to restart the iteration at the begining.
+    */
+    void restart() {
+        itStl = cloneIt.begin();
+    }
+    //=============================
+protected :
+        std::vector<itType> cloneIt;
+        typename std::vector<itType>::const_iterator itStl;
 };
-
+/*@}*/
 }
 #endif
