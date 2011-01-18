8fac8008509d44f531edfd3fdd5ecdc27356b1c4
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2011-01-18 10:58:03 +0000

    - GlVertexArrayManager fix : regenerate color data after edges layout has been modified (bends can be added or removed which invalidates the color array)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3703 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/GlVertexArrayManager.h
+++ library/tulip-ogl/include/tulip/GlVertexArrayManager.h
@@ -38,110 +38,111 @@
 
 namespace tlp {
 
-  class GlEdge;
-  class GlNode;
-  class GlGraphInputData;
+class GlEdge;
+class GlNode;
+class GlGraphInputData;
 
-  /** \brief Class used to render edges/nodes with vertex array
-   *
-   * Class used to render edges/nodes with vertex array
-   */
-  class TLP_GL_SCOPE GlVertexArrayManager : public GraphObserver, public PropertyObserver {
+/** \brief Class used to render edges/nodes with vertex array
+ *
+ * Class used to render edges/nodes with vertex array
+ */
+class TLP_GL_SCOPE GlVertexArrayManager : public GraphObserver, public PropertyObserver {
 
-  public:
+public:
 
-    GlVertexArrayManager(GlGraphInputData *inputData);
+	GlVertexArrayManager(GlGraphInputData *inputData);
 
-    ~GlVertexArrayManager();
+	~GlVertexArrayManager();
 
-    void setInputData(GlGraphInputData *inputData);
+	void setInputData(GlGraphInputData *inputData);
 
-    inline bool renderingIsBegin(){
-      return isBegin;
-    }
+	inline bool renderingIsBegin(){
+		return isBegin;
+	}
 
-    bool haveToCompute();
+	bool haveToCompute();
 
-		void setHaveToComputeAll(bool compute);
-		void setHaveToComputeLayout(bool compute);
-		void setHaveToComputeColor(bool compute);
+	void setHaveToComputeAll(bool compute);
+	void setHaveToComputeLayout(bool compute);
+	void setHaveToComputeColor(bool compute);
 
-    void beginRendering();
-    void endRendering();
+	void beginRendering();
+	void endRendering();
 
-    void pauseRendering(bool pause);
-    void activate(bool act);
+	void pauseRendering(bool pause);
+	void activate(bool act);
 
-    void addEdge(GlEdge *edge);
-    void addNode(GlNode *node);
+	void addEdge(GlEdge *edge);
+	void addNode(GlNode *node);
 
-    void activateLineEdgeDisplay(GlEdge *edge, bool selected);
-    void activatePointEdgeDisplay(GlEdge *edge, bool selected);
-    void activatePointNodeDisplay(GlNode *node, bool onePixel, bool selected);
+	void activateLineEdgeDisplay(GlEdge *edge, bool selected);
+	void activatePointEdgeDisplay(GlEdge *edge, bool selected);
+	void activatePointNodeDisplay(GlNode *node, bool onePixel, bool selected);
 
-    void propertyValueChanged(PropertyInterface *property);
+	void propertyValueChanged(PropertyInterface *property);
 
-    void addEdge(Graph *,const edge);
-    void addNode(Graph *,const node);
-    void beforeSetAllNodeValue(PropertyInterface*);
-    void beforeSetAllEdgeValue(PropertyInterface*);
-    void beforeSetNodeValue(PropertyInterface*, const node);
-    void beforeSetEdgeValue(PropertyInterface*, const edge);
-    void addLocalProperty(Graph*, const std::string&);
-    void delLocalProperty(Graph*, const std::string&);
+	void addEdge(Graph *,const edge);
+	void addNode(Graph *,const node);
+	void beforeSetAllNodeValue(PropertyInterface*);
+	void beforeSetAllEdgeValue(PropertyInterface*);
+	void beforeSetNodeValue(PropertyInterface*, const node);
+	void beforeSetEdgeValue(PropertyInterface*, const edge);
+	void addLocalProperty(Graph*, const std::string&);
+	void delLocalProperty(Graph*, const std::string&);
 
-    void destroy(Graph *);
-    void destroy(PropertyInterface*);
+	void destroy(Graph *);
+	void destroy(PropertyInterface*);
 
-  protected:
+protected:
 
-    void clearLayoutData();
-    void clearColorData();
-    void clearData();
-    void initObservers();
-    void clearObservers();
+	void clearLayoutData();
+	void clearColorData();
+	void clearData();
+	void initObservers();
+	void clearObservers();
 
-    GlGraphInputData *inputData;
-    Graph *graph;
-    bool graphObserverActivated;
-    bool layoutObserverActivated;
-    bool colorObserverActivated;
+	GlGraphInputData *inputData;
+	Graph *graph;
+	bool graphObserverActivated;
+	bool layoutObserverActivated;
+	bool colorObserverActivated;
 
-    bool activated;
-    bool isBegin;
-    bool toComputeAll;
-    bool toComputeLayout;
-    bool toComputeColor;
+	bool activated;
+	bool isBegin;
+	bool toComputeAll;
+	bool toComputeLayout;
+	bool toComputeColor;
 
-    bool vectorLayoutSizeInit;
-    bool vectorColorSizeInit;
-    bool vectorIndexSizeInit;
+	bool vectorLayoutSizeInit;
+	bool vectorColorSizeInit;
+	bool vectorIndexSizeInit;
 
-    bool colorInterpolate;
+	bool colorInterpolate;
+	bool edgesModified;
 
-    std::vector<Coord> linesCoordsArray;
-    std::vector<Color> linesColorsArray;
-    std::vector<GLint> linesIndexArray;
-    std::vector<GLsizei> linesIndexCountArray;
-    std::vector<GLuint> linesRenderingIndexArray;
-    std::vector<GLuint> linesSelectedRenderingIndexArray;
+	std::vector<Coord> linesCoordsArray;
+	std::vector<Color> linesColorsArray;
+	std::vector<GLint> linesIndexArray;
+	std::vector<GLsizei> linesIndexCountArray;
+	std::vector<GLuint> linesRenderingIndexArray;
+	std::vector<GLuint> linesSelectedRenderingIndexArray;
 
-    std::vector<Coord> pointsCoordsArray;
-    std::vector<Color> pointsColorsArray;
+	std::vector<Coord> pointsCoordsArray;
+	std::vector<Color> pointsColorsArray;
 
-    std::vector<GLuint> points1PNodesRenderingIndexArray;
-    std::vector<GLuint> points1PNodesSelectedRenderingIndexArray;
-    std::vector<GLuint> points2PNodesRenderingIndexArray;
-    std::vector<GLuint> points2PNodesSelectedRenderingIndexArray;
+	std::vector<GLuint> points1PNodesRenderingIndexArray;
+	std::vector<GLuint> points1PNodesSelectedRenderingIndexArray;
+	std::vector<GLuint> points2PNodesRenderingIndexArray;
+	std::vector<GLuint> points2PNodesSelectedRenderingIndexArray;
 
-    std::vector<GLuint> points1PEdgesRenderingIndexArray;
-    std::vector<GLuint> points1PEdgesSelectedRenderingIndexArray;
+	std::vector<GLuint> points1PEdgesRenderingIndexArray;
+	std::vector<GLuint> points1PEdgesSelectedRenderingIndexArray;
 
 
-    std::vector<std::pair<unsigned int,unsigned int> > edgeToLineIndexVector;
-    std::vector<unsigned int> edgeToPointIndexVector;
-    std::vector<unsigned int> nodeToPointIndexVector;
-  };
+	std::vector<std::pair<unsigned int,unsigned int> > edgeToLineIndexVector;
+	std::vector<unsigned int> edgeToPointIndexVector;
+	std::vector<unsigned int> nodeToPointIndexVector;
+};
 
 }
 
