922eef3bfe15852ab9661750121f33452977bc3d
Patrick Mary <mary@labri.fr>
2011-01-25 11:14:12 +0000

    add documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3743 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/PropertyInterface.h
+++ library/tulip/include/tulip/PropertyInterface.h
@@ -76,7 +76,6 @@ public:
     * Values are not copied.
     */
   virtual PropertyInterface* clonePrototype(Graph *, const std::string&) =0;
-  //=================================================================================
   /**
     * Returns a string describing the type of the property.
     * i.e. "graph", "double", "layout", "string", "integer", "color", "size", ...
@@ -104,35 +103,119 @@ public:
       return graph;
   }
 
-  // Untyped accessors
+  /**
+  * Returns a string representation of the node default value
+  */
   virtual std::string getNodeDefaultStringValue() const = 0;
-  virtual std::string getEdgeDefaultStringValue() const = 0;
-  virtual std::string getNodeStringValue( const node n ) const = 0;
-  virtual std::string getEdgeStringValue( const edge e ) const = 0;
-  virtual bool setNodeStringValue( const node n, const std::string & v ) = 0;
-  virtual bool setEdgeStringValue( const edge e, const std::string & v ) = 0;
+  /**
+   * Clear all nodes registered values, and set a new node default value
+   * in converting the given string representation of a value
+   */
   virtual bool setAllNodeStringValue( const std::string & v ) = 0;
+  /**
+  * Returns a string representation of the edge default value
+  */
+  virtual std::string getEdgeDefaultStringValue() const = 0;
+  /**
+   * Clear all edges registered values, and set a new edge default value
+   * in converting the given string representation of a value
+   */
   virtual bool setAllEdgeStringValue( const std::string & v ) = 0;
-  // two methods to compute the values for meta nodes or edges
-  // mN is the meta node, sg is the corresponding subgraph
-  // and g is the graph owning mN
-  virtual void computeMetaValue(node mN, Graph* sg, Graph* mg)=0;
-  // mE is the meta edge, itE is an iterator on the underlying edges
-  // mg is the graph owning mE
-  virtual void computeMetaValue(edge mE, tlp::Iterator<edge>* itE, Graph* mg)=0;
-  // the ones below are used by GraphUpdatesRecorder
-  virtual tlp::Iterator<node>* getNonDefaultValuatedNodes(const Graph* = NULL) const = 0;
-  virtual tlp::Iterator<edge>* getNonDefaultValuatedEdges(const Graph* = NULL) const = 0;
+  /**
+   * Returns a string conversion of the value registered for the given node
+   */
+  virtual std::string getNodeStringValue( const node n ) const = 0;
+  /**
+   * Register a new value for the given node in converting
+   * the given string representation
+   */
+  virtual bool setNodeStringValue( const node n, const std::string & v ) = 0;
+  /**
+   * Returns a string conversion of the value registered for the given edge
+   */
+  virtual std::string getEdgeStringValue( const edge e ) const = 0;
+  /**
+   * Register a new value for the given edge in converting
+   * the given string representation
+   */
+  virtual bool setEdgeStringValue( const edge e, const std::string & v ) = 0;
+  /**
+   * Returns a pointer to a DataMem structure embedding the node default value
+   * WARNING: it is of the caller responsability to delete the returned structure
+  */
   virtual DataMem* getNodeDefaultDataMemValue() const = 0;
-  virtual DataMem* getEdgeDefaultDataMemValue() const = 0;
-  virtual DataMem* getNodeDataMemValue( const node n ) const = 0;
-  virtual DataMem* getEdgeDataMemValue( const edge e ) const = 0;
-  virtual DataMem* getNonDefaultDataMemValue( const node n ) const = 0;
-  virtual DataMem* getNonDefaultDataMemValue( const edge e ) const = 0;
-  virtual void setNodeDataMemValue( const node n, const DataMem* v) = 0;
-  virtual void setEdgeDataMemValue( const edge e, const DataMem* v) = 0;
+  /**
+   * Clear all nodes registered values, and set a new node default value
+   * using the value embedded in the pointed DataMem structure
+   */
   virtual void setAllNodeDataMemValue(const DataMem* v ) = 0;
+  /**
+   * Returns a pointer to a DataMem structure embedding the edge default value
+   * WARNING: the caller have the responsability to delete the returned structure
+  */
+  virtual DataMem* getEdgeDefaultDataMemValue() const = 0;
+  /**
+   * Clear all edges registered values, and set a new edge default value
+   * using the value embedded in the pointed DataMem structure
+   */
   virtual void setAllEdgeDataMemValue(const DataMem* v ) = 0;
+  /**
+  * Returns a pointer to a DataMem structure embedding the node registered value
+  * WARNING: it is of the caller responsability to delete the returned structure
+  */
+  virtual DataMem* getNodeDataMemValue( const node n ) const = 0;
+  /**
+   * Returns a NULL pointer if the given node registered value is the default
+   * else returns a pointer to a DataMem structure embedding the value
+   * WARNING: it is of the caller responsability to delete the returned structure
+  */
+  virtual DataMem* getNonDefaultDataMemValue( const node n ) const = 0;
+  /**
+   * Register a new value for the given node
+   * using the value embedded in the pointed DataMem structure
+   */
+  virtual void setNodeDataMemValue( const node n, const DataMem* v) = 0;
+  /**
+  * Returns a pointer to a DataMem structure embedding the edge registered value
+  * WARNING: it is of the caller responsability to delete the returned structure
+  */
+  virtual DataMem* getEdgeDataMemValue( const edge e ) const = 0;
+  /**
+   * Returns a NULL pointer if the given edge registered value is the default
+   * else returns a pointer to a DataMem structure embedding the value.
+   * WARNING: it is of the caller responsability to delete the returned structure
+  */
+  virtual DataMem* getNonDefaultDataMemValue( const edge e ) const = 0;
+  /**
+   * Register a new value for the given node
+   * using the value embedded in the pointed DataMem structure
+   */
+  virtual void setEdgeDataMemValue( const edge e, const DataMem* v) = 0;
+  /**
+   * Returns an iterator on all nodes whose value is different
+   * from the default value. When the pointer to the graph is not NULL
+   * only the edges owned by this graph are returned by the iterator.
+   * WARNING: it is of the caller responsability to delete the returned iterator
+   */
+  virtual tlp::Iterator<node>* getNonDefaultValuatedNodes(const Graph* = NULL) const = 0;
+  /**
+   * Returns an iterator on all edges whose value is different
+   * from the default value. When the pointer to the graph is not NULL
+   * only the edges owned by this graph are returned by the iterator.
+   * WARNING: it is of the caller responsability to delete the returned iterator
+   */
+  virtual tlp::Iterator<edge>* getNonDefaultValuatedEdges(const Graph* = NULL) const = 0;
+  /**
+   * Set a computed value for the meta node mN pointing to the sub-graph sg
+   * mg is the graph owning the meta node
+   */
+  virtual void computeMetaValue(node mN, Graph* sg, Graph* mg)=0;
+  /**
+   * Set a computed value for the meta edge mE representing the edges
+   * from the iterator itE.
+   * mg is the graph owning the meta edge
+   */
+  virtual void computeMetaValue(edge mE, tlp::Iterator<edge>* itE, Graph* mg)=0;
 
   // superclass of further MetaValueCalculator
   class MetaValueCalculator {
@@ -140,15 +223,22 @@ public:
     virtual ~MetaValueCalculator() {}
   };
 
-  // method to specify the meta value calculator.
-  // be careful that its destruction is not managed by the property
+
+  /**
+   * Returns the meta value calculator associated to this property
+   */
+  MetaValueCalculator* getMetaValueCalculator() {
+    return metaValueCalculator;
+  }
+
+  /**
+   * Sets the meta value calculator.
+   * Be careful that its destruction is not managed by the property
+   */
   virtual void setMetaValueCalculator(MetaValueCalculator* mvCalc) {
     metaValueCalculator = mvCalc;
   }
 
-  MetaValueCalculator* getMetaValueCalculator() {
-    return metaValueCalculator;
-  }
 
  protected:
   MetaValueCalculator* metaValueCalculator;
