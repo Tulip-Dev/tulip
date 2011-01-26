31df40595c362da028c78209b7cda5a288943001
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2011-01-26 10:32:28 +0000

    - Document GlCPULODCalculator - Add a parameter to GlCPULODCalculator to say if we have to compute edges lod - Use this parameter in GlQuadTreeLODCalculator to compute edges lod when we render edges labels
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3752 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlQuadTreeLODCalculator.cpp
+++ library/tulip-ogl/src/GlQuadTreeLODCalculator.cpp
@@ -394,6 +394,10 @@ namespace tlp {
             }
         }
 
+        // If edge labels are rendered we have to compute the edges LOD because label rendering use LOD
+        computeEdgesLOD=inputData->parameters->isViewEdgeLabel();
+
+
         GlCPULODCalculator::computeFor3DCamera(layerLODUnit,eye,transformMatrix,globalViewport,currentViewport);
 
     }
