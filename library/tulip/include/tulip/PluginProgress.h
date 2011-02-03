c1db16151724e1bebcfeb0a30aaa606049cc569a
Charles Huet <packadal@gmail.com>
2011-02-03 13:20:31 +0000

    some tweaks on the documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3830 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/PluginProgress.h
+++ library/tulip/include/tulip/PluginProgress.h
@@ -22,19 +22,26 @@
 #ifdef HAVE_CONFIG_H
 #include <config.h>
 #endif
+
 #include <tulip/tulipconf.h>
 #include <string>
 
 namespace tlp {
 
-enum ProgressState{TLP_CONTINUE, TLP_CANCEL , TLP_STOP };
+  /**
+   * @brief Describes what the plug-in should do.
+   * @enum tlp::ProgressState::TLP_CONTINUE The plugin should continue its execution.
+   * @enum tlp::ProgressState::TLP_CANCEL The plugin should cancel, reverting all performed changes since the plugin was called.
+   * @enum tlp::ProgressState::TLP_STOP The plugin should stop, leaving the graph in its current state.
+   **/
+  enum ProgressState{TLP_CONTINUE, TLP_CANCEL , TLP_STOP };
 
 /**
   * @brief Interface to notify and control the progression of a process.
   *
   * To notify the progression use the progress function. You can ask
   * @code
-  * PluginProgress *progress;
+  * PluginProgress *progress = new QtProgress(parentWidget, \"Awesome task #1\");
   * progress->setComment("First step");
   * for(int i = 0 ; i<50 ;++i){
   * //Perform some actions
@@ -50,48 +57,91 @@ enum ProgressState{TLP_CONTINUE, TLP_CANCEL , TLP_STOP };
   * progress->progress(i,50);
   * }
   *
+  * //Delete the progress once you're done with it
+  * delete progress;
   * @endcode
   **/
 class TLP_SCOPE PluginProgress {
  public:  
      virtual ~PluginProgress(){}
+  
   /**
-    * @brief Notify the progression of the process.
-    * @brief The current step number.
-    * @brief The maximum step number.
-    **/
+   * @brief Notify the progression of the process.
+   *
+   * @param step The current step number.
+   * @param max_step The total number of steps.
+   * @return :ProgressState a value indicating whether the progress has been stopped, cancelled, or will continue.
+   **/
   virtual ProgressState progress(int step, int max_step)=0;
+    
   /**
-    * @brief Set the state flag to cancel to notify to the process that user want to cancel it.
-    *
-    * Canceling a process must stop it and revert all the changes performed since its start.
-    **/
+   * @brief Sets the state flag to cancel, notifying to the process that the user wants to cancel it.
+   * Canceling a process must stop it and revert all the changes performed since its start.
+   *
+   * @return void
+   **/
   virtual void cancel()=0;
-  /**
-    * @brief Set the state flag to stop to notify to the process that user want to stop it.
-    *
-    * Stopping a process dont revert changes.
+  
+    /**
+    * @brief Sets the state flag to stop, notifying to the process that the user wants to stop it.
+    * Stopping a process does not revert changes.
+    * @return void
     **/
   virtual void stop()=0;
+  
+  /**
+   * @brief The preview mode redraws the graph while applying the algorithm, making it slower.
+   *
+   * @return bool Whether the preview mode is activated.
+   **/
   virtual bool isPreviewMode() const =0;
-  virtual void setPreviewMode(bool)=0;
-  virtual void showPreview(bool)=0;
+  
+  
   /**
-    * @brief Get the internal state of the PluginProgress.
-    **/
+   * @brief The preview mode redraws the graph while applying the algorithm, making it slower.
+   *
+   * @param drawPreview Whether the preview should be drawn.
+   * @return void
+   **/
+  virtual void setPreviewMode(bool drawPreview)=0;
+  
+  /**
+   * @brief This tells the widget if it should show a preview checkbox, allowing the user to decide if the algorithm should draw a preview or not.
+   *
+   * @param showPreview Whether the progress widget should contain a preview checkbox or not.
+   * @return void
+   **/
+  virtual void showPreview(bool showPreview)=0;
+  
+  /**
+   * @brief Get the current internal state of the PluginProgress.
+   *
+   * @return :ProgressState The current state.
+   **/
   virtual ProgressState state() const=0;
+  
   /**
-    * @brief Return a message describing the error encountered during the process.
-    **/
+   * @brief Return a message describing the error encountered during the process. If no error has been encountered, an empty string is returned.
+   *
+   * @return :string A description of the encountered error, if any.
+   **/
   virtual std::string getError()=0;
+  
   /**
-    * @brief Set the message describing the error encountered during the process.
-    **/
+   * @brief Sets the message describing the error encountered during the process.
+   *
+   * @param error The description of the encountered error.
+   * @return void
+   **/
   virtual void setError(std::string error)=0;
+  
   /**
-    * @brief Change the comment about the process progression.
-    **/
-  virtual void setComment(std::string)=0;
+   * @brief Changes the comment about the process progression.
+   *
+   * @param comment A description of what the plugin is currently doing, displayed to inform the user.
+   * @return void
+   **/
+  virtual void setComment(std::string comment)=0;
 
 };
 
