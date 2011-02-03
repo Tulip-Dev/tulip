4c5c89c3efbc1c360465c69dd83d265611b5e026
Antoine Lambert <antoine.lambert33@gmail.com>
2011-02-03 10:56:31 +0000

    - rework ColorScale implementation :   * add a new constructor   * add setColorAtPos method   * make isGradient method const   * some cleanups
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3821 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/ColorScale.h
+++ library/tulip/include/tulip/ColorScale.h
@@ -28,74 +28,85 @@
 
 namespace tlp {
 
-  /**
-   * @brief Use this class to create a color scale and perform color mapping.
-   * The color scale can be either gradient or predefined colors steps.
-   * If the color scale is a gradient, returned color are interpolated in function of the position.
-   * If the color scale isn't a gradient returned colors are the predefined colors steps.
-   * @code
-   * \// Creating the color scale.
-   * ColorScale colorScale;
-   * \// Color scale initialization : from blue to red with gradient.
-   * vector<color> colors;
-   * colors.push_back(Color(0,0,255));
-   * colors.push_back(Color(255,0,0));
-   * colorScale.setColorScale(colors,true);
-   *
-   * \//Get the color for the position 0.5 eq Color(127,0,127).
-   * colorScale.getColorAtPos(0.5);
-   * \//Reinit the color scale : from blue to red without gradient.
-   * vector<color> newColors;
-   * newColors.push_back(Color(0,0,255));
-   * newColors.push_back(Color(255,0,0));
-   * colorScale.setColorScale(colors,false);
-   * \//Get the color for the position 0.3 eq Color(0,0,255).
-   * colorScale.getColorAtPos(0.3);
-   * \//Get the color for the position 0.7 eq Color(255,0,0).
-   * colorScale.getColorAtPos(0.7);
-   * @endcode
-   */
+/**
+ * @brief Use this class to create a color scale and perform color mapping.
+ * The color scale can be either gradient or predefined colors steps.
+ * If the color scale is a gradient, returned color are interpolated in function of the position.
+ * If the color scale isn't a gradient returned colors are the predefined colors steps.
+ * @code
+ * \// Creating the color scale.
+ * ColorScale colorScale;
+ * \// Color scale initialization : from blue to red with gradient.
+ * vector<color> colors;
+ * colors.push_back(Color(0,0,255));
+ * colors.push_back(Color(255,0,0));
+ * colorScale.setColorScale(colors,true);
+ *
+ * \//Get the color for the position 0.5 eq Color(127,0,127).
+ * colorScale.getColorAtPos(0.5);
+ * \//Reinit the color scale : from blue to red without gradient.
+ * vector<color> newColors;
+ * newColors.push_back(Color(0,0,255));
+ * newColors.push_back(Color(255,0,0));
+ * colorScale.setColorScale(colors,false);
+ * \//Get the color for the position 0.3 eq Color(0,0,255).
+ * colorScale.getColorAtPos(0.3);
+ * \//Get the color for the position 0.7 eq Color(255,0,0).
+ * colorScale.getColorAtPos(0.7);
+ * @endcode
+ */
 class TLP_SCOPE ColorScale : public Observable {
 
 public:
 
-  ColorScale();
-  ColorScale(const ColorScale& scale);
-  virtual ~ColorScale();
+	ColorScale(const bool gradient = true);
+	ColorScale(const std::vector<Color> &colors, const bool gradient = true);
+	ColorScale(const ColorScale& scale);
+	virtual ~ColorScale();
 
-  /**
-   * @brief Configure the color scale.
-   * This method is used to configure the color scale. If the scale was previously configured the old configuration is lost.
-   * @param colors The colors to use in the color scale.
-   * @param gradient If set to true perform a
-   */
-  virtual void setColorScale(const std::vector<Color> colors, const bool gradient = true);
-  /**
-   * @brief Compute the color for the given value.
-   * @param pos This value define the position of the color in the scale and must be between 0 and 1.0.
-   * @return The color corresponding to the position in the scale if the position is beetween 0 and 1.0. If the position is smaller than 0 the color corresponding to the position 0 is returned and if the position is greater than 1.0 the color corresponding to the position 1.0 is returned.
-   */
-  virtual Color getColorAtPos(const float pos) const;
+	/**
+	 * @brief Configure the color scale.
+	 * This method is used to configure the color scale. If the scale was previously configured the old configuration is lost.
+	 * @param colors The colors to use in the color scale.
+	 * @param gradient If set to true, color scale is a gradient
+	 */
+	virtual void setColorScale(const std::vector<Color> colors, const bool gradient = true);
 
-  /**
-   * @brief Return true is the color scale was initialized.
-   */
-  bool colorScaleInitialized() const {return colorScaleSet;}
-  /**
-   * @brief Return a map corresponding to the color scale.
-   * The index of the map is the position for the corresponding color in the color scale.
-   */
-  std::map<float, Color> getColorMap() const {return colorMap;}
-  /**
-   * @brief Return true if the color scale use gradient.
-   */
-  bool isGradient(){return gradient;}
+	/**
+	 * @brief Add a color to color scale at specific position.
+	 * This method is used to add a color to the color scale at a specific position
+	 * @param pos the position in the color scale (0 <= pos <= 1)
+	 * @param gradient If set to true, color scale is a gradient
+	 */
+	virtual void setColorAtPos(const float pos, const Color &color);
+
+	/**
+	 * @brief Compute the color for the given value.
+	 * @param pos This value defines the position of the color in the scale and must be between 0 and 1.0.
+	 * @return The color corresponding to the position in the scale if the position is beetween 0 and 1.0. If the position is smaller than 0 the color corresponding to the position 0 is returned and if the position is greater than 1.0 the color corresponding to the position 1.0 is returned.
+	 */
+	virtual Color getColorAtPos(const float pos) const;
+
+	/**
+	 * @brief Return true is the color scale was initialized.
+	 */
+	bool colorScaleInitialized() const {return colorScaleSet;}
+	/**
+	 * @brief Return a map corresponding to the color scale.
+	 * The index of the map is the position for the corresponding color in the color scale.
+	 */
+	std::map<float, Color> getColorMap() const {return colorMap;}
+	/**
+	 * @brief Return true if the color scale is a gradient.
+	 */
+	bool isGradient() const {return gradient;}
 
 protected:
 
-  std::map<float, Color> colorMap;
-  bool gradient;
-  bool colorScaleSet;
+	std::map<float, Color> colorMap;
+	bool gradient;
+	bool colorScaleSet;
+
 };
 
 }
--- library/tulip/src/ColorScale.cpp
+++ library/tulip/src/ColorScale.cpp
@@ -23,92 +23,105 @@ using namespace std;
 
 namespace tlp {
 
-ColorScale::ColorScale() :
-  gradient(true), colorScaleSet(false) {
-  colorMap[0.0f] = Color(229, 40, 0, 200);
-  colorMap[0.25f] = Color(255, 170, 0, 200);
-  colorMap[0.5f] = Color(255, 255, 127, 200);
-  colorMap[0.75f] = Color(156, 161, 255, 200);
-  colorMap[1.0f] = Color(75, 75, 255, 200);
+
+ColorScale::ColorScale(const bool gradient) :
+				gradient(gradient), colorScaleSet(false) {
+	colorMap[0.0f] = Color(229, 40, 0, 200);
+	colorMap[0.25f] = Color(255, 170, 0, 200);
+	colorMap[0.5f] = Color(255, 255, 127, 200);
+	colorMap[0.75f] = Color(156, 161, 255, 200);
+	colorMap[1.0f] = Color(75, 75, 255, 200);
+
+}
+
+ColorScale::ColorScale(const vector<Color> &colors, const bool gradient) :
+						  gradient(gradient), colorScaleSet(true) {
+	setColorScale(colors, gradient);
 }
 
 ColorScale::ColorScale(const ColorScale& scale) {
-  colorMap = scale.colorMap;
-  gradient = scale.gradient;
-  colorScaleSet = scale.colorScaleSet;
+	colorMap = scale.colorMap;
+	gradient = scale.gradient;
+	colorScaleSet = scale.colorScaleSet;
 }
 
-ColorScale::~ColorScale() {
-}
+ColorScale::~ColorScale() {}
 
 void ColorScale::setColorScale(const std::vector<Color> colors,
-    const bool gradientV) {
-  gradient = gradientV;
-  colorMap.clear();
-  if (!colors.empty()) {
-    colorScaleSet = true;
-    if (colors.size() == 1) {
-      colorMap[0.0f] = colors[0];
-      colorMap[1.0f] = colors[0];
-    } else {
-      float shift;
-      if (gradient) {
-        shift = 1.0f / (colors.size() - 1);
-      } else {
-        shift = 1.0f / colors.size();
-      }
-      for (unsigned int i = 0; i < colors.size(); ++i) {
-        //Ensure that the last color will be set to 1
-        if (i == colors.size() - 1) {
-          if (!gradient) {
-            colorMap[1.0f - shift] = colors[i];
-          }
-          colorMap[1.0f] = colors[i];
-        } else {
-          colorMap[(float) (i * shift)] = colors[i];
-          if (!gradient) {
-            colorMap[(float) (((i + 1) * shift) - 1E-6)] = colors[i];
-          }
-        }
-      }
-    }
-    notifyObservers();
-  }
+		const bool gradientV) {
+	gradient = gradientV;
+	colorMap.clear();
+	if (!colors.empty()) {
+		colorScaleSet = true;
+		if (colors.size() == 1) {
+			colorMap[0.0f] = colors[0];
+			colorMap[1.0f] = colors[0];
+		} else {
+			float shift;
+			if (gradient) {
+				shift = 1.0f / (colors.size() - 1);
+			} else {
+				shift = 1.0f / colors.size();
+			}
+			for (size_t i = 0; i < colors.size(); ++i) {
+				//Ensure that the last color will be set to 1
+				if (i == colors.size() - 1) {
+					if (!gradient) {
+						colorMap[1.0f - shift] = colors[i];
+					}
+					colorMap[1.0f] = colors[i];
+				} else {
+					colorMap[i * shift] = colors[i];
+					if (!gradient) {
+						colorMap[((i + 1) * shift) - 1E-6] = colors[i];
+					}
+				}
+			}
+		}
+		notifyObservers();
+	}
+}
+
+void ColorScale::setColorAtPos(const float pos, const Color &color) {
+	colorMap[pos] = color;
+	colorScaleSet = true;
 }
 
 Color ColorScale::getColorAtPos(const float pos) const {
-  if (colorMap.size() == 0) {
-    return Color(255, 255, 255, 255);
-  } else {
-    Color startColor;
-    Color endColor;
-    float startPos, endPos;
-    map<float, Color>::const_iterator it = colorMap.begin();
-    startPos = endPos = it->first;
-    startColor = endColor = it->second;
-    for (++it; it != colorMap.end(); ++it) {
-      endColor = it->second;
-      endPos = it->first;
-      if (pos >= startPos && pos <= endPos) {
-        break;
-      } else {
-        startColor = endColor;
-        startPos = endPos;
-      }
-    }
+	if (colorMap.size() == 0) {
+		return Color(255, 255, 255, 255);
+	} else {
+		Color startColor;
+		Color endColor;
+		float startPos, endPos;
+		map<float, Color>::const_iterator it = colorMap.begin();
+		startPos = endPos = it->first;
+		startColor = endColor = it->second;
+		for (++it; it != colorMap.end(); ++it) {
+			endColor = it->second;
+			endPos = it->first;
+			if (pos >= startPos && pos <= endPos) {
+				break;
+			} else {
+				startColor = endColor;
+				startPos = endPos;
+			}
+		}
 
-    Color ret;
-    if (gradient) {
-      double ratio = (pos - startPos) / (endPos - startPos);
-      for (unsigned int i = 0; i < 4; ++i) {
-        ret[i] = (unsigned char) (double(startColor[i]) + (double(endColor[i])
-            - double(startColor[i])) * ratio);
-      }
-    } else {
-      return startColor;
-    }
-    return ret;
-  }
+
+		if (gradient) {
+			Color ret;
+			double ratio = (pos - startPos) / (endPos - startPos);
+			for (unsigned int i = 0; i < 4; ++i) {
+				ret[i] = static_cast<unsigned char>((double(startColor[i]) + (double(endColor[i])
+						- double(startColor[i])) * ratio));
+			}
+			return ret;
+		} else {
+			return startColor;
+		}
+
+	}
 }
 
 }
