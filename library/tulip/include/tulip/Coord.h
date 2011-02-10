d96897865129f1a1708dee6c5aaeeaffa239f166
Charles Huet <packadal@gmail.com>
2011-02-10 15:09:31 +0000

    modified documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3869 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/Coord.h
+++ library/tulip/include/tulip/Coord.h
@@ -29,25 +29,50 @@
 
 namespace tlp {
 
-  /// Class for coordinates in 3D
+  /**
+   * @brief This class represents a point in 3-D space, with float precision.
+   **/
   class TLP_SCOPE Coord : public tlp::VectorCast<float, 3, tlp::Coord> {
   public:
-    // default constructor (x,y,z set to 0)
-    //   inline Coord() {set(0,0,0);}//{ memset(array, 0, sizeof(array)); }
-    // this constructor gives the same value to x,y,z
-    //    inline Coord(const float);
-
+    /**
+    * @brief Copy constructor. 
+    *
+    * @param v A tlp::Coord to copy.
+    **/
     inline explicit Coord(const tlp::Vector<float,3> &v):VectorCast<float, 3, tlp::Coord>(v) {
     }
 
-    inline explicit Coord(const float xx = 0, const float yy = 0, const float zz = 0)
-    {set(xx, yy, zz);}
+    /**
+    * @brief Default constructor. The values default on 0 if not filled.
+    *
+    * @param xx The X value of this Coord. Defaults to 0.
+    * @param yy The Y value of this Coord. Defaults to 0.
+    * @param zz The Z value of this Coord. Defaults to 0.
+    **/
+    inline explicit Coord(const float xx = 0, const float yy = 0, const float zz = 0) {
+      set(xx, yy, zz);
+    }
 
-    inline void set(const float xx= 0.f,
-		    const float yy= 0.f,
-		    const float zz= 0.f)
-    {(*this)[0] = xx; (*this)[1] = yy, (*this)[2] = zz;}
+    /**
+    * @brief Sets the value of the X, Y, and Z value of this Coordinate.
+    *
+    * @param xx The X value of this Coord. Defaults to 0.f.
+    * @param yy The Y value of this Coord. Defaults to 0.f.
+    * @param zz The Z value of this Coord. Defaults to 0.f.
+    * @return void
+    **/
+    inline void set(const float xx= 0.f, const float yy= 0.f, const float zz= 0.f) {
+      (*this)[0] = xx;
+      (*this)[1] = yy;
+      (*this)[2] = zz;
+    }
 
+    /**
+    * @brief Copy the values of the parameter coordinate into this coordinate.
+    *
+    * @param c A Coord to copy.
+    * @return void
+    **/
     inline void set(const Coord& c) {
       (*this) = c;
     }
@@ -59,12 +84,21 @@ namespace tlp {
     inline float getX() const {return (*this)[0];}
     inline float getY() const {return (*this)[1];}
     inline float getZ() const {return (*this)[2];}
+    
+    /**
+     * @brief Modifies the values of the parameters to the vaules of this Coordinate's composants.
+     *
+     * @param xx This Coord's X value.
+     * @param yy This Coord's Y value.
+     * @param zz This Coord's Z value.
+     * @return void
+     **/
     inline void get(float &xx,float &yy,float &zz) const {
       xx=(*this)[0];yy=(*this)[1];zz=(*this)[2];
     }
   };
 
-#ifdef _MSC_VER //visual studio needs to export all uses of the dllExport stuff
+#ifdef _MSC_VER //visual studio needs to export all uses of dllExport (TLP_SCOPE) classes
   template class TLP_SCOPE VectorCast<float, 3, Coord>;
   template struct TLP_SCOPE Array<tlp::Coord,4>;
 #endif
