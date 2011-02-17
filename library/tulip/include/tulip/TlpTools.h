2e58c4b5ad0c24a3406bd176f96931802a8c6b57
Charles Huet <packadal@gmail.com>
2011-02-17 12:29:56 +0000

    documented most functions
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@3968 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/TlpTools.h
+++ library/tulip/include/tulip/TlpTools.h
@@ -34,7 +34,15 @@ namespace tlp {
   extern TLP_SCOPE std::string TulipDocProfile;
   extern TLP_SCOPE std::string TulipUserHandBookIndex;
   extern TLP_SCOPE std::string TulipBitmapDir;
-  
+
+  /**
+   * @brief Initializes the Tulip library.
+   * Looks for the Tulip plug-ins directory, and loads the plug-ins from it.
+   * The plug-ins directory can be defined in different ways, given by order of prevalence :
+   * 1. the TLP_DIR environment variable, if it has a value
+   * 2. the appDirPath parameter, if it is not NULL
+   * 3. a fallback value of 'C:/Tulip/lib/' on windows, or '/usr/local/lib/' on unices.
+   */
   extern TLP_SCOPE void initTulipLib(char* appDirPath = 0);
     
   TLP_SCOPE void loadPlugins(PluginLoader *plug=0);
@@ -43,25 +51,42 @@ namespace tlp {
   TLP_SCOPE void loadPluginsCheckDependencies(PluginLoader* loader=0);
 
   /**
-   * returns the demangled name of a C++ class defines in the tlp namespace.
-   * The tlp:: prefix is omitted and the returned pointer
-   * do not have to be deallocated.
+   * @brief Demangles the name of a C++ class defined in the tlp namespace.
+   * Simply removes the 'tlp::' prefix from the class name.
+   * @return string The demangled name of a Tulip C++ class.
    */
   TLP_SCOPE std::string demangleTlpClassName(const char *className);
-
+  /**
+   * @brief Returns an istream to read from a gzipped file (uses gzstream lib).
+   * The stream has to be deleted after use.
+   * @param name The name of the file to read from.
+   * @param open_mode The mode to open the file with. Defaults to std::ios::in.
+   * @return istream gzipped input stream from a file.
+   */
   TLP_SCOPE std::istream *getIgzstream(const char *name, int open_mode = std::ios::in);
   /**
-   * Returns an ostream to write to a gzipped file (uses gzstream lib)
-   * the stream has to be deleted after use.
-   * \warning Don't forget to check the stream with ios::bad()!
+   * @brief Returns an ostream to write to a gzipped file (uses gzstream lib).
+   * The stream has to be deleted after use.
+   * @warning Don't forget to check the stream with ios::bad()!
+   * @param name The name of the file to write to.
+   * @param open_mode The mode to open the file with. Defaults to std::ios::out.
+   * @return ostream gzipped output stream to a file.
    */
   TLP_SCOPE std::ostream *getOgzstream(const char *name, int open_mode = std::ios::out);
   /**
-   * Extracts Major number from a release number
+   * @brief Splits the string and returns everything befor the first dot ('.').
+   * This is used to return major version number, as version numbers are formatted as X.Y.Z,
+   * X being the major, Y the minor, and Z the patch version.
+   *
+   * @return string The part of the string befor the first dot.
    */
   TLP_SCOPE std::string getMajor(const std::string &release);
   /**
-   * Extracts Minor number from a release number
+   * @brief Splits the string and return the minor version.
+   * If the string does not contain any dot, then 0 is returned.
+   * If the string contains only one dot (X.Y), then everything after the first dot is returned (Y).
+   * If the string is a full version with two dots (X.Y.Z), everything between the first and last dots is returned (Y).
+   * If there are more than two dots, everything between the first and last dots is returned.
    */
   TLP_SCOPE std::string getMinor(const std::string &release);
 }
