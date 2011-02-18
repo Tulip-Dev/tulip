57bf5b8532f1a63d2d887fd257362f3289530935
Patrick Mary <mary@labri.fr>
2011-02-18 17:31:24 +0000

    integrates Tulip header addition from tulip trunk
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4001 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/CSVParserConfigurationWidget.h
+++ library/tulip-qt/include/tulip/CSVParserConfigurationWidget.h
@@ -1,3 +1,21 @@
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
 #ifndef CSVPARSERCONFIGURATIONWIDGET_H
 #define CSVPARSERCONFIGURATIONWIDGET_H
 
