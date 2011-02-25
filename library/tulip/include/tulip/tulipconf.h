b4d68e54a5b091a0a8540f5513d52316965bd234
Charles Huet <packadal@gmail.com>
2011-02-25 15:27:04 +0000

    commented a bit these defines
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4048 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/tulipconf.h
+++ library/tulip/include/tulip/tulipconf.h
@@ -19,18 +19,26 @@
 #ifndef TULIPCONF_H
 #define TULIPCONF_H
 
+/**
+ * @brief this file contains various helper macros and functions to have a true cross-platform compilation.
+ * 
+ */
+
 #define STRINGIFY(PARAM) STRINGIFY_INTERNAL(PARAM)
 #define STRINGIFY_INTERNAL(PARAM) #PARAM
 
+//MSVC is the only compiler we use that does not have typeof
 #if !_MSC_VER
 #  define TYPEOF typeof
 #endif
 
 #if _MSC_VER
+//MSVC 2010 has a different keyword for typeof, and deprecated
 #  if _MSC_VER >= 1600
 #    define _DEPRECATED __declspec(deprecated)
 #    define TYPEOF decltype
 #  else
+//MSVC 2008 must use Boost's typeof
 #    include "boost/typeof/typeof.hpp"
 #    define TYPEOF BOOST_TYPEOF
 #  endif
@@ -59,21 +67,27 @@
   }
 
   static double round(double d) {
-	  return floor(d + 0.5);
+    return floor(d + 0.5);
   }
 
-#  define __PRETTY_FUNCTION__ __FUNCTION__ //workaround
+#  define __PRETTY_FUNCTION__ __FUNCTION__ //MSVC has a different name for pretty_function
 #  define strcasecmp stricmp  //strcasecmp does not exists for VC, workaround
 #  define cbrt(arg) pow((double)arg, 1.0/3) //VC does not have cbrt, little workaround
 #  define isnan(x) ((x) != (x)) //you guessed it, this is a C99 feature, and VC++ does not support C99. workaroud this.
-#  define rint(arg) arg > 0 ? (int)std::floor((double)arg) : (int)std::ceil((double)arg)
+#  define rint(arg) arg > 0 ? (int)std::floor((double)arg) : (int)std::ceil((double)arg) //Hey, nother C99 feature !
+
+//for GCC 3.X
 #elif (__GNUC__ < 3)
 #  define stdext std
 #  define _DEPRECATED
 #  include <stl_hash_fun.h>
+
+//clang does not define __GNUC_MINOR__, thus having a separate clang #elif seems cleaner than adding defined() in the #else
 #elif __clang__
 #  define _DEPRECATED __attribute__ ((deprecated))
 #  define stdext __gnu_cxx
+
+//for GCC 4.X
 #else
 #    define _DEPRECATED __attribute__ ((deprecated))
 #    define stdext __gnu_cxx
@@ -84,6 +98,7 @@
 #  endif
 #endif
 
+//WIN32 specific defines: dllimport and dllexport stuff
 #ifdef _WIN32
 # 	ifdef DLL_TULIP
 #		define TLP_SCOPE         __declspec(dllexport)
