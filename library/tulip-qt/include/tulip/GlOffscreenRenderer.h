4efa1a6c74b35ba2c55424f64378623e9829d2a5
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2011-03-09 13:42:37 +0000

    - integrate commit 4218 from tulip-trunk (antialiased offscreen rendering)
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4232 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/GlOffscreenRenderer.h
+++ library/tulip-qt/include/tulip/GlOffscreenRenderer.h
@@ -25,39 +25,39 @@
 
 namespace tlp {
 
-    /**
-      * @brief Render a scene in an image or in a texture.
-      *
-      * Here is an example to render a graph in a QImage to use it as preview.
-      * @code
-      * //Get the renderer
-      * glOffscreenRenderer *glOffscreenRenderer = GlOffscreenRenderer::getInstance();
-      * //Define the viewport size. Needed to initialize the offscreen rederer.
-      * glOffscreenRenderer->setViewPortSize(200,200);
-      * //Erase old elements
-      * glOffscreenRenderer->clearScene();
-      * //Change the background color of the scene to white
-      * glOffscreenRenderer->setSceneBackgroundColor(Color(255,255,255,255));
-      * //Add
-      * //Center and render the scene.
-      * glOffscreenRenderer->renderScene(true);
-      * //Get the result
-      * QImage preview = glOffscreenRenderer->getGLTexture(true);
-      * @endcode
-      **/
+/**
+ * @brief Render a scene in an image or in a texture.
+ *
+ * Here is an example to render a graph in a QImage to use it as preview.
+ * @code
+ * //Get the renderer
+ * glOffscreenRenderer *glOffscreenRenderer = GlOffscreenRenderer::getInstance();
+ * //Define the viewport size. Needed to initialize the offscreen rederer.
+ * glOffscreenRenderer->setViewPortSize(200,200);
+ * //Erase old elements
+ * glOffscreenRenderer->clearScene();
+ * //Change the background color of the scene to white
+ * glOffscreenRenderer->setSceneBackgroundColor(Color(255,255,255,255));
+ * //Add
+ * //Center and render the scene.
+ * glOffscreenRenderer->renderScene(true);
+ * //Get the result
+ * QImage preview = glOffscreenRenderer->getGLTexture(true);
+ * @endcode
+ **/
 class TLP_QT_SCOPE GlOffscreenRenderer {
 
 public :
-        /**
-          * @brief Get the renderer instance.
-        **/
+	/**
+	 * @brief Get the renderer instance.
+	 **/
 	static GlOffscreenRenderer *getInstance();
 
 	~GlOffscreenRenderer();
 
-        /**
-          * @brief Define the viewport size.
-          **/
+	/**
+	 * @brief Define the viewport size.
+	 **/
 	void setViewPortSize(const unsigned int viewPortWidth, const unsigned int viewPortHeight);
 	unsigned int getViewportWidth();
 	unsigned int getViewportHeight();
@@ -68,37 +68,37 @@ public :
 	void setCameraCenter(const Coord &cameraCenter) {this->cameraCenter = cameraCenter;}
 
 	void setSceneBackgroundColor(const Color &color);
-        /**
-          * @brief Add an entity to the scene. The scene become the owner of the object.
-          **/
+	/**
+	 * @brief Add an entity to the scene. The scene become the owner of the object.
+	 **/
 	void addGlEntityToScene(GlSimpleEntity *entity);
-        /**
-          * @brief Add a graph composite to the scene. The scene become the owner of the object.
-          **/
+	/**
+	 * @brief Add a graph composite to the scene. The scene become the owner of the object.
+	 **/
 	void addGraphCompositeToScene(GlGraphComposite *graphComposite);
 
-        /**
-          * @brief Add a graph to the scene. Just create a new GraphComposite and call GlGraphComposite.
-          **/
-        void addGraphToScene(Graph* graph);
+	/**
+	 * @brief Add a graph to the scene. Just create a new GraphComposite and call GlGraphComposite.
+	 **/
+	void addGraphToScene(Graph* graph);
 
-        /**
-          * @brief Delete all the elements of the scene and clear it.
-          **/
+	/**
+	 * @brief Delete all the elements of the scene and clear it.
+	 **/
 	void clearScene();
 
-        /**
-          * @brief Render the scene in a buffer. You need to call this function before getting the result with getImage or getGlTexture.
-          **/
-        void renderScene(const bool centerScene = true);
+	/**
+	 * @brief Render the scene in a buffer. You need to call this function before getting the result with getImage or getGlTexture.
+	 **/
+	void renderScene(const bool centerScene = true);
 
-        /**
-          * @brief Generate a QImage from the scene. You need to call the renderScene function before this function.
-          **/
+	/**
+	 * @brief Generate a QImage from the scene. You need to call the renderScene function before this function.
+	 **/
 	QImage getImage();
-        /**
-          * @brief Generate an open gl texture from the scene. You need to call the renderScene function before this function.
-          **/
+	/**
+	 * @brief Generate an open gl texture from the scene. You need to call the renderScene function before this function.
+	 **/
 	GLuint getGLTexture(const bool generateMipMaps = false);
 
 private :
@@ -108,12 +108,13 @@ private :
 	static GlOffscreenRenderer *instance;
 
 	unsigned int lastVPWidth, lastVPHeight;
-	QGLFramebufferObject *glFrameBuf;
+	QGLFramebufferObject *glFrameBuf, *glFrameBuf2;
 	GlScene scene;
 	GlLayer *mainLayer;
 	unsigned int entitiesCpt;
 	double zoomFactor;
 	Coord cameraCenter;
+	bool antialiasedFbo;
 
 };
 
