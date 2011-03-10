c6b731083f38564f9be5ea36170f75bf3a5a98b8
Charles Huet <packadal@gmail.com>
2011-03-10 13:22:41 +0000

    removed non-existing include (was used during development)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4249 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/ThreadedComputeProperty.cpp
+++ library/tulip-qt/src/ThreadedComputeProperty.cpp
@@ -3,7 +3,6 @@
 #include <QtCore/QCoreApplication>
 
 #include <tulip/Graph.h>
-#include <tulip/SlotProgress.h>
 
 AbstractComputeProperty::AbstractComputeProperty(tlp::Graph* graph, std::string name, std::string errorMsg, tlp::PluginProgress* progress, tlp::DataSet* dataset):
   _graph(graph), _name(name), _errorMsg(errorMsg), _progress(progress), _dataset(dataset) {
