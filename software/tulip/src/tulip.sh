4f756ee5b9fe39db5e22e99026b0c0ca341b9e18
Patrick Mary <mary@labri.fr>
2011-03-11 14:30:08 +0000

    finalize mac bundle build for Tulip 3.5
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4265 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/tulip.sh
+++ software/tulip/src/tulip.sh
@@ -5,10 +5,16 @@ THISDIR=$(dirname $0)
 case "`uname -s`" in
     *darwin* | *Darwin* | *DARWIN*)
 	export DYLD_LIBRARY_PATH=${THISDIR}/../Frameworks:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/interactors:${THISDIR}/../lib/tlp/view:${DYLD_LIBRARY_PATH}
+        # check for missing link to QtCLucene lib
+	if [ ! -f $THISDIR/../Frameworks/libQtCLucene.4.dylib ]; then
+	    QT_LUCENE=`ls $THISDIR/../Frameworks/libQtCLucene*.dylib`
+	    ln -s $QT_LUCENE $THISDIR/../Frameworks/libQtCLucene.4.dylib
+	fi
 	;;
     *Linux*|*GNU/kFreeBSD*)
 	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/interactors:${THISDIR}/../lib/tlp/view:${LD_LIBRARY_PATH}
-	;;	
+	;;
+
 esac
 
 export LC_NUMERIC=C
