595fb629300ee04ac3b2b04a6e6b7f487ba828ba
Patrick Mary <mary@labri.fr>
2011-03-14 14:59:04 +0000

    linux bundle build improvements & add 32/64 bits os check
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4284 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/tulip.sh
+++ software/tulip/src/tulip.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 THISDIR=$(dirname $0)
+if [ -f $THISDIR/bin/tulip ]; then
+  THISDIR=$THISDIR/bin
+fi
 
 case "`uname -s`" in
     *darwin* | *Darwin* | *DARWIN*)
@@ -13,10 +16,23 @@ case "`uname -s`" in
 	;;
     *Linux*|*GNU/kFreeBSD*)
 	export LD_LIBRARY_PATH=${THISDIR}/../lib:${THISDIR}/../lib/tlp:${THISDIR}/../lib/tlp/interactors:${THISDIR}/../lib/tlp/view:${LD_LIBRARY_PATH}
-	# define PYTHONHOME if we are in the bundle (Qt libs in ../lib)
-	QT_LIBS=$(ls ${THISDIR}/../lib/libQt*.*)
+	# make some additional stuff if we are in the bundle (Qt libs in ../lib)
+	QT_LIBS=$(ls ${THISDIR}/../lib/libQt*.* 2>&1 >/dev/null)
 	if [ $? -eq 0 ]; then
+	  # define PYTHONHOME
 	  export PYTHONHOME=${THISDIR}/../lib
+          # check tulip_app 32/64 bits version
+	  uname -m | grep x86_64 2>&1 >/dev/null
+	  if [ $? -eq 0 ]; then
+	    EXE_TYPE="ELF 64-bit"
+          else
+            EXE_TYPE="ELF 32-bit"
+	  fi
+          file -b $THISDIR/tulip_app | grep "$EXE_TYPE" 2>&1 >/dev/null
+          if [ $? -ne 0 ]; then
+	    echo tulip "($EXE_TYPE)" cannot be executed on your system
+            exit
+          fi
 	fi
 	;;
 esac
