ce3be6925f4133e812dc1ea2d9485283d5252633
Antoine Lambert <antoine.lambert33@gmail.com>
2011-03-27 17:59:18 +0000

    - start adding exceptions in Graph and Properties methods to prevent possible crash when using the Python bindings - Properties bindings SIP files are now generated with a CMake script to avoid code duplication and ease maintenance - bindings are now compiled as dynamic Python modules : that means that the tulip Python module can now be used ouside Tulip through the classic Python interpreter
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4429 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-python/tulip/generatePropertiesBindings.cmake
@@ -0,0 +1,46 @@
+
+MACRO(GEN_PROPERTY_BINDINGS TEMPLATE_FILE 
+			    GEN_FILE 
+			    PROPERTY_TYPE
+			    VEC_PROPERTY_TYPE
+			    CPP_NODE_TYPE 
+			    PYTHON_NODE_TYPE 
+			    CPP_EDGE_TYPE 
+			    PYTHON_EDGE_TYPE
+			    PROPERTY_SPECIFIC_METHODS_FILE)
+
+  FILE(READ ${WORKING_DIR}/${TEMPLATE_FILE} input)
+  STRING(REPLACE "@PROPERTY_TYPE@" "${PROPERTY_TYPE}" input "${input}")
+  STRING(REPLACE "@PROPERTY_NODE_TYPE@" "${CPP_NODE_TYPE}" input "${input}")
+  STRING(REPLACE "@PROPERTY_EDGE_TYPE@" "${CPP_EDGE_TYPE}" input "${input}")
+  STRING(REPLACE "@PYTHON_NODE_TYPE@" "${PYTHON_NODE_TYPE}" input "${input}")
+  STRING(REPLACE "@PYTHON_EDGE_TYPE@" "${PYTHON_EDGE_TYPE}" input "${input}")
+  
+  IF(NOT "${PROPERTY_SPECIFIC_METHODS_FILE}" STREQUAL "")
+    FILE(READ ${WORKING_DIR}/${PROPERTY_SPECIFIC_METHODS_FILE} specific)
+    STRING(REPLACE "@PROPERTY_SPECIFIC_METHODS@" "${specific}" input "${input}")
+  ELSE()
+    STRING(REPLACE "@PROPERTY_SPECIFIC_METHODS@" "" input "${input}")
+  ENDIF()
+  
+  FILE(WRITE ${WORKING_DIR}/${GEN_FILE} "${input}")
+  
+  IF(NOT "${VEC_PROPERTY_TYPE}" STREQUAL "")
+    FILE(READ ${WORKING_DIR}/AbstractVectorProperty.sip.in input)
+    STRING(REPLACE "@PROPERTY_TYPE@" "${PROPERTY_TYPE}" input "${input}")
+    STRING(REPLACE "@VEC_PROPERTY_TYPE@" "${VEC_PROPERTY_TYPE}" input "${input}")
+    STRING(REPLACE "@CPP_TYPE@" "${CPP_NODE_TYPE}" input "${input}")
+    STRING(REPLACE "@PYTHON_TYPE@" "${PYTHON_NODE_TYPE}" input "${input}")
+    FILE(APPEND ${WORKING_DIR}/${GEN_FILE} "${input}")
+  ENDIF()
+
+ENDMACRO(GEN_PROPERTY_BINDINGS)
+
+GEN_PROPERTY_BINDINGS("AbstractPropertyBaseType.sip.in" "BooleanProperty.sip" "Boolean" "Boolean" "bool" "bool" "bool" "bool" "BooleanPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("AbstractPropertyBaseType.sip.in" "DoubleProperty.sip" "Double" "Double" "double" "double" "double" "double" "DoublePropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("AbstractPropertyBaseType.sip.in" "IntegerProperty.sip" "Integer" "Integer" "int" "int" "int" "int" "IntegerPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("AbstractPropertyPointerType.sip.in" "ColorProperty.sip" "Color" "Color" "tlp::Color" "tlp.Color" "tlp::Color" "tlp.Color" "")
+GEN_PROPERTY_BINDINGS("AbstractPropertyPointerType.sip.in" "LayoutProperty.sip" "Layout" "Coord" "tlp::Coord" "tlp.Coord" "std::vector<tlp::Coord>" "list of tlp.Coord" "LayoutPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("AbstractPropertyPointerType.sip.in" "SizeProperty.sip" "Size" "Size" "tlp::Size" "tlp.Size" "tlp::Size" "tlp.Size" "SizePropertySpecific.sip")		   		   
+GEN_PROPERTY_BINDINGS("AbstractPropertyPointerType.sip.in" "StringProperty.sip" "String" "String" "std::string" "string" "std::string" "string" "")
+   		   
