3209dddbf05f4e96cb334e3cfecdeec203ade237
Patrick Mary <mary@labri.fr>
2011-03-28 14:49:59 +0000

    license cleanup
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4441 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/pythonscriptview/ConsoleOutputModule.h
+++ plugins/view/pythonscriptview/ConsoleOutputModule.h
@@ -17,28 +17,6 @@
  *
  */
 
-// +-------------------------------------------------------------------------+
-// | Tulip Python View                                                       |
-// | Author:  Antoine Lambert                                                |
-// | Contact:  antoine.lambert@labri.fr                                      |
-// +-------------------------------------------------------------------------+
-// | License:                                                                |
-// |                                                                         |
-// | Tulip Python View is free software; you can redistribute it             |
-// | and/or modify  it under the terms of the GNU General Public License     |
-// | as published by the Free Software Foundation; either version 2 of the   |
-// | License, or (at your option) any later version.                         |
-// |                                                                         |
-// | Tulip Python View is distributed in the hope that it will be            |
-// | useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
-// | GNU General Public License for more details.                            |
-// |                                                                         |
-// | You should have received a copy of the GNU General Public License       |
-// | along with this program.  If not, see <http://www.gnu.org/licenses/>.   |
-// |                                                                         |
-// +-------------------------------------------------------------------------+
-
 #if defined(__GNUC__) && __GNUC__ >= 4 && ((__GNUC_MINOR__ == 2 && __GNUC_PATCHLEVEL__ >= 1) || (__GNUC_MINOR__ >= 3))
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wstrict-aliasing"
