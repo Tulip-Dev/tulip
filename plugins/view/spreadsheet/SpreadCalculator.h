a77128b82dc36319ad4af816b3714cafbc8dff34
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2011-03-28 13:24:26 +0000

    remove not needed licence text
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_5_maint@4440 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/spreadsheet/SpreadCalculator.h
+++ plugins/view/spreadsheet/SpreadCalculator.h
@@ -16,53 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-// +-------------------------------------------------------------------------+
-// | Tulip Software                                                          |
-// | Author:  Morgan Mathiaut                                                |
-// +-------------------------------------------------------------------------+
-// | License:                                                                |
-// |                                                                         |
-// | Tulip is free software; you can redistribute it and/or modify           |
-// | it under the terms of the GNU General Public License as published by    |
-// | the Free Software Foundation; either version 2 of the License, or       |
-// | (at your option) any later version.                                     |
-// |                                                                         |
-// | Tulip is distributed in the hope that it will be useful,                |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
-// | GNU General Public License for more details.                            |
-// |                                                                         |
-// | You should have received a copy of the GNU General Public License       |
-// | along with QSimpleSheet; if not, write to the Free Software             |
-// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA|
-// |                                                                         |
-// +-------------------------------------------------------------------------+
-
-// Based on
-
-// +-------------------------------------------------------------------------+
-// | QSimpleSheet simple spreadsheet                                         |
-// | Copyright (C) 2008 Shahab <shahab.sh.70@gmail.com>                      |
-// +-------------------------------------------------------------------------+
-// | License:                                                                |
-// |                                                                         |
-// | QSimpleSheet is free software; you can redistribute it and/or modify    |
-// | it under the terms of the GNU General Public License as published by    |
-// | the Free Software Foundation; either version 2 of the License, or       |
-// | (at your option) any later version.                                     |
-// |                                                                         |
-// | QSimpleSheet is distributed in the hope that it will be useful,         |
-// | but WITHOUT ANY WARRANTY; without even the implied warranty of          |
-// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the            |
-// | GNU General Public License for more details.                            |
-// |                                                                         |
-// | You should have received a copy of the GNU General Public License       |
-// | along with QSimpleSheet; if not, write to the Free Software             |
-// | Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA|
-// |                                                                         |
-// +-------------------------------------------------------------------------+
-// | Author:  Shahab Shirazi <shahab.sh.70@gmail.com>                        |
-// +-------------------------------------------------------------------------+
 
 #ifndef Tulip_SPREADCALCULATOR_H
 #define Tulip_SPREADCALCULATOR_H
