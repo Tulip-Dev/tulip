3f2b4a9a08be5219c6f9c9bf75ebfbbb288af92c
Jonathan Dubois <jonathan.dubois@labri.fr>
2011-04-06 08:34:42 +0000

    Add missing return in eventFilter.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4540 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/SmallMultiplesNavigatorComponent.cpp
+++ library/tulip-qt/src/SmallMultiplesNavigatorComponent.cpp
@@ -31,44 +31,47 @@ using namespace std;
 
 namespace tlp {
 
-SmallMultiplesNavigatorComponent::SmallMultiplesNavigatorComponent(): _navigator(new MouseNKeysNavigator) {
-}
-
-SmallMultiplesNavigatorComponent::~SmallMultiplesNavigatorComponent() {
-  delete _navigator;
-}
-
-bool SmallMultiplesNavigatorComponent::draw(GlMainWidget *) {
-  return false;
-}
-
-bool SmallMultiplesNavigatorComponent::eventFilter(QObject *o, QEvent *e) {
-
-  SmallMultiplesView *smView = dynamic_cast<SmallMultiplesView *>(view);
-  if (!view)
-    return false;
-
-  if (e->type() == QEvent::MouseButtonDblClick) {
-    if (smView->isOverview()) {
-      QMouseEvent *mouseEv = static_cast<QMouseEvent *>(e);
-      GlScene *scene = smView->overview()->getScene();
-      vector<ulong> entities;
-      scene->selectEntities(RenderingNodes, mouseEv->x(), mouseEv->y(), 1, 1, smView->overviewLayer(), entities);
-      if (entities.size() > 0) {
-        int id = smView->nodeItemId(node(entities[0]));
-        if (id != -1)
-          smView->selectItem(id);
-      }
+    SmallMultiplesNavigatorComponent::SmallMultiplesNavigatorComponent(): _navigator(new MouseNKeysNavigator) {
     }
-    else
-      smView->overviewSelected();
-    return true;
-  }
-  else {
-    GlMainWidget *w = dynamic_cast<GlMainWidget *>(smView->overview());
-    if (w) return _navigator->eventFilter(o,e);
-  }
-  return false;
-}
+
+    SmallMultiplesNavigatorComponent::~SmallMultiplesNavigatorComponent() {
+        delete _navigator;
+    }
+
+    bool SmallMultiplesNavigatorComponent::draw(GlMainWidget *) {
+        return false;
+    }
+
+    bool SmallMultiplesNavigatorComponent::eventFilter(QObject *o, QEvent *e) {
+
+        SmallMultiplesView *smView = dynamic_cast<SmallMultiplesView *>(view);
+        if (!view)
+            return false;
+
+        if (e->type() == QEvent::MouseButtonDblClick) {
+            if (smView->isOverview()) {
+                QMouseEvent *mouseEv = static_cast<QMouseEvent *>(e);
+                GlScene *scene = smView->overview()->getScene();
+                vector<ulong> entities;
+                scene->selectEntities(RenderingNodes, mouseEv->x(), mouseEv->y(), 1, 1, smView->overviewLayer(), entities);
+                if (entities.size() > 0) {
+                    int id = smView->nodeItemId(node(entities[0]));
+                    if (id != -1) {
+                        smView->selectItem(id);
+                        return true;
+                    }
+                }
+            }
+            else {
+                smView->overviewSelected();
+                return true;
+            }
+        }
+        else {
+            GlMainWidget *w = dynamic_cast<GlMainWidget *>(smView->overview());
+            if (w) return _navigator->eventFilter(o,e);
+        }
+        return false;
+    }
 
 }
