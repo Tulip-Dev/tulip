7b5bf6fc4f4b859225b2046393d0151949007def
Charles Huet <packadal@gmail.com>
2011-04-07 14:15:35 +0000

    template instantiation now follows the new abstractVectorProperty in using tlp::*Type
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4560 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip/include/tulip/PropertyTypes.h
+++ library/tulip/include/tulip/PropertyTypes.h
@@ -286,13 +286,13 @@ public:
 
 extern TLP_SCOPE void initTypeSerializers();
 #ifdef _MSC_VER
-	template class AbstractVectorProperty<DoubleVectorType, double>;
-	template class AbstractVectorProperty<IntegerVectorType, int>;
-	template class AbstractVectorProperty<BooleanVectorType, bool>;
-	template class AbstractVectorProperty<StringVectorType, std::string>;
-	template class AbstractVectorProperty<ColorVectorType, tlp::Color>;
-	template class AbstractVectorProperty<SizeVectorType, tlp::Size>;
-	template class AbstractVectorProperty<CoordVectorType, tlp::Coord>;
+	template class AbstractVectorProperty<DoubleVectorType, tlp::DoubleType>;
+	template class AbstractVectorProperty<IntegerVectorType, tlp::IntegerType>;
+	template class AbstractVectorProperty<BooleanVectorType, tlp::BooleanType>;
+	template class AbstractVectorProperty<StringVectorType, tlp::StringType>;
+	template class AbstractVectorProperty<ColorVectorType, tlp::ColorType>;
+	template class AbstractVectorProperty<SizeVectorType, tlp::SizeType>;
+	template class AbstractVectorProperty<CoordVectorType, tlp::PointType>;
 #endif
 }
 
