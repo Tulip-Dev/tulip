7f4b13b258bc48138e16fa9ceaf4c944078fc38e
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-05-03 13:23:19 +0000

    Added recent documents wrapper to TulipSettings
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4716 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/TulipSettings.cpp
+++ library/tulip-qt/src/TulipSettings.cpp
@@ -4,3 +4,21 @@ TulipSettings *TulipSettings::_instance = 0;
 
 TulipSettings::TulipSettings(): QSettings("TulipSoftware","Tulip") {
 }
+
+QList<QString> TulipSettings::recentDocuments() const {
+  QList<QVariant> recentDocumentsValue = value("app/recent_documents").toList();
+  QList<QString> result;
+  for (QList<QVariant>::iterator it = recentDocumentsValue.begin(); it != recentDocumentsValue.end(); ++it)
+    result.append(it->toString());
+  return result;
+}
+
+void TulipSettings::addToRecentDocuments(const QString &name) {
+  QList<QVariant> recentDocumentsValue = value("app/recent_documents").toList();
+  if (recentDocumentsValue.contains(name))
+    recentDocumentsValue.removeAll(name);
+  recentDocumentsValue.push_front(name);
+  while (recentDocumentsValue.size() > 5)
+    recentDocumentsValue.pop_back();
+  setValue("app/recent_documents",recentDocumentsValue);
+}
