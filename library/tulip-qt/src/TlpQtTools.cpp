cae461f9cbdccdb0e047b7a05831a0fe260af39d
Charles Huet <packadal@gmail.com>
2011-05-06 14:13:42 +0000

    removed unused function
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4772 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/TlpQtTools.cpp
+++ library/tulip-qt/src/TlpQtTools.cpp
@@ -31,35 +31,15 @@
 #include "tulip/View.h"
 #include "tulip/Controller.h"
 
-
 /**
  * For openDataSetDialog function : see OpenDataSet.cpp
  */
-
 using namespace std;
-
 
 namespace tlp {
 
   void openGraphOnGlMainWidget(Graph *graph,DataSet *dataSet,GlMainWidget *glMainWidget){
     glMainWidget->setData(graph,*dataSet);
-  }
-
-  void loadInteractorPluginsFromDir(std::string dir,PluginLoader *loader) {
-    InteractorFactory::initFactory();
-    loadPluginsFromDir(dir, "Interactor", loader);
-  }
-  void loadViewPluginsFromDir(std::string dir,PluginLoader *loader) {
-    ViewFactory::initFactory();
-    loadPluginsFromDir(dir, "View", loader);
-  }
-  void loadControllerPluginsFromDir(std::string dir,PluginLoader *loader) {
-    ControllerFactory::initFactory();
-    loadPluginsFromDir(dir, "Controller", loader);
-  }
-  bool canUseQGLPixelBuffer() {
-    QGLPixelBuffer glFrameBuf(2,2);
-    return glFrameBuf.isValid();
   }
 
   bool getColorDialog(const QColor &color,QWidget *parent,const QString &title,QColor &result){
