5c46bb8c0b0931229243a22245b2def0e6d71cf6
Antoine Lambert <antoine.lambert33@gmail.com>
2011-05-09 08:49:07 +0000

    - add first bindings for libtulip-qt but don't enable their compilation by default
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4792 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-python/tulip-qt/PyQt4Config.cmake
@@ -0,0 +1,51 @@
+# Find PyQt4
+# ~~~~~~~~~~
+# Copyright (c) 2007-2008, Simon Edwards <simon@simonzone.com>
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+#
+# PyQt4 website: http://www.riverbankcomputing.co.uk/pyqt/index.php
+#
+# Find the installed version of PyQt4. FindPyQt4 should only be called after
+# Python has been found.
+#
+# This file defines the following variables:
+#
+# PYQT4_VERSION - The version of PyQt4 found expressed as a 6 digit hex number
+#     suitable for comparision as a string
+#
+# PYQT4_VERSION_STR - The version of PyQt4 as a human readable string.
+#
+# PYQT4_VERSION_TAG - The PyQt version tag using by PyQt's sip files.
+#
+# PYQT4_SIP_DIR - The directory holding the PyQt4 .sip files.
+#
+# PYQT4_SIP_FLAGS - The SIP flags used to build PyQt.
+#
+# PYQT4_SIP_VER - The SIP version used to compile PyQt
+
+IF(EXISTS PYQT4_VERSION)
+  # Already in cache, be silent
+  SET(PYQT4_FOUND TRUE)
+ELSE(EXISTS PYQT4_VERSION)
+
+  FIND_FILE(_find_pyqt_py FindPyQt.py PATHS ${CMAKE_CURRENT_SOURCE_DIR})
+
+  EXECUTE_PROCESS(COMMAND ${PYTHON_EXECUTABLE} ${_find_pyqt_py} OUTPUT_VARIABLE pyqt_config)
+  
+  IF(NOT ${pyqt_config} MATCHES "Traceback")
+    STRING(REGEX REPLACE "^pyqt_version:([^\n]+).*$" "\\1" PYQT4_VERSION ${pyqt_config})
+    STRING(REGEX REPLACE ".*\npyqt_version_str:([^\n]+).*$" "\\1" PYQT4_VERSION_STR ${pyqt_config})
+    STRING(REGEX REPLACE ".*\npyqt_version_tag:([^\n]+).*$" "\\1" PYQT4_VERSION_TAG ${pyqt_config})
+    STRING(REGEX REPLACE ".*\npyqt_sip_dir:([^\n]+).*$" "\\1" PYQT4_SIP_DIR ${pyqt_config})
+    STRING(REGEX REPLACE ".*\npyqt_sip_flags:([^\n]+).*$" "\\1" PYQT4_SIP_FLAGS ${pyqt_config})
+    STRING(REGEX REPLACE ".*\npyqt_sip_version:([^\n]+).*$" "\\1" PYQT4_SIP_VER ${pyqt_config})
+    SET(PYQT4_FOUND TRUE)
+  ENDIF()
+
+  INCLUDE(FindPackageHandleStandardArgs)
+  FIND_PACKAGE_HANDLE_STANDARD_ARGS(PyQt4 DEFAULT_MSG PYQT4_VERSION)
+
+  MARK_AS_ADVANCED(_find_pyqt_py)
+
+ENDIF(EXISTS PYQT4_VERSION)
