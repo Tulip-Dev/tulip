c37a7b6a9ac1d55b0906a9e60a1373d029b03d77
Jonathan Dubois <jonathan.dubois@labri.fr>
2011-05-17 16:38:00 +0000

    Refactor
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_6_maint@4899 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/spreadsheet/GlyphPreviewGenerator.cpp
+++ plugins/view/spreadsheet/GlyphPreviewGenerator.cpp
@@ -7,8 +7,17 @@ using namespace std;
 
 GlyphPreviewGenerator* GlyphPreviewGenerator::_instance=NULL;
 
-GlyphPreviewGenerator::GlyphPreviewGenerator()
+GlyphPreviewGenerator::GlyphPreviewGenerator():_graph(newGraph()),_node(_graph->addNode())
 {
+    //Init graph parameters.
+    GlGraphRenderingParameters parameters;
+    GlGraphInputData inputData(_graph,&parameters);
+    inputData.elementSize->setAllNodeValue(Size(1,1,1));
+
+}
+
+GlyphPreviewGenerator::~GlyphPreviewGenerator(){
+delete _graph;
 }
 
 GlyphPreviewGenerator& GlyphPreviewGenerator::getInst(){
@@ -20,28 +29,45 @@ GlyphPreviewGenerator& GlyphPreviewGenerator::getInst(){
 
 QPixmap GlyphPreviewGenerator::getPreview(unsigned int pluginId){
     if(_previews.find(pluginId) == _previews.end()){
-        Graph* graph = newGraph();
-        GlGraphRenderingParameters parameters;
-        GlGraphInputData inputData(graph,&parameters);
-        inputData.elementSize->setAllNodeValue(Size(1,1,1));
-        inputData.elementShape->setAllNodeValue(pluginId);
-        node n = graph->addNode();
+        _graph->getProperty<IntegerProperty>("viewShape")->setNodeValue(_node,pluginId);
         GlOffscreenRenderer *renderer = GlOffscreenRenderer::getInstance();
         renderer->setViewPortSize(16,16);
         renderer->clearScene();
-        renderer->addGraphToScene(graph);
+        renderer->addGraphToScene(_graph);
         renderer->renderScene(true);
         QImage preview = renderer->getImage();
         _previews[pluginId] = QPixmap::fromImage(preview);
-        delete graph;
     }
     return _previews[pluginId];
 }
 
 EdgeExtremityGlyphPreviewGenerator* EdgeExtremityGlyphPreviewGenerator::_instance = NULL;
-EdgeExtremityGlyphPreviewGenerator::EdgeExtremityGlyphPreviewGenerator(){
+EdgeExtremityGlyphPreviewGenerator::EdgeExtremityGlyphPreviewGenerator():_graph(newGraph()){
     //No edge extremity pixmap
     _previews[EdgeExtremityGlyphManager::NoEdgeExtremetiesId] = QPixmap();
+
+    //Init graph.
+    GlGraphRenderingParameters parameters;
+    GlGraphInputData inputData(_graph,&parameters);
+    inputData.elementSize->setAllNodeValue(Size(0.01,0.2,0.1));
+    inputData.elementSize->setAllEdgeValue(Size(0.125,0.125,0.125));
+    inputData.elementColor->setAllNodeValue(Color(255,255,255,0));
+    inputData.elementBorderColor->setAllNodeValue(Color(255,255,255,0));
+    node n1 = _graph->addNode();
+    node n2= _graph->addNode();
+    _edge = _graph->addEdge(n1,n2);
+    inputData.elementLayout->setNodeValue(n1,Coord(0,0,0));
+    inputData.elementLayout->setNodeValue(n2,Coord(0.3,0,0));
+    vector<Coord> bends;
+    bends.push_back(Coord(0.01,0,0));
+    inputData.elementLayout->setAllEdgeValue(bends);
+
+    inputData.elementSrcAnchorShape->setAllEdgeValue(EdgeExtremityGlyphManager::NoEdgeExtremetiesId);
+    inputData.elementTgtAnchorSize->setAllEdgeValue(Size(2,2,1));
+}
+
+EdgeExtremityGlyphPreviewGenerator::~EdgeExtremityGlyphPreviewGenerator(){
+    delete _graph;
 }
 
 EdgeExtremityGlyphPreviewGenerator & EdgeExtremityGlyphPreviewGenerator::getInst(){
@@ -53,30 +79,11 @@ EdgeExtremityGlyphPreviewGenerator & EdgeExtremityGlyphPreviewGenerator::getInst
 
 QPixmap EdgeExtremityGlyphPreviewGenerator::getPreview(unsigned int pluginId){
     if(_previews.find(pluginId) == _previews.end()){
-        Graph* graph = newGraph();
-        GlGraphRenderingParameters parameters;
-        GlGraphInputData inputData(graph,&parameters);
-        inputData.elementSize->setAllNodeValue(Size(0.01,0.2,0.1));
-        inputData.elementSize->setAllEdgeValue(Size(0.125,0.125,0.125));
-        inputData.elementColor->setAllNodeValue(Color(255,255,255,0));
-        inputData.elementBorderColor->setAllNodeValue(Color(255,255,255,0));
-        node n1 = graph->addNode();
-        node n2= graph->addNode();
-        edge e = graph->addEdge(n1,n2);
-        inputData.elementLayout->setNodeValue(n1,Coord(0,0,0));
-        inputData.elementLayout->setNodeValue(n2,Coord(0.3,0,0));
-        vector<Coord> bends;
-        bends.push_back(Coord(0.01,0,0));
-        inputData.elementLayout->setAllEdgeValue(bends);
-
-        inputData.elementSrcAnchorShape->setAllEdgeValue(EdgeExtremityGlyphManager::NoEdgeExtremetiesId);
-        inputData.elementTgtAnchorSize->setAllEdgeValue(Size(2,2,1));
-        inputData.elementTgtAnchorShape->setAllEdgeValue(pluginId);
-
+        _graph->getProperty<IntegerProperty>("viewTgtAnchorShape")->setEdgeValue(_edge,pluginId);
         GlOffscreenRenderer *renderer = GlOffscreenRenderer::getInstance();
         renderer->setViewPortSize(16,16);
         renderer->clearScene();
-        renderer->addGraphToScene(graph);
+        renderer->addGraphToScene(_graph);
         GlGraphRenderingParameters renderingParamerters = renderer->getScene()->getGlGraphComposite()->getRenderingParameters();
         renderingParamerters.setEdgeColorInterpolate(false);
         renderingParamerters.setEdgeSizeInterpolate(false);
@@ -85,7 +92,6 @@ QPixmap EdgeExtremityGlyphPreviewGenerator::getPreview(unsigned int pluginId){
         renderer->renderScene(true);
         QImage preview = renderer->getImage();
         _previews[pluginId] = QPixmap::fromImage(preview);
-        delete graph;
     }
     return _previews[pluginId];
 }
--- plugins/view/spreadsheet/GlyphPreviewGenerator.h
+++ plugins/view/spreadsheet/GlyphPreviewGenerator.h
@@ -2,28 +2,55 @@
 #define GLYPHPREVIEWGENERATOR_H
 #include <map>
 #include <QtGui/QPixmap>
+#include <tulip/Node.h>
+#include <tulip/Edge.h>
+
+namespace tlp{
+    class Graph;
+}
+
+/**
+  * @brief Handle previews for Glyphs plugins.
+  **/
 class GlyphPreviewGenerator
 {
 public:
     static GlyphPreviewGenerator & getInst();
+    ~GlyphPreviewGenerator();
+    /**
+      * @brief Get the preview for the glyph with the given Id.
+      */
     QPixmap getPreview(unsigned int pluginId);
 
 private:
     GlyphPreviewGenerator();
     static GlyphPreviewGenerator* _instance;
     std::map<unsigned int,QPixmap> _previews;
+    tlp::Graph* _graph;
+    tlp::node _node;
 };
 
+/**
+  * @brief Handle previews for Edge extremities glyphs plugins.
+  **/
 class EdgeExtremityGlyphPreviewGenerator
 {
 public:
+
+    ~EdgeExtremityGlyphPreviewGenerator();
     static EdgeExtremityGlyphPreviewGenerator & getInst();
+    /**
+      * @brief Get the preview for the edge extremity glyph with the given Id.
+      */
     QPixmap getPreview(unsigned int pluginId);
 
 private:
     EdgeExtremityGlyphPreviewGenerator();
     static EdgeExtremityGlyphPreviewGenerator* _instance;
     std::map<unsigned int,QPixmap> _previews;
+    tlp::Graph* _graph;
+    tlp::edge _edge;
+
 };
 
 #endif // GLYPHPREVIEWGENERATOR_H
