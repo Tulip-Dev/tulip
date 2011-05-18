6f35aad137bce2e79b208be25f9dee485fbce907
Charles Huet <packadal@gmail.com>
2011-05-18 07:39:42 +0000

    LocalPluginInformation now should find icons and doc
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4916 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/PluginInformations.cpp
+++ library/tulip-qt/src/PluginInformations.cpp
@@ -1,13 +1,36 @@
 #include <tulip/PluginInformations.h>
 
 #include <QtCore/QFileInfo>
+#include <QDir>
 
 using namespace tlp;
 
+const QString LocalPluginInformations::DefaultIconPath = QString::fromStdString(tlp::TulipBitmapDir) + "logo32x32.bmp";
+const QString LocalPluginInformations::DefaultLongDescriptionPath = QString::fromStdString(tlp::TulipLibDir) + "../doc/default.html";
+
 LocalPluginInformations::LocalPluginInformations(const tlp::AbstractPluginInfo* info, const std::string& type, const std::list< Dependency >& dependencies, const std::string& library)
-  :_infos(info), _type(type.c_str()), _dependencies(dependencies) {
+  :_infos(info), _type(type.c_str()), _libraryPath(library.c_str()), _dependencies(dependencies) {
     for(std::list<tlp::Dependency>::const_iterator it = _dependencies.begin(); it != _dependencies.end(); ++it) {
       _dependenciesNames.push_back(it->pluginName.c_str());
+    }
+    QFileInfo libraryInfo(library.c_str());
+    _installDate = libraryInfo.lastModified();
+    
+    QDir libraryDir(libraryInfo.canonicalPath());
+    libraryDir.cdUp();
+    libraryDir.cd("share/doc");
+    libraryDir.cd(_infos->getName().c_str());
+
+    _longDescriptionPath = libraryDir.canonicalPath() + "index.html";
+    if(!QFile::exists(_longDescriptionPath)) {
+      _longDescriptionPath = LocalPluginInformations::DefaultLongDescriptionPath;
+    }
+
+    libraryDir.cdUp();
+    libraryDir.cd("icons");
+    _iconPath = libraryDir.canonicalPath() + _infos->getName().c_str() + ".png";
+    if(!QFile::exists(_iconPath)) {
+      _iconPath = LocalPluginInformations::DefaultIconPath;
     }
 }
 
@@ -24,15 +47,15 @@ QString LocalPluginInformations::shortDescription() const{
 }
 
 QString LocalPluginInformations::longDescriptionPath() const{
-  return "index.html";
+  return _longDescriptionPath;
 }
 
 QString LocalPluginInformations::iconPath() const{
-  return ":/logo32x32.png";
+  return _iconPath;
 }
 
 QDateTime LocalPluginInformations::installDate() const{
-  return QDateTime::currentDateTime();
+  return _installDate;
 }
 
 QString LocalPluginInformations::type() const{
