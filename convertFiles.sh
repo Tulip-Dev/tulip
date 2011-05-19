72f10c45b2e851811e47de45b148e905ca512120
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-05-19 16:21:11 +0000

    Renamed libraries: libtulip to libtulip-core libtulip-qt to libtulip-gui libtulippython to libtulip-core-python libtulipoglpython to libtulip-ogl-python libtulipqtpython to libtulip-gui-python
    Added libtulip-ogdf headers into installation
    
    Fixed CPack component dependencies
    
    Removed doc and associated scripts
    
    Removed Tulip2To3 documentation
    
    Fixed FindTULIP3 and renamed it to FindTULIP (with version check)
    
    Fixed tulip-config.sh.in
    
    Fixed CPack external libraries detection
    
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4938 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- convertFiles.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "Converting Files to Unix format"
-for CMD in `find . -name "*.*"`
-do
-  `dos2unix $CMD`
-done
--- docs/doxygen/gen-allPlugins.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-HTML_PAGE=allPlugins.html
-cat header-allPlugins.html > $HTML_PAGE
-cat allPluginsBody.html >> $HTML_PAGE
-echo "</BODY>" >> $HTML_PAGE
