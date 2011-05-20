4224c0456ab93af1856cdcb8a029ddd7e8bfc5fc
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-05-20 09:59:39 +0000

    Pre version of file system import plugin using Qt
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@4947 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/import/FileSystem.cpp
+++ plugins/import/FileSystem.cpp
@@ -16,20 +16,13 @@
  * See the GNU General Public License for more details.
  *
  */
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <errno.h>
-#include <sstream>
 #include <tulip/TulipPlugin.h>
 
-#ifdef _WIN32
-#include <windows.h>
-#else
-#include <dirent.h>
-#endif
-
-using namespace std;
-using namespace tlp;
+#include <QtCore/QStack>
+#include <QtCore/QPair>
+#include <QtCore/QDir>
+#include <QtCore/QFileInfo>
+#include <QtCore/QDateTime>
 
 namespace {
   static const char * paramHelp[] = {
@@ -37,7 +30,7 @@ namespace {
     HTML_HELP_OPEN()				    \
     HTML_HELP_DEF( "type", "directory pathname" )		    \
     HTML_HELP_BODY()						      \
-    "This parameter indicates the directory to import."	      \
+    "The root directory of the filesystem part to import."	      \
     HTML_HELP_CLOSE(),
   };
 }
@@ -48,191 +41,84 @@ namespace {
 /** This plugin enables to capture in a tree the full hierarchy of
  *  of a given file system directory
  */
-class FileSystem:public ImportModule {
+class FileSystem:public tlp::ImportModule {
 public:
-  FileSystem(AlgorithmContext context):ImportModule(context) {
-    addParameter<string>("dir::directory", paramHelp[0]);
+  FileSystem(tlp::AlgorithmContext context):ImportModule(context) {
+    addParameter<std::string>("dir::directory", paramHelp[0]);
   }
   ~FileSystem(){}
 
-  DoubleProperty *size,*gid,*uid,*lastaccess,*lastmodif,*lastchange;
-  StringProperty *label;
-  StringProperty *url,*path;
-
-  LayoutProperty *layout;
-  int progress;
-
-
-  ProgressState readDir(node n, string directory, unsigned int &x, unsigned int y) {
-    if (pluginProgress->progress(progress,100)!=TLP_CONTINUE) { 
-      return pluginProgress->state();
-    }
-    progress++;
-    progress = progress%100;
-    #ifndef _WIN32
-    DIR *dir=opendir(directory.c_str());
-    if (dir == 0) {
-      pluginProgress->stop();
-      return pluginProgress->state();
-    }
-    dirent *entry;
-    #else
-    HANDLE hFind; 
-    WIN32_FIND_DATA FindData; 
-    SetCurrentDirectory (directory.c_str()); 
-    hFind=FindFirstFile ("*.*", &FindData); 
-    bool endOfDirectory = (hFind == NULL) || (hFind == INVALID_HANDLE_VALUE);
-    if (endOfDirectory) {
-      pluginProgress->stop();
-      return pluginProgress->state();
-    }
-    #endif
-    struct stat infoEntry;
-    #ifdef _WIN32
-     while (!endOfDirectory) {
-       if (!strcmp("..",FindData.cFileName) || !strcmp(".",FindData.cFileName)) {
-   	 endOfDirectory = !(FindNextFile (hFind, &FindData));
-   	 continue;
-       }
-       string entryName=FindData.cFileName;
-       string pathEntry=directory+entryName;
-       stat(pathEntry.c_str(),&infoEntry);
-     #else
-    while ((entry = readdir(dir))!=0) {
-      if (!strcmp("..",entry->d_name) || !strcmp(".",entry->d_name)) continue;
-      string entryName=entry->d_name;
-      string pathEntry=directory+entryName;
-      lstat(pathEntry.c_str(),&infoEntry);
-    #endif
-      if (infoEntry.st_dev==true) continue;
-      node newNode=graph->addNode();
-      graph->addEdge(n,newNode);
-      label->setNodeValue(newNode,entryName);
-      stringstream strTmp;
-      strTmp << "file://";
-      strTmp << pathEntry;
-      url->setNodeValue(newNode, strTmp.str());
-      path->setNodeValue(newNode,pathEntry);
-
-      if (infoEntry.st_size<1)
-	size->setNodeValue(newNode,1);
-      else
-	size->setNodeValue(newNode,infoEntry.st_size);
-      uid->setNodeValue(newNode,infoEntry.st_uid);
-      gid->setNodeValue(newNode,infoEntry.st_gid);
-      lastaccess->setNodeValue(newNode,infoEntry.st_atime);
-      lastmodif->setNodeValue(newNode,infoEntry.st_mtime);
-      lastchange->setNodeValue(newNode,infoEntry.st_ctime);
-      
-      if ((infoEntry.st_mode & S_IFMT) == S_IFDIR) {
-	x += 2;
-	if (readDir(newNode,pathEntry+"/", x , x ) == TLP_CANCEL)
-	  graph->delNode(newNode);
-	else {
-	  double newSize=0;
-	  Coord tmp(0,0,0);
-	  Iterator<node> *itN=graph->getOutNodes(newNode);
-	  for (;itN->hasNext();) {
-	    node itn=itN->next();
-	    newSize+=size->getNodeValue(itn);
-	    tmp += layout->getNodeValue(itn);
-	  }
-	  delete itN;
-	  size->setNodeValue(newNode,newSize/1024.0);
-	  if (graph->outdeg(newNode) == 0) {
-	    layout->setNodeValue(newNode, Coord(x, y, 0));
-	    x += 2;
-	  } else {
-	    tmp[0] /= graph->outdeg(newNode);
-	    tmp[1] = y;
-	    layout->setNodeValue(newNode, tmp);
-	  }
-	}
-      } else {
-	layout->setNodeValue(newNode, Coord(x, y, 0));
-	x += 2;
-      }
-       #ifdef _WIN32
-      endOfDirectory = !(FindNextFile (hFind, &FindData));
-      #endif 
-    } 
-    #ifdef _WIN32
-    FindClose (hFind); 
-    #else
-    closedir(dir);
-    #endif
-    return TLP_CONTINUE;
-  }
-
-  bool import(const string &) {
-    size=graph->getProperty<DoubleProperty>("size");
-    uid=graph->getProperty<DoubleProperty>("uid");
-    gid=graph->getProperty<DoubleProperty>("gid");
-    lastaccess=graph->getProperty<DoubleProperty>("lastaccess");
-    lastmodif=graph->getProperty<DoubleProperty>("lastmodif");
-    lastchange=graph->getProperty<DoubleProperty>("lastchange");
-    layout=graph->getProperty<LayoutProperty>("viewLayout");
-    label=graph->getProperty<StringProperty>("name");
-    url=graph->getProperty<StringProperty>("url");
-    path=graph->getProperty<StringProperty>("path");
-    layout->setAllNodeValue(Coord(0,0,0));
-    node newNode=graph->addNode();
-    string dirName;
-    if (!dataSet->get("dir::directory", dirName) || dirName.empty()) {
-      pluginProgress->setError("No directory");
+  bool import(const std::string &) {
+    if (dataSet == 0)
       return false;
-    }
-
-    struct stat infoEntry;
-    int result;
-    #ifdef _WIN32
-    result = stat(dirName.c_str(),&infoEntry);
-    #else
-    result = lstat(dirName.c_str(),&infoEntry);
-    #endif
-    if (result == -1) {
-      pluginProgress->setError(strerror(errno));
+    std::string rootPathStr;
+    dataSet->get("dir::directory",rootPathStr);
+    QFileInfo rootInfo(rootPathStr.c_str());
+    if (!rootInfo.exists())
       return false;
-    }
-    if (infoEntry.st_dev!=true)  {
-      label->setNodeValue(newNode,dirName.c_str());
-      if (infoEntry.st_size<1)
-	size->setNodeValue(newNode,1);
-      else
-	size->setNodeValue(newNode,infoEntry.st_size);
-      uid->setNodeValue(newNode,infoEntry.st_uid);
-      gid->setNodeValue(newNode,infoEntry.st_gid);
-      lastaccess->setNodeValue(newNode,infoEntry.st_atime);
-      lastmodif->setNodeValue(newNode,infoEntry.st_mtime);
-      lastchange->setNodeValue(newNode,infoEntry.st_ctime);
-    }
+    tlp::node rootNode = addFileNode(rootInfo,graph);
+    if (!rootInfo.isDir())
+      return true;
 
-    pluginProgress->showPreview(false);
+    _absolutePaths = graph->getProperty<tlp::StringProperty>("Absolute paths");
+    _baseNames = graph->getProperty<tlp::StringProperty>("Base name");
+    _createdDates = graph->getProperty<tlp::StringProperty>("Creation date");
+    _fileNames = graph->getProperty<tlp::StringProperty>("File name");
+    _isExecutable = graph->getProperty<tlp::BooleanProperty>("Is executable");
+    _isReadable = graph->getProperty<tlp::BooleanProperty>("Is readable");
+    _isSymlink = graph->getProperty<tlp::BooleanProperty>("Is symbolic link");
+    _isWritable = graph->getProperty<tlp::BooleanProperty>("Is writable");
+    _lastModifiedDates = graph->getProperty<tlp::StringProperty>("Last modification date");
+    _lastReadDates = graph->getProperty<tlp::StringProperty>("Last read date");
+    _owners = graph->getProperty<tlp::StringProperty>("Owner");
+    _permissions = graph->getProperty<tlp::IntegerProperty>("Permission ID");
+    _suffixes = graph->getProperty<tlp::StringProperty>("Suffix");
 
-    unsigned int x = 0, y = 2;
-    readDir(newNode,string(dirName.c_str())+"/", x , y);
-    double newSize=0;
-    Coord tmp(0,0,0);
-    if (pluginProgress->state()!=TLP_CANCEL) {
-      Iterator<node> *itN=graph->getOutNodes(newNode);
-      while (itN->hasNext()) {
-	node itn=itN->next();
-	newSize+=size->getNodeValue(itn);
-	tmp += layout->getNodeValue(itn);
-      } delete itN;
-      size->setNodeValue(newNode,newSize);
-      tmp /= graph->outdeg(newNode);
-      tmp[1] = 0;
-      layout->setNodeValue(newNode, tmp);
-      node itn;
-      forEach(itn, graph->getNodes()) {
-	tmp = layout->getNodeValue(itn);
-	tmp[1] = -tmp[1];
-	layout->setNodeValue(itn, tmp);
+    QStack<QPair<QString,tlp::node> > fsStack;
+    fsStack.push(QPair<QString,tlp::node>(rootInfo.absoluteFilePath(),rootNode));
+    while (!fsStack.empty()) {
+      QPair<QString,tlp::node> elem = fsStack.pop();
+      QDir currentDir(QDir(elem.first));
+      tlp::node parentNode(elem.second);
+      QFileInfoList entries(currentDir.entryInfoList(QDir::NoDotAndDotDot | QDir::System | QDir::Hidden  | QDir::AllDirs | QDir::Files, QDir::DirsFirst));
+      for (QFileInfoList::iterator it = entries.begin(); it != entries.end(); ++it) {
+        QFileInfo fileInfos(*it);
+        tlp::node fileNode=addFileNode(fileInfos, graph);
+        graph->addEdge(parentNode,fileNode);
+        if (fileInfos.isDir())
+          fsStack.push_back(QPair<QString,tlp::node>(fileInfos.absoluteFilePath(),fileNode));
       }
     }
-    return pluginProgress->state()!=TLP_CANCEL;
+    return true;
   }
+
+private:
+  tlp::node addFileNode(const QFileInfo &infos, tlp::Graph *g) {
+    tlp::node result = g->addNode();
+    _absolutePaths->setNodeValue(result,infos.absoluteFilePath().toStdString());
+    _baseNames->setNodeValue(n,infos.baseName().toStdString());
+    _createdDates->setNodeValue(n,infos.created().toString().toStdString());
+    _fileNames->setNodeValue(n,infos.fileName().toStdString());
+    _isExecutable->setNodeValue(n,infos.isExecutable());
+    _isReadable->setNodeValue(n,infos.isReadable());
+    _isSymlink->setNodeValue(n,infos.isSymLink());
+    _isWritable->setNodeValue(n,infos.isWritable());
+    _lastModifiedDates->setNodeValue(n,infos.lastModified().toString());
+  }
+
+  tlp::StringProperty *_absolutePaths;
+  tlp::StringProperty *_baseNames;
+  tlp::StringProperty *_createdDates;
+  tlp::StringProperty *_fileNames;
+  tlp::BooleanProperty *_isExecutable;
+  tlp::BooleanProperty *_isReadable;
+  tlp::BooleanProperty *_isSymlink;
+  tlp::BooleanProperty *_isWritable;
+  tlp::StringProperty *_lastModifiedDates;
+  tlp::StringProperty *_lastReadDates;
+  tlp::StringProperty *_owners;
+  tlp::IntegerProperty *_permissions;
+  tlp::StringProperty *_suffixes;
 };
 /*@}*/
-IMPORTPLUGINOFGROUP(FileSystem, "File System Directory", "Auber", "16/12/2002", "", "1.1", "Misc")
+IMPORTPLUGINOFGROUP(FileSystem, "File System Directory", "Auber", "16/12/2002", "", "2.0", "Misc")
