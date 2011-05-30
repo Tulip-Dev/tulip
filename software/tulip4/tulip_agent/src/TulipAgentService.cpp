db6ed65ed5681c9ab9c53e8d4fbb32396af1f06d
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-05-30 09:31:13 +0000

    First steps for the Tulip agent DBus service
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5035 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ software/tulip4/tulip_agent/src/TulipAgentService.cpp
@@ -0,0 +1,20 @@
+#include "TulipAgentService.h"
+
+TulipAgentService::TulipAgentService(QObject *parent): QObject(parent) {
+}
+
+QString TulipAgentService::pid() const {
+
+}
+
+void TulipAgentService::ShowAboutPage() {
+
+}
+
+void TulipAgentService::ShowPluginsCenter() {
+
+}
+
+void TulipAgentService::ShowWelcomeScreen() {
+
+}
