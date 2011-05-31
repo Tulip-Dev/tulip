0fc826c5a2a4265b2f4a0aa743e6dd9af1543576
Charles Huet <packadal@gmail.com>
2011-05-31 07:35:14 +0000

    added functions to retrieve remote serverDescription.xml, and creates a list of PluginInformations. Added a new constructor to LocalPluginInformations (needs to be renamed as it can represent local and remote objects) added a new class inheriting from AbstractPluginInfo to represent remote plugins
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5040 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/PluginInformations.cpp
+++ library/tulip-gui/src/PluginInformations.cpp
@@ -1,14 +1,24 @@
 #include <tulip/PluginInformations.h>
 
+#include <tulip/WithDependency.h>
+#include <tulip/AbstractPluginInfo.h>
+
 #include <QtCore/QFileInfo>
 
 using namespace tlp;
 
 LocalPluginInformations::LocalPluginInformations(const tlp::AbstractPluginInfo* info, const std::string& type, const std::list< Dependency >& dependencies, const std::string& library)
-  :_infos(info), _type(type.c_str()), _dependencies(dependencies) {
-    for(std::list<tlp::Dependency>::const_iterator it = _dependencies.begin(); it != _dependencies.end(); ++it) {
-      _dependenciesNames.push_back(it->pluginName.c_str());
-    }
+    :_infos(info), _type(type.c_str()), _dependencies(dependencies) {
+  for(std::list<tlp::Dependency>::const_iterator it = _dependencies.begin(); it != _dependencies.end(); ++it) {
+    _dependenciesNames.push_back(it->pluginName.c_str());
+  }
+}
+
+LocalPluginInformations::LocalPluginInformations(const tlp::AbstractPluginInfo* info, const std::string& type, const std::list<tlp::Dependency>& dependencies, const QString& longDescriptionPath, const QString& iconPath)
+    :_infos(info), _type(type.c_str()), _dependencies(dependencies) {
+  for(std::list<tlp::Dependency>::const_iterator it = _dependencies.begin(); it != _dependencies.end(); ++it) {
+    _dependenciesNames.push_back(it->pluginName.c_str());
+  }
 }
 
 QString LocalPluginInformations::identifier() const{
--- library/tulip-gui/src/PluginManager.cpp
+++ library/tulip-gui/src/PluginManager.cpp
@@ -1,8 +1,16 @@
 #include <tulip/PluginManager.h>
 #include <tulip/PluginLister.h>
 
+#include <QtXml/QDomDocument>
+#include <QtNetwork/QNetworkRequest>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+#include <QCoreApplication>
+
 using namespace std;
 using namespace tlp;
+
+QMap<QString, QList<tlp::PluginInformationsInterface*> > PluginManager::_remoteLocations;
 
 QList<const tlp::PluginInformationsInterface*> PluginManager::pluginsList(Location list) {
   QList<const tlp::PluginInformationsInterface*> result;
@@ -26,4 +34,70 @@ QList<const tlp::PluginInformationsInterface*> PluginManager::pluginsList(Locati
   }
 
   return result;
+}
+
+QString PluginManager::getPluginServerDescription(const QString& location) {
+  
+  QNetworkAccessManager manager;
+  QNetworkReply* reply = manager.get(QNetworkRequest(QUrl(location + "/serverDescription.xml")));
+
+  while(!reply->isFinished()) {
+    QCoreApplication::processEvents();
+  }
+
+  if(reply->error() != QNetworkReply::NoError) {
+    std::cout << "error while retrieving server description (" << location.toStdString() << ") : " << reply->errorString().toStdString() << std::endl;
+    return QString::null;
+  }
+  
+  QString content(reply->readAll());
+  return content;
+}
+
+QList<tlp::PluginInformationsInterface*> PluginManager::parseDescription(const QString& xmlDescription) {
+  QDomDocument description;
+  description.setContent(xmlDescription);
+  QDomElement elm = description.documentElement();
+  
+  QList<tlp::PluginInformationsInterface*> remotePlugins;
+  QDomNodeList pluginNodes = description.elementsByTagName("plugin");
+  for(int i = 0; i < pluginNodes.count(); ++i) {
+    const QDomNode& child = pluginNodes.at(i);
+    QDomElement childElement = child.toElement();
+    const std::string name = childElement.attribute("name").toStdString();
+    const std::string type = childElement.attribute("type").toStdString();
+    const std::string author = childElement.attribute("author").toStdString();
+    const std::string date = childElement.attribute("date").toStdString();
+    const std::string info = childElement.attribute("info").toStdString();
+    const std::string fileName = childElement.attribute("fileName").toStdString();
+    const std::string release = childElement.attribute("release").toStdString();
+    const std::string group = childElement.attribute("group").toStdString();
+    const std::string tulipRelease = childElement.attribute("tulipRelease").toStdString();
+    
+    tlp::AbstractPluginInfo* pluginInfo = new DistantPluginInfo(author, date, group, name, info, release, tulipRelease);
+    std::list<tlp::Dependency> dependencies;
+    for(QDomNode n = child.firstChild(); !n.isNull(); n = n.nextSibling()) {
+      QDomElement dependencyElement = n.toElement();
+      tlp::Dependency dep(dependencyElement.attribute("name").toStdString(), dependencyElement.attribute("type").toStdString(), dependencyElement.attribute("version").toStdString());
+      dependencies.push_back(dep);
+    }
+    
+    //TODO fill these values from the location
+    QString longDescriptionPath;
+    QString iconPath;
+    PluginInformationsInterface* pluginInformations = new LocalPluginInformations(pluginInfo, type, dependencies, longDescriptionPath, iconPath);
+    remotePlugins.push_back(pluginInformations);
+  }
+
+  return remotePlugins;
+}
+
+void PluginManager::addRemoteLocation(const QString& location) {
+
+  QString xmlDocument(getPluginServerDescription(location));
+  QDomDocument description;
+  description.setContent(xmlDocument);
+  QDomElement elm = description.documentElement();
+  
+  _remoteLocations[elm.attribute("serverName")] = parseDescription(xmlDocument);
 }
