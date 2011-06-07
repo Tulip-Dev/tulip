964875b49fc5f198a480d22bc630db377d10a00a
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-06-07 05:37:56 +0000

    horray for the unusable progress widget
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5090 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/SimplePluginProgressWidget.cpp
+++ library/tulip-gui/src/SimplePluginProgressWidget.cpp
@@ -6,7 +6,6 @@
 
 CircleProgressBar::CircleProgressBar(QWidget *parent)
   : QWidget(parent), _value(0), _max(100) {
-  qWarning("build");
 }
 
 void CircleProgressBar::setValue(int value, int max) {
@@ -18,13 +17,28 @@ void CircleProgressBar::setValue(int value, int max) {
 void CircleProgressBar::paintEvent(QPaintEvent *e) {
   QWidget::paintEvent(e);
   QStylePainter p(this);
-  p.setPen(Qt::transparent);
-  p.setRenderHints(QPainter::HighQualityAntialiasing | QPainter::SmoothPixmapTransform | QPainter::TextAntialiasing | QPainter::NonCosmeticDefaultPen | QPainter::Antialiasing);
+  p.setRenderHints(QPainter::SmoothPixmapTransform | QPainter::TextAntialiasing | QPainter::Antialiasing);
 
-  p.setBrush(Qt::red);
-  p.drawPie(rect().x(),rect().y(), rect().width()-10,rect().height()-10,0,360*16);
+  QPen pen(QApplication::palette().color(QPalette::Mid));
+  pen.setWidth(5);
+  p.setPen(pen);
+  p.setBrush(Qt::transparent);
+  QRect outRect(rect().x() + 5, rect().y() + 5, rect().width() - 10, rect().height() - 10);
+  int angle = _value * 5840 / _max;
+  p.drawPie(outRect, 0, angle);
+
+  p.setBrush(QApplication::palette().color(QPalette::Window));
+  p.setPen(QApplication::palette().color(QPalette::Window));
+  p.drawEllipse(outRect.x()+3,outRect.y()+3,outRect.width()-6,outRect.height()-6);
+
+  p.setBrush(QApplication::palette().text());
+  p.setPen(QApplication::palette().color(QPalette::Text));
+  p.drawText(outRect,Qt::AlignCenter | Qt::AlignVCenter, QString::number(_value * 100 / _max) + "%");
 }
 
+int CircleProgressBar::heightForWidth(int w) const  {
+  return w;
+}
 namespace tlp {
 
 SimplePluginProgressWidget::SimplePluginProgressWidget(QWidget *parent, Qt::WindowFlags f)
@@ -33,11 +47,15 @@ SimplePluginProgressWidget::SimplePluginProgressWidget(QWidget *parent, Qt::Wind
 }
 
 void SimplePluginProgressWidget::progress_handler(int step, int max_step) {
-
+  _ui->circleProgress->setValue(step,max_step);
 }
 
 void SimplePluginProgressWidget::preview_handler(bool p) {
 
+}
+
+void SimplePluginProgressWidget::setComment(std::string s) {
+  _ui->commentLabel->setText("<html><head/><body><b>" + QString(s.c_str()) + "</b></body></html>");
 }
 
 }
