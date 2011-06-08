40c5fb0cc99392568fb02fb62e9d69004b14ed13
Charles Huet <packadal@gmail.com>
2011-06-08 14:23:52 +0000

    added a small and minimalistic compatibility layer to ease the port from Tulip3
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5119 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip3-compat/include/tulip3/PluginSystem.h
@@ -0,0 +1,8 @@
+#include <tulip/AbstractPluginInfo.h>
+typedef AbstractPluginInfo Plugin;
+
+#include <tulip/WithParameter.h>
+typedef ParameterList StructDef;
+
+#include <tulip/PluginLister.h>
+typedef PluginLister TemplateFactory;
