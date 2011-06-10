646913da7fb9e0c45544bd2f6540815139c3b6ed
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-06-10 06:44:50 +0000

    A more standard version of progress widgets
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5142 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/SimplePluginProgressWidget.cpp
+++ library/tulip-gui/src/SimplePluginProgressWidget.cpp
@@ -2,64 +2,35 @@
 #include <QtGui/QStylePainter>
 #include <QtCore/QDebug>
 
+#include <tulip/Observable.h>
 #include "ui_SimplePluginProgressWidget.h"
 
-CircleProgressBar::CircleProgressBar(QWidget *parent)
-  : QWidget(parent), _value(0), _max(100) {
-}
-
-void CircleProgressBar::setValue(int value, int max) {
-  _value= value;
-  _max = max;
-  repaint();
-}
-
-void CircleProgressBar::paintEvent(QPaintEvent *e) {
-  QWidget::paintEvent(e);
-  QStylePainter p(this);
-  p.setRenderHints(QPainter::SmoothPixmapTransform | QPainter::TextAntialiasing | QPainter::Antialiasing);
-
-  QPen pen(QApplication::palette().color(QPalette::Mid));
-  pen.setWidth(5);
-  p.setPen(pen);
-  p.setBrush(Qt::transparent);
-  QRect outRect(rect().x() + 5, rect().y() + 5, rect().width() - 10, rect().height() - 10);
-  int angle = _value * 5840 / _max;
-  p.drawPie(outRect, 0, angle);
-
-  p.setBrush(QApplication::palette().color(QPalette::Window));
-  p.setPen(QApplication::palette().color(QPalette::Window));
-  p.drawEllipse(outRect.x()+3,outRect.y()+3,outRect.width()-6,outRect.height()-6);
-
-  p.setBrush(QApplication::palette().text());
-  p.setPen(QApplication::palette().color(QPalette::Text));
-  p.drawText(outRect,Qt::AlignCenter | Qt::AlignVCenter, QString::number(_value * 100 / _max) + "%");
-}
-
-int CircleProgressBar::heightForWidth(int w) const  {
-  return w;
-}
 namespace tlp {
 
 SimplePluginProgressWidget::SimplePluginProgressWidget(QWidget *parent, Qt::WindowFlags f)
   :QWidget(parent,f), _ui(new Ui::SimplePluginProgressWidgetData) {
   _ui->setupUi(this);
+  _ui->cancelButton->setIcon(QApplication::style()->standardIcon(QStyle::SP_DialogCancelButton));
 }
 
 void SimplePluginProgressWidget::progress_handler(int step, int max_step) {
-  _ui->circleProgress->setValue(step,max_step);
+  _ui->progressBar->setValue(step);
+  _ui->progressBar->setMaximum(max_step);
 }
 
 void SimplePluginProgressWidget::preview_handler(bool p) {
-
+  if (p)
+    Observable::unholdObservers();
+  else
+    Observable::holdObservers();
 }
 
 void SimplePluginProgressWidget::setComment(std::string s) {
-  _ui->commentLabel->setText("<html><head/><body><b>" + QString(s.c_str()) + "</b></body></html>");
+  setComment(QString(s.c_str()));
 }
 
 void SimplePluginProgressWidget::setComment(const QString &s) {
-  setComment(s.toStdString());
+  _ui->comment->setText("<b>" + s + "</b>");
 }
 
 void SimplePluginProgressWidget::setComment(const char *s) {
