8b510ea6e2c4d67bb3c6b40d52c5f243edc2f0e9
Charles Huet <packadal@gmail.com>
2011-07-19 14:00:58 +0000

    commited forgotten DownloadManager class to actually download plugins added a variable to FindTulip.cmake to find the TulipUseFile, required to create pluginServers (overriding Cmake's install in the FindTulip made Cmake segfault) added the TulipUseFile with an override of install (only defined if TULIP_GENERATE_PLUGINSERVER is set to ON) and installs to another location to create a pluginserver
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5437 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/include/tulip/DownloadManager.h
@@ -0,0 +1,23 @@
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QUrl>
+#include <QMap>
+
+class QNetworkReply;
+
+class DownloadManager: public QObject {
+    Q_OBJECT
+    QNetworkAccessManager manager;
+    QList<QNetworkReply *> currentDownloads;
+    QMap<QUrl, QString> downloadDestinations;
+
+    static DownloadManager* _instance;
+    DownloadManager();
+  public:
+    static DownloadManager* getInstance();
+    void downloadPlugin(const QUrl &url, const QString& destination);
+    bool saveToDisk(const QString &filename, QIODevice *data);
+
+  public slots:
+    void downloadFinished(QNetworkReply *reply);
+ };
