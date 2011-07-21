56876ade68492cf0aeaffe79e603f8f1c4dc15e3
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-07-21 12:55:18 +0000

    Added a very simple, yet effective, expandable group box directly usable and styleable in designer as a promoted widget
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5464 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/ExpandableGroupBox.cpp
+++ plugins/perspective/GraphPerspective/ExpandableGroupBox.cpp
@@ -1,7 +1,22 @@
 #include "ExpandableGroupBox.h"
 
-ExpandableGroupBox::ExpandableGroupBox(const QString &title,QWidget *parent): QGroupBox(title,parent) {
+#include <QtGui/QVBoxLayout>
+
+ExpandableGroupBox::ExpandableGroupBox(const QString &title,QWidget *parent): QGroupBox(title,parent), _expanded(true) {
+  setCheckable(true);
+  setChecked(true);
+  setExpanded(_expanded);
+  connect(this,SIGNAL(toggled(bool)),this,SLOT(setExpanded(bool)));
 }
 
 ExpandableGroupBox::~ExpandableGroupBox() {
+}
+
+void ExpandableGroupBox::setExpanded(bool e) {
+  QObject *o;
+  foreach(o,children()) {
+    QWidget *w = dynamic_cast<QWidget *>(o);
+    if (w)
+      w->setVisible(e);
+  }
 }
