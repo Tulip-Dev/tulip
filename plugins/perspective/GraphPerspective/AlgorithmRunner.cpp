5e3a96bbf0e7e25b1f6631f125d389d828e526eb
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-07-22 08:49:17 +0000

    Finalized AlgorithmRunner UI part
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5470 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/AlgorithmRunner.cpp
+++ plugins/perspective/GraphPerspective/AlgorithmRunner.cpp
@@ -30,48 +30,6 @@ AlgorithmRunnerItem::AlgorithmRunnerItem(const QString &group, const QString &na
 AlgorithmRunnerItem::~AlgorithmRunnerItem() {
 }
 
-QWidget *PluginListWidgetManagerInterface::buildListWidget() {
-  QMap<QString,QStringList> algorithmInfos = algorithms();
-  QWidget *result = new QWidget;
-  result->setObjectName("algorithmListContents");
-  QVBoxLayout *layout = new QVBoxLayout;
-  layout->setObjectName("algorithmListContentsLayout");
-  layout->setSpacing(10);
-
-  if (algorithmInfos.empty()) {
-    QLabel *errorLabel = new QLabel("<span style=\"color:#626262;\">" + AlgorithmRunner::trUtf8("There is no plugin to display.") + "</span>");
-    errorLabel->setObjectName("errorLabel");
-    errorLabel->setWordWrap(true);
-    layout->addWidget(errorLabel);
-  }
-  else {
-    layout->setContentsMargins(3,6,3,6);
-    QString group;
-    foreach(group,algorithmInfos.keys()) {
-      QWidget *groupWidget;
-
-      if (group == "")
-        groupWidget = new QWidget;
-      else
-        groupWidget = new ExpandableGroupBox(group);
-
-      groupWidget->setObjectName(group + "_groupBox");
-      QVBoxLayout *groupLayout = new QVBoxLayout;
-      groupLayout->setContentsMargins(6,15,6,15);
-      groupLayout->setObjectName(group + "_boxLayout");
-      QString algName;
-      foreach(algName,algorithmInfos[group])
-      groupLayout->addWidget(new AlgorithmRunnerItem(group,algName));
-      groupWidget->setLayout(groupLayout);
-      layout->addWidget(groupWidget);
-    }
-    layout->addItem(new QSpacerItem(2,2,QSizePolicy::Maximum,QSizePolicy::Expanding));
-  }
-
-  result->setLayout(layout);
-  return result;
-}
-
 template<typename ALG,typename PROPTYPE>
 class TemplatePluginListWidgetManager: public PluginListWidgetManagerInterface {
 public:
@@ -124,5 +82,52 @@ AlgorithmRunner::AlgorithmRunner(QWidget *parent): QWidget(parent), _ui(new Ui::
 void AlgorithmRunner::algorithmTypeChanged(const QString &type) {
   _pluginsListMgr = PLUGIN_LIST_MANAGERS_DISPLAY_NAMES[type];
   assert(_pluginsListMgr);
-  _ui->algorithmList->setWidget(_pluginsListMgr->buildListWidget());
+  _ui->algorithmList->setWidget(buildListWidget());
+}
+
+QWidget *AlgorithmRunner::buildListWidget() {
+  _currentAlgorithmsList = _pluginsListMgr->algorithms();
+
+  QWidget *result = new QWidget;
+  result->setObjectName("algorithmListContents");
+  QVBoxLayout *layout = new QVBoxLayout;
+  layout->setObjectName("algorithmListContentsLayout");
+  layout->setSpacing(10);
+
+  _ui->searchBox->setEnabled(!_currentAlgorithmsList.empty());
+  if (_currentAlgorithmsList.empty()) {
+    QLabel *errorLabel = new QLabel("<span style=\"color:#626262;\">" + AlgorithmRunner::trUtf8("There is no plugin to display.") + "</span>");
+    errorLabel->setObjectName("errorLabel");
+    errorLabel->setWordWrap(true);
+    layout->addWidget(errorLabel);
+  }
+  else {
+    layout->setContentsMargins(3,6,3,6);
+    QString group;
+    foreach(group,_currentAlgorithmsList.keys()) {
+      QWidget *groupWidget;
+
+      QVBoxLayout *groupLayout = new QVBoxLayout;
+      groupLayout->setObjectName(group + "_boxLayout");
+      if (group == "") {
+        groupWidget = new QWidget;
+        groupLayout->setContentsMargins(0,0,0,0);
+      }
+      else {
+        groupWidget = new ExpandableGroupBox(group);
+        groupLayout->setContentsMargins(6,15,6,15);
+      }
+
+      groupWidget->setObjectName(group + "_groupBox");
+      QString algName;
+      foreach(algName,_currentAlgorithmsList[group])
+      groupLayout->addWidget(new AlgorithmRunnerItem(group,algName));
+      groupWidget->setLayout(groupLayout);
+      layout->addWidget(groupWidget);
+    }
+    layout->addItem(new QSpacerItem(2,2,QSizePolicy::Maximum,QSizePolicy::Expanding));
+  }
+
+  result->setLayout(layout);
+  return result;
 }
