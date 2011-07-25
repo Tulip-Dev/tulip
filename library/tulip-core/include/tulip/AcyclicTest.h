0a55b5922a696ab25032c63c90c38da46647f5e9
Charles Huet <packadal@gmail.com>
2011-07-25 15:32:56 +0000

    Acyclic test now uses the new observation system, and is fully documented
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5490 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/AcyclicTest.h
+++ library/tulip-core/include/tulip/AcyclicTest.h
@@ -31,7 +31,12 @@ namespace tlp {
 class Graph;
 class BooleanProperty;
 
-class SelfLoops {
+/**
+ * @brief Stores all the added informations on self loops.
+ * Self loops are removed by adding two nodes and three edges.
+ * These are stores here, along with the old self looping edge.
+ **/
+struct SelfLoops {
 public:
   SelfLoops(node n1,node n2,edge e1, edge e2,edge e3,edge old):
     n1(n1),n2(n2),e1(e1),e2(e2),e3(e3),old(old) {
@@ -42,14 +47,27 @@ public:
 
 /** \addtogroup graph_test */
 /*@{*/
-/// Class for testing if the graph is acyclic
-class TLP_SCOPE AcyclicTest : private GraphObserver, private Observable {
+
+/**
+ * @brief This class provides tests for acyclicity on a graph.
+ * Results are cached in a map of graphs and result.
+ * This class observes the graphs that have been tested to remove the result from this graph if it is modified.
+ * This forces the use of the singleton pattern instead of simply using static functions/members.
+ **/
+class TLP_SCOPE AcyclicTest : private Observable {
 public:
 
   /**
    * Returns true if the graph is acyclic, false otherwise.
    * The result is cached (ie. the next call with the same graph is done in O(1) time)
    */
+  /**
+   * @brief Returns whether the graph is acyclic or not.
+   * The result is cached so subsequent calls are in O(1).
+   *
+   * @param graph The graph on which to perform the acyclicity test.
+   * @return bool True if the graph is acyclic, false otherwise.
+   **/
   static bool isAcyclic(const Graph *graph);
 
   /**
@@ -57,6 +75,14 @@ public:
     * If there is self loops, a new node is added with two edges that points to it.
     *
     */
+  /**
+   * @brief Makes the graph acyclic by removing edges.
+   *
+   * @param graph The graph to make acyclic.
+   * @param reversed The edges that were reversed during the process.
+   * @param selfLoops Sets of two nodes and three edges that were added in stead of self loops.
+   * @return void
+   **/
   static void makeAcyclic(Graph* graph, std::vector<edge> &reversed, std::vector<tlp::SelfLoops> &selfLoops);
 
   /**
@@ -65,19 +91,28 @@ public:
     * that create cycle.
     *
     */
-  static bool acyclicTest(const Graph *, std::vector<edge> *obstructionEdges = 0);
+  /**
+   * @brief Returns whether the graph is acyclic.
+   * Collection of obstruction edges takes a bit of time, as iteration over the graph must continue even when it has been found cyclic.
+   *
+   * @param graph the graph to  test for acyclicity
+   * @param obstructionEdges If not null, will be filled with edges that cause the graph to be cyclic. Defaults to 0.
+   * @return bool
+   **/
+  static bool acyclicTest(const Graph * graph, std::vector<edge> *obstructionEdges = 0);
 
 private:
-
-  //  override GraphObserver methods
-  void addEdge(Graph *,const edge);
-  void delEdge(Graph *,const edge);
-  void reverseEdge(Graph *,const edge);
-  void destroy(Graph *);
   AcyclicTest();
+  /**
+   * @brief Singleton instance of this class.
+   **/
   static AcyclicTest * instance;
+  /**
+   * @brief Stored results for graphs. When a graph is updated, its entry is removed from the hashmap.
+   **/
   TLP_HASH_MAP<unsigned long, bool> resultsBuffer;
-  // override Observable::treatEvent
+
+  //override of Observable::treatEvent to remove the cached result for a graph if it is modified.
   virtual void treatEvent(const Event&);
 };
 }
