50d41544814fc4c6b85b139702f9402c84dd75c6
Charles Huet <packadal@gmail.com>
2011-07-27 07:17:55 +0000

    documented the MinMaxCalculator
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5508 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/MinMaxCalculator.h
+++ library/tulip-core/include/tulip/MinMaxCalculator.h
@@ -28,23 +28,102 @@ template<class Tnode, class Tedge, class TPROPERTY>
 class AbstractProperty;
 
 template<typename nodeType, typename edgeType, typename algorithmType>
+/**
+ * @brief Abstracts the computation of minimal and maximal values on node and edge values of properties.
+ *
+ * The value is lazily computed on first request..
+ * The value is cached, and the cache is invalidated whenever it cannot be simply updated.
+ **/
 class MinMaxCalculator {
 public:
+  /**
+   * @brief Constructs a MinMaxCalculator.
+   *
+   * @param property The property on which to compute minimum and maximum values.
+   * @param NodeMin The minimal value the property can take for nodes (e.g. INT_MIN)
+   * @param NodeMax The maximal value the property can take for nodes (e.g. INT_MIN)
+   * @param EdgeMin The minimal value the property can take for edges (e.g. INT_MIN)
+   * @param EdgeMax The maximal value the property can take for edges (e.g. INT_MIN)
+   **/
   MinMaxCalculator(tlp::AbstractProperty<nodeType, edgeType, algorithmType>* property, typename nodeType::RealType NodeMin,
                    typename nodeType::RealType NodeMax, typename edgeType::RealType EdgeMin, typename edgeType::RealType EdgeMax);
+  /**
+   * @brief Constructs a MinMaxCalculator.
+   *
+   * @param property The property on which to compute minimum and maximum values.
+   * @param NodeMin The minimal value the property can take for nodes (e.g. INT_MIN)
+   * @param NodeMax The maximal value the property can take for nodes (e.g. INT_MIN)
+   **/
   MinMaxCalculator(tlp::AbstractProperty<nodeType, edgeType, algorithmType>* property, typename nodeType::RealType NodeMin,
                    typename nodeType::RealType NodeMax);
+
+  /**
+   * @brief A bridge to Observable's treatEvent() so this class can easily be delegated to.
+   *
+   * @param ev The event to treat.
+   * @return void
+   **/
   virtual void treatEvent(const tlp::Event& ev);
 
+  /**
+   * @brief Computes the minimum value on the nodes.
+   *
+   * @param graph The graph on which to compute.
+   * @return :RealType The minimal value on this graph for this property.
+   **/
   typename nodeType::RealType getNodeMin(Graph* graph);
+  /**
+   * @brief Computes the maximum value on the nodes.
+   *
+   * @param graph The graph on which to compute.
+   * @return :RealType The maximal value on this graph for this property.
+   **/
   typename nodeType::RealType getNodeMax(Graph* graph);
+  /**
+   * @brief Computes the minimum value on the edges.
+   *
+   * @param graph The graph on which to compute.
+   * @return :RealType The minimal value on this graph for this property.
+   **/
   typename edgeType::RealType getEdgeMin(Graph* graph);
+  /**
+   * @brief Computes the maximum value on the edges.
+   *
+   * @param graph The graph on which to compute.
+   * @return :RealType The maximal value on this graph for this property.
+   **/
   typename edgeType::RealType getEdgeMax(Graph* graph);
 
+  /**
+   * @brief Updates the value on a node, and updates the minimal/maximal cached values if necessary.
+   *
+   * @param n The node for which the value is updated.
+   * @param newValue The new value on this node.
+   * @return void
+   **/
   void updateNodeValue(tlp::node n, typename nodeType::RealType newValue);
+  /**
+   * @brief Updates the value on an edge, and updates the minimal/maximal cached values if necessary.
+   *
+   * @param e The edge for which the value is updated.
+   * @param newValue The new value on this edge.
+   * @return void
+   **/
   void updateEdgeValue(tlp::edge e, typename edgeType::RealType newValue);
 
+  /**
+   * @brief Updates the value of all nodes, setting the maximum and minimum values to this.
+   *
+   * @param newValue The new maximal and minimal value. 
+   * @return void
+   **/
   void updateAllNodesValues(typename nodeType::RealType newValue);
+  /**
+   * @brief Updates the value of all edges, setting the maximum and minimum values to this.
+   *
+   * @param newValue The new maximal and minimal value.
+   * @return void
+   **/
   void updateAllEdgesValues(typename edgeType::RealType newValue);
 
 protected:
