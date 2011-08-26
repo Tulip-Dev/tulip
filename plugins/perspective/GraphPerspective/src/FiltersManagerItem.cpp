be9ad8628cacff401b28a780dcc9437e7b8f8339
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-08-26 07:01:44 +0000

    Fixed compilation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5626 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/FiltersManagerItem.cpp
+++ plugins/perspective/GraphPerspective/src/FiltersManagerItem.cpp
@@ -8,20 +8,6 @@
 
 FiltersManagerItem::FiltersManagerItem(QWidget *parent): QWidget(parent), _ui(new Ui::FiltersManagerItemData), _removable(true) {
   _ui->setupUi(this);
-
-  _ui->settingsFrame->hide();
-  _ui->modeSelector->hide();
-
-  connect(_ui->addButton,SIGNAL(clicked()),this,SLOT(expand()));
-
-  tlp::Iterator<std::string> *it = tlp::StaticPluginLister<tlp::DoubleAlgorithm,tlp::PropertyContext>::availablePlugins();
-  while (it->hasNext()) {
-    std::string name = it->next();
-    _ui->firstMember->addItem(name.c_str());
-    _ui->secondMember->addItem(name.c_str());
-  }
-
-  delete it;
 }
 
 FiltersManagerItem::~FiltersManagerItem() {
@@ -33,7 +19,4 @@ void FiltersManagerItem::setRemovable(bool f) {
 }
 
 void FiltersManagerItem::expand() {
-  _ui->settingsFrame->show();
-  _ui->addButton->hide();
-  _ui->modeSelector->show();
 }
