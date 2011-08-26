be304d4b99122d5c766549870a1513de56042c79
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-08-26 06:49:00 +0000

    Still adding missing resources...Silly me
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5625 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/FiltersManager.cpp
+++ plugins/perspective/GraphPerspective/src/FiltersManager.cpp
@@ -1,13 +1,23 @@
 #include "FiltersManager.h"
 
+#include "FiltersManagerItem.h"
+
 #include "ui_FiltersManager.h"
 
 FiltersManager::FiltersManager(QWidget *parent): QWidget(parent), _ui(new Ui::FiltersManagerData) {
   _ui->setupUi(this);
   _ui->header->setMenus(QStringList()
                         << trUtf8("Filtering (no pre-selection)")
+                        << trUtf8("Filtering (on current selection)")
                         << trUtf8("Filtering (pre-select nodes)")
                         << trUtf8("Filtering (pre-select edges)")
                         << trUtf8("Filtering (pre-select all)"));
 
+  _ui->filtersListContents->setLayout(new QVBoxLayout);
+  _ui->filtersListContents->layout()->setMargin(0);
+  _ui->filtersListContents->layout()->setAlignment(Qt::AlignTop);
+}
+
+FiltersManager::~FiltersManager() {
+  delete _ui;
 }
