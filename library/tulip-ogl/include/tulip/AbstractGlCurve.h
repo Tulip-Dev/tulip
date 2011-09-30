d0da1da17a3516c94da142ac6f871a7d2e708ee7
Antoine Lambert <antoine.lambert33@gmail.com>
2011-09-30 12:52:58 +0000

    - modify shader implementation of parametric curves : don't store curve control points in a vec3 array anymore, read them from a texture buffer object (unlimited number of control points) or a 1D texture (up to 1024 control points, should be enough) - I took care of not breaking binary compatibility, I only added a static member to AbstractGlCurve (this is allowed) - also don't activate curve outline if border width equals 0
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_6_maint@5822 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/AbstractGlCurve.h
+++ library/tulip-ogl/include/tulip/AbstractGlCurve.h
@@ -84,12 +84,15 @@ protected:
   static std::map<unsigned int, GLfloat *> curveVertexBuffersData;
   static std::map<unsigned int, std::vector<GLushort *> > curveVertexBuffersIndices;
   static std::map<unsigned int, GLuint* > curveVertexBuffersObject;
+  // TODO : remove this static variable in Tulip 3.7 (if there is such a release in the future)
+  // can not remove it in Tulip 3.6.1 otherwise binary compatibility will be broken
   static GLint MAX_SHADER_CONTROL_POINTS;
   static std::map<std::string, GlShaderProgram *> curvesShadersMap;
   static std::map<std::string, GlShaderProgram *> curvesBillboardShadersMap;
   static GlShader *curveVertexShaderNormalMain;
   static GlShader *curveVertexShaderBillboardMain;
   static GlShader *fisheyeDistortionVertexShader;
+  static bool canUseTBO;
 
   std::string shaderProgramName;
   GlShaderProgram *curveShaderProgramNormal;
