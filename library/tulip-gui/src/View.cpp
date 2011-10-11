46cf5edbaf9a23dd2da86e1c1411cd55c446aefb
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-10-11 09:03:18 +0000

    Implementation of View interface. Replaced QList by QSet
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5864 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/View.cpp
+++ library/tulip-gui/src/View.cpp
@@ -2,4 +2,57 @@
 
 using namespace tlp;
 
+View::View()
+  : _hasInteractorsResponsibility(true), _hasConfigurationWidgetsResponsibility(true),
+    _interactors(QSet<Interactor*>()), _activeInteractor(NULL),
+    _triggers(QSet<Observable*>()),
+    _graph(NULL) {
+}
 
+View::~View() {
+}
+
+void View::setInteractors(const QSet<tlp::Interactor *> &inters) {
+  _interactors = inters;
+  interactorsInstalled(inters);
+}
+
+void View::setActiveInteractor(tlp::Interactor *i) {
+  _activeInteractor = i;
+  activeInteractorChanged(i);
+}
+
+Interactor* View::activeInteractor() const {
+  return _activeInteractor;
+}
+
+void View::registerTrigger(tlp::Observable *obs) {
+  if (_triggers.contains(obs))
+    return;
+  _triggers.insert(obs);
+  obs->addObserver(this);
+}
+
+void View::removeTrigger(tlp::Observable *obs) {
+  _triggers.remove(obs);
+  obs->removeObserver(this);
+}
+
+Graph* View::graph() const {
+  return _graph;
+}
+
+void View::setGraph(tlp::Graph *g) {
+  _graph = g;
+  graphChanged(g);
+}
+
+void View::interactorsInstalled(const QSet<tlp::Interactor*>&) {
+}
+
+void View::activeInteractorChanged(tlp::Interactor*) {
+}
+
+void View::graphChanged(tlp::Graph*) {
+  emit drawNeeded();
+}
