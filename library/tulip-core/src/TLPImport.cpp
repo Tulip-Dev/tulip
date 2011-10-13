5e98cc146fdbc2eb77187c707222f2aecb53bf3d
Patrick Mary <mary@labri.fr>
2011-10-13 07:33:57 +0000

    integrates fix for open on non tlp file sf bug #3410653 from tulip_3_6_maint
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@5884 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/TLPImport.cpp
+++ library/tulip-core/src/TLPImport.cpp
@@ -120,8 +120,11 @@ struct TLPGraphBuilder:public TLPTrue {
   bool addString(const std::string& str) {
     // only used to handle the version of tlp file format
     if (!version) {
-      version = (float) atof(str.c_str());
-      return version <= ((float)TLP_VERSION);
+      const char* cptr = str.c_str();
+      char* endptr;
+      version = strtof(cptr, &endptr);
+      // check for correctness of version parsing and value
+      return (endptr != cptr) && (version <= TLP_VERSION);
     }
 
     return false;
