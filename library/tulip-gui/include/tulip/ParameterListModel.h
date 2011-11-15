5168db5184a59edc131089a55cd45357c336ceb2
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2011-11-15 14:44:18 +0000

    Import wizard dialog done
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6049 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/include/tulip/ParameterListModel.h
+++ library/tulip-gui/include/tulip/ParameterListModel.h
@@ -45,6 +45,7 @@ class TLP_QT_SCOPE ParameterListModel : public TulipModel {
 
 public:
   explicit ParameterListModel(const tlp::ParameterList &params, tlp::Graph *graph=0, QObject *parent=0);
+  tlp::DataSet parametersValues() const;
 
   QModelIndex index(int row, int column,const QModelIndex &parent = QModelIndex()) const;
   QModelIndex parent(const QModelIndex &child) const;
