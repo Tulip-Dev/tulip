cf83f954f3da3eecb576d0bdc50ce5c8990cde12
Antoine Lambert <antoine.lambert33@gmail.com>
2011-11-24 21:16:11 +0000

    - use colors to display the stack trace (help to read it)
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6153 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/crash_handling/StackWalker.h
+++ software/crash_handling/StackWalker.h
@@ -36,41 +36,93 @@
 #include <windows.h>
 #endif
 
+enum FontType {NORMAL=0, BOLD=1};
+
+enum TextColor {BLACK=0, RED=1, GREEN=2, YELLOW=3, BLUE=4,
+	MAGENTA=5, CYAN=6, WHITE=7};
+
+inline std::ostream& defaultColor(std::ostream &s) {
+	if (s == std::cout || s == std::cerr) {
+		s << "\E[0m";
+	}
+	return s;
+}
+
+inline std::ostream& setTextColor(std::ostream &s, FontType ft, TextColor fg, TextColor bg) {
+	if (s == std::cout || s == std::cerr) {
+		s << "\E[" << ft << "m\E[3" << fg << "m\E[4" << bg << "m";
+	}
+	return s;
+}
+
+inline std::ostream& red(std::ostream &s) {
+	return setTextColor(s, BOLD, RED, BLACK);
+}
+
+inline std::ostream& green(std::ostream &s) {
+	return setTextColor(s, BOLD, GREEN, BLACK);
+}
+
+inline std::ostream& blue(std::ostream &s) {
+	return setTextColor(s, BOLD, BLUE, BLACK);
+}
+
+inline std::ostream& yellow(std::ostream &s) {
+	return setTextColor(s, BOLD, YELLOW, BLACK);
+}
+
+inline std::ostream& white(std::ostream &s) {
+	return setTextColor(s, BOLD, WHITE, BLACK);
+}
+
+inline std::ostream& magenta(std::ostream &s) {
+	return setTextColor(s, BOLD, MAGENTA, BLACK);
+}
+
+inline std::ostream& cyan(std::ostream &s) {
+	return setTextColor(s, BOLD, CYAN, BLACK);
+}
+
+
 class StackWalker {
 public:
-  virtual ~StackWalker() {}
-  virtual void printCallStack(std::ostream &os, unsigned int maxDepth = 50) = 0;
+	virtual ~StackWalker() {}
+	virtual void printCallStack(std::ostream &os, unsigned int maxDepth = 50) = 0;
 
-  void printCallStackToStdErr(unsigned int maxDepth = 50) {
-    printCallStack(std::cerr, maxDepth);
-  }
+	void printCallStackToStdErr(unsigned int maxDepth = 50) {
+		printCallStack(std::cerr, maxDepth);
+	}
 
-  virtual void printFrameInfos(std::ostream &os, unsigned int frameId, int64_t pcAddress, std::string moduleName, std::string funcName="", int64_t symbolOffset=0, std::string fileName="", unsigned int line=0) {
-    os << std::dec << std::setfill('0') << "#" << std::setw(2) << frameId
-       << " 0x" << std::hex << std::setw(16) << pcAddress << " in ";
+	virtual void printFrameInfos(std::ostream &os, unsigned int frameId, int64_t pcAddress, std::string moduleName, std::string funcName="", int64_t symbolOffset=0, std::string fileName="", unsigned int line=0) {
+		os << red << std::dec << std::setfill('0') << "#" << std::setw(2) << frameId
+				<< white << " 0x" << std::hex << std::setw(16) << pcAddress << red << " in ";
 
-    if (funcName != "") {
-      os << funcName;
-    }
-    else {
-      os << "??";
-    }
+		os << blue;
+		if (funcName != "") {
+			os << funcName;
+		}
+		else {
+			os << "??";
+		}
 
-    if (symbolOffset != 0) {
-      os << " (+0x" << std::hex << symbolOffset << ")";
-    }
+		if (symbolOffset != 0) {
+			os << white << " (+0x" << std::hex << symbolOffset << ")";
+		}
 
-    if (fileName != "") {
-      os << " at " << fileName << ":" << std::dec << line;
-    }
+		if (fileName != "") {
+			os << red << " at " << green << fileName << ":" << std::dec << yellow << line;
+		}
 
-    if (moduleName != "") {
-      os << " from " << moduleName << std::endl;
-    }
-    else {
-      os << " from ??" << std::endl;
-    }
-  }
+		if (moduleName != "") {
+			os << red << " from " << cyan << moduleName << std::endl;
+		}
+		else {
+			os << red << " from " << green << "??" << std::endl;
+		}
+
+		os << defaultColor;
+
+	}
 
 };
 
@@ -107,19 +159,19 @@ private:
 
 class StackWalkerMinGW : public StackWalker {
 public:
-  StackWalkerMinGW();
-  ~StackWalkerMinGW();
+	StackWalkerMinGW();
+	~StackWalkerMinGW();
 
-  void printCallStack(std::ostream &os, unsigned int maxDepth = 50);
+	void printCallStack(std::ostream &os, unsigned int maxDepth = 50);
 
-  void setContext(LPCONTEXT context) {
-    this->context = context;
-  }
+	void setContext(LPCONTEXT context) {
+		this->context = context;
+	}
 
 private:
-  LPCONTEXT context;
+	LPCONTEXT context;
 #ifdef HAVE_BFD
-  std::map<std::string, BfdWrapper *> bfdMap;
+	std::map<std::string, BfdWrapper *> bfdMap;
 #endif
 };
 
@@ -127,16 +179,16 @@ private:
 
 class StackWalkerMSVC : public StackWalker {
 public:
-  StackWalkerMSVC();
-  ~StackWalkerMSVC();
+	StackWalkerMSVC();
+	~StackWalkerMSVC();
 
-  void printCallStack(std::ostream &os, unsigned int maxDepth = 50);
-  void setContext(CONTEXT *context) {
-    this->context = context;
-  }
+	void printCallStack(std::ostream &os, unsigned int maxDepth = 50);
+	void setContext(CONTEXT *context) {
+		this->context = context;
+	}
 
 private :
-  CONTEXT *context;
+	CONTEXT *context;
 };
 
 #endif
