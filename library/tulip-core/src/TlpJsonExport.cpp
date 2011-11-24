8773f17294270b7d42d7591c7b7bbec247303993
Charles Huet <packadal@gmail.com>
2011-11-24 10:52:45 +0000

    forgot to actually add the import/export files
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6136 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-core/src/TlpJsonExport.cpp
@@ -0,0 +1,219 @@
+#include <tulip/ForEach.h>
+#include <tulip/ExportModule.h>
+#include <tulip/Graph.h>
+#include <tulip/DataSet.h>
+#include <tulip/MutableContainer.h>
+#include <tulip/MethodFactory.h>
+
+#include <sstream>
+
+#include <QDate>
+
+using namespace std;
+using namespace tlp;
+
+template<typename T, typename U>
+inline ostream& serializeKeyValue(ostream& out, T key, U value) {
+  out << "\"" << key << "\":\"" << value << "\"";
+  return out;
+}
+
+template<typename TYPE>
+class NewValueIterator : public tlp::Iterator<uint> {
+public:
+  NewValueIterator(Iterator<TYPE>* iterator, const MutableContainer<uint>& newValues) : _iterator(iterator), _newValues(&newValues) {
+  }
+
+  ~NewValueIterator() {
+    delete _iterator;
+  }
+
+  virtual uint next() {
+    return _newValues->get(_iterator->next().id);
+  }
+  
+  virtual bool hasNext() {
+    return _iterator->hasNext();
+  }
+  
+private:
+  tlp::Iterator<TYPE>* _iterator;
+  const MutableContainer<uint>* _newValues;
+};
+
+class TlpJsonExport : public ExportModule {
+  public:
+    TlpJsonExport(AlgorithmContext context) : ExportModule(context) {
+    }
+    
+    virtual bool exportGraph(ostream& fileOut, Graph* graph) {
+
+      std::string comment;
+      dataSet->get<string>("comment", comment);
+
+      stringstream out;
+      
+      out << "{";
+      
+      serializeKeyValue(out, "version", "4.0") << "," << endl;
+      serializeKeyValue(out, "date", QDate::currentDate().toString(Qt::ISODate).toStdString()) << "," << endl;
+      serializeKeyValue(out, "comment", comment) << "," << endl;
+      out << "\"graph\": {" << endl;
+
+      node n;
+      edge e;
+      int i = 0;
+      forEach(n, graph->getNodes()) {
+        _newNodeId.set(n.id, i++);
+      }
+      i = 0;
+      forEach(e, graph->getEdges()) {
+        _newEdgeId.set(e.id, i++);
+      }
+      
+      saveGraph(out, graph->getRoot());
+      out << "}" << endl;
+
+      out << "}";
+      
+      fileOut << out.str();
+      
+      return true;
+    }
+
+    void saveGraph(ostream& out, Graph* graph) {
+      node n;
+      edge e;
+
+      out << "\"" << "graphID" << "\":" << graph->getId() << "," << endl;
+      if(graph == graph->getRoot()) {
+        out << "\"" << "nodesNumber" << "\":" << graph->numberOfNodes() << "," << endl;
+      }
+      else {
+        writeInterval(out, "nodesIDs", new NewValueIterator<tlp::node>(graph->getNodes(), _newNodeId));
+      }
+
+      if(graph == graph->getRoot()) {
+        out << "\"edges\":[" << endl;
+        forEach(e, graph->getEdges()) {
+          uint source = _newNodeId.get(graph->source(e).id);
+          uint target = _newNodeId.get(graph->target(e).id);
+          out << "[" << source << "," << target << "]" << (_it_foreach._it->hasNext() ? "," : "") << endl;
+        }
+        out << "]," << endl;
+      }
+      else {
+        writeInterval(out, "edgesIDs", new NewValueIterator<tlp::edge>(graph->getEdges(), _newEdgeId));
+      }
+      
+      out << "\"properties\":{" << endl;
+      //saving properties
+      PropertyInterface* property;
+      forEach(property, graph->getObjectProperties()) {
+        out << "\"" << property->getName() << "\":{" << endl;
+
+        serializeKeyValue(out, "type", property->getTypename()) << "," << endl;
+        serializeKeyValue(out, "nodeDefault", property->getNodeDefaultStringValue()) << "," << endl;
+        serializeKeyValue(out, "edgeDefault", property->getEdgeDefaultStringValue());
+        
+        Iterator<node>* nodeIt = property->getNonDefaultValuatedNodes();
+        Iterator<edge>* edgeIt = property->getNonDefaultValuatedEdges();
+        bool hasNonDefaultValuatedNodes = nodeIt->hasNext();
+        bool hasNonDefaultValuatedEdges = edgeIt->hasNext();
+        delete edgeIt;
+        delete nodeIt;
+        if(hasNonDefaultValuatedNodes) {
+          out << ",\"nodes\":{";
+          forEach(n, property->getNonDefaultValuatedNodes()) {
+            serializeKeyValue(out, _newNodeId.get(n.id), property->getNodeStringValue(n)) << (_it_foreach._it->hasNext() ? "," : "") << endl;
+          }
+          out << "}";
+        }
+        if(hasNonDefaultValuatedEdges) {
+          out << ",\"edges\":{";
+          forEach(e, property->getNonDefaultValuatedEdges()) {
+            serializeKeyValue(out, _newEdgeId.get(e.id), property->getEdgeStringValue(e)) << (_it_foreach._it->hasNext() ? "," : "") << endl;
+          }
+          out << "}";
+        }
+
+        out << "}" << (_it_foreach._it->hasNext() ? "," : "");
+      }
+      out << "},";
+
+      out << "\"attributes\":{";
+      //saving attributes
+      DataSet attributes = graph->getAttributes();
+      pair<string, DataType*> attribute;
+      forEach(attribute, attributes.getValues()) {
+        DataTypeSerializer* serializer = DataSet::typenameToSerializer(attribute.second->getTypeName());
+        out << "\"" << attribute.first << "\":";
+        out << "[ \"" << serializer->outputTypeName << "\" ,";
+        serializer->writeData(out, attribute.second);
+        out << "]";
+        out << (_it_foreach._it->hasNext() ? "," : "") << endl;
+      }
+      out << "},";
+
+      //saving subgraphs
+      out << "\"subgraphs\":{";
+      Graph* sub;
+      forEach(sub, graph->getSubGraphs()) {
+        saveGraph(out, sub);
+        out << (_it_foreach._it->hasNext() ? "," : "") << endl;
+      }
+      out << "}";
+    }
+
+    void writeInterval(ostream& out, const std::string& intervalName, Iterator<uint>* iterator) {
+      out << "\"" + intervalName + "\":[" << endl;
+      uint intervalBegin = UINT_MAX;
+      uint intervalEnd = UINT_MAX;
+      uint previousId = UINT_MAX;
+      uint currentId = UINT_MAX;
+      forEach(currentId, iterator) {
+        //we don't need/want to do all this on the first time we loop
+        if(previousId != UINT_MAX) {
+          
+          //if the ID are continuous, define an interval, otherwise witre the IDs (either intervals or single)
+          if(currentId == previousId + 1) {
+            //if we have no interval being defined, set the lower bound to the previous edge ID
+            //if an interval is being defined, set its end to the current edge ID
+            if(intervalBegin == UINT_MAX) {
+              intervalBegin = previousId;
+            }
+            intervalEnd = currentId;
+          }
+          else {
+            //if an interval is defined, write it
+            if(intervalBegin != UINT_MAX) {
+              out << "[" << intervalBegin << "," << intervalEnd << "]," << endl;
+              intervalBegin = UINT_MAX;
+              intervalEnd = UINT_MAX;
+            }
+            else {
+              out << previousId << (_it_foreach._it->hasNext() ? "," : "") << endl;
+            }
+          }
+          
+          if(!_it_foreach._it->hasNext()) {
+            if(intervalBegin != UINT_MAX) {
+              out << "[" << intervalBegin << "," << intervalEnd << "]" << endl;
+            }
+            else {
+              out << currentId << (_it_foreach._it->hasNext() ? "," : "") << endl;
+            }
+          }  
+        }
+        
+        previousId = currentId;
+      }
+      out << "]," << endl;
+    }
+
+  protected:
+    MutableContainer<uint> _newNodeId;
+    MutableContainer<uint> _newEdgeId;
+};
+
+EXPORTPLUGIN(TlpJsonExport,"TlpJsonExport","Charles Huet","18/05/2011","Tulip JSon format","1.0")
---
+++ library/tulip-core/src/TlpJsonImport.cpp
@@ -0,0 +1,391 @@
+#include <sstream>
+#include <stack>
+
+#include <tulip/Graph.h>
+#include <tulip/ImportModule.h>
+#include <tulip/PropertyInterface.h>
+#include <tulip/ForEach.h>
+#include <tulip/MethodFactory.h>
+
+#include <tulip/YajlFacade.h>
+
+using namespace std;
+using namespace tlp;
+
+class TlpJsonGraphParser : public YajlFacade {
+public:
+  TlpJsonGraphParser(tlp::Graph* parentGraph) :
+  _parsingEdges(false),
+  _parsingNodes(false),
+  _newEdge(false),
+  _edgeSource(UINT_MAX),
+  _parsingNodesIds(false),
+  _parsingEdgesIds(false),
+  _parsingInterval(false),
+  _newInterval(false),
+  _intervalSource(UINT_MAX),
+  _graph(parentGraph),
+  _dataSet(&const_cast<DataSet&>(parentGraph->getAttributes())),
+  _parsingAttributes(false),
+  _currentAttributeName(std::string()),
+  _currentAttributeTypeName(std::string()),
+  _parsingProperties(false),
+  _currentProperty(NULL),
+  _propertyName(std::string()),
+  _currentIdentifier(UINT_MAX),
+  _parsingPropertyType(false),
+  _parsingPropertyNodeValues(false),
+  _parsingPropertyEdgeValues(false),
+  _parsingPropertyDefaultEdgeValue(false),
+  _parsingPropertyDefaultNodeValue(false),
+  _waitingForGraphId(false)
+  {
+  }
+  
+  virtual void parseEndArray() {
+    //if the current array was not an edge but was the array of edges, we are done parsing edges
+    if(!_newEdge && _parsingEdges)  {
+      _parsingEdges = false;
+    }
+    if(_newEdge) {
+      _newEdge = false;
+    }
+    
+    if((_parsingNodesIds || _parsingEdgesIds) && !_newInterval) {
+      _parsingNodesIds = false;
+      _parsingEdgesIds = false;
+    }
+    if(!_newInterval) {
+      _parsingInterval = false;
+    }
+    if(_newInterval) {
+      _newInterval = false;
+    }
+  }
+  
+  virtual void parseStartArray() {
+    if(_parsingEdges)  {
+      _newEdge = true;
+    }
+
+    if(_parsingInterval) {
+      _newInterval = true;
+    }
+    
+    if(_parsingNodesIds || _parsingEdgesIds) {
+      _parsingInterval = true;
+    }
+  }
+  
+  virtual void parseMapKey(const std::string& value) {
+    if(_parsingProperties && !_parsingPropertyNodeValues && !_parsingPropertyEdgeValues && !_parsingPropertyDefaultEdgeValue && !_parsingPropertyDefaultNodeValue && _propertyName.empty()) {
+      _propertyName = value;
+//         std::cout << "now parsing property: " << propertyName << std::endl;
+    }
+
+    if(value == "graphID") {
+      _waitingForGraphId = true;
+    }
+    else if(value == "nodesIDs") {
+      _parsingNodesIds = true;
+    }
+    else if(value == "edgesIDs") {
+      _parsingEdgesIds = true;
+    }
+    else if(value == "edges")  {
+      _parsingEdges = true;
+    }
+    else if(value == "attributes") {
+      _parsingAttributes = true;
+    }
+    else if(_currentProperty && value == "nodes") {
+      _parsingPropertyNodeValues = true;
+    }
+    else if(_currentProperty && value == "edges") {
+      _parsingPropertyEdgeValues = true;
+    }
+    else if(value == "edgeDefault") {
+      _parsingPropertyDefaultEdgeValue = true;
+    }
+    else if (value == "nodeDefault") {
+      _parsingPropertyDefaultNodeValue = true;
+    }
+    else if (value == "properties") {
+      _parsingProperties = true;
+    }
+    else if(value == "type") {
+      _parsingPropertyType = true;
+    }
+    else if(value == "nodesNumber") {
+      _parsingNodes = true;
+    }
+    else if(_parsingPropertyNodeValues || _parsingPropertyEdgeValues) {
+      _currentIdentifier = atoi(value.c_str());
+    }
+    else if(_parsingAttributes) {
+      _currentAttributeName = value;
+    }
+    else if(value == "subgraphs") {
+      _parsingSubgraph.push(0);
+    }
+  }
+
+  virtual void parseStartMap() {
+    if(!_parsingSubgraph.empty()) {
+      ++_parsingSubgraph.top();
+    }
+  }
+  
+  virtual void parseEndMap() {
+    if(_currentProperty && _propertyName.empty()) {
+      _currentProperty = NULL;
+      _parsingProperties = false;
+    }
+    if(!_parsingPropertyNodeValues && !_parsingPropertyEdgeValues && !_propertyName.empty()) {
+      _propertyName = string();
+//       cout << "finished parsing property " << propertyName << ", popping it" << endl;
+    }
+    if(_parsingPropertyNodeValues) {
+      _parsingPropertyNodeValues = false;
+    }
+    if(_parsingPropertyEdgeValues) {
+      _parsingPropertyEdgeValues = false;
+    }
+    if(_parsingAttributes) {
+      _parsingAttributes = false;
+    }
+    if(_parsingEdgesIds) {
+      _parsingEdgesIds = false;
+    }
+    if(_parsingNodesIds) {
+      _parsingNodesIds = false;
+    }
+    if(!_parsingSubgraph.empty()) {
+      --_parsingSubgraph.top();
+
+      if(_parsingSubgraph.top() == 0) {
+        _parsingSubgraph.pop();
+        _graph = _graph->getSuperGraph();
+      }
+    }
+  }
+  
+  virtual void parseInteger(long long integerVal) {
+    if(_waitingForGraphId) {
+      if(integerVal > 0) {
+        _graph = _graph->addSubGraph(0, integerVal);
+        _dataSet = &const_cast<DataSet&>(_graph->getAttributes());
+      }
+      _waitingForGraphId = false;
+    }
+    
+    if(_parsingNodes)  {
+      for(int i = 0; i < integerVal; ++i) {
+        _graph->addNode();
+      }
+      _parsingNodes = false;
+    }
+    
+    //if the int is the source or target of an edge
+    if(_newEdge) {
+      //if the source has not been set, this int is the source
+      if(_edgeSource == UINT_MAX) {
+        _edgeSource = integerVal;
+      }
+      //if the source has been set, this int is the target
+      else {
+        node source(_edgeSource);
+        node target(integerVal);
+        _graph->addEdge(source, target);
+        _edgeSource = UINT_MAX;
+      }
+    }
+
+    if(_parsingInterval) {
+      if(_newInterval) {
+        if(_intervalSource == UINT_MAX) {
+          _intervalSource = integerVal;
+        }
+        else {
+          for(uint id = _intervalSource; id <= integerVal; ++id) {
+            if(_parsingEdgesIds) {
+              edge e(id);
+              _graph->addEdge(e);
+            }
+            if(_parsingNodesIds) {
+              node n(id);
+              _graph->addNode(n);
+            }
+          }
+          _intervalSource = UINT_MAX;
+        }
+      }
+      else {
+        if(_parsingEdgesIds) {
+          edge e(integerVal);
+          _graph->addEdge(e);
+        }
+        if(_parsingNodesIds) {
+          node n(integerVal);
+          _graph->addNode(n);
+        }
+      }
+    }
+  }
+  
+  virtual void parseString(const std::string& value) {
+    if(_parsingProperties) {
+      if(_parsingPropertyType && !_propertyName.empty()) {
+        _parsingPropertyType = false;
+  //       std::cout << "getting new property: " << propertyName << "(" << value << ")" << std::sendl;
+  //       std::cout << "of type: " << value << std::endl;
+        _currentProperty = _graph->getProperty(_propertyName, value);
+
+        if(!_currentProperty) {
+          cout << "error, property is null" << endl;
+        }
+
+      }
+      if(_parsingPropertyDefaultNodeValue) {
+        _currentProperty->setAllNodeStringValue(value);
+        _parsingPropertyDefaultNodeValue = false;
+  //       std::cout << "of default node value: " << value << std::endl;
+      }
+      if(_parsingPropertyDefaultEdgeValue) {
+        _currentProperty->setAllEdgeStringValue(value);
+        _parsingPropertyDefaultEdgeValue = false;
+  //       std::cout << "of default edge value: " << value << std::endl;
+      }
+
+      if(_parsingPropertyNodeValues) {
+        assert(_currentIdentifier != UINT_MAX);
+        assert(_currentProperty);
+        node n(_currentIdentifier);
+        _currentProperty->setNodeStringValue(n, value);
+  //       cout << "setting node value: " << n.id << " " << value << "; "<< currentProperty->getNodeStringValue(n) << endl;;
+      }
+      if(_parsingPropertyEdgeValues) {
+        assert(_currentIdentifier != UINT_MAX);
+        assert(_currentProperty);
+        edge e(_currentIdentifier);
+        _currentProperty->setEdgeStringValue(e, value);
+      }
+    }
+    
+    if(_parsingAttributes) {
+      if(_currentAttributeTypeName.empty()) {
+        _currentAttributeTypeName = value;
+      }
+      else {
+        stringstream data;
+        if(_currentAttributeTypeName == "string") {
+          data << "\"" << value << "\"";
+        }
+        else {
+          data << value;
+        }
+
+        if(_dataSet->exist(_currentAttributeName)) {
+          std::cout << "error, attribute '" << _currentAttributeName << "' already exists" << std::endl;
+        }
+        else {
+          bool result = _dataSet->readData(data, _currentAttributeName, _currentAttributeTypeName);
+          _currentAttributeTypeName = string();
+          if(!result) {
+            std::cout << "error reading attribute: " << _currentAttributeName << " of type '" << _currentAttributeTypeName << "' and value: " << data.str() << std::endl;
+          }
+        }
+      }
+    }
+  }
+
+private:
+  std::stack<uint> _parsingSubgraph;
+  bool _parsingEdges;
+  bool _parsingNodes;
+  /**
+   * @brief indicates whether the parser is currently parsing an array representing an edge
+   **/
+  bool _newEdge;
+  uint _edgeSource;
+
+  bool _parsingNodesIds;
+  bool _parsingEdgesIds;
+  bool _parsingInterval;
+  bool _newInterval;
+  uint _intervalSource;
+
+  tlp::Graph* _graph;
+
+  tlp::DataSet* _dataSet;
+  
+  bool _parsingAttributes;
+  std::string _currentAttributeName;
+  std::string _currentAttributeTypeName;
+  
+  bool _parsingProperties;
+  PropertyInterface* _currentProperty;
+  std::string _propertyName;
+  uint _currentIdentifier;
+  bool _parsingPropertyType;
+  bool _parsingPropertyNodeValues;
+  bool _parsingPropertyEdgeValues;
+  bool _parsingPropertyDefaultEdgeValue;
+  bool _parsingPropertyDefaultNodeValue;
+
+  bool _waitingForGraphId;
+};
+
+class YajlProxy : public YajlFacade {
+  public:
+    virtual void parseBoolean(bool boolVal) { _proxy->parseBoolean(boolVal); }
+    virtual void parseDouble(double doubleVal) { _proxy->parseDouble(doubleVal); }
+    virtual void parseEndArray() { _proxy->parseEndArray(); }
+    virtual void parseEndMap() { _proxy->parseEndMap(); }
+    virtual void parseInteger(long long integerVal) { _proxy->parseInteger(integerVal); }
+    virtual void parseMapKey(const std::string& value) { _proxy->parseMapKey(value); }
+    virtual void parseNull() { _proxy->parseNull(); }
+    virtual void parseNumber(const char* numberVal, size_t numberLen) { _proxy->parseNumber(numberVal, numberLen); }
+    virtual void parseStartArray() { _proxy->parseStartArray(); }
+    virtual void parseStartMap() { _proxy->parseStartMap(); }
+    virtual void parseString(const std::string& value) { _proxy->parseString(value); }
+  protected:
+    YajlFacade* _proxy;
+};
+
+class TlpJsonImport : public ImportModule, YajlProxy {
+public:
+  TlpJsonImport(AlgorithmContext context) :ImportModule(context), parsingSubGraphs(false), openMaps(0) {
+    addParameter<std::string>("file::filename", "");
+  }
+
+  virtual bool import() {
+    std::string filename;
+    std::string data;
+    
+    if(dataSet->exist("file::filename")){
+      dataSet->get<string>("file::filename", filename);
+
+      _proxy = new YajlFacade();
+//       QTime t = QTime::currentTime();
+      parse(filename);
+//       cout << "msecs: " << t.msecsTo(QTime::currentTime()) << endl;
+    }
+
+    return true;
+  }
+
+  virtual void parseMapKey(const std::string& value) {
+    if(value == "graph") {
+      delete _proxy;
+      _proxy = new TlpJsonGraphParser(graph);
+    }
+    YajlProxy::parseMapKey(value);
+  }
+  
+private:
+  bool parsingSubGraphs;
+  uint openMaps;
+};
+
+IMPORTPLUGIN(TlpJsonImport,"TlpJsonImport","Charles Huet","18/05/2011","Tulip JSon format","1.0")
