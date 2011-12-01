43d2e8f4482c0c775d5bdc54c36f20d434862449
Charles Huet <packadal@gmail.com>
2011-12-01 08:21:48 +0000

    removed extraneous semicolon (pedantic warning)
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6211 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/include/tulip/GlMainWidget.h
+++ library/tulip-qt/include/tulip/GlMainWidget.h
@@ -79,7 +79,7 @@ public:
     RenderGraph=0x1, /** Force to render the graph even if there is a previous buffered render. You need to call this option if the graph is updated to regenerate the buffer. If not set try to use the last buffered graph render, if there is no valid buffer this flag is forced. **/
     SwapBuffers=0x2 /** Call the swapBuffer functions at the end of the rendering process. If the disabled it's up to you to call the swapBuffer function. **/
   };
-  Q_DECLARE_FLAGS ( RenderingOptions, RenderingOption );
+  Q_DECLARE_FLAGS ( RenderingOptions, RenderingOption )
 
   /**
    * Construct the GlMainWidget
