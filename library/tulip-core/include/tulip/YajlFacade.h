b03165291ca57dab268c21af98e6134829567672
Charles Huet <packadal@gmail.com>
2011-12-06 10:23:15 +0000

    rewrote the JSON export to use YAJL (free indentation, UTF8 validation and the code is easier to read)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6262 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/YajlFacade.h
+++ library/tulip-core/include/tulip/YajlFacade.h
@@ -1,14 +1,17 @@
 #include <yajl_parse.h>
+#include <yajl_gen.h>
 #include <string>
 
 /**
- * @brief A Simple C++ wrapper around the C library 'yajl'.
+ * @brief A Simple C++ wrapper around the C library 'yajl' parsing capabilities.
  *
  * It uses callbacks to signal what is being parsed (map start and end, strings, ...), and this class is intended to be subclassed,
  * with the subclass overriding the callbacks to process the events.
  **/
-class YajlFacade {
+class YajlParseFacade {
 public:
+  YajlParseFacade() : _parsingSucceeded(true) {}
+  
   /**
    * @brief Parses a JSON file.
    * Once this function is called, the callbacks (all the parse* functions) will get called when the corresponding event happens.
@@ -29,4 +32,32 @@ public:
   virtual void parseEndMap();
   virtual void parseStartArray();
   virtual void parseEndArray();
+
+  bool parsingSucceeded() const;
+  std::string errorMessage() const;
+
+protected:
+  bool _parsingSucceeded;
+  std::string _errorMessage;
+};
+
+class YajlWriteFacade {
+public:
+  YajlWriteFacade();
+  ~YajlWriteFacade();
+
+  std::string generatedString();
+  
+  virtual void writeInteger(long long int number);
+  virtual void writeDouble(double number);
+  virtual void writeNumber(const char* str, size_t len);
+  virtual void writeString(const std::string& text);
+  virtual void writeNull();
+  virtual void writeBool(bool value);
+  virtual void writeMapOpen();
+  virtual void writeMapClose();
+  virtual void writeArrayOpen();
+  virtual void writeArrayClose();
+protected:
+  yajl_gen _generator;
 };
--- library/tulip-core/src/YajlFacade.cpp
+++ library/tulip-core/src/YajlFacade.cpp
@@ -6,25 +6,25 @@
 #include <iostream>
 
 static int parse_null(void *ctx) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseNull();
   return 1;
 }
 
 static int parse_boolean(void * ctx, int boolVal) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseBoolean(boolVal);
   return 1;
 }
 
 static int parse_integer(void *ctx, long long integerVal) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseInteger(integerVal);
   return 1;
 }
 
 static int parse_double(void *ctx, double doubleVal) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseDouble(doubleVal);
   return 1;
 }
@@ -37,7 +37,7 @@ static int parse_double(void *ctx, double doubleVal) {
 // }
 
 static int parse_string(void *ctx, const unsigned char * stringVal, size_t stringLen) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
 
   char * str = (char *) malloc(stringLen + 1);
   str[stringLen] = 0;
@@ -49,7 +49,7 @@ static int parse_string(void *ctx, const unsigned char * stringVal, size_t strin
 }
 
 static int parse_map_key(void *ctx, const unsigned char * stringVal, size_t stringLen) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
 
   char * str = (char *) malloc(stringLen + 1);
   str[stringLen] = 0;
@@ -61,30 +61,30 @@ static int parse_map_key(void *ctx, const unsigned char * stringVal, size_t stri
 }
 
 static int parse_start_map(void *ctx) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseStartMap();
   return 1;
 }
 
 static int parse_end_map(void *ctx) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseEndMap();
   return 1;
 }
 
 static int parse_start_array(void *ctx) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseStartArray();
   return 1;
 }
 
 static int parse_end_array(void *ctx) {
-  YajlFacade* facade = (YajlFacade*) ctx;
+  YajlParseFacade* facade = (YajlParseFacade*) ctx;
   facade->parseEndArray();
   return 1;
 }
 
-void YajlFacade::parse(std::string filename) {
+void YajlParseFacade::parse(std::string filename) {
   QFile file(filename.c_str());
   file.open(QIODevice::ReadOnly);
   QByteArray contents = file.readAll();
@@ -109,7 +109,8 @@ void YajlFacade::parse(std::string filename) {
 
   if (stat != yajl_status_ok) {
     unsigned char * str = yajl_get_error(hand, 1, fileData, contents.length());
-    std::cout << (const char *)str << std::endl;
+    _parsingSucceeded = false;
+    _errorMessage = std::string((const char *)str);
     yajl_free_error(hand, str);
   }
 
@@ -117,35 +118,107 @@ void YajlFacade::parse(std::string filename) {
   yajl_free(hand);
 }
 
-void YajlFacade::parseBoolean(bool) {
+bool YajlParseFacade::parsingSucceeded() const {
+  return _parsingSucceeded;
 }
 
-void YajlFacade::parseDouble(double) {
+std::string YajlParseFacade::errorMessage() const {
+  return _errorMessage;
 }
 
-void YajlFacade::parseEndArray() {
+
+void YajlParseFacade::parseBoolean(bool) {
 }
 
-void YajlFacade::parseEndMap() {
+void YajlParseFacade::parseDouble(double) {
 }
 
-void YajlFacade::parseInteger(long long) {
+void YajlParseFacade::parseEndArray() {
 }
 
-void YajlFacade::parseMapKey(const std::string&) {
+void YajlParseFacade::parseEndMap() {
 }
 
-void YajlFacade::parseNull() {
+void YajlParseFacade::parseInteger(long long) {
 }
 
-void YajlFacade::parseNumber(const char*, size_t) {
+void YajlParseFacade::parseMapKey(const std::string&) {
 }
 
-void YajlFacade::parseStartArray() {
+void YajlParseFacade::parseNull() {
 }
 
-void YajlFacade::parseStartMap() {
+void YajlParseFacade::parseNumber(const char*, size_t) {
 }
 
-void YajlFacade::parseString(const std::string&) {
+void YajlParseFacade::parseStartArray() {
+}
+
+void YajlParseFacade::parseStartMap() {
+}
+
+void YajlParseFacade::parseString(const std::string&) {
+}
+
+/** ============================================================= **/
+
+YajlWriteFacade::YajlWriteFacade() {
+  _generator = yajl_gen_alloc(NULL);
+  yajl_gen_config(_generator, yajl_gen_validate_utf8, 1);
+  yajl_gen_config(_generator, yajl_gen_beautify, 1);
+}
+
+YajlWriteFacade::~YajlWriteFacade() {
+  yajl_gen_free(_generator);
+}
+
+void YajlWriteFacade::writeInteger(long long int number) {
+  yajl_gen_integer(_generator, number);
+}
+
+void YajlWriteFacade::writeDouble(double number) {
+  yajl_gen_double(_generator, number);
+}
+
+void YajlWriteFacade::writeNumber(const char* str, size_t len) {
+  yajl_gen_number(_generator, str, len);
+}
+
+void YajlWriteFacade::writeString(const std::string& text) {
+  yajl_gen_string(_generator, (unsigned char*)text.data(), text.size());
+}
+
+void YajlWriteFacade::writeNull() {
+  yajl_gen_null(_generator);
+}
+
+void YajlWriteFacade::writeBool(bool value) {
+  yajl_gen_bool(_generator, value);
+}
+
+void YajlWriteFacade::writeMapOpen() {
+  yajl_gen_map_open(_generator);
+}
+
+void YajlWriteFacade::writeMapClose() {
+  yajl_gen_map_close(_generator);
+}
+
+void YajlWriteFacade::writeArrayOpen() {
+  yajl_gen_array_open(_generator);
+}
+
+void YajlWriteFacade::writeArrayClose() {
+  yajl_gen_array_close(_generator);
+}
+
+std::string YajlWriteFacade::generatedString() {
+  size_t length;
+  const unsigned char* buffer;
+  yajl_gen_status status = yajl_gen_get_buf(_generator, &buffer, &length);
+  if(status != yajl_gen_status_ok) {
+    std::cout << "oh, shit." << std::endl;
+  }
+  std::string result((const char*)buffer);
+  return result;
 }
