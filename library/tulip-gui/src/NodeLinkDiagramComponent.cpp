6f14a85b329082b214bd47f61c51131a6e4620c2
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-01-02 08:15:18 +0000

    Code cleanup and method renaming in node link diagram component
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6384 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/NodeLinkDiagramComponent.cpp
+++ library/tulip-gui/src/NodeLinkDiagramComponent.cpp
@@ -37,6 +37,7 @@ void NodeLinkDiagramComponent::setState(const tlp::DataSet& data) {
   getGlMainWidget()->setData(graph(), data);
   registerTriggers();
   connect(getGlMainWidget(),SIGNAL(viewDrawn(GlMainWidget*,bool)),this,SLOT(glMainWidgetDrawn(GlMainWidget *,bool)));
+
   this->drawOverview(true);
 }
 
