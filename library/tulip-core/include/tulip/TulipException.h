d6985e51e5eb2cbd29212340d038521a02166ebc
Patrick Mary <mary@labri.fr>
2012-01-07 07:55:54 +0000

    add forgotten file in previous commit about Observable
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6429 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-core/include/tulip/TulipException.h
@@ -0,0 +1,45 @@
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+#ifndef TULIPEXCEPTION_H
+#define TULIPEXCEPTION_H
+
+#include <tulip/tulipconf.h>
+#include <string>
+
+namespace tlp {
+/*@{*/
+//=======================================
+/**
+ * @class TulipException
+ * @brief TulipException is a basic class to build exceptions from string
+ **/
+class TLP_SCOPE TulipException : public std::exception {
+public:
+  TulipException(const std::string &desc):desc(desc) {
+  }
+  virtual ~TulipException() throw () {
+  }
+  virtual const char* what() const throw() {
+    return desc.c_str();
+  }
+private:
+  std::string desc;
+};
+}
+#endif // TULIPEXCEPTION
