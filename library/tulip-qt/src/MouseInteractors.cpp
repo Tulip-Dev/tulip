ecf6068c92863da328c37d2bc76e99c2637c2567
tulipsoftware <tulipsoftware@mycompany.com>
2012-01-20 04:01:47 +0000

    [Jenkins] Astyle nightly commit
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6536 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-qt/src/MouseInteractors.cpp
+++ library/tulip-qt/src/MouseInteractors.cpp
@@ -45,9 +45,11 @@ bool MousePanNZoomNavigator::eventFilter(QObject *widget, QEvent *e) {
       (((QWheelEvent *) e)->orientation() == Qt::Vertical)) {
 
     GlMainWidget *g = (GlMainWidget *) widget;
+
     if(((QWheelEvent *) e)->delta() < 0 && g->getScene()->getCamera().getZoomFactor() < 0.5f) {
       return true;
     }
+
     g->getScene()->zoomXY(((QWheelEvent *) e)->delta() / WHEEL_DELTA,
                           ((QWheelEvent *) e)->x(), ((QWheelEvent *) e)->y());
     g->draw(false);
