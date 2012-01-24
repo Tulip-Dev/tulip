3c4845a025931ede90c70f2b41cddb656ab024fd
Jonathan Dubois <jonathan.dubois@labri.fr>
2012-01-24 16:42:48 +0000

    Add missing files
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6581 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/include/tulip/cxx/PropertyToQVariantMapper.cxx
@@ -0,0 +1,52 @@
+namespace tlp {
+
+template<typename PropertyClass, typename NodeType, typename EdgeType>
+QVariant StandardPropertyToQVariantConverter<PropertyClass,NodeType,EdgeType>::getValue(unsigned int id,tlp::ElementType elementType,tlp::PropertyInterface* property) const {
+    if(elementType == tlp::NODE) {
+      return QVariant::fromValue<NodeType>(static_cast<PropertyClass*>(property)->getNodeValue(tlp::node(id)));
+    }
+    else {
+      return QVariant::fromValue<EdgeType>(static_cast<PropertyClass*>(property)->getEdgeValue(tlp::edge(id)));
+    }
+  }
+
+  template<typename PropertyClass, typename NodeType, typename EdgeType>
+  bool StandardPropertyToQVariantConverter<PropertyClass,NodeType,EdgeType>::setValue(unsigned int id,tlp::ElementType elementType,tlp::PropertyInterface* property,const QVariant& data) const {
+    PropertyClass* p = static_cast<PropertyClass*>(property);
+
+    if(elementType == tlp::NODE) {
+      const NodeType& oldValue = p->getNodeValue(tlp::node(id));
+      NodeType newValue = data.value<NodeType>();
+
+      if(oldValue != newValue) {
+        p->setNodeValue(tlp::node(id),newValue);
+        return true;
+      }
+    }
+    else {
+      const EdgeType& oldValue = p->getEdgeValue(tlp::edge(id));
+      EdgeType newValue = data.value<EdgeType>();
+
+      if(oldValue != newValue) {
+        p->setEdgeValue(tlp::edge(id),data.value<EdgeType>());
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  template<typename PropertyClass, typename NodeType, typename EdgeType>
+  bool StandardPropertyToQVariantConverter<PropertyClass,NodeType,EdgeType>::setAllValue(tlp::ElementType elementType,tlp::PropertyInterface* property,const QVariant& data) const {
+    PropertyClass* p = static_cast<PropertyClass*>(property);
+
+    if(elementType == tlp::NODE) {
+      p->setAllNodeValue(data.value<NodeType>());
+    }
+    else {
+      p->setAllEdgeValue(data.value<EdgeType>());
+    }
+
+    return true;
+  }
+}
