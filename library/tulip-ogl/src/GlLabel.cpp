6c56f9777abc329fcea866a620324353288dc7f0
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2012-01-26 10:22:55 +0000

    - Fix bug #3479994   - I have an application that shows two graphs side to side, and one of them has multiline labels.     I set the same exact settings on the rendering parammeters, and the labels show up with very different sizes.   - Modify algorithm for multilines labels
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6605 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlLabel.cpp
+++ library/tulip-ogl/src/GlLabel.cpp
@@ -299,8 +299,9 @@ void GlLabel::draw(float lod, Camera *camera) {
   float screenH;
   float multiLineH=h;
 
+  //Here the 4.5 number is the size of space between two lines
   if(textVector.size()>1)
-    multiLineH=(h-(textVector.size()-1)*10)/textVector.size();
+    multiLineH=(h-(textVector.size()-1)*4.5)/textVector.size();
 
   if(camera) {
     //Here we compute the deformation of the bounding box when we have a rotation on the camera
