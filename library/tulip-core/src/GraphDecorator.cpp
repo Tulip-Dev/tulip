ca154c8eca7c5b7037f87417aad4f41b540ea146
tulipsoftware <tulipsoftware@mycompany.com>
2012-02-01 19:01:15 +0000

    [Jenkins] Astyle nightly commit
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6667 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/GraphDecorator.cpp
+++ library/tulip-core/src/GraphDecorator.cpp
@@ -115,7 +115,7 @@ Graph* GraphDecorator::getDescendantGraph(unsigned int sgId) const {
   return graph_component->getDescendantGraph(sgId);
 }
 //=========================================================================
-Graph* GraphDecorator::getDescendantGraph(const std::string &name) const{
+Graph* GraphDecorator::getDescendantGraph(const std::string &name) const {
   return graph_component->getDescendantGraph(name);
 }
 //============================================================
