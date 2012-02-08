28f271bd6a72b68dff8b89254a9f011f326781f7
Antoine Lambert <antoine.lambert33@gmail.com>
2012-02-08 13:01:21 +0000

    - fix FindGLEW.cmake : if GLEW has already been found, don't execute the headers and library search again - it fixes compilation error when compiling external plugins from the Tulip trunk (due to headers path being incorrectly redefined)
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6709 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- FindGLEW.cmake
+++ FindGLEW.cmake
@@ -8,49 +8,52 @@
 #   
 
 IF (GLEW_INCLUDE_DIR)
+
   # Already in cache, be silent
+  SET(GLEW_FOUND TRUE)
   SET(GLEW_FIND_QUIETLY TRUE)
-ENDIF (GLEW_INCLUDE_DIR)
 
-if( WIN32 )
-   if( MSVC80 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC" )
-   endif( MSVC80 )
-   if( MSVC71 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
-   endif( MSVC71 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS c:/glew/include ${COMPILER_PATH}/PlatformSDK/Include
-              PATH_SUFFIXES gl GL )
-   SET( GLEW_NAMES glew32 glew)
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS c:/glew/lib ${COMPILER_PATH}/PlatformSDK/Lib )
-else( WIN32 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS /usr/local/include /usr/include
-              PATH_SUFFIXES gl GL )
-   SET( GLEW_NAMES glew GLEW )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS /usr/lib /usr/local/lib )
-endif( WIN32 )
+ELSE (GLEW_INCLUDE_DIR)
 
-GET_FILENAME_COMPONENT( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} PATH )
-IF(GLEW_INCLUDE_DIR)
+  IF( WIN32 )
+    IF( MSVC80 )
+	SET( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC" )
+    ENDIF( MSVC80 )
+    IF( MSVC71 )
+	SET( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
+    ENDIF( MSVC71 )
+    FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
+	       PATHS c:/glew/include ${COMPILER_PATH}/PlatformSDK/Include
+               PATH_SUFFIXES gl GL )
+    SET( GLEW_NAMES glew32 glew)
+    FIND_LIBRARY( GLEW_LIBRARY
+		  NAMES ${GLEW_NAMES}
+		  PATHS c:/glew/lib ${COMPILER_PATH}/PlatformSDK/Lib )
+  ELSE( WIN32 )
+    FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
+	       PATHS /usr/local/include /usr/include
+               PATH_SUFFIXES gl GL )
+    SET( GLEW_NAMES glew GLEW )
+    FIND_LIBRARY( GLEW_LIBRARY
+                  NAMES ${GLEW_NAMES}
+                  PATHS /usr/lib /usr/local/lib )
+  ENDIF( WIN32 )
+
+  GET_FILENAME_COMPONENT( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} PATH )	
+  IF(GLEW_INCLUDE_DIR)
     # Remove the GL or gl subdirectory, because you should say #include <GL/foo.h>, not #include <foo.h>
-    set(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/..)
-ENDIF()
+    SET(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/..)
+  ENDIF()
 
-IF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET(GLEW_FOUND TRUE)
+  IF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+    SET(GLEW_FOUND TRUE)
     #SET( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} )
-ELSE (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET( GLEW_FOUND FALSE )
-   SET( GLEW_LIBRARY_DIR )
-ENDIF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+  ELSE (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+    SET( GLEW_FOUND FALSE )
+    SET( GLEW_LIBRARY_DIR )
+  ENDIF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
 
-MARK_AS_ADVANCED(
-  GLEW_LIBRARY
-  GLEW_INCLUDE_DIR
-)
+  MARK_AS_ADVANCED(GLEW_LIBRARY
+		   GLEW_INCLUDE_DIR)
+
+ENDIF(GLEW_INCLUDE_DIR)
