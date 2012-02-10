116cd1e73119edb60686219169b23e273470c317
Antoine Lambert <antoine.lambert33@gmail.com>
2012-02-10 15:23:34 +0000

    - add a new line at the end of a script code only if it is not already present
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_3_7_maint@6729 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/pythonscriptview/PythonScriptViewWidget.cpp
+++ plugins/view/pythonscriptview/PythonScriptViewWidget.cpp
@@ -96,19 +96,31 @@ PythonScriptViewWidget::PythonScriptViewWidget(PythonScriptView *view, QWidget *
 }
 
 std::string PythonScriptViewWidget::getCurrentMainScriptCode() const {
-  return static_cast<PythonCodeEditor *>(mainScriptsTabWidget->currentWidget())->toPlainText().replace("\r\n", "\n").toStdString() + "\n";
+  std::string code = static_cast<PythonCodeEditor *>(mainScriptsTabWidget->currentWidget())->toPlainText().replace("\r\n", "\n").toStdString();
+  if (code[code.size()-1] != '\n')
+      code += "\n";
+  return  code;
 }
 
 std::string PythonScriptViewWidget::getMainScriptCode(int idx) const {
-  return static_cast<PythonCodeEditor *>(mainScriptsTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString() + "\n";
+  std::string code = static_cast<PythonCodeEditor *>(mainScriptsTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString();
+  if (code[code.size()-1] != '\n')
+      code += "\n";
+  return  code;
 }
 
 std::string PythonScriptViewWidget::getModuleCode(int idx) const {
-  return static_cast<PythonCodeEditor *>(modulesTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString() + "\n";
+  std::string code = static_cast<PythonCodeEditor *>(modulesTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString();
+  if (code[code.size()-1] != '\n')
+      code += "\n";
+  return  code;
 }
 
 std::string PythonScriptViewWidget::getPluginCode(int idx) const {
-  return static_cast<PythonCodeEditor *>(pluginsTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString() + "\n";
+  std::string code = static_cast<PythonCodeEditor *>(pluginsTabWidget->widget(idx))->toPlainText().replace("\r\n", "\n").toStdString();
+  if (code[code.size()-1] != '\n')
+      code += "\n";
+  return  code;
 }
 
 void PythonScriptViewWidget::resizeEvent(QResizeEvent *e) {
