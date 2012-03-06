595924d8ce0988f74292d8c70c80ad9b3f212967
Charles Huet <packadal@gmail.com>
2012-03-06 15:22:43 +0000

    removed unused code
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6876 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/library/tulip/importexporttest.cpp
+++ unit_test/library/tulip/importexporttest.cpp
@@ -33,9 +33,6 @@ CPPUNIT_TEST_SUITE_REGISTRATION(ImportExportTest);
 
 // ImportExportTest::ImportExportTest() :importAlgorithm("tlp"), exportAlgorithm("tlp") {
 ImportExportTest::ImportExportTest() :importAlgorithm("TlpJsonImport"), exportAlgorithm("TlpJsonExport") {
-  // because we are testing the tulip core library
-  // the only needed plugins are those embedded in that library
-  //PluginLibraryLoader::loadPlugins();
 }
 
 void ImportExportTest::setUp() {
