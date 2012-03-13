3fd8fd9c55c6868d6d95d20fb642d4ea945caa18
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-03-13 15:28:40 +0000

    Temporary UI fix for DataSet mode
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@6960 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/SpreadViewTableWidget.cpp
+++ plugins/perspective/GraphPerspective/src/SpreadViewTableWidget.cpp
@@ -36,7 +36,6 @@ SpreadViewTableWidget::SpreadViewTableWidget(QWidget *parent) :
   //Filtering
   connect(ui->showOnlySelectedElementsCheckBox,SIGNAL(clicked()),this,SLOT(filterElements()));
   connect(ui->filterPatternLineEdit,SIGNAL(returnPressed()),this,SLOT(filterElements()));
-  connect(ui->filterPushButton,SIGNAL(clicked()),this,SLOT(filterElements()));
 #if (QT_VERSION >= QT_VERSION_CHECK(4, 7, 0))
   ui->filterPatternLineEdit->setPlaceholderText(QApplication::translate("SpreadViewTableWidget", "Input a filter pattern", 0, QApplication::UnicodeUTF8));
 #endif
