b4d23812145bd5b31c63e73deb7c4251660c399f
Patrick Mary <mary@labri.fr>
2012-03-19 08:56:16 +0000

    speed up the import in Tulip JSON format and reduce the memory space consumption using the Graph::reserveNodes & Graph::reserveEdges methods
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7010 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/JsonTokens.h
+++ library/tulip-core/include/tulip/JsonTokens.h
@@ -8,6 +8,7 @@ const std::string GraphIDToken = "graphID";
 const std::string NodesIDsToken = "nodesIDs";
 const std::string NodesNumberToken = "nodesNumber";
 const std::string EdgesToken = "edges";
+const std::string EdgesNumberToken = "edgesNumber";
 const std::string EdgesValuesToken = "edgesValues";
 const std::string NodesValuesToken = "nodes";
 const std::string AttributesToken = "attributes";
