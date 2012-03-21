14bc0910fa7628738a1ecef91e62d48d1f2ab50a
Antoine Lambert <antoine.lambert33@gmail.com>
2012-03-21 13:30:35 +0000

    - port Python Script View to Tulip 4 (only the Python plugins stuff is disabled for the moment) - activate its compilation and also the compilation of the bindings
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7051 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ plugins/view/pythonscriptview/PythonScriptViewWidget.h
@@ -0,0 +1,100 @@
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+
+#ifndef PYTHONSCRIPTVIEWWIDGET_H_
+#define PYTHONSCRIPTVIEWWIDGET_H_
+
+#include "ui_PythonScriptViewWidget.h"
+#include <QtGui/QAction>
+
+#include "PythonCodeEditor.h"
+
+#include <tulip/Graph.h>
+
+class QToolBar;
+
+class PythonScriptView;
+
+class PythonScriptViewWidget : public QWidget, public Ui::PythonScriptViewWidgetData {
+
+  Q_OBJECT
+
+public :
+
+  PythonScriptViewWidget(PythonScriptView *view, QWidget *parent=0);
+
+  void showEvent(QShowEvent *);
+  void resizeEvent(QResizeEvent *);
+
+  int addMainScriptEditor(const QString &fileName="");
+  int addModuleEditor(const QString &fileName="");
+  int addPluginEditor(const QString &fileName="");
+
+  PythonCodeEditor *getMainScriptEditor(int idx) const;
+  PythonCodeEditor *getCurrentMainScriptEditor() const;
+  PythonCodeEditor *getModuleEditor(int idx) const;
+  PythonCodeEditor *getCurrentModuleEditor() const;
+  PythonCodeEditor *getPluginEditor(int idx) const;
+  PythonCodeEditor *getCurrentPluginEditor() const;
+
+  std::string getCurrentMainScriptCode() const;
+  std::string getMainScriptCode(int idx) const;
+  std::string getModuleCode(int idx) const;
+  std::string getPluginCode(int idx) const;
+
+  void setGraph(tlp::Graph *graph);
+
+public slots :
+
+  void decreaseFontSize();
+  void increaseFontSize();
+  void mainScriptTextChanged();
+  void moduleScriptTextChanged();
+  void pluginScriptTextChanged();
+  void resizeToolBars();
+
+  void currentTabChanged(int index);
+
+public :
+
+  QAction *newMainScriptAction;
+  QAction *loadMainScriptAction;
+  QAction *saveMainScriptAction;
+  QAction *newStringModuleAction;
+  QAction *newFileModuleAction;
+  QAction *loadModuleAction;
+  QAction *saveModuleAction;
+  QAction *newPluginAction;
+  QAction *loadPluginAction;
+  QAction *savePluginAction;
+
+
+  QToolBar *mainScriptToolBar;
+  QToolBar *modulesToolBar;
+  QToolBar *pluginsToolBar;
+
+private :
+
+  int fontZoom;
+
+  PythonScriptView *pythonScriptView;
+  static GragKeyboardFocusEventFilter keyboardFocusEventFilter;
+};
+
+#endif /* PYTHONSCRIPTVIEWWIDGET_H_ */
---
+++ plugins/view/pythonscriptview/TulipUtilsModule.h
@@ -0,0 +1,178 @@
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+
+#ifndef TULIPUTILSMODULE_H_
+#define TULIPUTILSMODULE_H_
+
+#include "PythonIncludes.h"
+#include "PythonInterpreter.h"
+#include "PythonScriptView.h"
+
+#include <tulip/GlMainWidget.h>
+#include <tulip/PropertyAlgorithm.h>
+#include <tulip/ImportModule.h>
+#include <tulip/ExportModule.h>
+
+#include <QtGui/QMenu>
+
+static PyObject *
+tuliputils_updateVisualization(PyObject *, PyObject *args) {
+  int i;
+
+  if (!PyArg_ParseTuple(args, "|i", &i))
+    Py_RETURN_NONE;
+
+  bool centerViews = i > 0;
+
+  Py_RETURN_NONE;
+}
+
+static PyObject *
+tuliputils_pauseRunningScript(PyObject *, PyObject *) {
+  Py_RETURN_NONE;
+}
+
+template <typename T>
+static bool pluginExists(const std::string &) {
+//  std::map< std::string, tlp::TemplateFactoryInterface* >::iterator it = tlp::TemplateFactoryInterface::allFactories->begin();
+
+//  for (; it != tlp::TemplateFactoryInterface::allFactories->end() ; ++it) {
+//    if (it->first == tlp::TemplateFactoryInterface::standardizeName(typeid(T).name()) && it->second->pluginExists(pluginName)) {
+//      return true;
+//    }
+//  }
+
+  return false;
+}
+
+template <typename T>
+static void removePlugin(const std::string &) {
+//  std::map< std::string, tlp::TemplateFactoryInterface* >::iterator it = tlp::TemplateFactoryInterface::allFactories->begin();
+
+//  for (; it != tlp::TemplateFactoryInterface::allFactories->end() ; ++it) {
+//    if (it->first == tlp::TemplateFactoryInterface::standardizeName(typeid(T).name())) {
+//      it->second->removePlugin(pluginName);
+//      return;
+//    }
+//  }
+}
+
+static void removePlugin(const string &pluginName, const string &pluginType) {
+  if (pluginType == "General") {
+    if (pluginExists<tlp::Algorithm>(pluginName)) {
+      removePlugin<tlp::Algorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Layout") {
+    if (pluginExists<tlp::LayoutAlgorithm>(pluginName)) {
+      removePlugin<tlp::LayoutAlgorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Size") {
+    if (pluginExists<tlp::SizeAlgorithm>(pluginName)) {
+      removePlugin<tlp::SizeAlgorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Color") {
+    if (pluginExists<tlp::ColorAlgorithm>(pluginName)) {
+      removePlugin<tlp::ColorAlgorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Measure") {
+    if (pluginExists<tlp::DoubleAlgorithm>(pluginName)) {
+      removePlugin<tlp::DoubleAlgorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Selection") {
+    if (pluginExists<tlp::BooleanAlgorithm>(pluginName)) {
+      removePlugin<tlp::BooleanAlgorithm>(pluginName);
+    }
+  }
+  else if (pluginType == "Import") {
+    if (pluginExists<tlp::ImportModule>(pluginName)) {
+      removePlugin<tlp::ImportModule>(pluginName);
+    }
+  }
+  else if (pluginType == "Export") {
+    if (pluginExists<tlp::ExportModule>(pluginName)) {
+      removePlugin<tlp::ExportModule>(pluginName);
+    }
+  }
+  else if (pluginType == "Interactor") {
+    if (pluginExists<tlp::Interactor>(pluginName)) {
+      removePlugin<tlp::Interactor>(pluginName);
+    }
+  }
+}
+
+static PyObject *
+tuliputils_removePlugin(PyObject *, PyObject *args) {
+
+  char *buf, *buf2;
+
+  if (!PyArg_ParseTuple(args, "ss", &buf, &buf2))
+    Py_RETURN_NONE;
+
+  removePlugin(buf, buf2);
+
+  Py_RETURN_NONE;
+}
+
+static PyObject *
+tuliputils_updatePluginsMenus(PyObject *, PyObject *) {
+  Py_RETURN_NONE;
+}
+
+static PyMethodDef tulipUtilsMethods[] = {
+  {"updateVisualization",  tuliputils_updateVisualization, METH_VARARGS, "Update views on current graph."},
+  {"pauseRunningScript",  tuliputils_pauseRunningScript, METH_VARARGS, "Pause the execution of the current running script."},
+  {"updatePluginsMenus",  tuliputils_updatePluginsMenus, METH_VARARGS, "Update the plugins menus entries in the Tulip GUI."},
+  {"removePlugin",  tuliputils_removePlugin, METH_VARARGS, ""},
+  {NULL, NULL, 0, NULL}        /* Sentinel */
+};
+
+#if PY_MAJOR_VERSION >= 3
+static struct PyModuleDef tulipUtilsModuleDef = {
+  PyModuleDef_HEAD_INIT,
+  "tuliputils",     /* m_name */
+  "",  /* m_doc */
+  -1,                  /* m_size */
+  tulipUtilsMethods,    /* m_methods */
+  NULL,                /* m_reload */
+  NULL,                /* m_traverse */
+  NULL,                /* m_clear */
+  NULL,                /* m_free */
+};
+#endif
+
+void
+inittuliputils(void) {
+#if PY_MAJOR_VERSION >= 3
+  PyObject *m = PyModule_Create(&tulipUtilsModuleDef);
+  _PyImport_FixupBuiltin(m, const_cast<char *>("tuliputils"));
+#else
+  PyObject *m = Py_InitModule("tuliputils", tulipUtilsMethods);
+  _PyImport_FixupExtension(const_cast<char *>("tuliputils"), const_cast<char *>("tuliputils"));
+#endif
+
+  if (m == NULL)
+    return;
+}
+
+#endif /* TULIPUTILSMODULE_H_ */
