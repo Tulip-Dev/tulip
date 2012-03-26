e3a7ca65651b06d0f0ce270885487933f7716a89
Patrick Mary <mary@labri.fr>
2012-03-26 16:51:39 +0000

    Tulip License fixes
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7103 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/TemplateAlgorithm.h
+++ library/tulip-core/include/tulip/TemplateAlgorithm.h
@@ -17,16 +17,6 @@
  *
  */
 
-/*
- Author: David Auber
- Email : auber@labri.fr
- Last modification : 20/08/2001
- This program is free software; you can redistribute it and/or modify  *
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
-*/
-
 #ifndef TULIP_PROPERTY_H
 #define TULIP_PROPERTY_H
 
