250ba8f03501870066b6e00dbca6d4c89c32824d
Charles Huet <packadal@gmail.com>
2012-04-18 12:04:44 +0000

    added a kind of algorithms: GraphTest to reintroduce the functionality of t4ests we had in Tulip 3 Wrapped all the tests found in Tulip3, and the related graph modification algorithms (make simple, ...)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7299 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ plugins/test/Connected.cpp
@@ -0,0 +1,49 @@
+/**
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux 1 and Inria Bordeaux - Sud Ouest
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+
+#include <tulip/ConnectedTest.h>
+#include <tulip/GraphTest.h>
+
+class ConnetedTest : public tlp::GraphTest {
+public:
+  PLUGININFORMATIONS("Conneted Test", "Tulip team", "18/04/2012", "Tests whether a graph is connected or not.", "1.0", "Connectivity")
+  ConnetedTest(const tlp::PluginContext* context) : tlp::GraphTest(context) {
+  }
+  
+  virtual bool run() {
+    return tlp::ConnectedTest::isConnected(graph);
+  }
+  
+};
+PLUGIN(ConnetedTest)
+
+class MakeConnected : public tlp::Algorithm {
+public:
+  PLUGININFORMATIONS("Make Conneted", "Tulip team", "18/04/2012", "Makes a graph conneted.", "1.0", "Connectivity")
+  MakeConnected(const tlp::PluginContext* context) : tlp::Algorithm(context) {
+  }
+
+  virtual bool run() {
+    std::vector<tlp::edge> edges;
+    tlp::ConnectedTest::makeConnected(graph, edges);
+    return true;
+  }
+
+};
+PLUGIN(MakeConnected)
