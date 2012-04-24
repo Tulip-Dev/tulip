b97622b154dc6b73b57440a31f4277d365db648f
David Auber <david.auber@labri.fr>
2012-04-24 07:15:57 +0000

    reimplement hulls with exact grahma algorithm add the first step of quick hull algo to speed up Implmentation is not optimal but it should works even with aligned points on the convex hull.
    Upadte Vector to have better precision when we normalize
    
    
    
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7355 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/ConvexHull.h
+++ library/tulip-core/include/tulip/ConvexHull.h
@@ -45,56 +45,6 @@ namespace tlp {
 TLP_SCOPE void convexHull (const std::vector<Coord> &points,
                            std::vector<unsigned int> &convexHull);
 
-/**
- * Merges two convex hulls hull1 and hull2 using the rotating calipers
- * method of Pirzadeh and Touissaint.  The mergedConvexHull
- * vector is automatically cleared and the vertices of the hulls must
- * be specified in counter clockwise order.  Provide a full list of all
- * points involved in the convex hull computation in the vector points.
- * The vectors hull1 and hull2 contain a list of indexes into the
- * points vector that define the vertices of the convex hull.  The
- * indexes into points of the verticies on the merged convex hull are
- * returned in the vector mergedConvexHull.
- */
-TLP_SCOPE void mergeHulls (const std::vector<Coord> &points,
-                           const std::vector<unsigned int> &hull1,
-                           const std::vector<unsigned int> &hull2,
-                           std::vector<unsigned int> &mergedConvexHull);
-
-/**
- * Intersects two convex hulls using the algorithm of
- * O'Rourke, Chien, Olson, and Naddor, 1982.  Code inspired
- * by Computational Geometry in C by Joseph O'Rourke pp 243 -- 251.
- * and code on his website:
- * http://maven.smith.edu/~orourke/books/compgeom.html
- * Provide a full list of points involved in the convex hull computation
- * in "points".  The vectors hull1 and hull2 contain a list of indexes
- * into the points vector that define the vertices of the convex hull.
- * The indices corresponding to the points on the intersecting hull
- * are returned in "intersection".  Node that several points may
- * be inserted into the "points" list.  These points correspond to the
- * points generated when edges of hull1 and hull2 intersect.
- */
-TLP_SCOPE void intersectHulls (std::vector<Coord> &points,
-                               const std::vector<unsigned int> &hull1,
-                               const std::vector<unsigned int> &hull2,
-                               std::vector<unsigned int> &intersection);
-
-/**
- * Computes the area of the convex "hull" which is a list of indexes
- * into the vector of Coord "points".
- */
-TLP_SCOPE double areaOfHull (const std::vector<Coord> &points,
-                             const std::vector<unsigned int> &hull);
-
-/**
- * Returns true if "point" is inside "hull".  Hull is a list of
- * indexes into the vector of Coord "points."
- */
-TLP_SCOPE bool insideHull (const std::vector<Coord> &points,
-                           const std::vector<unsigned int> &hull,
-                           const Coord &point);
-
 /*@}*/
 
 }
--- library/tulip-core/src/ConvexHull.cpp
+++ library/tulip-core/src/ConvexHull.cpp
@@ -32,48 +32,88 @@ struct p0Vectors {
 };
 #endif // DOXYGEN_NOTFOR_DEVEL
 
+
 bool operator<(const p0Vectors &p1, const p0Vectors &p2) {
-  double lengthP1 = p1.pos.norm();
-  double lengthP2 = p2.pos.norm();
-  double z = p1.pos.x()/lengthP1 - p2.pos.x()/lengthP2;
-
-  // aligned points so check norm of vectors
-  if (fabs(z) < 1E-8)
-    return lengthP1 > lengthP2;
-
-  return z > 0;
+    Vec2d a(p1.pos);
+    Vec2d b(p2.pos);
+    a.normalize();
+    b.normalize();
+    double cosa = a.x();
+    double cosb = b.x();
+    double sina = a.y();
+    double sinb = b.y();
+    double deltacos = fabs(a.x() - b.x());
+    double deltasin = fabs(a.y() - b.y());
+    // aligned points so check norm of vectors
+    if (deltacos < 1E-10 && deltasin < 1E-10) {
+            return  (p1.pos.x()*p1.pos.x()) + (p1.pos.y() *p1.pos.y())
+                    >
+                    (p2.pos.x()*p2.pos.x()) + (p2.pos.y() *p2.pos.y());
+    }
+    if (sina > 0. && sinb < 0.) return true;
+    if (sina < 0. && sinb > 0.) return false;
+    if (sina > 0. && sinb > 0.)
+        return cosa > cosb;
+    else
+        return cosa < cosb;
 }
 //==============================================================
-//A function that returns a point on the convex hull
+//A function that returns a point inside the convex hull
 //(the point of minimum y-coordinate).  The index returned is
 //an index of the vector points.
-inline unsigned int findP0(const std::vector<Coord> &points) {
-  unsigned int p = 0;
+inline Vec2d findP0(const std::vector<Coord> &points,
+                    Vec2d &l, Vec2d &r, Vec2d &t, Vec2d &b) {
+    Vec2d pmin(points[0].x(), points[0].y());
+    Vec2d pmax(points[0].x(), points[0].y());
+    l.set(points[0].x(), points[0].y());
+    r.set(points[0].x(), points[0].y());
+    t.set(points[0].x(), points[0].y());
+    b.set(points[0].x(), points[0].y());
 
-  for (unsigned int i = 1; i < points.size(); ++i) {
-    if (points[i].y() < points[p].y()) {
-      p = i;
-      continue;
+    for (unsigned int i = 1; i < points.size(); ++i) {
+        if (points[i].x() > pmax.x()) {
+            pmax.x() = points[i].x();
+            r.set(points[i].x(), points[i].y());
+        }
+        if (points[i].y() > pmax.y()) {
+            pmax.y() = points[i].y();
+            t.set(points[i].x(), points[i].y());
+        }
+        if (points[i].x() < pmin.x()) {
+            pmin.x() = points[i].x();
+            l.set(points[i].x(), points[i].y());
+        }
+        if (points[i].y() < pmin.y()) {
+            pmin.y() = points[i].y();
+            b.set(points[i].x(), points[i].y());
+        }
     }
-
-    if (points[i].y() > points[p].y())
-      continue;
-
-    if (points[i].x() < points[p].x()) {
-      p = i;
-      continue;
-    }
-  }//end for i
-
-  return p;
+    return (pmin + pmax)/2.;
 }//end findP0
 
-double ccw(const Coord &p1, const Coord &p2, const Coord &p3) {
-  Vec2d a(p2.x() - p1.x(), p2.y() - p1.y());
-  a.normalize();
-  Vec2d b(p3.x() - p1.x(), p3.y() - p1.y());
-  b.normalize();
-  return a.x()*b.y() - a.y()*b.x();
+double ccw(const Vec2d &p1, const Vec2d &p2, const Vec2d &p3) {
+    double d1 = p1.dist(p2);
+    double d2 = p2.dist(p3);
+    double d3 = p1.dist(p3);
+    if (fabs(d1 + d2 - d3) < 1E-5) return 0.; //aligned
+    //cerr << p1 << " | " << p2 << " | " << p3 << endl;
+    Vec2d a((double)p2.x() - (double)p1.x(), (double)p2.y() - (double)p1.y());
+    a.normalize();
+    //cerr << a << " ^ ";
+    Vec2d b((double)p3.x() - (double)p1.x(), (double)p3.y() - (double)p1.y());
+    b.normalize();
+    //cerr << b << " = ";
+    //cerr << a.x()*b.y() - a.y()*b.x() << endl;
+    if (a.dotProduct(b) < -0.9999) return -1;
+    return a.x()*b.y() - a.y()*b.x();
+}
+bool inside(const Vec2d &p,
+            Vec2d &l, Vec2d &r, Vec2d &t, Vec2d &b) {
+    if (ccw(l, p, b) <= 0.) return false;
+    if (ccw(b, p, r) <= 0.) return false;
+    if (ccw(r, p, t) <= 0.) return false;
+    if (ccw(t, p, l) <= 0.) return false;
+    return true;
 }
 //==============================================================
 void tlp::convexHull (const std::vector<Coord> &points,
@@ -85,379 +125,71 @@ void tlp::convexHull (const std::vector<Coord> &points,
   assert(points.size() > 2);
 
   //get the starting point of the convex hull computation
-  unsigned int p0Index = findP0(points);
-  cerr << points[p0Index] << endl;
+  Vec2d lP, rP, tP, bP;
+  Vec2d p0 = findP0(points, lP, rP, tP, bP);
   //translate all points to have p0 in (0, 0);
-  vector<p0Vectors> vectors;
-
+  list<p0Vectors> vectors;
   for (unsigned int i = 0; i < points.size(); ++i) {
-    if (p0Index == i) continue;
-
     p0Vectors curPoint;
-    curPoint.pos.x() = points[i].x() - points[p0Index].x();
-    curPoint.pos.y() = points[i].y() - points[p0Index].y();
+    curPoint.pos.x() = (double)points[i].x();
+    curPoint.pos.y() = (double)points[i].y();
+    if (inside(curPoint.pos, lP, rP, tP, bP)) continue;
+    curPoint.pos -= p0;
+    if (curPoint.pos.length() < 1E-5) continue;
     curPoint.index = i;
     vectors.push_back(curPoint);
   }//end for
 
-  //sort vectors counterclockwise and compute the convex hull
-  stable_sort (vectors.begin(), vectors.end());
-
-  convexHull.push_back(p0Index);
-  convexHull.push_back(vectors[0].index);
-
-  for (unsigned int i=1; i<vectors.size(); ++i) {
-    int m = convexHull.size() - 1;
-
-    while(m>1 && ccw(points[convexHull[m-1]], points[convexHull[m]], points[vectors[i].index]) <= 1E-5) {
-      convexHull.pop_back();
-      --m;
-    }
-
-    convexHull.push_back(vectors[i].index);
+  vectors.sort();
+  bool stopG = false;
+  while (!stopG) {
+      stopG = true;
+      list<p0Vectors>::iterator it = vectors.begin();
+      while(it != vectors.end()) {
+          p0Vectors a,b,c;
+          if (it == vectors.begin())
+              a = vectors.back();
+          else {
+              list<p0Vectors>::iterator it2 = it;
+              --it2;
+              a = *it2;
+          }
+          b = *it;
+          list<p0Vectors>::iterator it3 = it;
+          ++it3;
+          if (it3 == vectors.end())
+              c = vectors.front();
+          else {
+              list<p0Vectors>::iterator it2 = it;
+              ++it2;
+              c = *it2;
+          }
+          if (ccw(a.pos, b.pos, c.pos) < 0.) {
+              list<p0Vectors>::iterator it2 = it;
+              if (it != vectors.begin())
+                  --it2;
+              else {
+                  ++it2;
+              }
+              stopG = false;
+              vectors.erase(it);
+              it = it2;
+          }
+          else {
+              ++it;
+          }
+      }
   }
 
+  list<p0Vectors>::iterator it = vectors.begin();
+  for (; it != vectors.end(); ++it)
+      convexHull.push_back((*it).index);
+/*
+  for (int i=1; i<vectors.size(); ++i) {
+      int m = convexHull.size() - 1;
+      while(m > 1 && (ccw(points[convexHull[m-1]], points[convexHull[m]], points[vectors[i].index]) <= 1E-2)) {
+      }
+      convexHull.push_back(vectors[i].index);
+  }
+*/
 }//end ConvexHull
-
-
-
-
-//==============================================================
-//the following inline function of cross product ensures we
-//ignore the z-coordinate.  Only 2d-hull merging allowed.
-inline double cross (Coord v1, Coord v2) {
-  v1.setZ (0.0);
-  v2.setZ (0.0);
-  return ((v1^v2)[2]);
-}//end cross
-
-//==============================================================
-//the following inline function returns when a double is "almost zero"
-inline bool almostZero (double n) {
-  return fabs(n) < 1e-6;
-}
-
-//==============================================================
-//the following function computes the intersection point
-//between two line segments v1 and v2.  It returns true if the
-//segments intersect and false otherwise.  The intersection
-//is returned in intersection.
-inline bool hit (const Coord &v1Tail, const Coord &v1Head,
-                 const Coord &v2Tail, const Coord &v2Head,
-                 Coord &intersection) {
-  float s, t;
-  float D = (v1Tail.getX()*(v2Head.getY() - v2Tail.getY()) +
-             v1Head.getX()*(v2Tail.getY() - v2Head.getY()) +
-             v2Head.getX()*(v1Head.getY() - v1Tail.getY()) +
-             v2Tail.getX()*(v1Tail.getY() - v1Head.getY()));
-
-  if (almostZero (D)) return false; //lines parallel
-
-  //compute two hit times and hit point
-  s = (v1Tail.getX()*(v2Head.getY() - v2Tail.getY()) +
-       v2Tail.getX()*(v1Tail.getY() - v2Head.getY()) +
-       v2Head.getX()*(v2Tail.getY() - v1Tail.getY()))/D;
-  t = -(v1Tail.getX()*(v2Tail.getY() - v1Head.getY()) +
-        v1Head.getX()*(v1Tail.getY() - v2Tail.getY()) +
-        v2Tail.getX()*(v1Head.getY() - v1Tail.getY()))/D;
-
-  //intersection = v1Tail;
-
-  //  Coord tmp = *(Coord *)&(operator-<float, 3u, Coord>(v1Head, v1Tail)*s);
-  //  Coord tmp = v1Head - v1Tail;
-  //  Coord tmp = operator-<float, 3u, Coord>(v1Head, v1Tail);
-  Coord tmp(v1Head - v1Tail);
-  tmp *= s;
-
-  intersection = v1Tail + tmp;
-
-  //If both hit times are less than one, the segments intersect
-  if (((0.0 <= s) && (s <= 1.0)) && ((0.0 <= t) && (t <= 1.0)))
-    return true;
-
-  return false;
-}
-
-
-//==============================================================
-typedef vector<unsigned int>::const_iterator constUintIt;
-//A function that returns the index of a point on the convex hull
-//(the point of minimum x-coordinate) given a list of indexes
-//in the integer array index.  The index returned is a index of the
-//vector index.
-inline unsigned int findP0(const vector<Coord> &points,
-                           const vector<unsigned int> &index) {
-  vector<Coord> pointsSubset;
-
-  for (constUintIt it = index.begin(); it != index.end(); ++it)
-    pointsSubset.push_back (points[*it]);
-
-  return findP0 (pointsSubset);
-}//end findP0
-
-
-
-
-//==============================================================
-void tlp::mergeHulls (const std::vector<Coord> &points,
-                      const std::vector<unsigned int> &hull1,
-                      const std::vector<unsigned int> &hull2,
-                      std::vector<unsigned int> &mergedConvexHull) {
-  //clear the input vector
-  mergedConvexHull.clear();
-
-  if (hull1.empty() || hull2.empty()) {
-    mergedConvexHull = hull1.empty() ? hull2 : hull1;
-    return;
-  }//end if
-
-  //These two variables are used to indicate the current positions
-  //of the calipers.  start1 and start2 are the end positions of the calipers
-  unsigned int caliper1, caliper2;
-  unsigned int start1, start2;
-  start1 = caliper1 = findP0 (points, hull1);
-  start2 = caliper2 = findP0 (points, hull2);
-
-  //determine which points of the hull is currently on the hull,
-  //(max of the two caliper positions in x coord)
-  bool onHull1 = true;
-
-  if (points[hull1[caliper1]].getX() > points[hull2[caliper2]].getX())
-    onHull1 = false;
-  else if (!(points[hull1[caliper1]].getX() <
-             points[hull2[caliper2]].getX()) &&
-           (points[hull1[caliper1]].getY() >
-            points[hull2[caliper2]].getY())) onHull1 = false;
-
-  //place the first vertex on the convex hull
-  if (onHull1) mergedConvexHull.push_back (hull1[caliper1]);
-  else mergedConvexHull.push_back (hull2[caliper2]);
-
-  //merge the two convex hulls
-  do {
-    unsigned int next1 = (caliper1 == hull1.size() - 1) ? 0 : caliper1 + 1;
-    unsigned int prev1 = (caliper1 == 0) ? hull1.size() - 1 : caliper1 - 1;
-    unsigned int next2 = (caliper2 == hull2.size() - 1) ? 0 : caliper2 + 1;
-    unsigned int prev2 = (caliper2 == 0) ? hull2.size() - 1 : caliper2 - 1;
-    Coord copodal = points[hull2[caliper2]] - points[hull1[caliper1]];
-
-    if (!onHull1) copodal *= -1.;
-
-    if ((cross (copodal,
-                points[hull1[next1]] - points[hull1[caliper1]]) >= 0) &&
-        (cross (copodal,
-                points[hull1[prev1]] - points[hull1[caliper1]]) >= 0) &&
-        (cross (copodal,
-                points[hull2[next2]] - points[hull2[caliper2]]) >= 0) &&
-        (cross (copodal,
-                points[hull2[prev2]] - points[hull2[caliper2]]) >= 0)) {
-      if (onHull1) {
-        if (mergedConvexHull[0] != hull2[caliper2])
-          mergedConvexHull.push_back (hull2[caliper2]);
-      }
-      else {
-        if (mergedConvexHull[0] != hull1[caliper1])
-          mergedConvexHull.push_back (hull1[caliper1]);
-      }//end else
-
-      onHull1 = !onHull1;
-    }//end if
-
-    //rotate the calipers
-    if ((cross (points[hull1[next1]] - points[hull1[caliper1]],
-                points[hull2[next2]] - points[hull2[caliper2]]) > 0) ||
-        (hull2.size() == 1)) {
-      ++caliper1;
-
-      if (caliper1 == hull1.size()) caliper1 = 0;
-
-      if (onHull1) {
-        if (mergedConvexHull[0] != hull1[caliper1])
-          mergedConvexHull.push_back (hull1[caliper1]);
-      }
-    }//end if
-    else {
-      ++caliper2;
-
-      if (caliper2 == hull2.size()) caliper2 = 0;
-
-      if (!onHull1) {
-        if (mergedConvexHull[0] != hull2[caliper2])
-          mergedConvexHull.push_back (hull2[caliper2]);
-      }//end if
-    }//end else
-  }
-  while ((caliper1 != start1) || (caliper2 != start2));
-}//end convex hull
-
-//==============================================================
-//helper function to advance calipers for intersecting hulls
-inline unsigned int advance (unsigned int caliper, unsigned int &adv,
-                             bool inside,
-                             const std::vector<unsigned int> &hull,
-                             std::vector<unsigned int> &intersection) {
-  if (inside)
-    intersection.push_back (hull[caliper]);
-
-  ++adv;
-  return (caliper + 1) % hull.size();
-}//end advance
-
-//==============================================================
-//A function to intersect hull1 and hull2 and place the result
-//in intersection.  Points will be inserted into the points list
-//if the hulls intersect.
-void tlp::intersectHulls (std::vector<Coord> &points,
-                          const std::vector<unsigned int> &hull1,
-                          const std::vector<unsigned int> &hull2,
-                          std::vector<unsigned int> &intersection) {
-  intersection.clear();
-
-  //no second hull to compute intersection empty intersection
-  if (hull1.empty() || hull2.empty()) return;
-
-  //These two variables are used to indicate the current positions
-  //of the calipers.  start1 and start2 are the end positions of the calipers
-  unsigned int caliper1, caliper2;
-  caliper1 = caliper2 = 0;
-  unsigned int adv1 = 0;
-  unsigned int adv2 = 0;
-
-  bool hullsIntersect = false;
-  bool hull1Inside = false;
-  Coord firstIntPoint;
-
-  if ((hull1.size() > 1) && (hull2.size() > 1)) {
-    do {
-      unsigned int prev1 = (caliper1 + hull1.size() - 1) % hull1.size();
-      unsigned int prev2 = (caliper2 + hull2.size() - 1) % hull2.size();
-      Coord vec1 = points[hull1[caliper1]] - points[hull1[prev1]];
-      Coord vec2 = points[hull2[caliper2]] - points[hull2[prev2]];
-      Coord oneLeftTwoVec = points[hull1[caliper1]] - points[hull2[prev2]];
-      Coord twoLeftOneVec = points[hull2[caliper2]] - points[hull1[prev1]];
-      double oneLeftVecTwo = cross (vec2, oneLeftTwoVec);
-      double twoLeftVecOne = cross (vec1, twoLeftOneVec);
-      double oneCrossTwo = cross (vec1, vec2);
-
-      //check for intersection
-      Coord intPoint;
-
-      if (hit (points[hull1[prev1]], points[hull1[caliper1]],
-               points[hull2[prev2]], points[hull2[caliper2]],
-               intPoint)) {
-        if (!hullsIntersect) {
-          adv1 = adv2 = 0;
-          hullsIntersect = true;
-          firstIntPoint = intPoint;
-        }//end if
-
-        //insert the same intersection point only once
-        //(if one convex hull is a line add only the first intersect)
-        if (!almostZero ((intPoint - points[points.size() - 1]).norm())) {
-          intersection.push_back (points.size());
-          points.push_back (intPoint);
-        }
-
-        if (oneLeftVecTwo > 0) hull1Inside = true;
-        else if (twoLeftVecOne > 0) hull1Inside = false;
-      }//end if
-
-      //special cases
-      //colinear segments
-      if (almostZero (oneCrossTwo) && (almostZero (oneLeftVecTwo))
-          && (almostZero (twoLeftVecOne))) {
-        if (hullsIntersect) {
-          if (hull1Inside)
-            caliper2 = advance (caliper2, adv2, false,
-                                hull2, intersection);
-          else
-            caliper1 = advance (caliper1, adv1, false,
-                                hull1, intersection);
-        }//end if
-      }//end if
-
-      //normal case -- advance calipers
-      if (oneCrossTwo >= 0) {
-        if (twoLeftVecOne > 0)
-          caliper1 = advance (caliper1, adv1,
-                              (hullsIntersect && hull1Inside),
-                              hull1, intersection);
-        else
-          caliper2 = advance (caliper2, adv2,
-                              (hullsIntersect && !hull1Inside),
-                              hull2, intersection);
-      }//end if
-      else {
-        if (oneLeftVecTwo > 0)
-          caliper2 = advance (caliper2, adv2,
-                              (hullsIntersect && !hull1Inside),
-                              hull2, intersection);
-        else
-          caliper1 = advance (caliper1, adv1,
-                              (hullsIntersect && hull1Inside),
-                              hull1, intersection);
-      }//end else
-    }
-    while (((adv1 < hull1.size()) || (adv2 < hull2.size())) &&
-           ((adv1 < 2*hull1.size()) && (adv2 < 2*hull2.size())));
-  }//end if
-
-  //check to see if one hull is inside the other, otherwise no intersect.
-  if (!hullsIntersect) {
-    if (insideHull (points, hull1, points[hull2[0]]))
-      intersection = hull2;
-    else if (insideHull (points, hull2, points[hull1[0]]))
-      intersection = hull1;
-    else intersection.clear();
-  }//end if
-}
-
-//==============================================================
-//Function to compute the area of the passed hull using
-//convex combinations of its boundary.
-double tlp::areaOfHull (const std::vector<Coord> &points,
-                        const std::vector<unsigned int> &hull) {
-  if (hull.size() < 3) return 0.0;
-
-  double area = 0;
-  unsigned int prevElement = hull[0];
-  vector<unsigned int>::const_iterator hullIt = hull.begin();
-  ++hullIt;
-
-  for (; hullIt != hull.end(); ++hullIt) {
-    area += cross (points[prevElement], points[*hullIt]);
-    prevElement = *hullIt;
-  }//end for
-
-  area += cross (points[prevElement], points[hull[0]]);
-  return area/2.0;
-}
-
-//==============================================================
-//This function to test if point is inside the convex hull
-//It does so by making sure that point is in the left
-//halfspace of hull edges on a counterclockwise traversal
-//of the boundary.
-bool tlp::insideHull (const std::vector<Coord> &points,
-                      const std::vector<unsigned int> &hull,
-                      const Coord &point) {
-  if (hull.size() < 3) return false;
-
-  unsigned int prevElement = hull[0];
-  vector<unsigned int>::const_iterator hullIt = hull.begin();
-  ++hullIt;
-  Coord vec, pointVec;
-
-  for (; hullIt != hull.end(); ++hullIt) {
-    vec = points[*hullIt] - points[prevElement];
-    pointVec = point - points[prevElement];
-
-    if (cross (vec, pointVec) < 0) return false;
-
-    prevElement = *hullIt;
-  }
-
-  vec = points[hull[0]] - points[prevElement];
-  pointVec = point - points[prevElement];
-  return (cross (vec, pointVec) > 0);
-}
