1a7ef05b07cac933beac9753f706a9bbb9a68719
Charles Huet <packadal@gmail.com>
2012-04-24 09:58:37 +0000

    added DragHandle files
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7359 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/src/DragHandle.cpp
@@ -0,0 +1,41 @@
+#include <tulip/DragHandle.h>
+#include <tulip/TulipMimes.h>
+
+#include <assert.h>
+
+#include <QtCore/QMimeData>
+#include <QtGui/QDrag>
+#include <QtGui/QMouseEvent>
+#include <QApplication>
+
+using namespace tlp;
+
+DragHandle::DragHandle(QWidget* parent, Qt::WindowFlags f) : _panel(NULL), _pressed(false) {
+}
+
+void DragHandle::mousePressEvent(QMouseEvent* ev){
+  _pressed = true;
+  _clickPosition = ev->pos();
+}
+
+void DragHandle::mouseReleaseEvent(QMouseEvent* ev){
+    _pressed = false;
+}
+
+void DragHandle::mouseMoveEvent(QMouseEvent* ev){
+  assert(_panel != NULL);
+  
+  if(!_panel || !_pressed || (ev->pos() - _clickPosition).manhattanLength() < QApplication::startDragDistance())
+    return;
+  
+  QDrag* drag = new QDrag(this);
+  PanelMimeType* mimedata = new PanelMimeType();
+  mimedata->setPanel(_panel);
+  drag->setMimeData(mimedata);
+
+  Qt::DropAction dropaction = drag->exec(Qt::MoveAction);
+}
+
+void DragHandle::setPanel(tlp::WorkspacePanel* panel) {
+  _panel = panel;
+}
