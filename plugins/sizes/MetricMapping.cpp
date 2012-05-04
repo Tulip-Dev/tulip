4739fd8a6c66fc300f9b72c9d61ad601b70feab3
Patrick Mary <mary@labri.fr>
2012-05-04 08:51:18 +0000

    integrates updates from revisions #6911(new linear mapping option) & #7441(fix for sf bug #3517677: enable user interaction)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7462 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/sizes/MetricMapping.cpp
+++ plugins/sizes/MetricMapping.cpp
@@ -27,12 +27,14 @@ const char * paramHelp[] = {
   // property
   HTML_HELP_OPEN() \
   HTML_HELP_DEF( "type", "DoubleProperty" ) \
+  HTML_HELP_DEF( "default", "\"viewMetric\"" ) \
   HTML_HELP_BODY() \
   "Metric to map to size." \
   HTML_HELP_CLOSE(),
   // input
   HTML_HELP_OPEN() \
   HTML_HELP_DEF( "type", "SizeProperty" ) \
+  HTML_HELP_DEF( "default", "\"viewSize\"" ) \
   HTML_HELP_BODY() \
   "If not all dimensions (width, height, depth) are checked below, the dimensions not computed are copied from this property." \
   HTML_HELP_CLOSE(),
@@ -48,13 +50,13 @@ const char * paramHelp[] = {
   HTML_HELP_OPEN() \
   HTML_HELP_DEF( "type", "double" ) \
   HTML_HELP_BODY() \
-  "Minimum value of the range of computed sizes." \
+  "Gives the minimum value of the range of computed sizes." \
   HTML_HELP_CLOSE(),
   // max
   HTML_HELP_OPEN() \
   HTML_HELP_DEF( "type", "double" ) \
   HTML_HELP_BODY() \
-  "Maximum value of the range of computed sizes." \
+  "Gives the maximum value of the range of computed sizes." \
   HTML_HELP_CLOSE(),
   // Mapping type
   HTML_HELP_OPEN() \
@@ -72,15 +74,25 @@ const char * paramHelp[] = {
   HTML_HELP_DEF( "values", "true / false" ) \
   HTML_HELP_DEF( "default", "true" ) \
   HTML_HELP_BODY() \
-  "If true, compute the size of nodes, else compute the size of edges." \
+  "If true the algorithm will compute the size of nodes else it will compute the size of edges :" \
+  "<ul><li>true : node size</li><li>false: edge size</li></ul>" \
+  HTML_HELP_CLOSE(),
+
+  //proportional mapping
+  HTML_HELP_OPEN() \
+  HTML_HELP_DEF( "type", "string" ) \
+  HTML_HELP_DEF( "default", "Area Proportional" ) \
+  HTML_HELP_BODY() \
+  "The mapping can either be area/volume proportional, or square/cubic;" \
+  "i.e. the areas/volumes will be proportional, or the dimensions (width, height and depth) will be." \
   HTML_HELP_CLOSE(),
 };
 }
 
 // error msg for invalid range value
-static const char* rangeSizeErrorMsg = "max size must be greater than min size";
-static const char* rangeMetricErrorMsg = "All values are the same";
-
+static const string rangeSizeErrorMsg = "max size must be greater than min size";
+static const string rangeMetricErrorMsg = "All values are the same";
+static const string AREA_PROPORTIONAL = "Area Proportional";
 /** \addtogroup size */
 /*@{*/
 /// Metric Mapping - Compute size of elements according to a metric.
@@ -91,55 +103,18 @@ static const char* rangeMetricErrorMsg = "All values are the same";
  */
 class MetricSizeMapping:public SizeAlgorithm {
 public:
-  PLUGININFORMATIONS("Metric Mapping","Auber","08/08/2003","","1.0", "Size")
-  MetricSizeMapping(const tlp::PluginContext* context):SizeAlgorithm(context), entryMetric(NULL), entrySize(NULL), xaxis(true), yaxis(true), zaxis(true),
-    min(1), max(10), range(false), shift(DBL_MAX), nodeoredge(true) {
+  PLUGININFORMATIONS("Metric Mapping","Auber","08/08/2003","","2.0", "Size")
+  MetricSizeMapping(const PluginContext* context):SizeAlgorithm(context) {
     addInParameter<DoubleProperty>("property", paramHelp[0], "viewMetric");
     addInParameter<SizeProperty>("input", paramHelp[1], "viewSize");
     addInParameter<bool>("width", paramHelp[2],"true");
     addInParameter<bool>("height", paramHelp[2],"true");
-    addInParameter<bool>("depth", paramHelp[2],"true");
+    addInParameter<bool>("depth", paramHelp[2],"false");
     addInParameter<double>("min size",paramHelp[3],"1");
     addInParameter<double>("max size",paramHelp[4],"10");
     addInParameter<bool>("type", paramHelp[5],"true");
     addInParameter<bool>("node/edge", paramHelp[6],"true");
-  }
-
-  ~MetricSizeMapping() {}
-
-  void computeNodeSize() {
-    Iterator<node> *itN=graph->getNodes();
-
-    while(itN->hasNext()) {
-      node itn=itN->next();
-      double sizos=min+(entryMetric->getNodeValue(itn)-shift)*(max-min)/range;
-      Size res=entrySize->getNodeValue(itn);
-
-      if (xaxis) res[0]=sizos;
-
-      if (yaxis) res[1]=sizos;
-
-      if (zaxis) res[2]=sizos;
-
-      result->setNodeValue(itn, res);
-    }
-
-    delete itN;
-  }
-
-  void computeEdgeSize() {
-    Iterator<edge> *itE=graph->getEdges();
-
-    while(itE->hasNext()) {
-      edge ite=itE->next();
-      double sizos = min+(entryMetric->getEdgeValue(ite)-shift)*(max-min)/range;
-      Size res  = entrySize->getEdgeValue(ite);
-      res[0] = sizos;
-      res[1] = sizos;
-      result->setEdgeValue(ite, res);
-    }
-
-    delete itE;
+    addInParameter<StringCollection>("area proportional", paramHelp[7], "Area Proportional;Quadratic/Cubic");
   }
 
   bool check(std::string &errorMsg) {
@@ -147,9 +122,11 @@ public:
     min=1;
     max=10;
     nodeoredge = true;
-    entryMetric=graph->getProperty<DoubleProperty>("viewMetric");
-    entrySize=graph->getProperty<SizeProperty>("viewSize");
+    proportional = "Area Proportional";
+    entryMetric = NULL;
+    entrySize = NULL;
     mappingType = true;
+    StringCollection proportionalType;
 
     if ( dataSet!=0 ) {
       dataSet->get("property",entryMetric);
@@ -161,12 +138,18 @@ public:
       dataSet->get("max size",max);
       dataSet->get("type",mappingType);
       dataSet->get("node/edge",nodeoredge);
+      dataSet->get("area proportional",proportionalType);
+      proportional = proportionalType.getCurrentString();
     }
 
+    if (!entryMetric)
+      entryMetric=graph->getProperty<DoubleProperty>("viewMetric");
+
+    if (!entrySize)
+      entrySize=graph->getProperty<SizeProperty>("viewSize");
+
     if (min >= max) {
-      /*qWarning() << rangeErrorMsg << endl;
-        pluginProgress->setError(rangeSizeErrorMsg); */
-      errorMsg = std::string(rangeSizeErrorMsg);
+      errorMsg = rangeSizeErrorMsg;
       return false;
     }
 
@@ -176,8 +159,17 @@ public:
       range = entryMetric->getEdgeMax(graph) - entryMetric->getEdgeMin(graph);
 
     if (!range) {
-      errorMsg = std::string(rangeMetricErrorMsg);
+      errorMsg = rangeMetricErrorMsg;
       return false;
+    }
+
+    if(!xaxis && !yaxis && !zaxis) {
+      errorMsg = "You need at least one axis to map on.";
+      return false;
+    }
+
+    if(proportional == AREA_PROPORTIONAL) {
+      max = max*max;
     }
 
     return true;
@@ -193,19 +185,108 @@ public:
       entryMetric = tmp;
     }
 
+    unsigned int maxIter = graph->numberOfNodes() + graph->numberOfEdges();
+    unsigned int iter = 0;
+
+    pluginProgress->showPreview(false);
+
     if(nodeoredge) {
       shift = entryMetric->getNodeMin(graph);
-      computeNodeSize();
+
+      // compute size of nodes
+      Iterator<node> *itN=graph->getNodes();
+
+      while(itN->hasNext()) {
+        node itn=itN->next();
+        double sizos = 0;
+
+        if (proportional == AREA_PROPORTIONAL) {
+          const double power = 1.0 / (float(xaxis) + float(yaxis) + float(zaxis));
+          sizos = pow((entryMetric->getNodeValue(itn)-shift)*(max-min)/range, power);
+        }
+        else {
+          sizos = min + (entryMetric->getNodeValue(itn)-shift)*(max-min)/range;
+        }
+
+        Size size=entrySize->getNodeValue(itn);
+
+        if (xaxis) size[0]=static_cast<float>(sizos);
+
+        if (yaxis) size[1]=static_cast<float>(sizos);
+
+        if (zaxis) size[2]=static_cast<float>(sizos);
+
+        result->setNodeValue(itn, size);
+
+        if ((iter % 100 == 0) &&
+            (pluginProgress->progress(iter, maxIter)!=TLP_CONTINUE)) {
+          delete itN;
+
+          if (!mappingType) delete tmp;
+
+          return false;
+        }
+
+        ++iter;
+      }
+
+      delete itN;
+
       edge e;
-      forEach(e, graph->getEdges())
-      result->setEdgeValue(e, entrySize->getEdgeValue(e));
+      forEach(e, graph->getEdges()) {
+        result->setEdgeValue(e, entrySize->getEdgeValue(e));
+
+        if ((iter % 100 == 0) &&
+            (pluginProgress->progress(iter, maxIter)!=TLP_CONTINUE)) {
+          if (!mappingType) delete tmp;
+
+          return false;
+        }
+
+        ++iter;
+      }
     }
     else {
       shift = entryMetric->getEdgeMin(graph);
-      computeEdgeSize();
+      // compute size of edges
+      Iterator<edge> *itE=graph->getEdges();
+
+      while(itE->hasNext()) {
+        edge ite=itE->next();
+        double sizos =
+          min+(entryMetric->getEdgeValue(ite)-shift)*(max-min)/range;
+        Size size = entrySize->getEdgeValue(ite);
+        size[0] = static_cast<float>(sizos);
+        size[1] = static_cast<float>(sizos);
+        result->setEdgeValue(ite, size);
+
+        if ((iter % 100 == 0) &&
+            (pluginProgress->progress(iter, maxIter)!=TLP_CONTINUE)) {
+          delete itE;
+
+          if (!mappingType) delete tmp;
+
+          return pluginProgress->state()!=TLP_CANCEL;
+        }
+
+        ++iter;
+      }
+
+      delete itE;
+
       node n;
-      forEach(n, graph->getNodes())
-      result->setNodeValue(n, entrySize->getNodeValue(n));
+      forEach(n, graph->getNodes()) {
+        result->setNodeValue(n, entrySize->getNodeValue(n));
+
+        if ((iter % 100 == 0) &&
+            (pluginProgress->progress(iter, maxIter)!=TLP_CONTINUE)) {
+          if (!mappingType) delete tmp;
+
+          return pluginProgress->state()!=TLP_CANCEL;
+        }
+
+        ++iter;
+      }
     }
 
     if (!mappingType) delete tmp;
@@ -221,6 +302,7 @@ private:
   double range;
   double shift;
   bool nodeoredge;
+  std::string proportional;
 };
 /*@}*/
 PLUGIN(MetricSizeMapping)
