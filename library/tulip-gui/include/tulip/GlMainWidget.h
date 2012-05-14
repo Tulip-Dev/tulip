0b85807896b8113516299fd4d82a2c3260a4ae77
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2012-05-14 08:01:45 +0000

    - GlMainWidget :   - Remove unused include   - Clear documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7519 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/include/tulip/GlMainWidget.h
+++ library/tulip-gui/include/tulip/GlMainWidget.h
@@ -17,16 +17,6 @@
  *
  */
 
-/**
- Author: David Auber
- Email : auber@labri.fr
- Last modification : 06/09/2005
- This program is free software; you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation; either version 2 of the License, or
- (at your option) any later version.
- */
-
 #ifndef Tulip_GLMAINWIDGET_H
 #define Tulip_GLMAINWIDGET_H
 
@@ -38,16 +28,12 @@
 #include <QtOpenGL/QGLFramebufferObject>
 
 #include "tulip/View.h"
-#include "tulip/ForegroundEntity.h"
 
 class QGLFramebufferObject;
 
 namespace tlp {
 
 class GlCompositeHierarchyManager;
-
-
-//class QTextView;
 
 /** \addtogroup Tulip_Widgets */
 /*@{*/
@@ -113,9 +99,13 @@ public:
                       SelectedEntity &selectedEntity,
                       tlp::GlLayer* layer=NULL);
 
-  // EPS output of a GlGraph
+  /**
+   * \brief EPS output of a GlGraph
+   */
   bool outputEPS(int size, int doSort, const char *filename);
-  // SVG output of a GlGraph
+  /**
+   * \brief SVG output of a GlGraph
+   */
   bool outputSVG(int size, const char* filename);
 
   /**
