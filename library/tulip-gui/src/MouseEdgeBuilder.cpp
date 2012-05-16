3cf6e19a8e4f590ccc172c891f37859ff7ebd53c
Bruno Pinaud <bruno.pinaud@labri.fr>
2012-05-16 13:52:14 +0000

    bug fix: set source to an invalid node when cancelling edge creation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7553 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/MouseEdgeBuilder.cpp
+++ library/tulip-gui/src/MouseEdgeBuilder.cpp
@@ -95,6 +95,7 @@ bool MouseEdgeBuilder::eventFilter(QObject *widget, QEvent *e) {
     if (qMouseEv->buttons()==Qt::MidButton) {
       bends.clear();
       started=false;
+      source=node();
       clearObserver();
       glMainWidget->draw();
       return true;
