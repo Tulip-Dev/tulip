816ccd5616f1826f81512fc56a672add6baf2efe
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-05-23 16:45:43 +0000

    Pretty bundle, walking down the streets, pretty bundle, tadadadada
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7605 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- bundlers/mac/mac_bundle.sh
+++ bundlers/mac/mac_bundle.sh
@@ -5,9 +5,12 @@ DEST_DIR=$2
 SRC_DIR=$(pwd)
 
 echo 'Wiping previous bundles into '$DEST_DIR
-mkdir -p $DEST_DIR
-cd $DEST_DIR
-rm -fr Tulip.app
+rm -fr $DEST_DIR
+mkdir -p $DEST_DIR/application
+cd $DEST_DIR/application
+mkdir .background
+cp $SRC_DIR/background.png .background/
+rm -fr *
 echo 'Creating base directories'
 mkdir -p Tulip.app/Contents/{MacOS,Resources,Frameworks,PlugIns,SharedSupport}
 cd Tulip.app/Contents
@@ -20,3 +23,6 @@ cp -r $TULIP_DIR/* .
 rm -fr macos
 cd ..
 cp -r $TULIP_DIR/macos/* .
+echo 'Generating DMG'
+cd $DEST_DIR
+sh $SRC_DIR/make_dmg.sh "Tulip" "4.0" $DEST_DIR/application
