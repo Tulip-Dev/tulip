68316d3ac53625b9c23d0bb3819f86269628e44b
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-05-25 13:10:50 +0000

    Removed references to tulip3 compat library and removed python variables from FindTulip until feature is repackaged
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7653 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/tulipconf.h
+++ library/tulip-core/include/tulip/tulipconf.h
@@ -155,17 +155,6 @@ static double round(double d) {
 #  define TLP_OGDF_SCOPE
 #endif
 
-#ifdef _WIN32
-#  ifdef DLL_TULIP3_COMPAT
-#    define TLP3_COMPAT_SCOPE       __declspec(dllexport)
-#  else
-#    define TLP3_COMPAT_SCOPE       __declspec(dllimport)
-#  endif
-#endif
-#ifndef TLP3_COMPAT_SCOPE
-#  define TLP3_COMPAT_SCOPE
-#endif
-
 QDebug TLP_SCOPE operator<<(QDebug,const std::string&);
 
 #endif //TULIPCONF_H
