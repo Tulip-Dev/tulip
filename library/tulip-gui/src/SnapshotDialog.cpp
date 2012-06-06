687c91a6f52504a269d23ef031c0408ce525f2ed
Bruno Pinaud <bruno.pinaud@labri.fr>
2012-06-06 11:45:46 +0000

    Port svn commit 7757 to tulip4: add a "Copy to clipboard button" to the snapshot dialog
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7767 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/SnapshotDialog.cpp
+++ library/tulip-gui/src/SnapshotDialog.cpp
@@ -19,11 +19,14 @@
 
 #include "tulip/SnapshotDialog.h"
 
+#include <tulip/GlMainView.h>
+#include <tulip/GlMainWidget.h>
+
 #include <QtGui/QMessageBox>
 #include <QtGui/QImageWriter>
 #include <QtGui/QFileDialog>
-
-#include "tulip/GlMainWidget.h"
+#include <QtGui/QGraphicsPixmapItem>
+#include <QtGui/QClipboard>
 
 using namespace std;
 
@@ -31,164 +34,161 @@ namespace tlp {
 
 class LockLabel : public QLabel {
 public :
-  LockLabel():QLabel(),locked(true) {
-    installEventFilter(this);
-    setPixmap(QPixmap(":/tulip/gui/icons/i_locked.png"));
-  }
-
-  bool isLocked() {
-    return locked;
-  }
-
-protected :
-  bool eventFilter(QObject *, QEvent *event) {
-    if(event->type() == QEvent::MouseButtonRelease) {
-      if(locked) {
-        setPixmap(QPixmap(":/tulip/gui/icons/i_unlocked.png"));
-        locked=false;
-      }
-      else {
+    LockLabel():QLabel(),locked(true) {
+        installEventFilter(this);
         setPixmap(QPixmap(":/tulip/gui/icons/i_locked.png"));
-        locked=true;
-      }
-
-      return true;
     }
 
-    return false;
-  }
+    bool isLocked() {
+        return locked;
+    }
 
-  bool locked;
+protected :
+    bool eventFilter(QObject *, QEvent *event) {
+        if(event->type() == QEvent::MouseButtonRelease) {
+            if(locked) {
+                setPixmap(QPixmap(":/tulip/gui/icons/i_unlocked.png"));
+                locked=false;
+            }
+            else {
+                setPixmap(QPixmap(":/tulip/gui/icons/i_locked.png"));
+                locked=true;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+
+    bool locked;
 
 };
 
-SnapshotDialog::SnapshotDialog(GlMainView &v,QWidget *parent):QDialog(parent),view(&v),pixmapItem(NULL),inSizeSpinBoxValueChanged(false) {
-  setupUi(this);
-  scene=new QGraphicsScene();
-  scene->setBackgroundBrush(QApplication::palette().color(QPalette::Midlight));
-  graphicsView->setScene(scene);
+SnapshotDialog::SnapshotDialog(GlMainView &v,QWidget *parent):QDialog(parent),view(&v),scene(NULL),pixmapItem(NULL),inSizeSpinBoxValueChanged(false) {
+    setupUi(this);
 
-  widthSpinBox->setValue(view->getGlMainWidget()->rect().width());
-  heightSpinBox->setValue(view->getGlMainWidget()->rect().height());
+    widthSpinBox->setValue(view->getGlMainWidget()->rect().width());
+    heightSpinBox->setValue(view->getGlMainWidget()->rect().height());
 
-  pixmapItem=scene->addPixmap(QPixmap(10,10));
+    sizeSpinBoxValueChanged();
 
-  sizeSpinBoxValueChanged();
+    connect(widthSpinBox,SIGNAL(valueChanged(int)),this,SLOT(widthSpinBoxValueChanged(int)));
+    connect(heightSpinBox,SIGNAL(valueChanged(int)),this,SLOT(heightSpinBoxValueChanged(int)));
+    connect(fileName,SIGNAL(textChanged(QString)),this,SLOT(fileNameTextChanged(const QString &)));
+    connect(browseButton,SIGNAL(clicked()),this,SLOT(browseClicked()));
+    connect(copybutton, SIGNAL(clicked()), this, SLOT(copyClicked()));
 
-  connect(widthSpinBox,SIGNAL(valueChanged(int)),this,SLOT(widthSpinBoxValueChanged(int)));
-  connect(heightSpinBox,SIGNAL(valueChanged(int)),this,SLOT(heightSpinBoxValueChanged(int)));
-  connect(fileName,SIGNAL(textChanged(QString)),this,SLOT(fileNameTextChanged(QString)));
-  connect(browseButton,SIGNAL(clicked()),this,SLOT(browseClicked()));
+    lockLabel=new LockLabel();
+    lockLayout->addWidget(lockLabel);
 
-  lockLabel=new LockLabel();
-  lockLayout->addWidget(lockLabel);
-
-#if (QT_VERSION >= QT_VERSION_CHECK(4, 7, 0))
-  fileName->setPlaceholderText(QApplication::translate("SnapshotDialogData", "Enter the filename or use the browse button", 0, QApplication::UnicodeUTF8));
-#endif
+    fileName->setPlaceholderText(QApplication::translate("SnapshotDialogData", "Enter the filename or use the browse button", 0, QApplication::UnicodeUTF8));
 }
 
 SnapshotDialog::~SnapshotDialog() {
 }
 
 void SnapshotDialog::resizeEvent(QResizeEvent *) {
-  sizeSpinBoxValueChanged();
+    sizeSpinBoxValueChanged();
 }
 
 void SnapshotDialog::accept() {
-  QPixmap pixmap=view->snapshot(QSize(widthSpinBox->value(),heightSpinBox->value()));
+    QPixmap pixmap=view->snapshot(QSize(widthSpinBox->value(),heightSpinBox->value()));
 
-  QImage image(pixmap.toImage());
+    QImage image(pixmap.toImage());
 
-  if(!image.save(fileName->text(),0,qualitySpinBox->value())) {
-    QMessageBox::critical(this,"Snapshot cannot be saved","Snapshot cannot be saved in file : "+fileName->text());
-  }
-  else {
-    QDialog::accept();
-  }
+    if(!image.save(fileName->text(),0,qualitySpinBox->value())) {
+        QMessageBox::critical(this,"Snapshot cannot be saved","Snapshot cannot be saved in file : "+fileName->text());
+    }
+    else {
+        QDialog::accept();
+    }
 }
 
 void SnapshotDialog::widthSpinBoxValueChanged(int value) {
-  if(inSizeSpinBoxValueChanged)
-    return;
+    if(inSizeSpinBoxValueChanged)
+        return;
 
-  inSizeSpinBoxValueChanged=true;
+    inSizeSpinBoxValueChanged=true;
 
-  if(lockLabel->isLocked()) {
-    heightSpinBox->setValue(value/ratio);
-  }
-  else {
-    sizeSpinBoxValueChanged();
-  }
+    if(lockLabel->isLocked()) {
+        heightSpinBox->setValue(value/ratio);
+    }
+    else {
+        sizeSpinBoxValueChanged();
+    }
 
-  inSizeSpinBoxValueChanged=false;
+    inSizeSpinBoxValueChanged=false;
 }
 
 void SnapshotDialog::heightSpinBoxValueChanged(int value) {
-  if(inSizeSpinBoxValueChanged)
-    return;
+    if(inSizeSpinBoxValueChanged)
+        return;
 
-  inSizeSpinBoxValueChanged=true;
+    inSizeSpinBoxValueChanged=true;
 
-  if(lockLabel->isLocked()) {
-    widthSpinBox->setValue(value*ratio);
-  }
-  else {
-    sizeSpinBoxValueChanged();
-  }
+    if(lockLabel->isLocked()) {
+        widthSpinBox->setValue(value*ratio);
+    }
+    else {
+        sizeSpinBoxValueChanged();
+    }
 
-  inSizeSpinBoxValueChanged=false;
+    inSizeSpinBoxValueChanged=false;
 }
 
 void SnapshotDialog::browseClicked() {
-  QList<QByteArray> formatList=QImageWriter::supportedImageFormats();
-  QString formatedFormatList;
+    QList<QByteArray> formatList=QImageWriter::supportedImageFormats();
+    QString formatedFormatList;
 
-  for(QList<QByteArray>::iterator it=formatList.begin(); it!=formatList.end(); ++it) {
-    formatedFormatList+=QString(*it).toLower()+" (*."+QString(*it).toLower()+");;";
-  }
+    for(QList<QByteArray>::iterator it=formatList.begin(); it!=formatList.end(); ++it) {
+        formatedFormatList+=QString(*it).toLower()+" (*."+QString(*it).toLower()+");;";
+    }
 
-  QString newFileName=QFileDialog::getSaveFileName(this,tr("Save Image As"), QDir::homePath(), tr(QString(formatedFormatList).toStdString().c_str()));
-  fileName->setText(newFileName);
+    QString newFileName=QFileDialog::getSaveFileName(this,tr("Save image as..."), QDir::homePath(), tr(QString(formatedFormatList).toStdString().c_str()));
+    fileName->setText(newFileName);
 }
 
-void SnapshotDialog::fileNameTextChanged(QString text) {
-  if(text!="")
-    okButton->setEnabled(true);
-  else
-    okButton->setEnabled(false);
+void SnapshotDialog::fileNameTextChanged(const QString &text) {
+    okButton->setEnabled(text.isEmpty()?false:true);
 }
 
 void SnapshotDialog::sizeSpinBoxValueChanged() {
 
-  float viewRatio=((float)graphicsView->width())/((float)graphicsView->height());
-  float imageRatio=((float)widthSpinBox->value())/((float)heightSpinBox->value());
+    float viewRatio=((float)graphicsView->width())/((float)graphicsView->height());
+    float imageRatio=((float)widthSpinBox->value())/((float)heightSpinBox->value());
 
-  QPixmap pixmap;
-  QImage image;
+    QPixmap pixmap;
 
-  if(viewRatio>imageRatio) {
-    pixmap=view->snapshot(QSize((graphicsView->height()-2)*imageRatio,graphicsView->height()-7));
-  }
-  else {
-    pixmap=view->snapshot(QSize(graphicsView->width()-2,(graphicsView->width()-2)/imageRatio));
-  }
+    if(viewRatio>imageRatio) {
+        pixmap=view->snapshot(QSize((graphicsView->height()-2)*imageRatio,graphicsView->height()-7));
+    }
+    else {
+        pixmap=view->snapshot(QSize(graphicsView->width()-2,(graphicsView->width()-2)/imageRatio));
+    }
 
-  ratio=((float)widthSpinBox->value())/((float)heightSpinBox->value());
+    ratio=((float)widthSpinBox->value())/((float)heightSpinBox->value());
 
-  delete pixmapItem;
-  delete scene;
-  scene=new QGraphicsScene();
-  scene->setBackgroundBrush(QApplication::palette().color(QPalette::Midlight));
-  graphicsView->setScene(scene);
-  QPixmap pixmap2;
-  QImage img(pixmap.toImage());
-  img = QImage(img.bits(),img.width(),img.height(),QImage::Format_ARGB32);
-  pixmap2.convertFromImage(img);
-  pixmapItem=scene->addPixmap(pixmap2);
-  pixmapItem->setPos(graphicsView->sceneRect().center()-pixmapItem->boundingRect().center());
+    if(pixmapItem==NULL) {
+        delete pixmapItem;
+        delete scene;
+    }
+    scene=new QGraphicsScene();
+    scene->setBackgroundBrush(QApplication::palette().color(QPalette::Midlight));
+    graphicsView->setScene(scene);
+    QPixmap pixmap2;
+    QImage img(pixmap.toImage());
+    img = QImage(img.bits(),img.width(),img.height(),QImage::Format_ARGB32);
+    pixmap2.convertFromImage(img);
+    pixmapItem=scene->addPixmap(pixmap2);
+    pixmapItem->setPos(graphicsView->sceneRect().center()-pixmapItem->boundingRect().center());
 }
 
+void SnapshotDialog::copyClicked() {
+    QPixmap pixmap=view->snapshot(QSize(widthSpinBox->value(),heightSpinBox->value()));
 
+    QClipboard *clipboard = QApplication::clipboard();
+    clipboard->setPixmap(pixmap);
+}
 
 }
