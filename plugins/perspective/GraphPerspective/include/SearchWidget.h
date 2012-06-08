d7a18335e76467419ef7f0c2ee61593dc07a0071
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-06-08 14:52:29 +0000

    Search is now working properly, still working on various features in this widget though
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7817 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/include/SearchWidget.h
+++ plugins/perspective/GraphPerspective/include/SearchWidget.h
@@ -7,15 +7,26 @@ namespace Ui {
 class SearchWidget;
 }
 
+class QComboBox;
+class SearchOperator;
 namespace tlp {
 class GraphHierarchiesModel;
 class Graph;
+class PropertyInterface;
 }
 
 class SearchWidget : public QWidget {
   Q_OBJECT
 
+  static QVector<SearchOperator*> NUMERIC_OPERATORS;
+  static QVector<SearchOperator*> STRING_OPERATORS;
+
   Ui::SearchWidget* _ui;
+  tlp::PropertyInterface* term(QComboBox* combo);
+
+  SearchOperator* searchOperator();
+  bool isNumericComparison();
+
 
 public:
   explicit SearchWidget(QWidget *parent = 0);
@@ -27,9 +38,15 @@ public slots:
   void setModel(tlp::GraphHierarchiesModel* model);
   void currentGraphChanged(tlp::Graph* g);
   void setGraph(tlp::Graph* g);
+  void search();
 
 protected slots:
   void graphIndexChanged(int);
+  void termAChanged();
+  void termBChanged();
+  void updateOperators(tlp::PropertyInterface* a, tlp::PropertyInterface* b);
+  void updateOperators(tlp::PropertyInterface* a, const QString& b);
+  void setNumericOperatorsEnabled(bool e);
 };
 
 #endif // SEARCHWIDGET_H
