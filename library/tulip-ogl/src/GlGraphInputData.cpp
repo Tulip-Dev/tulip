2a77a6fc79271de55ad74ca4dee2eea7fce7303c
Morgan Mathiaut <morgan.mathiaut@labri.fr>
2012-06-13 08:52:49 +0000

    - GlGraphInputData : Fix OpenGL update problem with sub-graph   - Now GlGraphInputData observe graph to detect added/removed properties
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@7847 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlGraphInputData.cpp
+++ library/tulip-ogl/src/GlGraphInputData.cpp
@@ -34,42 +34,63 @@ GlGraphInputData::GlGraphInputData(Graph* graph,GlGraphRenderingParameters* para
   if(graph) {
     propertiesMap[VIEW_COLOR]=graph->getProperty<ColorProperty>("viewColor");
     _properties.insert(propertiesMap[VIEW_COLOR]);
+    propertiesNameMap["viewColor"]=VIEW_COLOR;
     propertiesMap[VIEW_LABELCOLOR]=graph->getProperty<ColorProperty>("viewLabelColor");
     _properties.insert(propertiesMap[VIEW_LABELCOLOR]);
+    propertiesNameMap["viewLabelColor"]=VIEW_LABELCOLOR;
     propertiesMap[VIEW_SIZE]=graph->getProperty<SizeProperty>("viewSize");
     _properties.insert(propertiesMap[VIEW_SIZE]);
+    propertiesNameMap["viewSize"]=VIEW_SIZE;
     propertiesMap[VIEW_LABEL]=graph->getProperty<StringProperty>("viewLabel");
     _properties.insert(propertiesMap[VIEW_LABEL]);
+    propertiesNameMap["viewLabel"]=VIEW_LABEL;
     propertiesMap[VIEW_LABELPOSITION]=graph->getProperty<IntegerProperty>("viewLabelPosition");
     _properties.insert(propertiesMap[VIEW_LABELPOSITION]);
+    propertiesNameMap["viewLabelPosition"]=VIEW_LABELPOSITION;
     propertiesMap[VIEW_SHAPE]=graph->getProperty<IntegerProperty>("viewShape");
     _properties.insert(propertiesMap[VIEW_SHAPE]);
+    propertiesNameMap["viewShape"]=VIEW_SHAPE;
     propertiesMap[VIEW_ROTATION]=graph->getProperty<DoubleProperty>("viewRotation");
     _properties.insert(propertiesMap[VIEW_ROTATION]);
+    propertiesNameMap["viewRotation"]=VIEW_ROTATION;
     propertiesMap[VIEW_SELECTED]=graph->getProperty<BooleanProperty>("viewSelection");
     _properties.insert(propertiesMap[VIEW_SELECTED]);
+    propertiesNameMap["viewSelection"]=VIEW_SELECTED;
     propertiesMap[VIEW_FONT]=graph->getProperty<StringProperty>("viewFont");
     _properties.insert(propertiesMap[VIEW_FONT]);
+    propertiesNameMap["viewFont"]=VIEW_FONT;
     propertiesMap[VIEW_FONTSIZE]=graph->getProperty<IntegerProperty>("viewFontSize");
     _properties.insert(propertiesMap[VIEW_FONTSIZE]);
+    propertiesNameMap["viewFontSize"]=VIEW_FONTSIZE;
     propertiesMap[VIEW_TEXTURE]=graph->getProperty<StringProperty>("viewTexture");
     _properties.insert(propertiesMap[VIEW_TEXTURE]);
+    propertiesNameMap["viewTexture"]=VIEW_TEXTURE;
     propertiesMap[VIEW_BORDERCOLOR]=graph->getProperty<ColorProperty>("viewBorderColor");
     _properties.insert(propertiesMap[VIEW_BORDERCOLOR]);
+    propertiesNameMap["viewBorderColor"]=VIEW_BORDERCOLOR;
     propertiesMap[VIEW_BORDERWIDTH]=graph->getProperty<DoubleProperty>("viewBorderWidth");
     _properties.insert(propertiesMap[VIEW_BORDERWIDTH]);
+    propertiesNameMap["viewBorderWidth"]=VIEW_BORDERWIDTH;
     propertiesMap[VIEW_LAYOUT]=graph->getProperty<LayoutProperty>("viewLayout");
     _properties.insert(propertiesMap[VIEW_LAYOUT]);
+    propertiesNameMap["viewLayout"]=VIEW_LAYOUT;
     propertiesMap[VIEW_SRCANCHORSHAPE]=graph->getProperty<IntegerProperty>("viewSrcAnchorShape");
     _properties.insert(propertiesMap[VIEW_SRCANCHORSHAPE]);
+    propertiesNameMap["viewSrcAnchorShape"]=VIEW_SRCANCHORSHAPE;
     propertiesMap[VIEW_SRCANCHORSIZE]=graph->getProperty<SizeProperty>("viewSrcAnchorSize");
     _properties.insert(propertiesMap[VIEW_SRCANCHORSIZE]);
+    propertiesNameMap["viewSrcAnchorSize"]=VIEW_SRCANCHORSIZE;
     propertiesMap[VIEW_TGTANCHORSHAPE]=graph->getProperty<IntegerProperty>("viewTgtAnchorShape");
     _properties.insert(propertiesMap[VIEW_TGTANCHORSHAPE]);
+    propertiesNameMap["viewTgtAnchorShape"]=VIEW_TGTANCHORSHAPE;
     propertiesMap[VIEW_TGTANCHORSIZE]=graph->getProperty<SizeProperty>("viewTgtAnchorSize");
     _properties.insert(propertiesMap[VIEW_TGTANCHORSIZE]);
+    propertiesNameMap["viewTgtAnchorSize"]=VIEW_TGTANCHORSIZE;
     propertiesMap[VIEW_ANIMATIONFRAME]=new IntegerProperty(graph,"viewAnimationFrame");
     _properties.insert(propertiesMap[VIEW_ANIMATIONFRAME]);
+    propertiesNameMap["viewAnimationFrame"]=VIEW_ANIMATIONFRAME;
+
+    graph->addListener(this);
   }
 
   GlyphManager::getInst().initGlyphList(&this->graph, this, glyphs);
@@ -95,5 +116,23 @@ GlGraphInputData::~GlGraphInputData() {
 
   if(deleteMetaNodeRendererAtDestructor)
     delete metaNodeRenderer;
+}
+
+void GlGraphInputData::treatEvent(const Event &ev) {
+  if (dynamic_cast<const GraphEvent*>(&ev) != NULL) {
+    const GraphEvent* graphEv = static_cast<const GraphEvent*>(&ev);
+
+    if (graphEv->getType() == GraphEvent::TLP_ADD_LOCAL_PROPERTY ||
+        graphEv->getType() == GraphEvent::TLP_AFTER_DEL_LOCAL_PROPERTY ||
+        graphEv->getType() == GraphEvent::TLP_ADD_INHERITED_PROPERTY ||
+        graphEv->getType() == GraphEvent::TLP_AFTER_DEL_INHERITED_PROPERTY){
+      if(propertiesNameMap.count(graphEv->getPropertyName())!=0){
+        PropertyInterface *oldProperty=propertiesMap[propertiesNameMap[graphEv->getPropertyName()]];
+        _properties.erase(oldProperty);
+        propertiesMap[propertiesNameMap[graphEv->getPropertyName()]]=graph->getProperty(graphEv->getPropertyName());
+        _properties.insert(propertiesMap[propertiesNameMap[graphEv->getPropertyName()]]);
+      }
+    }
+  }
 }
 }
