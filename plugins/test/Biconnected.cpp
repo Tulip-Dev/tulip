ce30b6ccbbdef98309e52808b1274d9548175a5e
Bruno Pinaud <bruno.pinaud@labri.fr>
2012-08-30 13:39:32 +0000

    fix plugin name
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8085 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/test/Biconnected.cpp
+++ plugins/test/Biconnected.cpp
@@ -22,7 +22,7 @@
 
 class BiconnectedTest : public tlp::GraphTest {
 public:
-  PLUGININFORMATIONS("Biconneted Test", "Tulip team", "18/04/2012", "Tests whether a graph is biconnected or not.", "1.0", "Connectivity")
+  PLUGININFORMATIONS("Biconnected Test", "Tulip team", "18/04/2012", "Tests whether a graph is biconnected or not.", "1.0", "Connectivity")
   BiconnectedTest(const tlp::PluginContext* context) : tlp::GraphTest(context) {
   }
 
@@ -35,7 +35,7 @@ PLUGIN(BiconnectedTest)
 
 class MakeBiconnected : public tlp::Algorithm {
 public:
-  PLUGININFORMATIONS("Make Biconneted", "Tulip team", "18/04/2012", "Makes a graph biconnected.", "1.0", "Connectivity")
+  PLUGININFORMATIONS("Make Biconnected", "Tulip team", "18/04/2012", "Makes a graph biconnected.", "1.0", "Connectivity")
   MakeBiconnected(const tlp::PluginContext* context) : tlp::Algorithm(context) {
   }
 
