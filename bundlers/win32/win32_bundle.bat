a5a0df7bce97901aa26d1ed93240040813c10c1f
Charles Huet <packadal@gmail.com>
2012-09-11 09:46:17 +0000

    converted the msys script to a bat script, makes it way easier to run on windows machines through Jenkins.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8114 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ bundlers/win32/win32_bundle.bat
@@ -0,0 +1,21 @@
+@echo OFF
+set TULIP_DIR=%1
+set DEST_DIR=%2
+set SRC_DIR=%cd%
+
+echo 'Wiping destination directory'
+del /Q /F /S %DEST_DIR%
+mkdir "%DEST_DIR%\\files\\lib"
+mkdir "%DEST_DIR%\\files\\bin"
+mkdir "%DEST_DIR%\\files\\include"
+
+echo 'Copying tulip files'
+xcopy "%TULIP_DIR%" "%DEST_DIR%\\files" /E /Q
+
+echo 'Copying NSIS script'
+copy "%SRC_DIR%\\Tulip.nsi" "%DEST_DIR%\\"
+copy "%SRC_DIR%\\..\\..\\COPYING" "%DEST_DIR%\\"
+
+echo 'Running NSIS generator'
+cd %DEST_DIR%
+makensis Tulip.nsi
