72d2a0db760e45297a6383c5b03427c5b1c7234b
Charles Huet <packadal@gmail.com>
2012-09-28 08:26:29 +0000

    documented edge. also changed include orders in Node.h
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8329 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/Edge.h
+++ library/tulip-core/include/tulip/Edge.h
@@ -16,7 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
 
 #ifndef Tulip_EDGE_H
 #define Tulip_EDGE_H
@@ -28,23 +27,66 @@ namespace tlp {
 /**
  * @ingroup Graph
  * @brief The edge struct represents an edge in a Graph object.
- * An edge is represented by its id (unsigned int). An edge is considered invalid when id == UINT_MAX
- * Any operation performed on an edge (getting the source, target etc), is available into the tlp::Graph object.
+ *
+ * This structure only contains an identifier, and a function to check if the node is valid.
+ * A node is considered invalid when its id has the UINT_MAX value.
+ *
+ * Most operations performed on an edge (getting the source, target, etc) are available into the tlp::Graph object.
+ *
  * @see tlp::node
+ * @see tlp::Graph
  */
 struct edge {
+  /**
+   * @brief id The identifier of the edge.
+   */
   unsigned int id;
+
+  /**
+   * @brief edge creates an invalid edge.
+   */
   edge():id(UINT_MAX) {}
+
+  /**
+   * @brief edge Create an edge of given identifier.
+   * It is your responsibility to make sure an edge of this ID exists when you create the edge.
+   * If you want to make sure this edge exists, use Graph::isElement(), as isValid() will only tell is the edge was correctly initialized.
+   *
+   * @param j the identifier this edge will use.
+   */
   explicit edge(unsigned int j):id(j) {}
+
+  /**
+   * @brief operator unsigned int A convenience function to get the id of an edge.
+   */
   operator unsigned int() const {
     return id;
   }
+
+  /**
+   * @brief operator != Compares two edges, checking that they are different..
+   * @param n The other edge to compare this one to.
+   * @return Whether or not the two edges are different.
+   */
   bool operator==(const edge e) const {
     return id==e.id;
   }
+
+  /**
+   * @brief operator != Compares two edges, checking that they are identical.
+   * @param n The other edge to compare this one to.
+   * @return Whether or not the two edges are the same.
+   */
   bool operator!=(const edge e) const {
     return id!=e.id;
   }
+
+  /**
+   * @brief isValid checks if the edge is valid.
+   * An invalid edge is an edge whose id is UINT_MAX.
+   *
+   * @return whether the edge is valid or not.
+   */
   bool isValid() const {
     return id!=UINT_MAX;
   }
@@ -53,7 +95,7 @@ struct edge {
 }
 
 #ifndef DOXYGEN_NOTFOR_DEVEL
-
+//these three functions allow to use tlp::edge as a key in a hash-based data structure (e.g. hashmap).
 TLP_BEGIN_HASH_NAMESPACE {
   template<>
   struct hash<tlp::edge>{
@@ -77,4 +119,3 @@ struct less<tlp::edge> {
 }
 #endif
 #endif
-///@endcond
