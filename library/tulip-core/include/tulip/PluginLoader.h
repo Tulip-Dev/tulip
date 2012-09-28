1646cd98ef7d2edb51e79b24325ca233a9c40178
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-09-28 09:39:41 +0000

    Completed PluginLoader doxygen
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8339 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/PluginLoader.h
+++ library/tulip-core/include/tulip/PluginLoader.h
@@ -16,7 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
 
 #ifndef TLP_PLUGINLOADER
 #define TLP_PLUGINLOADER
@@ -28,16 +27,60 @@ namespace tlp {
 
 class Plugin;
 
+/**
+ * @ingroup Plugins
+ *
+ * @brief A callback class when loading plugins into Tulip
+ *
+ * This interface can be subclassed and passed to the tlp::PluginLibraryLoader to implement custom event handling when loading plugins into Tulip
+ *
+ * @see tlp::PluginLibraryLoader
+ * @see tlp::PluginLoaderTxt
+ */
 struct TLP_SCOPE PluginLoader {
   virtual ~PluginLoader() {}
+
+  /**
+   * @brief Called when starting to load plugins into a given directory
+   * @param path The absolute path of the directory
+   */
   virtual void start(const std::string &path)=0;
+
+  /**
+   * @brief Indicates the number of files to be loaded
+   * @param int the number of files
+   */
   virtual void numberOfFiles(int) {}
+
+  /**
+   * @brief Indicates that a new file is being loaded
+   * @param filename The absolute path of the file
+   */
   virtual void loading(const std::string &filename)=0;
+
+  /**
+   * @brief Indicates that a plugin has been loaded sucessfully
+   * @param infos The Plugin object that has just been loaded
+   * @param dependencies The plugin dependencies
+   *
+   * @see tlp::Dependency
+   */
   virtual void loaded(const Plugin* infos, const std::list <Dependency>& dependencies)=0;
+
+  /**
+   * @brief Indicates that an error occurred when trying to load a file.
+   * @param filename The absolute path of where the error occured.
+   * @param errormsg A human-readable error message.
+   */
   virtual void aborted(const std::string &filename,const  std::string &errormsg)=0;
+
+  /**
+   * @brief Indicates that a file has finished being loaded
+   * @param state true if the file was loaded sucessfully
+   * @param msg An additional human-readable message about the load state
+   */
   virtual void finished(bool state,const std::string &msg)=0;
 };
 
 }
 #endif
-///@endcond
