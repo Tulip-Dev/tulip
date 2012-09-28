4ea19e2ac685b4257eb6ec3c9b9412035e915767
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-09-28 06:24:47 +0000

    Added a developer note for documentation writing conventions
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8321 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- doc/doxygen/doxygen_groups.h
+++ doc/doxygen/doxygen_groups.h
@@ -16,11 +16,19 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
 
-
-#ifndef DOXYGEN_GROUPS_H
-#define DOXYGEN_GROUPS_H
+/*
+ * Note for developers
+ *
+ * When writing the doxygen documentation for a class, one should follow those steps:
+ *
+ * 1/ Remove the /// @cond DOXYGEN_HIDDEN condition on top of the header and the /// @endcond tag at the bottom.
+ *   If you are adding a new class to Tulip and didn't write the documentation yet, you should add those tags until the documentation is written.
+ * 2/ Every class should belong to a doxygen group. This means that a @ingroup declaration must be added before the class @brief tag.
+ * 3/ Definitions for doxygen groups are found in this file.
+ * 4/ Code examples and tutorials belong in this directory too. There should be one header per tutorial to ease documentation process.
+ * 5/ Please remember that code comments should NEVER begin with /** (use /* instead) in order to exclude them from doxygen.
+ */
 
 /**
  * @defgroup Checks
@@ -56,6 +64,3 @@
  * @brief Tulip plugins interfaces
  * Classes in this group are interfaces for the plugins system. You will subclass one of them when creating your own Tulip plugins.
  */
-
-#endif // DOXYGEN_GROUPS_H
-///@endcond
