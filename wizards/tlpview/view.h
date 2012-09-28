5e0986bc2026f28ea665747ca22fb28d931096f7
Charles Huet <packadal@gmail.com>
2012-09-28 14:43:21 +0000

    added the updated view wizard (still WIP)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8350 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ wizards/tlpview/view.h
@@ -0,0 +1,24 @@
+#ifndef Tulip_%ProjectName:c%_VIEW_H
+#define Tulip_%ProjectName:c%_VIEW_H
+
+#include <tulip/NodeLinkDiagramComponent.h>
+
+#include "%ProjectName:c%ConfigWidget.h"
+
+class %ProjectName:c% : public tlp::NodeLinkDiagramComponent {
+	Q_OBJECT
+public:
+	PLUGININFORMATIONS("%PluginName%", "%Author%", "%Date%", "%Informations%", "%Version%", "%Group%")
+
+	%ProjectName:c%(const tlp::PluginContext* context = NULL);
+	virtual ~%ProjectName:c%() {}
+	
+	tlp::DataSet state() const;
+	void setState(const tlp::DataSet&);
+	QList<QWidget*> configurationWidgets() const;
+	
+private :
+	%ProjectName:c%ConfigWidget *configWidget;
+};
+
+#endif
