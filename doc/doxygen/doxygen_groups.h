aac5cfa656dc898791e063e0f07a8a342af36040
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-09-28 06:27:41 +0000

    Completed developer notes and added pretty names for doxygen groups
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8322 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- doc/doxygen/doxygen_groups.h
+++ doc/doxygen/doxygen_groups.h
@@ -25,42 +25,43 @@
  * 1/ Remove the /// @cond DOXYGEN_HIDDEN condition on top of the header and the /// @endcond tag at the bottom.
  *   If you are adding a new class to Tulip and didn't write the documentation yet, you should add those tags until the documentation is written.
  * 2/ Every class should belong to a doxygen group. This means that a @ingroup declaration must be added before the class @brief tag.
+ * 3/ A doxygen group is identified by a single-word identifier (first word after the @defgroup declaration). What follows in the  @defgroup declaration is the pretty name that will appear into the final HTML document.
  * 3/ Definitions for doxygen groups are found in this file.
  * 4/ Code examples and tutorials belong in this directory too. There should be one header per tutorial to ease documentation process.
  * 5/ Please remember that code comments should NEVER begin with /** (use /* instead) in order to exclude them from doxygen.
  */
 
 /**
- * @defgroup Checks
+ * @defgroup Checks Model Checking
  * @brief Graph model checking
  * Classes in this group allow to perform various checks on a graph data structure.
  */
 
 /**
- * @defgroup Structures
+ * @defgroup Structures Data structures
  * @brief Tulip basic data structures
  */
 
 /**
- * @defgroup Graph
+ * @defgroup Graph Graph model
  * @brief Tulip graphs manipulation system
  * Classes in this group are related to the core system of Graph manipulation.
  */
 
 /**
- * @defgroup Observation
+ * @defgroup Observation Observation system
  * @brief Tulip observer pattern implementation
  * Classes in this group are involved in the observation system.
  */
 
 /**
- * @defgroup OpenGL
+ * @defgroup OpenGL 3D graph rendering
  * @brief OpenGL graph rendering
  * Classes in this group are related to the OpenGL rendering of graphs and to the OpenGL augmented display
  */
 
 /**
- * @defgroup Plugins
+ * @defgroup Plugins Plugins system
  * @brief Tulip plugins interfaces
  * Classes in this group are interfaces for the plugins system. You will subclass one of them when creating your own Tulip plugins.
  */
