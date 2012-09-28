b52d838761dafa69e9e5d0208a273a82d743895d
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-09-28 08:56:14 +0000

    Completed documentation for tlp::PluginContext and tlp::AlgorithmContext
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8334 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/PluginContext.h
+++ library/tulip-core/include/tulip/PluginContext.h
@@ -16,9 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
-
-
 #ifndef _PLUGINCONTEXT
 #define _PLUGINCONTEXT
 
@@ -30,38 +27,51 @@ class Graph;
 class PropertyInterface;
 class PluginProgress;
 
+/**
+ * @ingroup Plugins
+ * @brief Contains runtime parameters for a plugin.
+ *
+ * tlp::PluginContext is a data structure containing a set of parameters required by a plugin.
+ * Each tlp::Plugin subclass has its associated tlp::PluginContext subclass that contains specific parameters related to the Plugin.
+ * For instance, the tlp::AlgorithmContext (related to tlp::Algorithm) contains the Graph on which the algorithm should be run
+ * In essence, you will never have to access to a plugin's context since the base class constructor will copy its members into protected members of the plugin interface.
+ */
 class PluginContext {
 public:
-  /**
-   * @ingroup Plugins
-   * @brief required for dynamic casting
-   *
-   **/
+  // Required for dynamic casting
   virtual ~PluginContext() {}
 };
 
-///Parameter for calling clustering plugins
+/**
+ * @ingroup Plugins
+ * @brief Parameters structure for a tlp::Algorithm
+ *
+ * This class contains data required for a tlp::Algorithm to run. Each of its member is copied into a protected member at the object construction.
+ */
 class AlgorithmContext : public tlp::PluginContext {
 public :
-  ///
+  /**
+   * @brief The pointer to the tlp::Graph on which the algorithm will be run.
+   */
   Graph *graph;
+
+  /**
+   * @brief Input parameters set by the user when running the plugin.
+   * @see tlp::DataSet
+   * @see tlp::WithDependency
+   */
   DataSet *dataSet;
+
+  /**
+   * @brief A progress handler to notify the user about the progress state of the algorithm when run.
+   * @see tlp::PluginProgress
+   */
   PluginProgress *pluginProgress;
-  ///
+
   AlgorithmContext (tlp::Graph* graph = NULL, tlp::DataSet* dataSet = NULL, tlp::PluginProgress* progress = NULL): graph(graph), dataSet(dataSet), pluginProgress(progress) {}
-  ///
   ~AlgorithmContext() {}
 };
 
 
 }
 #endif
-
-
-
-
-
-
-
-
-///@endcond
