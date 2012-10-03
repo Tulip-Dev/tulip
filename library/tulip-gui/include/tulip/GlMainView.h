9636b28929323cab1c9e4726efbb8fda32611cd9
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-10-03 07:52:35 +0000

    Completed doxygen for GlMainView
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8402 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/include/tulip/GlMainView.h
+++ library/tulip-gui/include/tulip/GlMainView.h
@@ -16,7 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
 
 #ifndef Tulip_GLMAINVIEW_H
 #define Tulip_GLMAINVIEW_H
@@ -34,6 +33,31 @@ class SceneLayersConfigWidget;
 class GlMainWidget;
 class QuickAccessBar;
 
+/**
+ * @ingroup Plugins
+ *
+ * @brief An abstract view that displays a GlMainWidget as its central widget.
+ *
+ * The GlMainView subclasses ViewWidget and always uses a GlMainWidget as the central widget of the panel. It also adds the following features:
+ * @list
+ * @li An overview of the scene that can be toggled on or off.
+ * @li Some configuration widgets that modify the rendering parameters.
+ * @li A quick access bar widget that allows the user to quickly modify some of the most used rendering parameters and graph properties (nodes color, edges display, etc)
+ * @li The possibility to make snapshots of the current scene
+ * @endlist
+ *
+ * Subclassing GlMainView means you will only want to display graphs in a single GlMainWidget. Switching the central widget can only be achieved from the ViewWidget class.
+ *
+ * @warning It is strongly unadvised to re-implement methods already implemented into tlp::View or tlp::ViewWidget. If you have to add custom behavior to those method, make sure to call the upper-class methods first:
+ @code
+ void MyView::setupWidget() { // Where MyView is a subclass of tlp::GlMainView
+   GlMainView::setupWidget(); // call this first
+   // insert custom behavior here
+ }
+ @endcode
+
+ * @see tlp::ViewWidget
+ */
 class TLP_QT_SCOPE GlMainView: public tlp::ViewWidget {
   Q_OBJECT
 
@@ -56,13 +80,44 @@ public:
   QPixmap snapshot(const QSize &outputSize=QSize());
 
 public slots:
+  /**
+   * @brief Calls GlMainWidget::draw();
+   */
   virtual void draw(tlp::PluginProgress* pluginProgress);
+
+  /**
+   * @brief Calls GlMainWidget::redraw();
+   */
   void redraw();
+
+  /**
+   * @brief Calls GlMainWidget::redraw();
+   */
   virtual void refresh(PluginProgress *pluginProgress);
+
+  /**
+   * @brief Force the overview to be redrawn. Since GlMainView already detects graph's modifications, this method should not be called manually to avoid extra rendering.
+   */
   virtual void drawOverview(bool generatePixmap=true);
+
+  /**
+   * @brief Centers the scene's camera
+   */
   virtual void centerView();
+
+  /**
+   * @brief Toggles the overview on or off
+   */
   void setOverviewVisible(bool);
+
+  /**
+   * @brief Force the settings set in the configuration widgets to be re-applied.
+   */
   void applySettings();
+
+  /**
+   * @brief Display a dialog that takes a snapshot of the current scene.
+   */
   void openSnapshotDialog();
 
 protected slots:
@@ -81,4 +136,3 @@ protected:
 }
 
 #endif /* GLMAINVIEW_H_ */
-///@endcond
