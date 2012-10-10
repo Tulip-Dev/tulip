d8bedab20cb8780728ab249ead0a4bb07adb91ba
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-10-10 08:48:14 +0000

    [ Mantis #004961 ] Added a reverse edges plugin
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8521 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ plugins/general/ReverseEdges/reverseedges.h
@@ -0,0 +1,20 @@
+#ifndef REVERSEEDGES_H
+#define REVERSEEDGES_H
+
+#include <tulip/Algorithm.h>
+
+class ReverseEdges:public tlp::Algorithm {
+public:
+  PLUGININFORMATIONS("Reverse edges", "Ludwig Fiolka", "10/10/2012", "Reverse all/selected edges on the current graph", "0.1", "")
+  
+  ReverseEdges(tlp::PluginContext* context);
+  virtual ~ReverseEdges();
+  
+  bool run();
+  bool check(std::string &);
+};
+
+#endif // REVERSEEDGES_H
+
+
+
