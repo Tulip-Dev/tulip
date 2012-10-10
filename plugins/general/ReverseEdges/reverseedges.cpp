238f740bbf1b12fef132dc1202a61cd31132885c
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-10-10 08:48:14 +0000

    [ Mantis #004961 ] Added a reverse edges plugin
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8521 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ plugins/general/ReverseEdges/reverseedges.cpp
@@ -0,0 +1,61 @@
+#include "reverseedges.h"
+
+#include <tulip/BooleanProperty.h>
+
+PLUGIN(ReverseEdges)
+
+using namespace tlp;
+
+ReverseEdges::ReverseEdges(tlp::PluginContext* context): Algorithm(context) {
+  addInParameter<BooleanProperty>("Selection","Only reverse edges selected in this property", "viewMetric");
+}
+
+ReverseEdges::~ReverseEdges() {
+}
+
+bool ReverseEdges::run() {
+  bool result = true;
+
+  BooleanProperty* selection = NULL;
+  bool deleteSelection = false;
+  if (dataSet->exist("Selection")) {
+    dataSet->get<BooleanProperty*>("Selection",selection);
+  }
+  if (selection == NULL) {
+    deleteSelection = true;
+    selection = new BooleanProperty(graph);
+    selection->setAllNodeValue(true);
+    selection->setAllEdgeValue(true);
+  }
+
+  graph->push();
+  int step = 0, max_step = graph->numberOfEdges();
+  edge e;
+  stableForEach(e, selection->getEdgesEqualTo(true)) {
+    ProgressState state = pluginProgress->progress(step++, max_step);
+    result = state == TLP_CONTINUE;
+    if (state == TLP_CANCEL) {
+      graph->pop();
+      break;
+    }
+    else if (state == TLP_STOP) {
+      break;
+    }
+    node src = graph->source(e);
+    node tgt = graph->target(e);
+    graph->delEdge(e,true);
+    edge newEdge = graph->addEdge(tgt,src);
+    selection->setEdgeValue(newEdge,true);
+  }
+
+  if (deleteSelection)
+    delete selection;
+
+  return result;
+}
+
+bool ReverseEdges::check(std::string &msg) {
+  msg="";
+  return true;
+}
+
