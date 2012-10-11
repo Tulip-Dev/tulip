b075aa738399d2da4807bb222192c742eb1c2c7e
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-10-11 06:11:47 +0000

    Completed doxygen for interactor system
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8529 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/include/tulip/GLInteractor.h
+++ library/tulip-gui/include/tulip/GLInteractor.h
@@ -16,8 +16,6 @@
  * See the GNU General Public License for more details.
  *
  */
-///@cond DOXYGEN_HIDDEN
-
 #ifndef GLINTERACTOR_H
 #define GLINTERACTOR_H
 
@@ -27,17 +25,43 @@ namespace tlp {
 
 class GlMainWidget;
 
+/**
+ * @brief The GLInteractorComponent class is an InteractorComponent that can do OpenGL rendering on a GlMainWidget.
+ *
+ * @warning Only GLInteractorComponent that are stored into a GLInteractorComposite will be able to do OpenGL rendering. Storing them into an InteractorComposite will have no effect.
+ *
+ * GLInteractorComponent are meant to be stored into a GLInteractorComposite
+ * When installed on a GlMainWidget, this interactor will have two new methods getting called:
+ * @list
+ * @li draw: Draws a visual feedback into the OpenGL scene. This is called after the scene was rendered
+ * @li compute: Allows the interactor to do some pre-initialisation steps before the OpenGL scene is rendered.
+ * @endlist
+ */
 class TLP_QT_SCOPE GLInteractorComponent: public tlp::InteractorComponent {
   Q_OBJECT
 public slots:
+
+  /**
+   * @brief Draws an OpenGL visual feedback for the interactor into a given GlMainWidget.
+   * This method is called after the scene was rendered.
+   * @return true if the rendering completed sucessfully
+   */
   virtual bool draw(tlp::GlMainWidget*) {
     return false;
   }
+
+  /**
+   * @brief Initializes the interactor before the scene in the given GlMainWidget is rendered
+   */
   virtual bool compute(tlp::GlMainWidget*) {
     return false;
   }
 };
 
+/**
+ * @brief The GLInteractorComposite class behaves like a InteractorComposite but is meant to store GLInteractorComponent.
+ * @warning Only GLInteractorComponent that are stored into a GLInteractorComposite will be able to do OpenGL rendering. Storing them into an InteractorComposite will have no effect.
+ */
 class TLP_QT_SCOPE GLInteractorComposite: public tlp::InteractorComposite {
   Q_OBJECT
 
@@ -45,11 +69,19 @@ public:
   GLInteractorComposite(const QIcon& icon, const QString& text="");
 
 public slots:
+  /**
+   * @brief Calls the compute method on every sub-components
+   * @note You can subclass this method to add custom behavior before or after components are called.
+   */
   virtual void compute(tlp::GlMainWidget*);
+
+  /**
+   * @brief Calls the draw method on every sub-components
+   * @note You can subclass this method to add custom behavior before or after components are called.
+   */
   virtual void draw(tlp::GlMainWidget*);
 };
 
 }
 
 #endif // GLINTERACTOR_H
-///@endcond
