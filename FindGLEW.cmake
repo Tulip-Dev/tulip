a506bfef52bf74c581e09454036a5c0cca6728a8
Ludwig Fiolka <ludwig.fiolka@labri.fr>
2012-10-11 10:48:58 +0000

    Trying to remove dependencies to libjpeg, libpng and cleanup FindGLEW. Will commit this to try on windows/mac environments and revert it if it poses problems
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8534 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- FindGLEW.cmake
+++ FindGLEW.cmake
@@ -3,54 +3,12 @@
 #  
 #  GLEW_FOUND        - system has GLEW
 #  GLEW_INCLUDE_DIR  - the GLEW include directory
-#  GLEW_LIBRARY_DIR  - where the libraries are
 #  GLEW_LIBRARY      - Link these to use GLEW
-#   
 
-IF (GLEW_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(GLEW_FIND_QUIETLY TRUE)
-ENDIF (GLEW_INCLUDE_DIR)
-
-if( WIN32 )
-   if( MSVC80 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ 8/VC" )
-   endif( MSVC80 )
-   if( MSVC71 )
-       set( COMPILER_PATH "C:/Program\ Files/Microsoft\ Visual\ Studio\ .NET\ 2003/Vc7" )
-   endif( MSVC71 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS c:/glew/include ${COMPILER_PATH}/PlatformSDK/Include
-              PATH_SUFFIXES gl GL )
-   SET( GLEW_NAMES glew32 glew )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS c:/glew/lib ${COMPILER_PATH}/PlatformSDK/Lib )
-else( WIN32 )
-   FIND_PATH( GLEW_INCLUDE_DIR glew.h wglew.h
-              PATHS /usr/local/include /usr/include
-              PATH_SUFFIXES gl GL )
-   SET( GLEW_NAMES glew GLEW )
-   FIND_LIBRARY( GLEW_LIBRARY
-                 NAMES ${GLEW_NAMES}
-                 PATHS /usr/lib /usr/local/lib )
-endif( WIN32 )
-
-GET_FILENAME_COMPONENT( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} PATH )
-IF(GLEW_INCLUDE_DIR)
-    # Remove the GL or gl subdirectory, because you should say #include <GL/foo.h>, not #include <foo.h>
-    set(GLEW_INCLUDE_DIR ${GLEW_INCLUDE_DIR}/..)
-ENDIF()
-
-IF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET(GLEW_FOUND TRUE)
-    #SET( GLEW_LIBRARY_DIR ${GLEW_LIBRARY} )
-ELSE (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-   SET( GLEW_FOUND FALSE )
-   SET( GLEW_LIBRARY_DIR )
-ENDIF (GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
-
-MARK_AS_ADVANCED(
-  GLEW_LIBRARY
-  GLEW_INCLUDE_DIR
-)
+FIND_LIBRARY(GLEW_LIBRARY NAMES glew GLEW)
+FIND_PATH(GLEW_INCLUDE_DIR NAMES GL/glew.h GL/wglew.h glew.h wglew.h)
+IF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+  SET(GLEW_FOUND TRUE)
+ELSE(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
+   SET(GLEW_FOUND FALSE)
+ENDIF(GLEW_INCLUDE_DIR AND GLEW_LIBRARY)
