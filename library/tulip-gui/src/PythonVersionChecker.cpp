c06ee222d7fdaf57e3e182a7ff61a81b3f1d6502
Antoine Lambert <antoine.lambert33@gmail.com>
2012-10-22 12:29:00 +0000

    - small improvement for the Python version checker :     * try to run "python<TLP_PYTHON>" command first (multiple version of Python can be installed on the host sytem)     * run the "python" command if the above one failed
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8652 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/PythonVersionChecker.cpp
+++ library/tulip-gui/src/PythonVersionChecker.cpp
@@ -6,23 +6,30 @@ using namespace tlp;
 
 QString PythonVersionChecker::installedVersion() {
   QProcess pythonCheck;
-  pythonCheck.start("python",QStringList() << "--version");
+  pythonCheck.start(QString("python")+compiledVersion(),QStringList() << "--version");
   pythonCheck.waitForFinished(-1);
 
-  if (pythonCheck.exitStatus() == QProcess::NormalExit) {
-    pythonCheck.setReadChannel(QProcess::StandardError);
-    QString result = pythonCheck.readAll();
-    QRegExp versionRegexp(".*([0-9]*\\.[0-9]*)\\..*");
-    if (versionRegexp.exactMatch(result)) {
-      QString installedVersion = versionRegexp.cap(1);
-      return installedVersion;
-    }
-    else {
-      qWarning() << QObject::trUtf8("Error parsing python version");
-    }
-  }
-  else {
-    qWarning() << QObject::trUtf8("Python installation not found");
+  if (pythonCheck.exitStatus() != QProcess::NormalExit) {
+      pythonCheck.start(QString("python"),QStringList() << "--version");
+      pythonCheck.waitForFinished(-1);
+
+      if (pythonCheck.exitStatus() == QProcess::NormalExit) {
+        pythonCheck.setReadChannel(QProcess::StandardError);
+        QString result = pythonCheck.readAll();
+        QRegExp versionRegexp(".*([0-9]*\\.[0-9]*)\\..*");
+        if (versionRegexp.exactMatch(result)) {
+          QString installedVersion = versionRegexp.cap(1);
+          return installedVersion;
+        }
+        else {
+          qWarning() << QObject::trUtf8("Error parsing python version");
+        }
+      }
+      else {
+        qWarning() << QObject::trUtf8("Python installation not found");
+      }
+  } else {
+      return compiledVersion();
   }
 
   return QString::null;
