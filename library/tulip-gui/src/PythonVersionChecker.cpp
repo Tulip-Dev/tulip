2c2a67db8fc08c73fde559355225c728d6ed8139
Antoine Lambert <antoine.lambert33@gmail.com>
2012-10-25 17:39:18 +0000

    - use a more robust implementation for checking which version of Python is installed on windows platform (based on registry keys)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8741 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/PythonVersionChecker.cpp
+++ library/tulip-gui/src/PythonVersionChecker.cpp
@@ -1,19 +1,50 @@
 #include <tulip/PythonVersionChecker.h>
 
 #include <QtCore/QProcess>
+#include <QtCore/QSettings>
+
+#include <iostream>
 
 using namespace tlp;
+using namespace std;
+
+#ifdef WIN32
+static const char *pythonVersion[]  = {
+  "3.3", "3.2", "3.1", "3.0", "2.7", "2.6", "2.5", 0
+};
+
+static bool isPythonVersionInstalled(const QString &pythonVersion) {
+	QString win32RegKey = QString("HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\") + pythonVersion + QString("\\InstallPath");
+	QString win64RegKey = QString("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Python\\PythonCore\\") + pythonVersion + QString("\\InstallPath");
+	QSettings win32Settings(win32RegKey, QSettings::NativeFormat);
+	QSettings win64Settings(win64RegKey, QSettings::NativeFormat);
+	return win32Settings.value("Default").toString() != "" || win64Settings.value("Default").toString() != "";
+}
+
+static QString getInstalledPythonVersionIfAny() {
+	int i = 0;
+	while (pythonVersion[i]) {
+		if (isPythonVersionInstalled(pythonVersion[i])) {
+			return pythonVersion[i];
+		}
+		++i;
+	}
+	return "";
+}
+#endif
+
 
 QString PythonVersionChecker::installedVersion() {
+#ifndef WIN32
   QProcess pythonCheck;
-  pythonCheck.start(QString("python")+compiledVersion(),QStringList() << "--version");
+  pythonCheck.start(QString("python")+compiledVersion(),QStringList() << "--version");  
   pythonCheck.waitForFinished(-1);
 
   if (pythonCheck.exitStatus() != QProcess::NormalExit) {
       pythonCheck.start(QString("python"),QStringList() << "--version");
       pythonCheck.waitForFinished(-1);
 
-      if (pythonCheck.exitStatus() == QProcess::NormalExit) {
+      if (pythonCheck.exitStatus() == QProcess::NormalExit) {	
         pythonCheck.setReadChannel(QProcess::StandardError);
         QString result = pythonCheck.readAll();
         QRegExp versionRegexp(".*([0-9]*\\.[0-9]*)\\..*");
@@ -31,7 +62,16 @@ QString PythonVersionChecker::installedVersion() {
   } else {
       return compiledVersion();
   }
-
+#else
+	if (isPythonVersionInstalled(compiledVersion())) {
+		return compiledVersion();
+	}
+	QString installedPythonVersion = getInstalledPythonVersionIfAny();
+	if (!installedPythonVersion.isEmpty()) {
+		return installedPythonVersion;
+	}
+#endif  
+  
   return QString::null;
 }
 
