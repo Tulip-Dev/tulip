7d4ef3437bc633810343ca4252d86fd0e10e8b1b
Patrick Mary <mary@labri.fr>
2012-11-22 11:10:18 +0000

    bug fix: the Perfect aspect ratio plugin must have a layout input parameter to initialize its result
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8832 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/layout/PerfectLayout.cpp
+++ plugins/layout/PerfectLayout.cpp
@@ -6,12 +6,28 @@ public:
   PLUGININFORMATIONS("Perfect aspect ratio","Tulip team","09/19/2010","","1.0","")
 
   PerfectLayout(const tlp::PluginContext* context): LayoutAlgorithm(context) {
+    addInParameter<tlp::LayoutProperty>("layout",
+					HTML_HELP_OPEN()	\
+					HTML_HELP_DEF("type","LayoutProperty") \
+					HTML_HELP_DEF("default","viewLayout") \
+					HTML_HELP_BODY()		\
+					"The layout property from which a perfect aspect ratio has to be computed." \
+					HTML_HELP_CLOSE(),
+					"viewLayout", false);
   }
 
   virtual ~PerfectLayout() {
   }
 
   bool run() {
+    tlp::LayoutProperty *layout = NULL;
+    if ( dataSet!=NULL ) {
+      dataSet->get("layout", layout);
+    }
+    if (!layout)
+      layout = graph->getProperty<tlp::LayoutProperty>("viewLayout");
+
+    result->copy(layout);
     result->perfectAspectRatio();
     return true;
   }
