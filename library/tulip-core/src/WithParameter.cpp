ef585f945938e0a3967c972c03a8e61589345274
Patrick Mary <mary@labri.fr>
2012-11-29 10:32:22 +0000

    add WithParameter::inputRequired method then refine display of plugin parameters
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8847 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/WithParameter.cpp
+++ library/tulip-core/src/WithParameter.cpp
@@ -161,3 +161,29 @@ void ParameterDescriptionList::buildDefaultDataSet(DataSet &dataSet, Graph *g) c
   }
 }
 
+bool WithParameter::inputRequired() const {
+  ParameterDescription param;
+  forEach(param, parameters.getParameters()) {
+    if (param.getDirection() != OUT_PARAM)
+      return true;
+    const string& type = param.getTypeName();
+    if (type.compare(typeid(tlp::BooleanProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::ColorProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::DoubleProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::IntegerProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::LayoutProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::SizeProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::StringProperty).name()) == 0)
+      return true;
+    if (type.compare(typeid(tlp::PropertyInterface*).name()) == 0)
+      return true;
+  }
+  return false;
+}
+
