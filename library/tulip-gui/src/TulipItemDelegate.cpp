9211d6db697b42982a453c0b39383f73841f93bc
Patrick Mary <mary@labri.fr>
2012-12-07 15:01:58 +0000

    ensure value editor widget has the focus when displaying the default 'set all nodes/edges values' dialog
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8877 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/TulipItemDelegate.cpp
+++ library/tulip-gui/src/TulipItemDelegate.cpp
@@ -233,6 +233,7 @@ QVariant TulipItemDelegate::showEditorDialog(tlp::ElementType elType,tlp::Proper
     layout->addWidget(new QLabel(pi->getName().c_str()));
     layout->addWidget(w);
     QDialogButtonBox* buttonBox = new QDialogButtonBox(QDialogButtonBox::Cancel|QDialogButtonBox::Ok,Qt::Horizontal);
+    QWidget::setTabOrder(w, buttonBox);
     layout->addWidget(buttonBox);
     QObject::connect(buttonBox,SIGNAL(accepted()),dlg,SLOT(accept()));
     QObject::connect(buttonBox,SIGNAL(rejected()),dlg,SLOT(reject()));
