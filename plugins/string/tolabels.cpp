226e6d50eeca3b9d3b9c7283f5aa897cf41ed70b
Patrick Mary <mary@labri.fr>
2013-01-11 15:09:36 +0000

    add "selection" parameter to allow to set labels for selected elements only
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@8958 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/string/tolabels.cpp
+++ plugins/string/tolabels.cpp
@@ -29,6 +29,12 @@ const char * paramHelp[] = {
   HTML_HELP_BODY() \
   "Property to copy on labels" \
   HTML_HELP_CLOSE(),
+  HTML_HELP_OPEN() \
+  HTML_HELP_DEF( "type", "BooleanProperty" ) \
+  HTML_HELP_DEF( "default", "none" ) \
+  HTML_HELP_BODY() \
+  "Set of elements for which to set the labels." \
+  HTML_HELP_CLOSE(),
   // on nodes
   HTML_HELP_OPEN() \
   HTML_HELP_DEF( "type", "bool" ) \
@@ -48,18 +54,21 @@ class ToLabels: public tlp::StringAlgorithm {
 public:
   PLUGININFORMATIONS("To labels","Ludwig Fiolka","2012/03/16","Map a property on nodes/edges labels","1.0","")
   ToLabels(const tlp::PluginContext* context): StringAlgorithm(context) {
-    addInParameter<PropertyInterface*>("input",paramHelp[0],"viewMetric");
-    addInParameter<bool>("nodes",paramHelp[1],"true");
-    addInParameter<bool>("edges",paramHelp[2],"true");
+    addInParameter<PropertyInterface*>("input",paramHelp[0],"viewMetric",true);
+    addInParameter<BooleanProperty>("selection",paramHelp[1],"");
+    addInParameter<bool>("nodes",paramHelp[2],"true");
+    addInParameter<bool>("edges",paramHelp[3],"true");
   }
 
   bool run() {
     PropertyInterface* input = NULL;
+    BooleanProperty* selection = NULL;
     bool onNodes = true;
     bool onEdges = true;
 
     if (dataSet != NULL) {
       dataSet->get("input",input);
+      dataSet->get("selection",selection);
       dataSet->get("nodes",onNodes);
       dataSet->get("edges",onEdges);
     }
@@ -68,7 +77,7 @@ public:
       pluginProgress->setComment("Copying nodes values");
       int step=0,max_step = graph->numberOfNodes();
       node n;
-      forEach(n,graph->getNodes()) {
+      forEach(n, selection ? selection->getNonDefaultValuatedNodes() : graph->getNodes()) {
         pluginProgress->progress(step++,max_step);
         result->setNodeValue(n,input->getNodeStringValue(n));
       }
@@ -78,7 +87,7 @@ public:
       pluginProgress->setComment("Copying edges values");
       int step=0,max_step = graph->numberOfEdges();
       edge e;
-      forEach(e,graph->getEdges()) {
+      forEach(e, selection ? selection->getNonDefaultValuatedEdges() : graph->getEdges()) {
         pluginProgress->progress(step++,max_step);
         result->setEdgeValue(e,input->getEdgeStringValue(e));
       }
