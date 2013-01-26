2fc42c6a71babbcf73ac9a1468ed61d838dfd02e
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2013-01-26 16:09:03 +0000

    Improve Mac OS bundle creation script : reorganize bundle contents, reduce the bundle size by 2 (don't include Qt headers, only dylibs), when building the bundle on Snow Leopard and lower with Python 2.7 integrate Python 2.7 framework in the bundle
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9019 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- bundlers/mac/mac_bundle.sh
+++ bundlers/mac/mac_bundle.sh
@@ -1,16 +1,32 @@
 #!/bin/sh
 
+function usage {
+	echo "Usage : $0 <Tulip Install Dir> <Bundle Destination Dir>"
+}
+
+if [ $# -ne 2 ]
+then
+	usage $0
+	exit 1
+fi
+
 TULIP_DIR=$1
 DEST_DIR=$2
 SRC_DIR=$(pwd)
 
+if [ ! -d $TULIP_DIR ]
+then
+	echo "Error : the specified Tulip installation directory does not exist"
+	exit 1
+fi
+
 echo 'Wiping previous bundles into '$DEST_DIR
-rm -fr $DEST_DIR
+rm -rf $DEST_DIR 2>/dev/null
 mkdir -p $DEST_DIR/application
 cd $DEST_DIR/application
 mkdir .background
 cp $SRC_DIR/background.png .background/
-rm -fr *
+rm -rf * 2>/dev/null
 
 echo 'Creating base directories'
 cd $DEST_DIR/application
@@ -20,28 +36,42 @@ cp "$SRC_DIR/Info.plist" .
 cp "$SRC_DIR/tulip.icns" "Resources/"
 
 echo 'Copying binaries'
-cd $DEST_DIR/application/Tulip.app/Contents/MacOS
-mkdir tulip
-cd tulip
+cd $DEST_DIR/application/Tulip.app/Contents/
 cp -r $TULIP_DIR/* .
-mv macos/* ..
-chmod +x ../tulip_launcher
-rm -fr macos
-rm -fr include/
+mv lib/*.dylib Frameworks/
 
+
+chmod +x MacOS/tulip_launcher
+	
 echo 'Copying license'
 cd $DEST_DIR/application
 cp $SRC_DIR/../../COPYING .
 
 echo 'Copying Frameworks'
 cd $DEST_DIR/application/Tulip.app/Contents/Frameworks/
-for cmp in QtOpenGL QtWebKit QtXmlPatterns QtGui QtTest QtXml QtNetwork QtCore phonon QtDBus; do
-  echo "Copying /Library/Frameworks/$cmp".framework to $(pwd)
-  cp -r "/Library/Frameworks/$cmp".framework .
+for cmp in QtOpenGL QtWebKit QtXmlPatterns QtGui QtTest QtXml QtNetwork QtCore phonon QtScript QtHelp QtDBus; do
+  echo "Copying /Library/Frameworks/${cmp}.framework/$cmp" to $(pwd)
+  cp "/Library/Frameworks/${cmp}.framework/$cmp" .
 done
+cp -r /Library/Frameworks/QtGui.framework/Resources/qt_menu.nib ../Resources
+cp -r /Developer/Applications/Qt/plugins/imageformats ../
+cp /usr/lib/libQtCLucene.4.dylib .
+
+# Case when we build the bundle on Snow Leopard or lower version of MacOS with Python 2.7
+# Python 2.7 has to be manually installed for building Tulip
+# We include the Python Framework in the bundle in order for Tulip to run
+# when Python 2.7 is not installed on the user sytem (Snow Leopard and Leopard)
+if [ ! -e /usr/lib/libpython2.7.dylib -a -d /Library/Frameworks/Python.framework/Versions/2.7 ]
+then
+	echo "Copying /Library/Frameworks/Python".framework to $(pwd)
+	# Copy the whole Python framework
+    cp "/Library/Frameworks/Python.framework/Versions/2.7/lib/libpython2.7.dylib" .
+    cp "/Library/Frameworks/Python.framework/Versions/2.7/Python" .
+    cp -r "/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7" ../lib/ 
+    find ../lib/python2.7 \( -iname \*\.pyc -o -iname \*\.pyo \) -print0 | xargs -0 rm -f     
+fi
 
 echo 'Copying libraries'
-cd $DEST_DIR/application/Tulip.app/Contents/MacOS/tulip/lib
 for lib in libGLEW libfreetype libz libstdc++; do
   for search_path in /usr/lib /opt/local/lib /usr/X11/lib; do
     [ "$(ls $search_path/$lib*.dylib 2> /dev/null)" != "" ] && cp $search_path/$lib*.dylib .
