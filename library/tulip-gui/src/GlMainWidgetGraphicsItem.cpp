0a71c53340e58889960ce661ea20d97295e0efe2
Patrick Mary <mary@labri.fr>
2013-02-06 09:21:15 +0000

    fix for sf bug #3603411: revert commit #9000 which introduces this bug, but the overview refresh bug remains
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9078 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/GlMainWidgetGraphicsItem.cpp
+++ library/tulip-gui/src/GlMainWidgetGraphicsItem.cpp
@@ -84,7 +84,7 @@ void GlMainWidgetGraphicsItem::glMainWidgetRedraw(GlMainWidget *) {
 void GlMainWidgetGraphicsItem::paint(QPainter *painter, const QStyleOptionGraphicsItem *, QWidget *) {
 
   if(_redrawNeeded) {
-    emit widgetPainted(true);
+    emit widgetPainted(_graphChanged);
   }
 
   painter->beginNativePainting();
