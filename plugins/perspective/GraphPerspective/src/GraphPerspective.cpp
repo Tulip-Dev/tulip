5528fa9624865fb450a270c6426826cee206873a
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2013-02-13 18:49:39 +0000

    - that call is no longer needed
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9122 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
+++ plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -278,7 +278,6 @@ void GraphPerspective::start(tlp::PluginProgress *progress) {
 
   tlp::PluginLister::instance()->addListener(this);
   PythonInterpreter::getInstance()->setDefaultConsoleWidget(_ui->pythonPanel->consoleWidget());
-  PythonInterpreter::getInstance()->importModule("tulip");
 
   // Setting initial sizes for splitters
   _ui->mainSplitter->setSizes(QList<int>() << 200 << 1000);
