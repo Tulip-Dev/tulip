25e2527a4f5afd995b1861b991fb362f667aa819
Patrick Mary <mary@labri.fr>
2013-02-15 14:43:44 +0000

    move some layout plugins from testing repository + add correct algorithm dependencies on Grip + fix size bug on Fast Overlap removal + remove unneeded files csolve_VPSC.h and csolve_VPSC.cpp
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9132 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ plugins/layout/FastOverlapRemoval/FastOverlapRemoval.h
@@ -0,0 +1,54 @@
+//-*-c++-*-
+#ifndef _RESOLVE_OVERLAP_H
+#define _RESOLVE_OVERLAP_H
+
+/** \addtogroup layout */
+/*@{*/
+/** \file
+ *  \brief A layout algorithm to resolve all node overlaps.
+ *
+ * An implementation of a layout algorithm resolving the problem
+ * of node overlaps. It was first published as:
+ *
+ * Tim Dwyer, Kim Marriot, Peter J. Stuckey,
+ * "Fast Node Overlap Removal" in Graph Drawing, Vol. 3843 (2006), pp. 153-164.
+ *
+ * <b>NOTES</b>
+ *
+ * The algorithm works on general graphs. It finds and applies
+ * the minimum scale factor to ensure that no two pairs of nodes
+ * overlap in the graph.
+ * Let n be the number of nodes, the algorithm complexity is in O(n).
+ * If two nodes start out at the same postion the algorithm cannot
+ * succeed.  It will return failure.
+ *
+ * <b>HISTORY</b>
+ *
+ *  - 2006 Version 1.0: first version by Daniel Archambault, 
+ *  Department of Computer Science, University of British Columbia
+ *
+ * <b>LICENCE</b>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by  
+ *  the Free Software Foundation; either version 2 of the License, or     
+ *  (at your option) any later version.
+ *
+ */
+
+#include <tulip/TulipPluginHeaders.h>
+#include <tulip/MutableContainer.h>
+
+class FastOverlapRemoval:public tlp::LayoutAlgorithm { 
+public:
+
+  PLUGININFORMATIONS("Fast Overlap Removal" ,"Daniel Archambault", "08/12/2006", "Remove all nodes overlap.", "1.3", "Misc")
+
+  FastOverlapRemoval(const tlp::PluginContext *context);
+  inline ~FastOverlapRemoval() {}
+  inline bool check (std::string &errMsg) { errMsg = ""; return true; }
+  inline void reset () {}
+  bool run();
+};
+/*@}*/
+#endif
