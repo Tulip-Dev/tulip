56022e8f260934c29c6f4178f35f22c794e3806d
Bruno Pinaud <bruno.pinaud@labri.fr>
2013-02-27 16:23:25 +0000

    Change scatterplot2d priority to have the navigation interactor by default. It is mandatory to be able to enter a matrix cell also fix some memory leaks
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9219 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/ScatterPlot2DView/ScatterPlot2DInteractors.cpp
+++ plugins/view/ScatterPlot2DView/ScatterPlot2DInteractors.cpp
@@ -24,61 +24,51 @@
 #include "ScatterPlotTrendLine.h"
 #include "ScatterPlotCorrelCoeffSelector.h"
 #include "ScatterPlot2DView.h"
+#include "ScatterPlotCorrelCoeffSelectorOptionsWidget.h"
 
 using namespace std;
 
 namespace tlp {
 
-ScatterPlot2DInteractor::ScatterPlot2DInteractor(const QString &iconPath, const QString &text) : GLInteractorComposite(QIcon(iconPath), text) {}
+ScatterPlot2DInteractor::ScatterPlot2DInteractor(const QString &iconPath, const QString &text) : NodeLinkDiagramComponentInteractor(iconPath, text) {}
 
 bool ScatterPlot2DInteractor::isCompatible(const std::string &viewName) const {
     return (viewName == ScatterPlot2DView::viewName);
 }
 
-INTERACTORPLUGINVIEWEXTENSION(ScatterPlot2DInteractorZoom,"ScatterPlot2DZoomInteractor","InteractorRectangleZoom",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D rectangle zoom interactor","1.0")
-INTERACTORPLUGINVIEWEXTENSION(ScatterPlot2DInteractorGetInformation,"ScatterPlot2DInteractorGetInformation","InteractorGetInformation",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D get information interactor","1.0")
-INTERACTORPLUGINVIEWEXTENSION(ScatterPlot2DInteractorSelection,"ScatterPlot2DSelectionInteractor","InteractorSelection",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D selection interactor","1.0")
+INTERACTORPLUGINVIEWEXTENSIONWITHPRIORITY(ScatterPlot2DInteractorZoom,"ScatterPlot2DZoomInteractor","InteractorRectangleZoom",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D rectangle zoom interactor","1.0",1)
+INTERACTORPLUGINVIEWEXTENSIONWITHPRIORITY(ScatterPlot2DInteractorGetInformation,"ScatterPlot2DInteractorGetInformation","InteractorGetInformation",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D get information interactor","1.0",2)
+INTERACTORPLUGINVIEWEXTENSIONWITHPRIORITY(ScatterPlot2DInteractorSelection,"ScatterPlot2DSelectionInteractor","InteractorSelection",ScatterPlot2DView::viewName, "Tulip Team" ,"02/04/09","ScatterPlot2D selection interactor","1.0",3)
+INTERACTORPLUGINVIEWEXTENSIONWITHPRIORITY(ScatterLassoSelectorInteractor,"ScatterLassoSelectorInteractor","MouseLassoNodesSelectorInteractor",ScatterPlot2DView::viewName, "Antoine Lambert" ,"02/04/09","Scatter Plot lasso selector","1.0",4)
 
 PLUGIN(ScatterPlot2DInteractorNavigation)
 PLUGIN(ScatterPlot2DInteractorTrendLine)
 PLUGIN(ScatterPlot2DInteractorCorrelCoeffSelector)
 
-QString interactorScatterPlotNavigationHelpText =
-	"<html>"
-	"<head>"
-	"<title></title>"
-	"</head>"
-	"<body>"
-	"<h3>View navigation interactor</h3>"
-	"<p>This interactor allows to navigate in the scatter plot view.</p>"
-	"<p>When there is more than one graph properties selected, the corresponding scatter plots previews are generated and displayed in a matrix form. By <b>double clicking on a scatter plot, "
-	"this one is displayed in fullscreen </b> in a more detailed way and the others interactors become available. To go back to the scatter plots matrix, double click anywhere in the view.</p>"
-	"<p>Otherwise, this interactor offers the same functionnalities as the one in the \"Node Link Diagram view\". The commands are described below :</p>"
-	"<b>Ctrl + Mouse up/down</b> : zoom<br>"
-	"<b>Ctrl + Mouse left/right</b> : z rotation<br>"
-	"<b>Shift + Mouse</b> : rotation<br>"
-	"<b>Key up/down</b> : up/down<br>"
-	"<b>Key left/right</b> : left/right<br>"
-	"<b>Key page up/down</b> : zoom<br>"
-	"<b>Key insert</b> : rotate<br>"
-	"</body>"
-	"</html>";
-
 ScatterPlot2DInteractorNavigation::ScatterPlot2DInteractorNavigation(const tlp::PluginContext *) : ScatterPlot2DInteractor(":/tulip/gui/icons/i_navigation.png", "Navigate in view") {
-  //setPriority(5);
+    setConfigurationWidgetText(QString("<html><head>")
+                                       +"<title></title>"
+                                       +"</head>"
+                                       +"<body>"
+                                       +"<h3>View navigation interactor</h3>"
+                                       +"<p>This interactor allows to navigate in the scatter plot view.</p>"
+                                       +"<p>When there is more than one graph properties selected, the corresponding scatter plots previews are generated and displayed in a matrix form. By <b>double clicking on a scatter plot, "
+                                       +"this one is displayed in fullscreen </b> in a more detailed way and the others interactors become available. To go back to the scatter plots matrix, double click anywhere in the view.</p>"
+                                       +"<p>Otherwise, this interactor offers the same functionnalities as the one in the \"Node Link Diagram view\". The commands are described below :</p>"
+                                       +"<b>Ctrl + Mouse up/down</b> : zoom<br>"
+                                       +"<b>Ctrl + Mouse left/right</b> : z rotation<br>"
+                                       +"<b>Shift + Mouse</b> : rotation<br>"
+                                       +"<b>Key up/down</b> : up/down<br>"
+                                       +"<b>Key left/right</b> : left/right<br>"
+                                       +"<b>Key page up/down</b> : zoom<br>"
+                                       +"<b>Key insert</b> : rotate<br>"
+                                       +"</body>"
+                                       +"</html>");
 }
 
 void ScatterPlot2DInteractorNavigation::construct() {
   push_back(new ScatterPlot2DViewNavigator);
   push_back(new MouseNKeysNavigator);
-}
-
-QWidget *ScatterPlot2DInteractorNavigation::configurationWidget() const {
-  QLabel *_label = new QLabel(interactorScatterPlotNavigationHelpText);
-  _label->setWordWrap(true);
-  _label->setAlignment(Qt::AlignTop);
-  _label->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
-  return _label;
 }
 
 ScatterPlot2DInteractorTrendLine::ScatterPlot2DInteractorTrendLine(const PluginContext *) : ScatterPlot2DInteractor(":/i_scatter_trendline.png", "Trend line") {}
@@ -88,12 +78,16 @@ void ScatterPlot2DInteractorTrendLine::construct() {
   push_back(new MousePanNZoomNavigator);
 }
 
-ScatterPlot2DInteractorCorrelCoeffSelector::ScatterPlot2DInteractorCorrelCoeffSelector(const tlp::PluginContext *) : ScatterPlot2DInteractor(":/tulip/gui/icons/i_magic.png", "Correlation Coefficient Selector") {}
+ScatterPlot2DInteractorCorrelCoeffSelector::ScatterPlot2DInteractorCorrelCoeffSelector(const tlp::PluginContext *) : ScatterPlot2DInteractor(":/tulip/gui/icons/i_magic.png", "Correlation Coefficient Selector"),optionsWidget(NULL) {}
 
 void ScatterPlot2DInteractorCorrelCoeffSelector::construct() {
 	optionsWidget = new ScatterPlotCorrelCoeffSelectorOptionsWidget();
   push_back(new ScatterPlotCorrelCoeffSelector(optionsWidget));
   push_back(new MousePanNZoomNavigator);
+}
+
+ScatterPlot2DInteractorCorrelCoeffSelector::~ScatterPlot2DInteractorCorrelCoeffSelector() {
+    delete optionsWidget;
 }
 
 QWidget *ScatterPlot2DInteractorCorrelCoeffSelector::configurationWidget() const {
