e92d45c8c8a496d1d6bb1e2f98855408be23baf1
tulipsoftware <tulipsoftware@mycompany.com>
2013-03-11 22:03:08 +0000

    [Jenkins] Astyle nightly commit
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9263 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/ConvexHull.cpp
+++ library/tulip-core/src/ConvexHull.cpp
@@ -26,57 +26,64 @@ using namespace tlp;
 
 //================================================================================
 static double distanceToSeg(const tlp::Coord& p,
-                     const std::pair<tlp::Coord, tlp::Coord>& seg) {
+                            const std::pair<tlp::Coord, tlp::Coord>& seg) {
   double segx = seg.second.getX()-seg.first.getX();
   double segy = seg.second.getY()-seg.first.getY();
   return segx*(seg.first.getY()-p.getY())-segy*(seg.first.getX()-p.getX());
 }
 //================================================================================
 static bool findMostDistantPointFromBaseLine(const vector<Coord>& points,
-                                      const std::pair<unsigned int, unsigned int>& baseLine,
-                                      const std::list<unsigned int> &subset,
-                                      unsigned int &mostDistantPointIdx,
-                                      list<unsigned int>& newSubset) {
-    bool pointsOutside = false;
-    double maxD = 0;
-    for (std::list<unsigned int>::const_iterator it = subset.begin() ; it != subset.end() ; ++it) {
-        double d = distanceToSeg(points[*it], make_pair(points[baseLine.first], points[baseLine.second]));
-        if (d > 0) {
-            newSubset.push_back(*it);
-        } else {
-            continue;
-        }
-        if (d > maxD) {
-            maxD = d;
-            mostDistantPointIdx = *it;
-            pointsOutside = true;
-        }
+    const std::pair<unsigned int, unsigned int>& baseLine,
+    const std::list<unsigned int> &subset,
+    unsigned int &mostDistantPointIdx,
+    list<unsigned int>& newSubset) {
+  bool pointsOutside = false;
+  double maxD = 0;
+
+  for (std::list<unsigned int>::const_iterator it = subset.begin() ; it != subset.end() ; ++it) {
+    double d = distanceToSeg(points[*it], make_pair(points[baseLine.first], points[baseLine.second]));
+
+    if (d > 0) {
+      newSubset.push_back(*it);
     }
-    return pointsOutside;
+    else {
+      continue;
+    }
+
+    if (d > maxD) {
+      maxD = d;
+      mostDistantPointIdx = *it;
+      pointsOutside = true;
+    }
+  }
+
+  return pointsOutside;
 }
 //================================================================================
 static void buildConvexHull(const std::vector<Coord> &points,
-                     const std::pair<unsigned int, unsigned int>& baseLine,
-                     const std::list<unsigned int> &subset,
-                     std::vector<unsigned int> &hull) {
-    unsigned int mostDistantPointIdx;
-    std::list<unsigned int> newSubset;
-    // compute the most distant point from the base line if any and the subset of points on the left of the base line
-    bool pointsOutside = findMostDistantPointFromBaseLine(points, baseLine, subset, mostDistantPointIdx, newSubset);
-    // case when there is points on the left of the base line
-    // this function is then called recusively for two new base lines
-    if (pointsOutside) {
-        buildConvexHull(points, make_pair(baseLine.first, mostDistantPointIdx), newSubset, hull);
-        buildConvexHull(points, make_pair(mostDistantPointIdx, baseLine.second), newSubset, hull);
-    }
-    // no points on the left of the base line
-    // the base line is then part of the convex hull
-    else {
-        if (hull.empty())
-            hull.push_back(baseLine.first);
-        if (baseLine.second != hull.front())
-            hull.push_back(baseLine.second);
-    }
+                            const std::pair<unsigned int, unsigned int>& baseLine,
+                            const std::list<unsigned int> &subset,
+                            std::vector<unsigned int> &hull) {
+  unsigned int mostDistantPointIdx;
+  std::list<unsigned int> newSubset;
+  // compute the most distant point from the base line if any and the subset of points on the left of the base line
+  bool pointsOutside = findMostDistantPointFromBaseLine(points, baseLine, subset, mostDistantPointIdx, newSubset);
+
+  // case when there is points on the left of the base line
+  // this function is then called recusively for two new base lines
+  if (pointsOutside) {
+    buildConvexHull(points, make_pair(baseLine.first, mostDistantPointIdx), newSubset, hull);
+    buildConvexHull(points, make_pair(mostDistantPointIdx, baseLine.second), newSubset, hull);
+  }
+  // no points on the left of the base line
+  // the base line is then part of the convex hull
+  else {
+    if (hull.empty())
+      hull.push_back(baseLine.first);
+
+    if (baseLine.second != hull.front())
+      hull.push_back(baseLine.second);
+  }
 }
 //================================================================================
 void tlp::convexHull(const std::vector<Coord> &points,
@@ -88,6 +95,7 @@ void tlp::convexHull(const std::vector<Coord> &points,
   if(points.size()<=3) {
     for(unsigned int i=0; i<points.size(); ++i)
       hull.push_back(i);
+
     return;
   }
 
@@ -106,6 +114,7 @@ void tlp::convexHull(const std::vector<Coord> &points,
 
   // all points need to be considered for the first iterations
   std::list<unsigned int> idxs;
+
   for (unsigned int i = 0 ; i < points.size() ; ++i) {
     idxs.push_back(i);
   }
