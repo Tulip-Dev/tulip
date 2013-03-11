6018a6b078518adb04d8278d758c7951caf9b7d0
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2013-03-11 15:47:24 +0000

    - rework the Quick Hull implementation as the last committed one was not working (use a true recursive implementation)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9262 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/ConvexHull.cpp
+++ library/tulip-core/src/ConvexHull.cpp
@@ -19,108 +19,64 @@
 
 #include <tulip/ConvexHull.h>
 
-#include <stack>
 #include <list>
 
 using namespace std;
 using namespace tlp;
 
 //================================================================================
-bool isLeft(const tlp::Coord& p, const std::pair<tlp::Coord, tlp::Coord>& seg) {
-  double cp1 =
-    (seg.second.getX()-seg.first.getX())*(p.getY()-seg.first.getY())
-    - (seg.second.getY()-seg.first.getY())*(p.getX()-seg.first.getX());
-  return (cp1<=0);
+static double distanceToSeg(const tlp::Coord& p,
+                     const std::pair<tlp::Coord, tlp::Coord>& seg) {
+  double segx = seg.second.getX()-seg.first.getX();
+  double segy = seg.second.getY()-seg.first.getY();
+  return segx*(seg.first.getY()-p.getY())-segy*(seg.first.getX()-p.getX());
 }
 //================================================================================
-double distanceToSeg(const tlp::Coord& p,
-                     const std::pair<tlp::Coord, tlp::Coord>& seg,
-                     double segx, double segy) {
-  double dist = segx*(seg.first.getY()-p.getY())-segy*(seg.first.getX()-p.getX());
-
-  if (dist < 0.)
-    dist = -dist;
-
-  return dist;
-}
-
-// used to store call infos of the original
-// recursive algorithm
-struct hullLoopIteration {
-  unsigned int insertPos;
-  std::pair<Coord, Coord> seg;
-  list<unsigned int> subset;
-};
-//================================================================================
-void hullSet(stack<hullLoopIteration>& loopIterStack,
-             const vector<Coord>& points,
-             vector<unsigned int> &hull) {
-  while(loopIterStack.empty() != false) {
-    hullLoopIteration current(loopIterStack.top());
-    loopIterStack.pop();
-
-    //test end of loop
-    if (current.subset.empty())
-      continue;
-
-    //Get furthest point from seg
-    list<unsigned int>::iterator itS = current.subset.begin();
-    unsigned int iFurther = *itS;
-
-    if (current.subset.size() > 1) {
-      double segx = current.seg.second.getX()-current.seg.first.getX();
-      double segy = current.seg.second.getY()-current.seg.first.getY();
-      double pmaxDist=distanceToSeg(points[iFurther], current.seg, segx, segy);
-      list<unsigned int>::iterator itMax = itS;
-
-      for (++itS; itS != current.subset.end(); ++itS) {
-        double dist = distanceToSeg(points[*itS], current.seg, segx, segy);
-
-        if (dist > pmaxDist) {
-          pmaxDist = dist;
-          itMax = itS;
+static bool findMostDistantPointFromBaseLine(const vector<Coord>& points,
+                                      const std::pair<unsigned int, unsigned int>& baseLine,
+                                      const std::list<unsigned int> &subset,
+                                      unsigned int &mostDistantPointIdx,
+                                      list<unsigned int>& newSubset) {
+    bool pointsOutside = false;
+    double maxD = 0;
+    for (std::list<unsigned int>::const_iterator it = subset.begin() ; it != subset.end() ; ++it) {
+        double d = distanceToSeg(points[*it], make_pair(points[baseLine.first], points[baseLine.second]));
+        if (d > 0) {
+            newSubset.push_back(*it);
+        } else {
+            continue;
         }
-      }
-
-      iFurther = *itMax;
-      current.subset.erase(itMax);
+        if (d > maxD) {
+            maxD = d;
+            mostDistantPointIdx = *it;
+            pointsOutside = true;
+        }
     }
-    else current.subset.clear();
-
-    // insert new hull point
-    hull.insert(hull.begin() + current.insertPos, iFurther);
-
-    if(current.subset.empty())
-      continue;
-
-    // divide points
-    // so push 2 new loop iterations
-    // on the stack
-    hullLoopIteration leftPBloop;
-    leftPBloop.insertPos=current.insertPos;
-    pair<Coord,Coord>& segPB = leftPBloop.seg;
-    segPB.first = points[iFurther];
-    segPB.second = current.seg.second;
-    list<unsigned int>& leftPB = leftPBloop.subset;
-
-    hullLoopIteration leftABloop;
-    leftABloop.insertPos=current.insertPos;
-    pair<Coord,Coord>& segAP = leftABloop.seg;
-    segAP.first = points[iFurther];
-    segAP.second = current.seg.second;
-    list<unsigned int>& leftAP = leftABloop.subset;
-
-
-    for(itS=current.subset.begin(); itS!=current.subset.end(); ++itS) {
-      if(isLeft(points[*itS],segPB))
-        leftPB.push_front(*itS);
-      else if(isLeft(points[*itS],segAP))
-        leftAP.push_front(*itS);
+    return pointsOutside;
+}
+//================================================================================
+static void buildConvexHull(const std::vector<Coord> &points,
+                     const std::pair<unsigned int, unsigned int>& baseLine,
+                     const std::list<unsigned int> &subset,
+                     std::vector<unsigned int> &hull) {
+    unsigned int mostDistantPointIdx;
+    std::list<unsigned int> newSubset;
+    // compute the most distant point from the base line if any and the subset of points on the left of the base line
+    bool pointsOutside = findMostDistantPointFromBaseLine(points, baseLine, subset, mostDistantPointIdx, newSubset);
+    // case when there is points on the left of the base line
+    // this function is then called recusively for two new base lines
+    if (pointsOutside) {
+        buildConvexHull(points, make_pair(baseLine.first, mostDistantPointIdx), newSubset, hull);
+        buildConvexHull(points, make_pair(mostDistantPointIdx, baseLine.second), newSubset, hull);
     }
-
-    loopIterStack.push(leftPBloop);
-    loopIterStack.push(leftABloop);
-  }
+    // no points on the left of the base line
+    // the base line is then part of the convex hull
+    else {
+        if (hull.empty())
+            hull.push_back(baseLine.first);
+        if (baseLine.second != hull.front())
+            hull.push_back(baseLine.second);
+    }
 }
 //================================================================================
 void tlp::convexHull(const std::vector<Coord> &points,
@@ -132,11 +88,10 @@ void tlp::convexHull(const std::vector<Coord> &points,
   if(points.size()<=3) {
     for(unsigned int i=0; i<points.size(); ++i)
       hull.push_back(i);
-
     return;
   }
 
-  //get first segment
+  //get first base line
   Coord pMinX = points[0];
   Coord pMaxX = pMinX;
   unsigned int iMinX = 0;
@@ -149,34 +104,13 @@ void tlp::convexHull(const std::vector<Coord> &points,
       pMaxX=points[iMaxX = i];
   }
 
-  hull.push_back(iMinX);
-  hull.push_back(iMaxX);
-  pair<Coord,Coord> seg = make_pair(pMinX,pMaxX);
-  // divide points
-  // so push 2 loop iterations
-  // on the stack
-  stack<hullLoopIteration> loopIterStack;
-  loopIterStack.push(hullLoopIteration());
-  loopIterStack.top().insertPos = 0;
-  loopIterStack.top().seg.first = pMaxX;
-  loopIterStack.top().seg.second = pMinX;
-  list<unsigned int>& rightPoints = loopIterStack.top().subset;
-  loopIterStack.push(hullLoopIteration());
-  loopIterStack.top().insertPos = 1;
-  loopIterStack.top().seg.first = pMinX;
-  loopIterStack.top().seg.second = pMaxX;
-  list<unsigned int>& leftPoints = loopIterStack.top().subset;
-
-  for(unsigned int i=0; i<points.size(); ++i) {
-    if((i!=iMinX) && (i!=iMaxX)) {
-      if(isLeft(points[i], seg))
-        leftPoints.push_front(i);
-      else
-        rightPoints.push_front(i);
-    }
+  // all points need to be considered for the first iterations
+  std::list<unsigned int> idxs;
+  for (unsigned int i = 0 ; i < points.size() ; ++i) {
+    idxs.push_back(i);
   }
 
-  //loop calls
-  hullSet(loopIterStack, points, hull);
+  buildConvexHull(points, make_pair(iMinX, iMaxX), idxs, hull);
+  buildConvexHull(points, make_pair(iMaxX, iMinX), idxs, hull);
 }
 //================================================================================
