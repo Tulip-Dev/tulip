b314a9df82386cc3eb86880d2063fbb44db6187f
Patrick Mary <mary@labri.fr>
2013-03-29 13:20:28 +0000

    removed unneeded librairies from delivered lib directory
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9291 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- bundlers/win32/win32_bundle.bat
+++ bundlers/win32/win32_bundle.bat
@@ -11,6 +11,13 @@ mkdir "%DEST_DIR%\\files\\include"
 
 echo 'Copying tulip files'
 xcopy "%TULIP_DIR%" "%DEST_DIR%\\files" /E /Q
+echo 'Deleting tulip unneeded libraries'
+del "%DEST_DIR%\\files\\lib\*.dll.a" /Q
+del "%DEST_DIR%\\files\\lib\tulip\*.dll.a" /Q
+del "%DEST_DIR%\\files\\lib\tulip\glyph\*.dll.a" /Q
+del "%DEST_DIR%\\files\\lib\tulip\interactor\*.dll.a" /Q
+del "%DEST_DIR%\\files\\lib\tulip\perspective\*.dll.a" /Q
+del "%DEST_DIR%\\files\\lib\tulip\view\*.dll.a" /Q
 
 echo 'Copying NSIS script'
 copy "%SRC_DIR%\\Tulip.nsi" "%DEST_DIR%\\"
