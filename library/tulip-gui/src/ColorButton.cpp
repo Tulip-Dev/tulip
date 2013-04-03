2ffcf8cbb83dc61f940ebac49509e4fa8334ca15
Patrick Mary <mary@labri.fr>
2013-04-03 16:01:19 +0000

    first step of QuickAccessBar redesign
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9294 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/ColorButton.cpp
+++ library/tulip-gui/src/ColorButton.cpp
@@ -26,21 +26,39 @@
 
 using namespace tlp;
 
-ColorButton::ColorButton(QWidget *parent): QPushButton(parent), _color(Qt::black), _dialogParent(parent) {
+ChooseColorButton::ChooseColorButton(QWidget *parent): QPushButton(parent), _color(Qt::black), _dialogParent(parent) {
   connect(this,SIGNAL(clicked()),this,SLOT(chooseColor()));
   setFocusPolicy(Qt::WheelFocus);
 }
 
-QColor ColorButton::color() const {
+QColor ChooseColorButton::color() const {
   return _color;
 }
 
-void ColorButton::setDialogParent(QWidget *w) {
+void ChooseColorButton::setDialogParent(QWidget *w) {
   _dialogParent = w;
 }
 
-tlp::Color ColorButton::tulipColor() const {
+tlp::Color ChooseColorButton::tulipColor() const {
   return QColorToColor(_color);
+}
+
+void ChooseColorButton::setColor(const QColor& c) {
+  _color = c;
+  repaint();
+  emit colorChanged(_color);
+  emit tulipColorChanged(QColorToColor(_color));
+}
+
+void ChooseColorButton::setTulipColor(const tlp::Color& c) {
+  setColor(colorToQColor(c));
+}
+
+void ChooseColorButton::chooseColor() {
+  QColor c = QColorDialog::getColor(_color,_dialogParent,trUtf8("Choose a color"),QColorDialog::ShowAlphaChannel);
+
+  if (c.isValid())
+    setColor(c);
 }
 
 void ColorButton::paintEvent(QPaintEvent *event) {
@@ -54,20 +72,3 @@ void ColorButton::paintEvent(QPaintEvent *event) {
   p.drawRect(tickW, tickH, tickW * 2, tickH * 2);
 }
 
-void ColorButton::setColor(const QColor& c) {
-  _color = c;
-  repaint();
-  emit colorChanged(_color);
-  emit tulipColorChanged(QColorToColor(_color));
-}
-
-void ColorButton::setTulipColor(const tlp::Color& c) {
-  setColor(colorToQColor(c));
-}
-
-void ColorButton::chooseColor() {
-  QColor c = QColorDialog::getColor(_color,_dialogParent,trUtf8("Choose a color"),QColorDialog::ShowAlphaChannel);
-
-  if (c.isValid())
-    setColor(c);
-}
