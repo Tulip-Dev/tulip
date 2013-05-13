c99593a0365f79a640a9a654436e3a3f8e71f92c
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2013-05-13 12:24:12 +0000

    - add QtXmlPatterns to the list of required Qt Modules (for plugins in testing)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9446 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- FindQtX.cmake
+++ FindQtX.cmake
@@ -6,6 +6,7 @@ IF(USE_QT5_IF_INSTALLED)
     FIND_PACKAGE(Qt5Widgets)
     FIND_PACKAGE(Qt5OpenGL)
     FIND_PACKAGE(Qt5Xml)
+    FIND_PACKAGE(Qt5XmlPatterns)
     FIND_PACKAGE(Qt5Network)
     FIND_PACKAGE(Qt5WebKit)
     FIND_PACKAGE(Qt5WebKitWidgets)
@@ -14,50 +15,52 @@ ENDIF(USE_QT5_IF_INSTALLED)
 
  # Qt5 and all the required modules are present, do global setup
 IF(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND
-   ${Qt5Xml_FOUND} AND ${Qt5Network_FOUND} AND
+   ${Qt5Xml_FOUND} AND ${Qt5XmlPatterns_FOUND} AND ${Qt5Network_FOUND} AND
    ${Qt5WebKit_FOUND} AND ${Qt5WebKitWidgets_FOUND})
 
     SET(USE_QT5 true)
-   
+
     IF(WIN32)
         GET_TARGET_PROPERTY(QtCore_location Qt5::Core LOCATION)
         GET_FILENAME_COMPONENT(QT_BINARY_DIR ${QtCore_location} PATH)
         SET(QT_BINARY_DIR ${QT_BINARY_DIR}/../bin)
     ENDIF(WIN32)
-   
+
     INCLUDE_DIRECTORIES(${Qt5Widgets_INCLUDE_DIRS})
     INCLUDE_DIRECTORIES(${Qt5OpenGL_INCLUDE_DIRS})
     INCLUDE_DIRECTORIES(${Qt5Xml_INCLUDE_DIRS})
+    INCLUDE_DIRECTORIES(${Qt5XmlPatterns_INCLUDE_DIRS})
     INCLUDE_DIRECTORIES(${Qt5Network_INCLUDE_DIRS})
     INCLUDE_DIRECTORIES(${Qt5WebKit_INCLUDE_DIRS})
     INCLUDE_DIRECTORIES(${Qt5WebKitWidgets_INCLUDE_DIRS})
-	
+
     ADD_DEFINITIONS(${Qt5Widgets_DEFINITIONS})
     ADD_DEFINITIONS(${Qt5OpenGL_DEFINITIONS})
     ADD_DEFINITIONS(${Qt5Xml_DEFINITIONS})
+    ADD_DEFINITIONS(${Qt5XmlPatterns_DEFINITIONS})
     ADD_DEFINITIONS(${Qt5Network_DEFINITIONS})
     ADD_DEFINITIONS(${Qt5WebKit_DEFINITIONS})
     ADD_DEFINITIONS(${Qt5WebKitWidgets_DEFINITIONS})
-	
-    SET(QT_LIBRARIES ${Qt5Widgets_LIBRARIES} ${Qt5OpenGL_LIBRARIES} ${Qt5Xml_LIBRARIES}
+
+    SET(QT_LIBRARIES ${Qt5Widgets_LIBRARIES} ${Qt5OpenGL_LIBRARIES} ${Qt5Xml_LIBRARIES} ${Qt5XmlPatterns_LIBRARIES}
                      ${Qt5Network_LIBRARIES} ${Qt5WebKit_LIBRARIES} ${Qt5WebKitWidgets_LIBRARIES})
 
      # define aliases for Qt macros in order to build the project
     MACRO(QTX_WRAP_CPP outfiles )
       QT5_WRAP_CPP(${outfiles} ${ARGN})
     ENDMACRO()
-	
+
     MACRO(QTX_WRAP_UI outfiles )
       QT5_WRAP_UI(${outfiles} ${ARGN})
     ENDMACRO()
-	
+
     MACRO(QTX_ADD_RESOURCES outfiles )
       QT5_ADD_RESOURCES(${outfiles} ${ARGN})
     ENDMACRO()
 
 # Use Qt4 otherwise
 ELSE(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND
-     ${Qt5Xml_FOUND} AND ${Qt5Network_FOUND} AND
+     ${Qt5Xml_FOUND} AND ${Qt5XmlPatterns_FOUND} AND ${Qt5Network_FOUND} AND
      ${Qt5WebKit_FOUND} AND ${Qt5WebKitWidgets_FOUND})
 
     IF(USE_QT5_IF_INSTALLED)
@@ -69,6 +72,7 @@ ELSE(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND
     SET(USE_QT4 true)
     SET(QT_USE_QTOPENGL true)
     SET(QT_USE_QTXML true)
+    SET(QT_USE_QTXMLPATTERNS true)
     SET(QT_USE_QTNETWORK true)
     SET(QT_USE_QTWEBKIT true)
     SET(QT_USE_QTTEST false)
@@ -89,5 +93,5 @@ ELSE(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND
     ENDMACRO()
 
 ENDIF(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND
-      ${Qt5Xml_FOUND} AND ${Qt5Network_FOUND} AND
+      ${Qt5Xml_FOUND} AND ${Qt5XmlPatterns_FOUND} AND ${Qt5Network_FOUND} AND
       ${Qt5WebKit_FOUND} AND ${Qt5WebKitWidgets_FOUND})
