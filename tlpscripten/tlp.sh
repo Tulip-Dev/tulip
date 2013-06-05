c7addd73f95d98cf3a34e2473fb1ed224a81e6fa
Patrick Mary <mary@labri.fr>
2013-06-05 16:33:41 +0000

    refine build of libtulip.o: only Dendrogram.cpp ImprovedWalker.cpp TreeLeaf.cpp cause an error
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9543 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- tlpscripten/tlp.sh
+++ tlpscripten/tlp.sh
@@ -2,9 +2,11 @@
 
 echo "compiling tulip"
 
-em++ -s ALLOW_MEMORY_GROWTH=1 -std=c++0x -DNDEBUG -I${TLP_SRC_DIR}/thirdparty -I${TLP_SRC_DIR}/thirdparty/gzstream -I${TLP_SRC_DIR}/thirdparty/yajl/src -I${TLP_SRC_DIR}/thirdparty/OGDF -I${TLP_SRC_DIR}/library/tulip-core/include/ -I${TLP_INSTALL_DIR}/include/ ${TLP_SRC_DIR}/library/tulip-core/src/*.cpp ${TLP_SRC_DIR}/library/tulip-ogdf/*.cpp ${TLP_SRC_DIR}/plugins/metric/*.cpp ${TLP_SRC_DIR}/plugins/selection/*.cpp ${TLP_SRC_DIR}/plugins/sizes/*.cpp ${TLP_SRC_DIR}/plugins/test/*.cpp ${TLP_SRC_DIR}/thirdparty/OGDF/src/*/*.cpp ${TLP_SRC_DIR}/plugins/layout/GEMLayout.cpp ${TLP_SRC_DIR}/plugins/layout/Circular.cpp ${TLP_SRC_DIR}/plugins/layout/Rand*.cpp ${TLP_SRC_DIR}/plugins/layout/DatasetTools.cpp ${TLP_SRC_DIR}/plugins/layout/*/*.cpp ${TLP_SRC_DIR}/plugins/layout/Orien*cpp ${TLP_SRC_DIR}/plugins/layout/Bubb*.cpp ${TLP_SRC_DIR}/plugins/layout/Num*cpp ${TLP_SRC_DIR}/plugins/clustering/Delaunay/*.cpp ${TLP_SRC_DIR}/plugins/clustering/EqualValueClustering.cpp ${TLP_SRC_DIR}/plugins/clustering/QuotientClustering.cpp ${TLP_SRC_DIR}/plugins/general/EdgeBundling/*.cpp -o libtulip.o
+LAYOUT_FILES=$(find ${TLP_SRC_DIR}/plugins/layout -name '*.cpp' -a ! -name Dendrogram.cpp -a ! -name ImprovedWalker.cpp -a ! -name TreeLeaf.cpp)
+CLUSTERING_FILES=$(find ${TLP_SRC_DIR}/plugins/clustering -name '*.cpp' -a ! -name 'Convolution*.cpp')
+
+em++ -s ALLOW_MEMORY_GROWTH=1 -std=c++0x -DNDEBUG -I${TLP_SRC_DIR}/thirdparty -I${TLP_SRC_DIR}/thirdparty/gzstream -I${TLP_SRC_DIR}/thirdparty/yajl/src -I${TLP_SRC_DIR}/thirdparty/OGDF -I${TLP_SRC_DIR}/library/tulip-core/include/ -I${TLP_INSTALL_DIR}/include/ ${TLP_SRC_DIR}/library/tulip-core/src/*.cpp ${TLP_SRC_DIR}/library/tulip-ogdf/*.cpp ${TLP_SRC_DIR}/plugins/metric/*.cpp ${TLP_SRC_DIR}/plugins/selection/*.cpp ${TLP_SRC_DIR}/plugins/sizes/*.cpp ${TLP_SRC_DIR}/plugins/test/*.cpp ${TLP_SRC_DIR}/thirdparty/OGDF/src/*/*.cpp ${LAYOUT_FILES} ${CLUSTERING_FILES} ${TLP_SRC_DIR}/plugins/general/EdgeBundling/*.cpp -o libtulip.o
 
 if [ $? -eq 0 ]; then
   echo "libtulip.o generated"
 fi
-
