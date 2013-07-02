1060ade75db19aea2f748764325e5b6155f13ca1
Patrick Mary <mary@labri.fr>
2013-07-02 12:20:00 +0000

    fix grep of cnee process
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9634 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/gui/launch_tulip.sh
+++ unit_test/gui/launch_tulip.sh
@@ -2,11 +2,18 @@ echo "Launching Tulip ..."
 if [ -f tulip.pid ]; then
   rm tulip.pid
 fi
-$1 $2 --perspective=Tulip --geometry=100,50,1600,900 -style plastique &
+
+TULIP=$1
+
+if [ "$1" = "" ]; then
+  TULIP=tulip_perspective
+fi
+
+$TULIP $2 --perspective=Tulip --geometry=100,50,1600,900 -style plastique &
 TULIP_PID=$!
 echo $TULIP_PID > tulip.pid
 wait $TULIP_PID
-CNEE_PID=`ps -ef | grep cnee | awk '{if (NR == 1) print $2}'`
+CNEE_PID=`ps -eo pid,comm | grep cnee | awk '{if (NR == 1) print $1}'`
 if [ "$CNEE_PID" != "" ]; then
   kill -9 $CNEE_PID
 fi
