3c5df939601d57ec2e79ffeb4b88865c4cd49860
Patrick Mary <mary@labri.fr>
2013-10-17 13:38:33 +0000

    fix list of ogdg files + add variables to group file in order to ease script tweaking
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9783 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- tlpscripten/tlp.sh
+++ tlpscripten/tlp.sh
@@ -2,10 +2,17 @@
 
 echo "compiling tulip"
 
-LAYOUT_FILES=$(find ${TLP_SRC_DIR}/plugins/layout -name '*.cpp' -a ! -name Dendrogram.cpp -a ! -name ImprovedWalker.cpp -a ! -name TreeLeaf.cpp)
 CLUSTERING_FILES=$(find ${TLP_SRC_DIR}/plugins/clustering -name '*.cpp' -a ! -name 'Convolution*.cpp')
+EDGE_BUNDLING_FILES=$(find ${TLP_SRC_DIR}/plugins/general/EdgeBundling -name '*.cpp')
+LAYOUT_FILES=$(find ${TLP_SRC_DIR}/plugins/layout -name '*.cpp' -a ! -name Dendrogram.cpp -a ! -name ImprovedWalker.cpp -a ! -name TreeLeaf.cpp)
+OGDF_FILES=$(find ${TLP_SRC_DIR} -path '*library/tulip-ogdf*' -a -name '*.cpp' -o -path '*thirdparty/OGDF/src*' -a -name '*.cpp')
+OGDF_INCLUDES=-I${TLP_SRC_DIR}/thirdparty/OGDF
+METRIC_FILES=$(find ${TLP_SRC_DIR}/plugins/metric -name '*.cpp')
+SELECTION_FILES=$(find ${TLP_SRC_DIR}/plugins/selection -name '*.cpp')
+SIZES_FILES=$(find ${TLP_SRC_DIR}/plugins/sizes -name '*.cpp')
+TEST_FILES=$(find ${TLP_SRC_DIR}/plugins/test -name '*.cpp')
 
-em++ -s ALLOW_MEMORY_GROWTH=1 -std=c++0x -DNDEBUG -I${TLP_SRC_DIR}/thirdparty -I${TLP_SRC_DIR}/thirdparty/gzstream -I${TLP_SRC_DIR}/thirdparty/yajl/src -I${TLP_SRC_DIR}/thirdparty/OGDF -I${TLP_SRC_DIR}/library/tulip-core/include/ -I${TLP_INSTALL_DIR}/include/ ${TLP_SRC_DIR}/library/tulip-core/src/*.cpp ${TLP_SRC_DIR}/library/tulip-ogdf/*.cpp ${TLP_SRC_DIR}/plugins/metric/*.cpp ${TLP_SRC_DIR}/plugins/selection/*.cpp ${TLP_SRC_DIR}/plugins/sizes/*.cpp ${TLP_SRC_DIR}/plugins/test/*.cpp ${TLP_SRC_DIR}/thirdparty/OGDF/src/*/*.cpp ${LAYOUT_FILES} ${CLUSTERING_FILES} ${TLP_SRC_DIR}/plugins/general/EdgeBundling/*.cpp -o libtulip.o
+em++ -s ALLOW_MEMORY_GROWTH=1 -std=c++0x -DNDEBUG -I${TLP_SRC_DIR}/thirdparty -I${TLP_SRC_DIR}/thirdparty/gzstream -I${TLP_SRC_DIR}/thirdparty/yajl/src ${OGDF_INCLUDES} -I${TLP_SRC_DIR}/library/tulip-core/include/ -I${TLP_INSTALL_DIR}/include/ ${TLP_SRC_DIR}/library/tulip-core/src/*.cpp ${OGDF_FILES} ${METRIC_FILES} ${SELECTION_FILES} ${SIZES_FILES} ${TEST_FILES} ${LAYOUT_FILES} ${CLUSTERING_FILES} ${EDGE_BUNDLING_FILES} -o libtulip.o
 
 if [ $? -eq 0 ]; then
   echo "libtulip.o generated"
