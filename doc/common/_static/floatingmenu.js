a4a11c96a995ae4cd895a84d6c47f81ba7d8404b
Patrick Mary <mary@labri.fr>
2013-11-08 17:35:19 +0000

    improvement for the sphinx generated html doc: allow to pin the floating menu
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9815 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- doc/common/_static/floatingmenu.js
+++ doc/common/_static/floatingmenu.js
@@ -8,14 +8,31 @@
 
 (function($) {
   var menuYloc = null;
+  var bgUrl = null;
 
   $(document).ready(function(){
-    menuYloc = parseInt($(".sidebar").css("top").substring(0,$(".sidebar").css("top").indexOf("px")));
-    $(window).scroll(function () {
-      offset = menuYloc+$(document).scrollTop()+"px";  
-      $(".sidebar").animate({top:offset},{duration:500,queue:false});
-    });
-  })
-})(jQuery);
+      menuYloc = parseInt($(".sidebar").css("top").substring(0,$(".sidebar").css("top").indexOf("px")));
+      bgUrl = $(".sidebar").css("background-image").substring(0, $(".sidebar").css("background-image").indexOf("logo"));
+      $(window).scroll(function () {
+	  if ($(".sidebar").attr("sticked") != "true") {
+	    offset = menuYloc+$(document).scrollTop()+"px";  
+	    $(".sidebar").animate({top:offset},{duration:500,queue:false});
+	  }
+	});
+      $(".sidebar" ).click(event, function() {
+	  var y = event.pageY - menuYloc - $(document).scrollTop();
+	  if (y > 0 && y < 181) {
+	    if ($(".sidebar").attr("sticked") != "true") {
+	      $(".sidebar").attr("sticked", "true");
+	      $(".sidebar").css("background-image", bgUrl + "logo_sticked.png)");
+	    }
+	    else {
+	      $(".sidebar").attr("sticked", "false");
+	      $(".sidebar").css("background-image", bgUrl + "logo.png)");
+	    }
+          }
+	});
+    })
+    })(jQuery);
 
 
