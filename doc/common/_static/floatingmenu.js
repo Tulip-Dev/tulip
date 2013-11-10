93fc16774ff71ad651d52006a2d54be86d95fbf4
Patrick Mary <mary@labri.fr>
2013-11-10 18:50:20 +0000

    improve pin/unpin of floating table of contents
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9817 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- doc/common/_static/floatingmenu.js
+++ doc/common/_static/floatingmenu.js
@@ -5,34 +5,31 @@
  *  tutorial code from :
  *  http://net.tutsplus.com/tutorials/html-css-techniques/creating-a-floating-html-menu-using-jquery-and-css/
  */
-
 (function($) {
   var menuYloc = null;
   var bgUrl = null;
 
-  $(document).ready(function(){
+  $(document).ready(function() {
+      $(".sidebar").prepend('<span id="pin"><img src="_static/unpin.png" title="click here to pin the table of contents"></span>');
+
       menuYloc = parseInt($(".sidebar").css("top").substring(0,$(".sidebar").css("top").indexOf("px")));
-      bgUrl = $(".sidebar").css("background-image").substring(0, $(".sidebar").css("background-image").indexOf("logo"));
+      $(".sidebar").attr("sticked", "false");
+
       $(window).scroll(function () {
 	  if ($(".sidebar").attr("sticked") != "true") {
 	    offset = menuYloc+$(document).scrollTop()+"px";  
 	    $(".sidebar").animate({top:offset},{duration:500,queue:false});
 	  }
 	});
-      $(".sidebar" ).click(event, function() {
-	  var y = event.pageY - menuYloc - $(document).scrollTop();
-	  if (y > 0 && y < 181) {
+      $("#pin").click(event, function() {
 	    if ($(".sidebar").attr("sticked") != "true") {
 	      $(".sidebar").attr("sticked", "true");
-	      $(".sidebar").css("background-image", bgUrl + "logo_sticked.png)");
+		$("#pin").html('<img src="_static/pin.png" title="click here to unpin the table of contents">');
 	    }
 	    else {
 	      $(".sidebar").attr("sticked", "false");
-	      $(".sidebar").css("background-image", bgUrl + "logo.png)");
+		$("#pin").html('<img src="_static/unpin.png" title="click here to pin the table of contents">');
 	    }
-          }
-	});
+ 	});
     })
     })(jQuery);
-
-
