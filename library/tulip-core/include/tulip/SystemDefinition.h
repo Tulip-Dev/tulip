230ff5ca02c8a5412d2f3b694bed1cb8ec3f98a6
Patrick Mary <mary@labri.fr>
2014-01-17 13:10:45 +0000

    use POSIX compliant macro to detect a linux platform
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9933 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/include/tulip/SystemDefinition.h
+++ library/tulip-core/include/tulip/SystemDefinition.h
@@ -25,7 +25,7 @@
 #define OS_PLATFORM "win"
 #elif defined(__APPLE__)
 #define OS_PLATFORM "mac"
-#elif defined(__linux)
+#elif defined(__linux__)
 #define OS_PLATFORM "linux"
 #else
 #define OS_PLATFORM "other"
@@ -35,6 +35,8 @@
 #define OS_ARCHITECTURE "x86_64"
 #elif defined(__i386__) || defined(_M_IX86)
 #define OS_ARCHITECTURE "x86"
+#else
+#define OS_ARCHITECTURE "other"
 #endif
 
 #if defined(__clang__)
