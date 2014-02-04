8a9d0f552676ff6e4f073c7b97242e15a28437ab
Patrick Mary <mary@labri.fr>
2014-02-04 11:06:46 +0000

    remove debug shared libs and archive libs from packaged files + add Tulip version to setup name
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@9961 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- bundlers/win/win_bundle.bat
+++ bundlers/win/win_bundle.bat
@@ -1,7 +1,7 @@
 @echo off
 set TULIP_DIR=%1
 set DEST_DIR=%2
-set QT_PLUGINS_DIR=%3
+set DEBUG_MODE=%3
 set SRC_DIR=%cd%
 
 echo 'Wiping destination directory'
@@ -11,13 +11,19 @@ mkdir "%DEST_DIR%\\files"
 
 echo 'Copying Tulip files'
 xcopy "%TULIP_DIR%" "%DEST_DIR%\\files" /E /Q
-mkdir "%DEST_DIR%\\files\\bin\\imageformats"
-xcopy "%QT_PLUGINS_DIR%"\\imageformats "%DEST_DIR%\\files\\bin\\imageformats" /E /Q
 
+if NOT "%DEBUG_MODE%" == "TRUE" (
+echo 'Removing debug libs'
+del /Q /F /S "%DEST_DIR%\\files\\bin\\*d4.dll" >nul 2>&1
+del /Q /F /S "%DEST_DIR%\\files\\bin\\Qt5*d.dll" >nul 2>&1
 del /Q /F /S "%DEST_DIR%\\files\\bin\\imageformats\\q*d4.dll" >nul 2>&1
+del /Q /F /S "%DEST_DIR%\\files\\bin\\imageformats\\libq*.a" >nul 2>&1
 del /Q /F /S "%DEST_DIR%\\files\\bin\\imageformats\\q*d4.pdb" >nul 2>&1
 del /Q /F /S "%DEST_DIR%\\files\\bin\\imageformats\\q*d.dll" >nul 2>&1
 del /Q /F /S "%DEST_DIR%\\files\\bin\\imageformats\\q*d.pdb" >nul 2>&1
+)
+echo 'Removing shared libs archives'
+del /Q /F /S "%DEST_DIR%\\files\\lib\\lib*.dll.a" >nul 2>&1
 
 echo 'Copying NSIS script and licence'
 copy "%SRC_DIR%\\Tulip.nsi" "%DEST_DIR%\\"
