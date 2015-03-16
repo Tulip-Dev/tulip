8196d10fb6ae6ee8ea40e38ca4cc15584182dd51
Patrick Mary <mary@labri.fr>
2015-03-16 10:45:04 +0000

    Louvain clustering minor fix: as enumeration ordering in an hash map is not predictable, when looking for the best neighbour cluster, choose the one with the minimum id to ensure a predictable computation of the metric
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@10615 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/metric/LouvainClustering.cpp
+++ plugins/metric/LouvainClustering.cpp
@@ -154,7 +154,7 @@ void LouvainClustering::createQuotient() {
 
     nodeEW[t] = deg;
     //Update comToInfo for modularity computing
-    comToInfo.insert(make_pair(t, make_pair(iW[t], 2.0*iW[t] + deg)));
+    comToInfo[t] = make_pair(iW[t], 2.0*iW[t] + deg);
   }
 
   //Copy Edges and the sum of edge weights
@@ -269,7 +269,7 @@ double LouvainClustering::modularity() const {
 void LouvainClustering::clustersNeighborhood(const node &toMove, TLP_HASH_MAP<node,double>& neigh) const {
   //Insert node's own clusters
   node comm = qclusters.get(toMove.id);
-  neigh.insert(make_pair(comm,0.0));
+  neigh[comm] = 0.0;
   const std::vector<edge> neighEdges = quotient.star(toMove);
   unsigned int nbNeigh = neighEdges.size();
 
@@ -332,6 +332,10 @@ double LouvainClustering::oneLevel() {
           best_increase = increase;
           best_comm = it->first;
         }
+	// as enumeration ordering in TLP_HASH_MAP is not predicatable
+	// keep the best cluster with the minimum id
+	else if (increase == best_increase && it->first < best_comm)
+	  best_comm = it->first;
       }
 
       //Insert n in its new cluster
