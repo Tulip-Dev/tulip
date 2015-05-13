78775cfceefffe5857646b34a08dc4a493ebcd70
Bruno Pinaud <bruno.pinaud@labri.fr>
2015-05-13 11:09:44 +0000

    backport svn commit #10735: Fix typo
    git-svn-id: https://svn.code.sf.net/p/auber/code/branches/tulip_4_7_maint@10736 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/SceneConfigWidget.cpp
+++ library/tulip-gui/src/SceneConfigWidget.cpp
@@ -50,7 +50,7 @@ SceneConfigWidget::SceneConfigWidget(QWidget *parent): QWidget(parent), _ui(new 
   connect(_ui->labelSizesSpanSlider, SIGNAL(upperValueChanged(int)), this, SLOT(updateSliderRangeLabels()));
 
   _ui->selectionColorButton->setDialogTitle("Choose the color of selected nodes or edges");
-  _ui->backgroundColorButton->setDialogTitle("Choose the color of the backagroud");
+  _ui->backgroundColorButton->setDialogTitle("Choose the background color");
 
   if (Perspective::instance() != NULL && Perspective::instance()->mainWindow() != NULL) {
     _ui->selectionColorButton->setDialogParent(Perspective::instance()->mainWindow());
