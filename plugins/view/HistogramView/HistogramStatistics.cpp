b99f8215813ea95952df3a2aeb27a347d4d1c1aa
Bruno Pinaud <bruno.pinaud@labri.fr>
2015-06-19 12:24:04 +0000

    fix compilation with clang on macos
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@10801 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/HistogramView/HistogramStatistics.cpp
+++ plugins/view/HistogramView/HistogramStatistics.cpp
@@ -30,7 +30,7 @@
 #include "HistoStatsConfigWidget.h"
 #include "HistogramView.h"
 
-#if defined(_MSC_VER) || (defined(__clang_major__) && __clang_major__ > 4)
+#if defined(_MSC_VER) || (defined(__clang_major__) && __clang_major__ >= 4)
 
 #include <functional>
 
