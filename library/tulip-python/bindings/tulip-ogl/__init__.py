9e1ca793f0890003e32181f060168f05a7a12724
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2015-07-07 11:26:10 +0000

    ensure paths for loading Tulip libraries and dependencies (Python wheels case) are inserted in the beginning of the paths list contained in the PATH environment variable on windows (DYLD_LIBRARY_PATH on MacOS)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@10834 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/bindings/tulip-ogl/__init__.py
+++ library/tulip-python/bindings/tulip-ogl/__init__.py
@@ -11,9 +11,9 @@ _tulipOglNativeLibsPath = os.path.dirname(__file__) + '/native/'
 sys.path.append(_tulipOglNativeLibsPath)
 
 if platform.system() == 'Windows':
-        os.environ['PATH'] += ';' + _tulipOglNativeLibsPath
+        os.environ['PATH'] = _tulipOglNativeLibsPath + ';' + os.environ['PATH']
 elif platform.system() == 'Darwin':
-        os.environ['DYLD_LIBRARY_PATH'] += ':' + _tulipOglNativeLibsPath
+        os.environ['DYLD_LIBRARY_PATH'] = _tulipOglNativeLibsPath + ':' + os.environ['DYLD_LIBRARY_PATH']
 
 import _tulipogl
 
