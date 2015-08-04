178284b866adcc3dbc2e0fd3f9fbc5de2dcb2326
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2015-08-04 18:41:34 +0000

    - "TLPB Export" now supports output gzstream (remove calls to std::ostream::tellp and std::ostream::seekp as it is not supported by ogzstream, cache subgraphs infos and write them to output file once all of them have been processed) - Produced export is of course the same as before - Remove the gzip restriction for TLPB Export in GraphPerspective
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@10892 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
+++ plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -493,12 +493,6 @@ void GraphPerspective::exportGraph(Graph* g) {
   std::string exportPluginName = wizard.algorithm().toStdString();
 
   if (filename.rfind(".gz") == (filename.length() - 3)) {
-    if (exportPluginName == "TLPB Export") {
-      QMessageBox::critical(_mainWindow, QString("File error"),
-                            QString("TLPB Export does not support on the fly compression\n(induced by .gz extension) for " + exportFile));
-      return;
-    }
-
     os = tlp::getOgzstream(filename);
   }
   else {
