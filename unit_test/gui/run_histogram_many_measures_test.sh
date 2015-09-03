4ccd82e678d41b6604ef0ee2d5ffb47488af5694
Patrick Mary <mary@labri.fr>
2015-09-03 09:48:35 +0000

    fix histogram gui test failure since last system updates: use keyboard to navigate into the Color Scale dialog + ensure the original Tulip settings are always restored after this test to avoid a failure for the following ones
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@10986 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/gui/run_histogram_many_measures_test.sh
+++ unit_test/gui/run_histogram_many_measures_test.sh
@@ -1,1 +1,7 @@
+# backup original Tulip settings
+cp ~/.config/TulipSoftware/Tulip.conf /tmp
 sh tulip_run_test.sh histogram_many_measures 
+# restore original Tulip settings to ensure that a failure
+# of this test will not impact the following ones
+cp /tmp/Tulip.conf ~/.config/TulipSoftware
+
