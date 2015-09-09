80f1ba938721cde3c78fec8edfb29e1e0f278bfe
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2015-09-09 09:43:15 +0000

    continue removing the dependency to the deprecated GLU library : add the implementation to 'gluPickMatrix' as a static function 'pickMatrix'
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11008 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/GlScene.cpp
+++ library/tulip-ogl/src/GlScene.cpp
@@ -674,6 +674,40 @@ void GlScene::glGraphCompositeRemoved(GlLayer* layer,GlGraphComposite *glGraphCo
     this->glGraphComposite=NULL;
   }
 }
+
+// original gluPickMatrix code from Mesa
+static void pickMatrix(GLdouble x, GLdouble y, GLdouble width, GLdouble height,  GLint viewport[4]) {
+ GLfloat m[16];
+ GLfloat sx, sy;
+ GLfloat tx, ty;
+
+ sx = viewport[2] / width;
+ sy = viewport[3] / height;
+ tx = (viewport[2] + 2.0 * (viewport[0] - x)) / width;
+ ty = (viewport[3] + 2.0 * (viewport[1] - y)) / height;
+
+ #define M(row, col) m[col*4+row]
+  M(0, 0) = sx;
+  M(0, 1) = 0.0;
+  M(0, 2) = 0.0;
+  M(0, 3) = tx;
+  M(1, 0) = 0.0;
+  M(1, 1) = sy;
+  M(1, 2) = 0.0;
+  M(1, 3) = ty;
+  M(2, 0) = 0.0;
+  M(2, 1) = 0.0;
+  M(2, 2) = 1.0;
+  M(2, 3) = 0.0;
+  M(3, 0) = 0.0;
+  M(3, 1) = 0.0;
+  M(3, 2) = 0.0;
+  M(3, 3) = 1.0;
+ #undef M
+
+ glMultMatrixf(m);
+}
+
 //========================================================================================================
 bool GlScene::selectEntities(RenderingEntitiesFlag type,int x, int y, int w, int h, GlLayer* layer,
                              vector<SelectedEntity>& selectedEntities) {
@@ -758,8 +792,7 @@ bool GlScene::selectEntities(RenderingEntitiesFlag type,int x, int y, int w, int
     glLoadIdentity();
     int newX = x + w/2;
     int newY = viewport[3] - (y + h/2);
-    gluPickMatrix(newX, newY, w, h, (GLint *)&viewport);
-
+    pickMatrix(newX, newY, w, h, (GLint *)&viewport);
 
     camera->initProjection(false);
 
