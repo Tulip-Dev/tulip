285eba4f23356942bf75a9669becab0e92a601ed
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2015-09-09 09:51:17 +0000

    bye bye GLU : remove any reference to the inclusion of its header file and linkage to its library
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11010 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/src/Camera.cpp
+++ library/tulip-ogl/src/Camera.cpp
@@ -30,10 +30,8 @@
 
 #if defined(__APPLE__)
 #include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
-#include <GL/glu.h>
 #endif
 
 using namespace std;
--- library/tulip-ogl/src/GlMetaNodeRenderer.cpp
+++ library/tulip-ogl/src/GlMetaNodeRenderer.cpp
@@ -32,10 +32,8 @@
 
 #if defined(__APPLE__)
 #include <OpenGL/gl.h>
-#include <OpenGL/glu.h>
 #else
 #include <GL/gl.h>
-#include <GL/glu.h>
 #endif
 
 using namespace std;
