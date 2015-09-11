6a530bd8e881e1d89a5f2fe11b2e46672f38e384
Patrick Mary <mary@labri.fr>
2015-09-11 07:24:51 +0000

    add gui test for scatter plot view
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11020 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ unit_test/gui/run_scatter_plot_test.sh
@@ -0,0 +1,6 @@
+# backup original Tulip settings
+cp ~/.config/TulipSoftware/Tulip.conf /tmp
+sh tulip_run_test.sh scatter_plot 
+# restore original Tulip settings to ensure that a failure
+# of this test will not impact the following ones
+cp /tmp/Tulip.conf ~/.config/TulipSoftware
