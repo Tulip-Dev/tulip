6d03de3ab9db69bf13ce09036207bd11714f87a3
Antoine Lambert <antoine.lambert33@gmail.com>
2015-10-01 09:53:29 +0000

    bug fix in View class : when a redraw trigger is deleted, ensure it is removed from the triggers set to avoid ObservableException to be thrown when using undo/redo (I just noticed that behaviour, surely due to recent commits).
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11059 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/View.cpp
+++ library/tulip-gui/src/View.cpp
@@ -192,6 +192,11 @@ void View::treatEvents(const std::vector<Event> &events) {
   for(unsigned int i=0; i<events.size(); ++i) {
     Event e = events[i];
 
+    // ensure redraw trigger is removed from the triggers set when it is deleted
+    if (e.type() == Event::TLP_DELETE && _triggers.contains(e.sender())) {
+      removeRedrawTrigger(e.sender());
+    }
+
     if (_triggers.contains(e.sender())) {
       emit drawNeeded();
       break;
