5fc6e1f45dfbe22733ec5190908fa4c9bbba14b3
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2015-12-30 13:23:39 +0000

    apply the workaround described here : https://bugreports.qt.io/browse/QTBUG-32639, to get rid of the Qt5 warning messages : 'QMacCGContext:: Unsupported painter devtype type 1' on MacOS
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11154 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/view/GeographicView/ProgressWidgetGraphicsProxy.cpp
+++ plugins/view/GeographicView/ProgressWidgetGraphicsProxy.cpp
@@ -27,6 +27,11 @@ using namespace tlp;
 ProgressWidget::ProgressWidget(QWidget *parent) : QWidget(parent), _ui(new Ui::ProgressWidgetData), cancelClicked(true) {
   _ui->setupUi(this);
   connect(_ui->cancelButton, SIGNAL(clicked()), this, SLOT(cancelButtonClicked()));
+// workaround to get rid of Qt5 warning messages : "QMacCGContext:: Unsupported painter devtype type 1"
+// see https://bugreports.qt.io/browse/QTBUG-32639
+#if defined(__APPLE__) && QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+  setWindowOpacity(0.99);
+#endif
 }
 
 ProgressWidget::~ProgressWidget() {
