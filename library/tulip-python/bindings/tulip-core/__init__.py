3a395dbf67726b7cae1dfe3e45b0af8d84158bfc
Antoine Lambert <antoine.lambert33@gmail.com>
2016-01-28 21:12:22 +0000

    proper indent and format tulip-core sip files used to generate the Python bindings :   - use consistent indentation level : 2 spaces   - remove a lot of useless tab characters   - plus other code style polishing
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11217 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/bindings/tulip-core/__init__.py
+++ library/tulip-python/bindings/tulip-core/__init__.py
@@ -31,14 +31,15 @@ class tlpType(_tulip.tlp.__class__):
 
 # utility function from the 'six' module
 def with_metaclass(meta, *bases):
-    """Create a base class with a metaclass."""
-    # This requires a bit of explanation: the basic idea is to make a dummy
-    # metaclass for one level of class instantiation that replaces itself with
-    # the actual metaclass.
-    class metaclass(meta):
-        def __new__(cls, name, this_bases, d):
-            return meta(name, bases, d)
-    return type.__new__(metaclass, 'temporary_class', (), {})
+  """Create a base class with a metaclass."""
+  # This requires a bit of explanation: the basic idea is to make a dummy
+  # metaclass for one level of class instantiation that replaces itself with
+  # the actual metaclass.
+  class metaclass(meta):
+    def __new__(cls, name, this_bases, d):
+      return meta(name, bases, d)
+    
+  return type.__new__(metaclass, 'temporary_class', (), {})
 
 class tlp(with_metaclass(tlpType, _tulip.tlp)):
   @staticmethod
