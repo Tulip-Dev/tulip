02037016be031461fc47b1dfa9bddb826c092f0f
tulipsoftware <tulipsoftware@mycompany.com>
2016-03-14 22:57:40 +0000

    [Jenkins] Astyle nightly commit (14/03/16)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11298 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/metric/Random.cpp
+++ plugins/metric/Random.cpp
@@ -25,13 +25,13 @@ using namespace tlp;
 namespace {
 const char * paramHelp[] = {
 // target
-HTML_HELP_OPEN()         \
-HTML_HELP_DEF( "type", "String Collection" ) \
-HTML_HELP_DEF("values", "nodes <BR> edges") \
-HTML_HELP_DEF( "default", "nodes" )  \
-HTML_HELP_BODY() \
-"Whether metric is computed only for nodes, only for edges, or for both."  \
-HTML_HELP_CLOSE(),
+  HTML_HELP_OPEN()         \
+  HTML_HELP_DEF( "type", "String Collection" ) \
+  HTML_HELP_DEF("values", "nodes <BR> edges") \
+  HTML_HELP_DEF( "default", "nodes" )  \
+  HTML_HELP_BODY() \
+  "Whether metric is computed only for nodes, only for edges, or for both."  \
+  HTML_HELP_CLOSE(),
 };
 }
 
@@ -42,53 +42,58 @@ HTML_HELP_CLOSE(),
 #define BOTH_TARGET 0
 
 class RandomMetric: public DoubleAlgorithm {
-    //===========================================
+  //===========================================
 public:
-    PLUGININFORMATION("Random metric","David Auber","04/10/2001","Assigns random values to nodes and edges.","1.1","Misc")
-    RandomMetric(const tlp::PluginContext* context):DoubleAlgorithm(context) {
-        addInParameter<StringCollection>(TARGET_TYPE, paramHelp[0], TARGET_TYPES);
+  PLUGININFORMATION("Random metric","David Auber","04/10/2001","Assigns random values to nodes and edges.","1.1","Misc")
+  RandomMetric(const tlp::PluginContext* context):DoubleAlgorithm(context) {
+    addInParameter<StringCollection>(TARGET_TYPE, paramHelp[0], TARGET_TYPES);
 
-        // result needs to be an inout parameter
-        // in order to preserve the original values of non targetted elements
-        // i.e if "target" = "nodes", the values of edges must be preserved
-        // and if "target" = "edges", the values of nodes must be preserved
-        parameters.setDirection("result", INOUT_PARAM);
-    }
+    // result needs to be an inout parameter
+    // in order to preserve the original values of non targetted elements
+    // i.e if "target" = "nodes", the values of edges must be preserved
+    // and if "target" = "edges", the values of nodes must be preserved
+    parameters.setDirection("result", INOUT_PARAM);
+  }
 
 //===========================================
-bool run() {
+  bool run() {
     // initialize a random sequence according the given seed
     tlp::initRandomSequence();
 
     bool nodes(true), edges(true);
+
     if ( dataSet!=NULL ) {
-        StringCollection targetType;
-        dataSet->get(TARGET_TYPE, targetType);
-        if(targetType.getCurrent()==NODES_TARGET) {
-            edges=false;
-            nodes=true;
-        }
-        else if (targetType.getCurrent()==NODES_TARGET) {
-            edges=true;
-            nodes=false;
-        }
-        else {
-            edges=true;
-            nodes=true;
-        }
+      StringCollection targetType;
+      dataSet->get(TARGET_TYPE, targetType);
+
+      if(targetType.getCurrent()==NODES_TARGET) {
+        edges=false;
+        nodes=true;
+      }
+      else if (targetType.getCurrent()==NODES_TARGET) {
+        edges=true;
+        nodes=false;
+      }
+      else {
+        edges=true;
+        nodes=true;
+      }
     }
+
     if(nodes) {
-        node n;
-        forEach(n, graph->getNodes())
-                result->setNodeValue(n, (double)rand() / double(RAND_MAX));
+      node n;
+      forEach(n, graph->getNodes())
+      result->setNodeValue(n, (double)rand() / double(RAND_MAX));
     }
+
     if(edges) {
-        edge e;
-        forEach(e, graph->getEdges())
-                result->setEdgeValue(e, (double)rand() / double(RAND_MAX));
+      edge e;
+      forEach(e, graph->getEdges())
+      result->setEdgeValue(e, (double)rand() / double(RAND_MAX));
     }
+
     return true;
-}
+  }
 //===========================================
 };
 PLUGIN(RandomMetric)
