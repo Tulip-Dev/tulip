794aacaa14f4015e9b940e8d8e8223de49e8c8c5
Antoine Lambert <antoine.lambert33@gmail.com>
2016-03-24 19:16:20 +0000

    new feature regarding the handling of image files for texturing nodes and edges : it is now possible to store an http url of an image file in the 'viewTexture' graph property, image files will then be downloaded on the fly in order to create the associated textures (add an helper class FileDownloader to handle these tasks)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11306 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/src/FileDownloader.cpp
@@ -0,0 +1,24 @@
+#include "tulip/FileDownloader.h"
+
+#include <QEventLoop>
+
+using namespace tlp;
+
+FileDownloader::FileDownloader() : QObject() {
+  connect(&_webCtrl, SIGNAL (finished(QNetworkReply*)),this, SLOT (fileDownloaded(QNetworkReply*)));
+}
+
+void FileDownloader::fileDownloaded(QNetworkReply* pReply) {
+  _downloadedData = pReply->readAll();
+  pReply->deleteLater();
+  emit downloaded();
+}
+
+const QByteArray &FileDownloader::download(const QUrl &url) {
+  QNetworkRequest request(url);
+  _webCtrl.get(request);
+  QEventLoop loop;
+  connect(this, SIGNAL(downloaded()), &loop, SLOT(quit()));
+  loop.exec();
+  return _downloadedData;
+}
