df029ded0d6aed91e50d70ef282b088c97ad50a1
Bruno Pinaud <bruno.pinaud@labri.fr>
2016-05-23 10:13:10 +0000

    add a viewbox attribute. This allow to use the svg inside a html img tag for zooming
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11352 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/export/SVGExport/SvgExportPlugin.cpp
+++ plugins/export/SVGExport/SvgExportPlugin.cpp
@@ -139,7 +139,7 @@ const char * paramHelp[] = {
 class ExportMultiple_svg:public tlp::ExportModule {
 
 public:
-  PLUGININFORMATION("SVG Export","Sami Gasri, Charles-Antoine Lami, Bruno Pinaud","16/07/2013","Exports a graph drawing in a SVG formatted file","1.6", "File")
+  PLUGININFORMATION("SVG Export","Sami Gasri, Charles-Antoine Lami, Bruno Pinaud","16/07/2013","Exports a graph drawing in a SVG formatted file.","1.6", "File")
 
   std::string icon() const {
     return ":/tulip/graphperspective/icons/32/export_svg.png";
