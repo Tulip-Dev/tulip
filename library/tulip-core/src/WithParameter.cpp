ce02cdf93baf2673624ffc77fec127af9db251d5
Patrick Mary <mary@labri.fr>
2016-06-06 09:45:26 +0000

    WithParameter fix: log an error message when a parameter default value cannot be parsed
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11392 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-core/src/WithParameter.cpp
+++ library/tulip-core/src/WithParameter.cpp
@@ -91,10 +91,9 @@ void ParameterDescriptionList::buildDefaultDataSet(DataSet &dataSet, Graph *g) c
     DataTypeSerializer* dts = DataSet::typenameToSerializer(type);
 
     if (dts) {
-#ifndef NDEBUG
-      bool result =
-#endif
-        dts->setData(dataSet, name, defaultValue);
+      bool result = dts->setData(dataSet, name, defaultValue);
+      if (!result)
+	tlp::error() << "Unable to parse \"" << defaultValue.c_str() << "\" as a default value for parameter \"" << name.c_str() << "\"" << std::endl;
       assert(result);
       continue;
     }
