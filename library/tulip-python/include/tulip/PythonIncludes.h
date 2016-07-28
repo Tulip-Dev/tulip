17bd9f744508ce3485e2cde588e7a505dc379228
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-07-28 20:26:48 +0000

    fix Tulip compilation with GCC 6.1 on MSYS2
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11478 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/include/tulip/PythonIncludes.h
+++ library/tulip-python/include/tulip/PythonIncludes.h
@@ -23,6 +23,7 @@
 // Need to include cmath before Python.h when compiling with MinGW and C++11 standard
 // to avoid a compilation error (see http://stackoverflow.com/questions/28683358/)
 #if defined(__MINGW32__) && __cplusplus >= 201103
+#include <math.h>
 #include <cmath>
 #endif
 
