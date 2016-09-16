145e05a0ab527af706a40dffbc680ae58f7ca46b
Patrick Mary <mary@labri.fr>
2016-09-16 11:02:21 +0000

    "About" panel enhancements: add links to Screenshots web site page and sourceforge bugs page
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11525 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- software/tulip/src/AboutTulipPage.cpp
+++ software/tulip/src/AboutTulipPage.cpp
@@ -17,72 +17,18 @@
  *
  */
 #include "AboutTulipPage.h"
-
-#include "ui_AboutTulipPage.h"
-
-#include <QAbstractItemModel>
-#include <QDir>
-#include <QUrl>
-
-#include <QDesktopServices>
-
 #include <tulip/TlpTools.h>
 
-class PictureModel: public QAbstractItemModel {
-  QDir _rootPath;
-
-public:
-  PictureModel(const QString& picturesPath,QObject* parent=0): QAbstractItemModel(parent), _rootPath(picturesPath) {
-  }
-
-  int rowCount(const QModelIndex & = QModelIndex()) const {
-    return _rootPath.entryList(QDir::Files).size();
-  }
-
-  int columnCount(const QModelIndex & = QModelIndex()) const {
-    return 1;
-  }
-
-  QModelIndex parent(const QModelIndex &) const {
-    return QModelIndex();
-  }
-
-  QModelIndex index(int row, int column,const QModelIndex &parent = QModelIndex()) const {
-    if (!hasIndex(row,column,parent)) {
-      return QModelIndex();
-    }
-
-    return createIndex(row,column);
-  }
-
-  QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const {
-    if (role == Qt::DecorationRole) {
-      return QImage(_rootPath.entryInfoList(QDir::Files)[index.row()].absoluteFilePath());
-    }
-    else if (role == Qt::DisplayRole) {
-      return _rootPath.entryInfoList(QDir::Files)[index.row()].baseName();
-    }
-
-    return QVariant();
-  }
-};
+#include "ui_AboutTulipPage.h"
 
 AboutTulipPage::AboutTulipPage(QWidget *parent) :
   QWidget(parent), _ui(new Ui::AboutTulipPageData()) {
   _ui->setupUi(this);
-  _ui->flowView->setBackgroundColor(Qt::white);
-  _ui->flowView->setModel(new PictureModel(QDir(tlp::TulipBitmapDir.c_str()).filePath("samplePictures"),this));
-  _ui->flowView->setSlideSize(QSize(250,250));
-  _ui->flowView->setCurrentIndex(_ui->flowView->model()->index(_ui->flowView->model()->rowCount()/2,0));
-  _ui->flowView->installEventFilter(this);
-  _ui->flowView->setReflectionEffect(QxtFlowView::BlurredReflection);
-}
 
-bool AboutTulipPage::eventFilter(QObject* obj, QEvent* ev) {
-  if (obj == _ui->flowView && ev->type() == QEvent::MouseButtonPress) {
-    QDesktopServices::openUrl( QUrl("http://tulip.labri.fr/TulipDrupal/?q=node/" + _ui->flowView->model()->data(_ui->flowView->currentIndex()).toString() ));
-    return true;
-  }
-
-  return false;
+  QPixmap qp(QString((tlp::TulipBitmapDir + "/samplePictures/1221.png").c_str()));
+  _ui->sample_1221->setPixmap(qp.scaled(230, 128, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+  qp = QPixmap(QString((tlp::TulipBitmapDir + "/samplePictures/1861.jpg").c_str()));
+  _ui->sample_1861->setPixmap(qp.scaled(230, 128, Qt::KeepAspectRatio, Qt::SmoothTransformation));
+  qp = QPixmap(QString((tlp::TulipBitmapDir + "/samplePictures/1531.png").c_str()));
+  _ui->sample_1531->setPixmap(qp.scaled(230, 128, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 }
