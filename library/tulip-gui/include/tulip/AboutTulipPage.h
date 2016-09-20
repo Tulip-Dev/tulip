903bb2b718d7bd2f652d45ddedf9722d901da00a
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-09-20 21:46:49 +0000

    - move the AboutTulipPage widget to the tulip-gui library and add authors and license informations - in the main Tulip perspective, also display a dialog with the same AboutTulipPage when selecting the 'About Tulip' menu entry (like any software)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11538 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/include/tulip/AboutTulipPage.h
+++ library/tulip-gui/include/tulip/AboutTulipPage.h
@@ -20,23 +20,29 @@
 #ifndef ABOUTTULIPPAGE_H
 #define ABOUTTULIPPAGE_H
 
+#include <tulip/tulipconf.h>
+
 #include <QWidget>
 
 namespace Ui {
 class AboutTulipPageData;
 }
 
+namespace tlp {
+
 /**
   @brief The "About us/Getting help" screen displayed in the Tulip app startup window.
   This widget is mainly meant to provide links to the user and developer documentations, direct user to Tulip forums
   and provide basic offline support.
   */
-class AboutTulipPage : public QWidget {
+class TLP_QT_SCOPE AboutTulipPage : public QWidget {
 public:
   explicit AboutTulipPage(QWidget *parent = 0);
 
 private:
   Ui::AboutTulipPageData *_ui;
 };
+
+}
 
 #endif // ABOUTTULIPPAGE_H
