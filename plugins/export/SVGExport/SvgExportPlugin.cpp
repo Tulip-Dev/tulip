9daf77e2f6a348e304b62e690afc898df9eb3bc6
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-09-23 20:37:21 +0000

    class name should start with a capital letter
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11559 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/export/SVGExport/SvgExportPlugin.cpp
+++ plugins/export/SVGExport/SvgExportPlugin.cpp
@@ -63,7 +63,7 @@ static const char *paramHelp[] = {
     "Uses Web Open Font Format version 2 (woff2) to reduce generated file "
     "length. This format is supported in almost all recent Internet browsers."};
 
-class svgExport : public tlp::ExportModule {
+class SvgExport : public tlp::ExportModule {
 
 public:
   PLUGININFORMATION("SVG Export",
@@ -78,7 +78,7 @@ public:
 
   string fileExtension() const { return "svg"; }
 
-  svgExport(tlp::PluginContext *context) : tlp::ExportModule(context) {
+  SvgExport(tlp::PluginContext *context) : tlp::ExportModule(context) {
     addInParameter<bool>("Edge color interpolation", paramHelp[0], "false");
     addInParameter<bool>("Edge size interpolation", paramHelp[1], "true");
     addInParameter<bool>("Edge extremities", paramHelp[2], "false");
@@ -112,4 +112,4 @@ public:
     return ret;
   }
 };
-PLUGIN(svgExport)
+PLUGIN(SvgExport)
