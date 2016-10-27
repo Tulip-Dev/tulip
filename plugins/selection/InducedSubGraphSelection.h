3c26f193d151ff98ce1a2fae13a6b89cd222181e
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-10-27 19:33:15 +0000

    implement feature request posted as sf bug #841 : Induced subgraph from selection of nodes and edges   * add method "tlp::Graph *tlp::Graph::inducedSubGraph(tlp::BooleanProperty *selection, tlp::Graph *parentSubGraph = NULL)" and its Python bindings   * Increment "Induced Sub-Graph" plugin version to 1.1 : add a 'Use edges' boolean parameter in order to also consider selected edges from the input selection
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11696 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/selection/InducedSubGraphSelection.h
+++ plugins/selection/InducedSubGraphSelection.h
@@ -36,7 +36,7 @@
 class InducedSubGraphSelection:public tlp::BooleanAlgorithm {
 public:
   PLUGININFORMATION(tlp::SelectionAlgorithm::InducedSubGraphSelection,"David Auber","08/08/2001",
-                    "Selects all the nodes/edges of the subgraph induced by a set of selected nodes.","1.0", "Selection")
+                    "Selects all the nodes/edges of the subgraph induced by a set of selected nodes.","1.1", "Selection")
   InducedSubGraphSelection(const tlp::PluginContext* context);
   bool run();
 };
