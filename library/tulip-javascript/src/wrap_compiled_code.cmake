d15ecbe0ebae7573493fc77c5514643ee7f5ce4d
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2016-12-22 17:41:58 +0100

    now rely on a Promise for initializing tulip.js

--- library/tulip-javascript/src/wrap_compiled_code.cmake
+++ library/tulip-javascript/src/wrap_compiled_code.cmake
@@ -1,46 +1,69 @@
 FILE(READ ${JSFILE} EMSCRIPTEN_GENERATED_CODE)
 FILE(READ ${JSUTILSFILE} JSUTILS_CODE)
 
-FILE(WRITE ${JSFILE} "!function() {")
-FILE(APPEND ${JSFILE} "  ${JSUTILS_CODE}")
-FILE(APPEND ${JSFILE} "  ${EMSCRIPTEN_GENERATED_CODE}")
-FILE(APPEND ${JSFILE} "  if(typeof tulip == 'undefined') tulip = {};")
-FILE(APPEND ${JSFILE} "  if(typeof tulipConf == 'undefined') tulipConf = {};")
-FILE(APPEND ${JSFILE} "  this.tulip = tulip;")
-FILE(APPEND ${JSFILE} "  var scriptName = getScriptName();")
-FILE(APPEND ${JSFILE} "  var scriptPath = getScriptPath();")
-FILE(APPEND ${JSFILE} "  if(tulipConf.modulePrefixURL) tulip.modulePrefixURL = tulipConf.modulePrefixURL;")
-FILE(APPEND ${JSFILE} "  if(tulipConf.filePackagePrefixURL) tulip.filePackagePrefixURL = tulipConf.filePackagePrefixURL;")
-FILE(APPEND ${JSFILE} "  if(tulipConf.memoryInitializerPrefixURL) tulip.memoryInitializerPrefixURL = tulipConf.memoryInitializerPrefixURL;")
-FILE(APPEND ${JSFILE} "  if(tulipConf.TOTAL_MEMORY) tulip.TOTAL_MEMORY = tulipConf.TOTAL_MEMORY;")
-FILE(APPEND ${JSFILE} "  if(!tulip.modulePrefixURL) tulip.modulePrefixURL = scriptPath;")
-FILE(APPEND ${JSFILE} "  if(!tulip.filePackagePrefixURL) tulip.filePackagePrefixURL = scriptPath;")
-FILE(APPEND ${JSFILE} "  if(!tulip.memoryInitializerPrefixURL) tulip.memoryInitializerPrefixURL = scriptPath;")
+FILE(WRITE ${JSFILE} "
+!function() {
+  ${JSUTILS_CODE}
+  ${EMSCRIPTEN_GENERATED_CODE}
+  if(typeof tulip == 'undefined') tulip = {};
+  if(typeof tulipConf == 'undefined') tulipConf = {};
+  this.tulip = tulip;
+  var scriptName = getScriptName();
+  var scriptPath = getScriptPath();
+  if(tulipConf.modulePrefixURL) tulip.modulePrefixURL = tulipConf.modulePrefixURL;
+  if(tulipConf.filePackagePrefixURL) tulip.filePackagePrefixURL = tulipConf.filePackagePrefixURL;
+  if(tulipConf.memoryInitializerPrefixURL) tulip.memoryInitializerPrefixURL = tulipConf.memoryInitializerPrefixURL;
+  if(tulipConf.TOTAL_MEMORY) tulip.TOTAL_MEMORY = tulipConf.TOTAL_MEMORY;
+  if(!tulip.modulePrefixURL) tulip.modulePrefixURL = scriptPath;
+  if(!tulip.filePackagePrefixURL) tulip.filePackagePrefixURL = scriptPath;
+  if(!tulip.memoryInitializerPrefixURL) tulip.memoryInitializerPrefixURL = scriptPath;
+  tulip.loadingFailed = false;
+  tulip.wasm = false;
+  tulip.vizFeatures = false;
+  tulip.isLoaded = function() {return false;};
+")
 IF(TULIP_VIZ_FEATURES)
-  FILE(APPEND ${JSFILE} "  tulip.vizFeatures = true;")
-ELSE(TULIP_VIZ_FEATURES)
-  FILE(APPEND ${JSFILE} "  tulip.vizFeatures = false;")
+FILE(APPEND ${JSFILE} "
+  tulip.vizFeatures = true;
+")
 ENDIF(TULIP_VIZ_FEATURES)
 IF(USE_WASM)
-  FILE(APPEND ${JSFILE} "  tulip.isLoaded = function() {return false;};")
-  FILE(APPEND ${JSFILE} "  var xhr = new XMLHttpRequest();")
-  FILE(APPEND ${JSFILE} "  xhr.open('GET', tulip.modulePrefixURL + 'tulip.wasm');")
-  FILE(APPEND ${JSFILE} "  xhr.responseType = 'arraybuffer';")
-  FILE(APPEND ${JSFILE} "  xhr.onload = function() {")
-  FILE(APPEND ${JSFILE} "    tulip.wasmBinary = xhr.response;")
+FILE(APPEND ${JSFILE} "
+  tulip.wasm = true;
+")
 ENDIF(USE_WASM)
-FILE(APPEND ${JSFILE} "    tulip = tulipjs(tulip);")
-FILE(APPEND ${JSFILE} "    if (typeof define === 'function' && define.amd) define(tulip); else if (typeof module === 'object' && module.exports) module.exports = tulip;")
-IF(USE_WASM)
-  FILE(APPEND ${JSFILE} "  };")
-  FILE(APPEND ${JSFILE} "  xhr.send(null);")
-ENDIF(USE_WASM)
-FILE(APPEND ${JSFILE} "}();")
-
-
-
-
-
-
-
-
+FILE(APPEND ${JSFILE} "
+  tulip.init = function() {
+    return new Promise(function(resolve, reject) {
+      function checkTulipIsLoaded() {
+        if (tulip.isLoaded()) {
+          resolve();
+        } else {
+          setTimeout(checkTulipIsLoaded);
+        }
+      }
+      if (!tulip.wasm) {
+        try {
+          tulip = tulipjs(tulip);
+        } catch (e) {
+          reject(e);
+        }
+      } else {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', tulip.modulePrefixURL + 'tulip.wasm');
+        xhr.responseType = 'arraybuffer';
+        xhr.onload = function() {
+          tulip.wasmBinary = xhr.response;
+          try {
+            tulip = tulipjs(tulip);
+          } catch (e) {
+            reject(e);
+          }
+        };
+        xhr.send(null);
+      }
+      checkTulipIsLoaded();
+    });
+  };
+  if (typeof define === 'function' && define.amd) define(tulip); else if (typeof module === 'object' && module.exports) module.exports = tulip;
+}();")
