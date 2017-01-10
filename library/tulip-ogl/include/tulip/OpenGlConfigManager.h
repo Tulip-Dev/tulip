6ab3565f521aa7264b7aca45d6d4ee642ba0c4dc
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-01-10 20:49:46 +0000

    some improvements to the OpenGlConfigManager class:   - fix getOpenGLVersion method implementation   - add method getOpenGLVersionString   - add missing const
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11890 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-ogl/include/tulip/OpenGlConfigManager.h
+++ library/tulip-ogl/include/tulip/OpenGlConfigManager.h
@@ -40,18 +40,26 @@ public:
 
   /**
    * Return the current instance. If instance doesn't exist, create it.
+   *
    */
   static OpenGlConfigManager &getInst();
 
   /**
-   * Returns the OpenGL version number supported by the host system.
+   * Returns the OpenGL version number supported by the host system as a string.
+   *
+   * \since Tulip 4.11
    */
-  double getOpenGLVersion();
+  std::string getOpenGLVersionString() const;
+
+  /**
+   * Returns the OpenGL version number supported by the host system as a number.
+   */
+  double getOpenGLVersion() const;
 
   /**
    * Return the vendor name of the OpenGL driver installed on the host system.
    */
-  std::string getOpenGLVendor();
+  std::string getOpenGLVendor() const;
 
   void initExtensions();
 
