e7541159dc85d7795a9f8f6b538ed3142770305f
Patrick Mary <mary@labri.fr>
2017-01-24 09:39:56 +0000

    Delaunay & Voronoi plugins enhancement: add "original clone" boolean parameter to enable/disable the creation of a clone subgraph; for ascendant compatibility the default value is set to "true"
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11929 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/clustering/Delaunay/VoronoiDiagram.cpp
+++ plugins/clustering/Delaunay/VoronoiDiagram.cpp
@@ -24,7 +24,8 @@
 
 using namespace std;
 
-static bool voronoiDiagram(tlp::Graph *graph, bool voronoiCellsSubGraphs, bool connectNodeToCellBorder) {
+static bool voronoiDiagram(tlp::Graph *graph, bool voronoiCellsSubGraphs,
+			   bool connectNodeToCellBorder, bool originalClone) {
   vector<tlp::node> nodes;
   vector<tlp::Coord> sites;
   tlp::node n;
@@ -43,7 +44,8 @@ static bool voronoiDiagram(tlp::Graph *graph, bool voronoiCellsSubGraphs, bool c
 
   if (ret) {
     tlp::Graph *voronoiSg = graph->addSubGraph("Voronoi");
-    graph->addCloneSubGraph("Original graph");
+    if (originalClone) 
+      graph->addCloneSubGraph("Original graph");
     TLP_HASH_MAP<unsigned int, tlp::node> voronoiVertexToNode;
 
     for (size_t i = 0 ; i < voronoiDiag.nbVertices() ; ++i) {
@@ -95,6 +97,9 @@ static const char *paramHelp[] = {
 
   // connect
   "If true, existing graph nodes will be connected to the vertices of their voronoi cell."
+
+  // original clone
+  "If true, a clone subgraph named 'Original graph' will be first added."
 };
 
 class VoronoiDiagram : public tlp::Algorithm {
@@ -104,23 +109,27 @@ public :
   VoronoiDiagram(tlp::PluginContext *context) : tlp::Algorithm(context) {
     addInParameter<bool>("voronoi cells", paramHelp[0], "false");
     addInParameter<bool>("connect", paramHelp[1], "false");
+    addInParameter<bool>("original clone", paramHelp[2], "true");
   }
 
   PLUGININFORMATION("Voronoi diagram", "Antoine Lambert", "",
-                    "Performs a Voronoi decomposition, in considering the positions of the graph nodes as a set of points. These points define the seeds (or sites) of the voronoi cells. New nodes and edges are added to build the convex polygons defining the contours of these cells.","1.0","Triangulation")
+                    "Performs a Voronoi decomposition, in considering the positions of the graph nodes as a set of points. These points define the seeds (or sites) of the voronoi cells. New nodes and edges are added to build the convex polygons defining the contours of these cells.","1.1","Triangulation")
 
   bool run() {
     tlp::Observable::holdObservers();
 
     bool voronoiCellSg = false;
     bool connectNodesToVoronoiCell = false;
+    bool originalClone = true;
 
     if (dataSet) {
       dataSet->get("voronoi cells", voronoiCellSg);
       dataSet->get("connect", connectNodesToVoronoiCell);
+      dataSet->get("original clone", originalClone);
     }
 
-    bool ret = voronoiDiagram(graph, voronoiCellSg, connectNodesToVoronoiCell);
+    bool ret = voronoiDiagram(graph, voronoiCellSg, connectNodesToVoronoiCell,
+			      originalClone);
 
     tlp::Observable::unholdObservers();
 
