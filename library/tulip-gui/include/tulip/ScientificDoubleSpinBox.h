a7afe4cc6a1380a72ac89503ece22f872177dd6e
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-01-24 22:00:33 +0000

    More GUI improvements : now uses spin boxes instead of line edits to edit number values in most parts of the Tulip interface (plugin parameters, spreadsheet view, ...). In order to keep previous editing behaviour for floating point number values, add a new class in the tulip-gui library: ScientificDoubleSpinBox, implementing a QDoubleSpinBox supporting scientific notation.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11939 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/include/tulip/ScientificDoubleSpinBox.h
@@ -0,0 +1,74 @@
+/*
+ *
+ * This file is part of Tulip (www.tulip-software.org)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
+
+// original code is written in Python and is taken from https://jdreaver.com/posts/2014-07-28-scientific-notation-spin-box-pyside.html
+// see also https://gist.github.com/jdreaver/0be2e44981159d0854f5
+
+#ifndef SCIENTIFICDOUBLESPINBOX_H
+#define SCIENTIFICDOUBLESPINBOX_H
+
+#include <tulip/tulipconf.h>
+
+#include <QDoubleSpinBox>
+#include <QValidator>
+
+namespace tlp {
+
+/**
+ * @brief Utility class used to validate that a QString contains a valid representation of a floating point number
+ * including scientific notation
+ *
+ * @since Tulip 4.11
+ */
+class TLP_QT_SCOPE FloatValidator : public QValidator {
+
+public:
+
+  State validate(QString &input, int &pos) const;
+
+  void fixup(QString &text) const;
+
+};
+
+/**
+ * @brief Utility class implementing a QDoubleSpinBox supporting scientific notation
+ *
+ * @since Tulip 4.11
+ */
+class TLP_QT_SCOPE ScientificDoubleSpinBox : public QDoubleSpinBox {
+
+public:
+
+  ScientificDoubleSpinBox(QWidget *parent=0);
+
+  QValidator::State validate(QString &input, int &pos) const;
+
+  void fixup(QString &input) const;
+
+  double valueFromText(const QString &text) const;
+
+  QString textFromValue(double value) const;
+
+  void stepBy(int steps);
+
+};
+
+}
+
+#endif // SCIENTIFICDOUBLESPINBOX_H
---
+++ library/tulip-gui/src/ScientificDoubleSpinBox.cpp
@@ -0,0 +1,82 @@
+// original code is written in Python and is taken from https://jdreaver.com/posts/2014-07-28-scientific-notation-spin-box-pyside.html
+// see also https://gist.github.com/jdreaver/0be2e44981159d0854f5
+
+#include <cfloat>
+
+#include <tulip/ScientificDoubleSpinBox.h>
+#include <tulip/PropertyTypes.h>
+#include <tulip/TlpQtTools.h>
+
+#include <QRegExp>
+#include <QLineEdit>
+
+using namespace tlp;
+
+static QRegExp floatRe("(([+-]?\\d+(\\.\\d*)?|\\.\\d+)([eE][+-]?\\d+)?)");
+static FloatValidator floatValidator;
+
+bool validFloatString(const QString &s) {
+  int pos = floatRe.indexIn(s);
+  if (pos != - 1) {
+    return floatRe.capturedTexts()[1] == s;
+  } else {
+    return false;
+  }
+}
+
+QValidator::State FloatValidator::validate(QString &input, int &pos) const {
+  if (validFloatString(input)) {
+    return QValidator::Acceptable;
+  }
+  if (input == "" || input.mid(pos-1, 1).count(QRegExp("[e.-+]+")) == 1) {
+    return QValidator::Intermediate;
+  }
+  return QValidator::Invalid;
+}
+
+void FloatValidator::fixup(QString &text) const {
+  int pos = floatRe.indexIn(text);
+  if (pos != - 1) {
+    text = floatRe.capturedTexts()[1];
+  } else {
+    text = "";
+  }
+}
+
+ScientificDoubleSpinBox::ScientificDoubleSpinBox(QWidget *parent) : QDoubleSpinBox(parent) {
+  setMinimum(-DBL_MAX);
+  setMaximum(DBL_MAX);
+  setDecimals(1000);
+}
+
+QValidator::State ScientificDoubleSpinBox::validate(QString &input, int &pos) const {
+  return floatValidator.validate(input, pos);
+}
+
+void ScientificDoubleSpinBox::fixup(QString &input) const {
+  floatValidator.fixup(input);
+}
+
+double ScientificDoubleSpinBox::valueFromText(const QString &text) const {
+  double val = 0;
+  tlp::DoubleType::fromString(val, QStringToTlpString(text));
+  return val;
+}
+
+QString ScientificDoubleSpinBox::textFromValue(double value) const {
+  return tlpStringToQString(tlp::DoubleType::toString(value));
+}
+
+void ScientificDoubleSpinBox::stepBy(int steps) {
+  QString text = cleanText();
+  floatRe.indexIn(text);
+  QStringList groups = floatRe.capturedTexts();
+  double decimal = 0;
+  tlp::DoubleType::fromString(decimal, QStringToTlpString(groups[2]));
+  decimal += steps;
+  text = tlpStringToQString(tlp::DoubleType::toString(decimal));
+  if (groups.count() > 4) {
+    text += groups[4];
+  }
+  lineEdit()->setText(text);
+}
