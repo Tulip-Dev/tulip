caa5e3196223f399879a94df778b692b82a07fd6
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-02-08 21:51:12 +0000

    no need to set the file attribute of a graph when loading it from a Tulip project
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11989 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
+++ plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -803,11 +803,6 @@ void GraphPerspective::openProjectFile(const QString &path) {
 #ifdef TULIP_BUILD_PYTHON_COMPONENTS
     _developFrame->setProject(_project);
 #endif
-
-    for (QMap<QString,tlp::Graph*>::iterator it = rootIds.begin(); it != rootIds.end() ; ++it) {
-      it.value()->setAttribute("file", QStringToTlpString(path));
-    }
-
     delete prg;
   }
   else {
