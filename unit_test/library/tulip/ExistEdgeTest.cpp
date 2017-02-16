3521d5e84a4deb59dad5c6232a62a5f0e2af5959
Patrick Mary <mary@labri.fr>
2017-02-16 13:32:27 +0000

    add non regression unit test for bug fixed in commit #12021
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12022 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/library/tulip/ExistEdgeTest.cpp
+++ unit_test/library/tulip/ExistEdgeTest.cpp
@@ -32,8 +32,11 @@ void ExistEdgeTest::setUp() {
   graph = newGraph();
   n0 = graph->addNode();
   n1 = graph->addNode();
+  n2 = graph->addNode();
   e0 = graph->addEdge(n0, n1);
   e1 = graph->addEdge(n1, n0);
+  e2 = graph->addEdge(n2, n2);
+  e3 = graph->addEdge(n2, n0);
 }
 
 void ExistEdgeTest::tearDown() {
@@ -49,5 +52,11 @@ void ExistEdgeTest::testExistEdge() {
   CPPUNIT_ASSERT(graph->existEdge(n0, n1, false) == e0);
   CPPUNIT_ASSERT(graph->existEdge(n1, n0) == e1);
   CPPUNIT_ASSERT(graph->existEdge(n1, n0, false) == e0);
+  CPPUNIT_ASSERT(graph->existEdge(n2, n2) == e2);
+  CPPUNIT_ASSERT(graph->existEdge(n2, n0) == e3);
+  CPPUNIT_ASSERT(graph->existEdge(n0, n2).isValid() == false);
+  CPPUNIT_ASSERT(graph->existEdge(n0, n2, false) == e3);
+  CPPUNIT_ASSERT(graph->existEdge(n2, n1, false).isValid() == false);
+  CPPUNIT_ASSERT(graph->existEdge(n1, n2, false).isValid() == false);
 }
 
