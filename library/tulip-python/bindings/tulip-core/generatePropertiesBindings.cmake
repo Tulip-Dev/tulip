ef0ca302b8704271c5ad8094eccff0562d4ee3d7
Antoine Lambert <antoine.lambert33@gmail.com>
2017-03-08 22:48:46 +0000

    Add type hints to tulip Python module through SIP by following the specification of the PEP 484. For the moment it enables to get a more accurate api file used in Tulip Python code editors for displaying API tooltips. Next steps are the update of the documentation in the same maneer but also the generation of a type hint stub file and try to make it work in PyCharm.
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12054 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/bindings/tulip-core/generatePropertiesBindings.cmake
+++ library/tulip-python/bindings/tulip-core/generatePropertiesBindings.cmake
@@ -7,6 +7,8 @@ MACRO(GEN_PROPERTY_BINDINGS GEN_FILE
                             PYTHON_NODE_TYPE 
                             CPP_EDGE_TYPE 
                             PYTHON_EDGE_TYPE
+                            PYTHON_NODE_TYPEHINT
+                            PYTHON_EDGE_TYPEHINT
                             PROPERTY_SPECIFIC_METHODS_FILE)
 
   FILE(READ ${SOURCE_DIR}/AbstractProperty.sip.in input)
@@ -19,6 +21,8 @@ MACRO(GEN_PROPERTY_BINDINGS GEN_FILE
   STRING(REPLACE "@VEC_PROPERTY_TYPE@" "${VEC_PROPERTY_TYPE}" input "${input}")  
   STRING(REPLACE "@CPP_TYPE@" "${CPP_NODE_TYPE}" input "${input}")
   STRING(REPLACE "@PYTHON_TYPE@" "${PYTHON_NODE_TYPE}" input "${input}")
+  STRING(REPLACE "@PYTHON_NODE_TYPEHINT@" "${PYTHON_NODE_TYPEHINT}" input "${input}")
+  STRING(REPLACE "@PYTHON_EDGE_TYPEHINT@" "${PYTHON_EDGE_TYPEHINT}" input "${input}")
 
   IF(NOT "${PROPERTY_SPECIFIC_METHODS_FILE}" STREQUAL "")
     FILE(READ ${SOURCE_DIR}/${PROPERTY_SPECIFIC_METHODS_FILE} specific)
@@ -31,12 +35,12 @@ MACRO(GEN_PROPERTY_BINDINGS GEN_FILE
   
 ENDMACRO(GEN_PROPERTY_BINDINGS)
 
-GEN_PROPERTY_BINDINGS("BooleanProperty.sip" "PropertyInterface" "Boolean" "Boolean" "bool" "boolean" "bool" "boolean" "BooleanPropertySpecific.sip")
-GEN_PROPERTY_BINDINGS("DoubleProperty.sip" "NumericProperty" "Double" "Double" "double" "float" "double" "float" "DoublePropertySpecific.sip")
-GEN_PROPERTY_BINDINGS("IntegerProperty.sip" "NumericProperty" "Integer" "Integer" "int" "integer" "int" "integer" "IntegerPropertySpecific.sip")
-GEN_PROPERTY_BINDINGS("ColorProperty.sip" "PropertyInterface" "Color" "Color" "tlp::Color" ":class:`tlp.Color`" "tlp::Color" ":class:`tlp.Color`" "")
-GEN_PROPERTY_BINDINGS("LayoutProperty.sip" "PropertyInterface" "Layout" "Coord" "tlp::Coord" ":class:`tlp.Coord`" "std::vector<tlp::Coord>" "list of :class:`tlp.Coord`" "LayoutPropertySpecific.sip")
-GEN_PROPERTY_BINDINGS("SizeProperty.sip" "PropertyInterface" "Size" "Size" "tlp::Size" ":class:`tlp.Size`" "tlp::Size" ":class:`tlp.Size`" "SizePropertySpecific.sip")
-GEN_PROPERTY_BINDINGS("StringProperty.sip" "PropertyInterface" "String" "String" "std::string" "string" "std::string" "string" "")
+GEN_PROPERTY_BINDINGS("BooleanProperty.sip" "PropertyInterface" "Boolean" "Boolean" "bool" "boolean" "bool" "boolean" "bool" "bool" "BooleanPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("DoubleProperty.sip" "NumericProperty" "Double" "Double" "double" "float" "double" "float" "float" "float" "DoublePropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("IntegerProperty.sip" "NumericProperty" "Integer" "Integer" "int" "integer" "int" "integer" "int" "int" "IntegerPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("ColorProperty.sip" "PropertyInterface" "Color" "Color" "tlp::Color" ":class:`tlp.Color`" "tlp::Color" ":class:`tlp.Color`" "tlp.Color" "tlp.Color" "")
+GEN_PROPERTY_BINDINGS("LayoutProperty.sip" "PropertyInterface" "Layout" "Coord" "tlp::Coord" ":class:`tlp.Coord`" "std::vector<tlp::Coord>" "list of :class:`tlp.Coord`" "tlp.Coord" "List[tlp.Coord]" "LayoutPropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("SizeProperty.sip" "PropertyInterface" "Size" "Size" "tlp::Size" ":class:`tlp.Size`" "tlp::Size" ":class:`tlp.Size`" "tlp.Size" "tlp.Size" "SizePropertySpecific.sip")
+GEN_PROPERTY_BINDINGS("StringProperty.sip" "PropertyInterface" "String" "String" "std::string" "string" "std::string" "string" "str" "str" "")
 
    		   
