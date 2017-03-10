7933748dbc1c6fee967895203c823033f2e3b866
tulipsoftware <tulipsoftware@mycompany.com>
2017-03-10 21:53:56 +0000

    [Jenkins] Astyle nightly commit (10/03/17)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12069 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-gui/src/AboutTulipPageUtils.cpp
+++ library/tulip-gui/src/AboutTulipPageUtils.cpp
@@ -3,13 +3,13 @@
 namespace tlp {
 
 #ifdef TULIP_BUILD_PYTHON_COMPONENTS
-  QString getSipVersion() {
-    return SIP_VERSION;
-  }
+QString getSipVersion() {
+  return SIP_VERSION;
+}
 #endif
 
-  QString getTulipSvnRevision() {
-    return TULIP_SVN_REVISION;
-  }
+QString getTulipSvnRevision() {
+  return TULIP_SVN_REVISION;
+}
 
 }
