55fae3d80764bd2cf0eb2b54c80987c649b8b83d
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-03-31 21:22:22 +0000

    only print Python plugin registration details when an error occured + add missing license header
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12166 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/modules/tulipplugins.py
+++ library/tulip-python/modules/tulipplugins.py
@@ -1,3 +1,18 @@
+# This file is part of Tulip (www.tulip-software.org)
+#
+# Authors: David Auber and the Tulip development Team
+# from LaBRI, University of Bordeaux
+#
+# Tulip is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License
+# as published by the Free Software Foundation, either version 3
+# of the License, or (at your option) any later version.
+#
+# Tulip is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
 """
 This module provides utility functions to register Tulip plugins written in Python in the plugins database.
 """
@@ -166,8 +181,6 @@ def registerPluginOfGroup(pluginClassName, pluginName, author, date, info, relea
                      "__init__": (lambda self: initFactory(self)), \
                      "createPluginObject" : (lambda self, context: createPlugin(context, pluginModule, pluginClassName, pluginName, author, date, info, release, group))} \
                      )()
-
-    sys.stdout.write('Tulip Python Plug-in "' + pluginName + '" loaded, Author: "' + author + '", Date: "' + date + '", Release: "' + release + '"\n')
 
   except:
     sys.stdout.write("There was an error when registering Python plugin named \"" + pluginName + "\". See stack trace below.\n")
