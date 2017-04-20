88f67842cc4807b28e525e7c8aa79c0931e07dbe
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-04-20 19:13:07 +0000

    cleanup sphinx documentation CMakeLists.txt + fix possible doc build issue on windows with MSYS2
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12229 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- doc/developer/genSphinxDoc.cmake
+++ doc/developer/genSphinxDoc.cmake
@@ -1,25 +1,14 @@
-SET(CHAR_SEP ":")
+# Turn back the CMAKE_LIBRARY_PATH string into a list
+STRING(REPLACE "," ";" CMAKE_LIBRARY_PATH "${CMAKE_LIBRARY_PATH}")
+
+# Fix possible sphinx-build issue on Windows
 IF(WIN32)
-SET(CHAR_SEP ";")
-IF(MSVC)
-SET(ENV{PATH} "${BIN_DIR}/../../library/tulip-core/src/Debug;${BIN_DIR}/../../library/tulip-gui/src/Debug;${BIN_DIR}/../../library/tulip-ogl/src/Debug;${BIN_DIR}/../../library/tulip-ogdf/src/Debug;${BIN_DIR}/../../library/tulip-python/src/Debug;${BIN_DIR}/../../thirdparty/gzstream/Debug;${BIN_DIR}/../../thirdparty/quazip/Debug;${BIN_DIR}/../../thirdparty/ftgl/Debug;${BIN_DIR}/../../thirdparty/OGDF/Debug;${BIN_DIR}/../../thirdparty/yajl/src/Debug;${QT_BIN_DIR};$ENV{PATH}")
-ELSE(MSVC)
-SET(ENV{PATH} "${BIN_DIR}/../../library/tulip-core/src;${BIN_DIR}/../../library/tulip-gui/src;${BIN_DIR}/../../library/tulip-ogl/src;${BIN_DIR}/../../library/tulip-ogdf/src/;${BIN_DIR}/../../library/tulip-python/src/;${BIN_DIR}/../../thirdparty/gzstream;${BIN_DIR}/../../thirdparty/quazip;${BIN_DIR}/../../thirdparty/ftgl;${BIN_DIR}/../../thirdparty/OGDF;${BIN_DIR}/../../thirdparty/yajl/src;${QT_BIN_DIR};$ENV{PATH}")
-ENDIF(MSVC)
+  SET(ENV{PATH} "${CMAKE_LIBRARY_PATH};$ENV{PATH}")
 ENDIF(WIN32)
-IF(MSVC)
-SET(ENV{PYTHONPATH} "${BIN_DIR}/../../library/tulip-python/bindings/tulip-core/Debug${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/src/Debug${CHAR_SEP}${SRC_DIR}/../../library/tulip-python/modules${CHAR_SEP}${SRC_DIR}/../../library/tulip-python/bindings/tulip-core${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/bindings/stl/Debug${CHAR_SEP}$ENV{PYTHONPATH}")
-ELSE(MSVC)
-SET(ENV{PYTHONPATH} "${BIN_DIR}/../../library/tulip-python/bindings/tulip-core${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/bindings/tulip-ogl${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/bindings/tulip-gui${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/src${CHAR_SEP}${SRC_DIR}/../../library/tulip-python/modules${CHAR_SEP}${SRC_DIR}/../../library/tulip-python/bindings/tulip-core${CHAR_SEP}${BIN_DIR}/../../library/tulip-python/bindings/stl${CHAR_SEP}$ENV{PYTHONPATH}")
-ENDIF(MSVC)
-IF(NOT SYSTEM_SIP)
-IF(MSVC)
-SET(ENV{PYTHONPATH} "${BIN_DIR}/../../thirdparty/sip-${SIP_VERSION}/siplib/Debug${CHAR_SEP}$ENV{PYTHONPATH}")
-ELSE(MSVC)
-SET(ENV{PYTHONPATH} "${BIN_DIR}/../../thirdparty/sip-${SIP_VERSION}/siplib${CHAR_SEP}$ENV{PYTHONPATH}")
-ENDIF(MSVC)
-ENDIF()
+
+# Fix an encoding issue on Mac OS
 IF(APPLE)
-SET(ENV{LC_ALL} "en_EN.UTF-8")
+  SET(ENV{LC_ALL} "en_EN.UTF-8")
 ENDIF(APPLE)
-EXECUTE_PROCESS(COMMAND ${SPHINX_EXECUTABLE} -c ${BIN_DIR} -b html -E -d ${BIN_DIR}/doctrees ${SOURCE_DIR} ${BIN_DIR}/html)
+
+EXECUTE_PROCESS(COMMAND ${SPHINX_EXECUTABLE} -c ${BIN_DIR} -b html -E -d ${BIN_DIR}/doctrees ${SRC_DIR} ${BIN_DIR}/html)
