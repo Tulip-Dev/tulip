4f5b753ffb4ef3cfb14df639d3eaf368d2b10f36
Antoine Lambert <antoine-e.lambert@thalesgroup.com>
2017-04-25 18:49:28 +0000

    don't set a 'Color' group for the Color Mapping plugin as there is already a 'Coloring' entry in the algorithms list in the Tulip GUI
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12252 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/colors/ColorMapping.cpp
+++ plugins/colors/ColorMapping.cpp
@@ -93,9 +93,9 @@ private:
 
 public:
   PLUGININFORMATION("Color Mapping", "Mathiaut", "16/09/2010", "Colorizes the nodes or edges of a graph according to the values of a given property.",
-                    "2.2", "Color")
+                    "2.2", "")
   ColorMapping(const tlp::PluginContext *context)
-      : ColorAlgorithm(context), entryMetric(nullptr), eltTypes(ELT_TYPES), maxInput(std::numeric_limits<double>::quiet_NaN()),
+      : ColorAlgorithm(context), entryMetric(NULL), eltTypes(ELT_TYPES), maxInput(std::numeric_limits<double>::quiet_NaN()),
         minInput(std::numeric_limits<double>::quiet_NaN()), overrideMaxInput(false), overrideMinInput(false) {
     addInParameter<StringCollection>(ELT_TYPE, paramHelp[0], ELT_TYPES, true, "linear <br> uniform <br> enumerated <br> logarithmic");
     addInParameter<PropertyInterface *>("input property", paramHelp[1], "viewMetric");
