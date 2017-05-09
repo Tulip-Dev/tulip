de4b9df374e663b9d971710808dde664978cce54
Patrick Mary <mary@labri.fr>
2017-05-09 10:08:36 +0000

    gui test diff script improvements
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12299 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- unit_test/gui/tlp_diff.sh
+++ unit_test/gui/tlp_diff.sh
@@ -17,20 +17,14 @@ N
 d}' -e '/(property  0 string "viewIcon"/ {
 N
 N
-d}' -e '/(graph_attributes / {
-N
-N
-d}' -e '/(string "file" "/,0d' $1 > tlp_diff_arg1.tlp
+d}' -e '/(graph_attributes /,/^)/d' $1 > tlp_diff_arg1.tlp
 sed -e '/(tlp /,/Tulip.")/d' -e '/(property  0 string "viewFontAwesomeIcon"/ {
 N
 N
 d}' -e '/(property  0 string "viewIcon"/ {
 N
 N
-d}' -e '/(graph_attributes / {
-N
-N
-d}' -e '/(string "file" "/,0d' $2 > tlp_diff_arg2.tlp
+d}' -e '/(graph_attributes /,/^)/d' $2 > tlp_diff_arg2.tlp
 
 # and controller description if needed
 if [ "$TLP_DIFF_CONTROLLER" = "NO" ]; then
