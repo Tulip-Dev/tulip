59796c0ed4611ce44d72eacc44eab7e04f7c2bce
Patrick Mary <mary@labri.fr>
2017-05-09 13:10:08 +0000

    fix for sf bug #857: add ShapeDialog to circumvent a Qt issue when using a too large QComboBox in a QGraphicsScene
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12300 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ library/tulip-gui/include/tulip/ShapeDialog.h
@@ -0,0 +1,43 @@
+#ifndef NODESHAPEDIALOG_H
+#define NODESHAPEDIALOG_H
+
+#include <tulip/tulipconf.h>
+
+#include <QDialog>
+#include <QString>
+#include <QPixmap>
+
+namespace Ui {
+class ShapeDialog;
+}
+
+namespace tlp {
+
+class TLP_QT_SCOPE ShapeDialog : public QDialog {
+
+  Q_OBJECT
+
+  Ui::ShapeDialog *_ui;
+  QString _selectedShapeName;
+  std::list<std::pair<QString, QPixmap> > shapes;
+
+public:
+
+  ShapeDialog(std::list<std::pair<QString, QPixmap> >& nodeShapes,
+		  QWidget *parent = NULL);
+
+  QString getSelectedShapeName() const;
+
+  void setSelectedShapeName(const QString &shapeName);
+
+  void accept();
+
+  void showEvent(QShowEvent *);
+
+protected slots:
+  void updateShapeList();
+};
+
+}
+
+#endif
