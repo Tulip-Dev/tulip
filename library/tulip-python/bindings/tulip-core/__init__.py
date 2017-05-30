d29ea665676031d2faff46751243caa4065360e8
Antoine Lambert <antoine.lambert33@gmail.com>
2017-05-30 19:51:04 +0000

    fix for Python 3.6 on Linux
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12415 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- library/tulip-python/bindings/tulip-core/__init__.py
+++ library/tulip-python/bindings/tulip-core/__init__.py
@@ -140,9 +140,13 @@ _tulipNativePluginsPath = _tulipNativeLibsPath + 'plugins'
 
 # fix loading of Tulip plugins when the tulip module has been installed through pip
 if platform.system() == 'Linux' and os.path.exists(_tulipNativePluginsPath):
-  import DLFCN
   dlOpenFlagsBackup = sys.getdlopenflags()
-  sys.setdlopenflags(DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL)
+  if sys.version_info < (3, 6):
+    import DLFCN
+    dlOpenFlags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
+  else:
+    dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
+  sys.setdlopenflags(dlOpenFlags)
 
 tlp.loadTulipPluginsFromDir(_tulipNativePluginsPath)
 
