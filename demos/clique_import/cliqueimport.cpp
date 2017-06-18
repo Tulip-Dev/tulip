fdee51cd5a9ce5d2ee7b2da26d29dbef0b07f2da
Antoine Lambert <antoine.lambert33@gmail.com>
2017-06-18 19:31:26 +0000

    update Tulip plugins documentation
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12521 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- demos/clique_import/cliqueimport.cpp
+++ demos/clique_import/cliqueimport.cpp
@@ -15,13 +15,7 @@ public:
 PLUGIN(CliqueImport)
 
 //a description of the parameter, that will appear in the GUI to explain how the parameter influences the plugin.
-const char * nodeCountDescription =
-  // number of nodes (equivalent to degree)
-  HTML_HELP_OPEN() \
-  HTML_HELP_DEF( "type", "unsigned int" ) \
-  HTML_HELP_BODY() \
-  "How many nodes the clique will contain" \
-  HTML_HELP_CLOSE();
+const char * nodeCountDescription = "How many nodes the clique will contain";
 
 CliqueImport::CliqueImport(tlp::PluginContext* context):ImportModule(context) {
   //how many nodes we want in our clique
