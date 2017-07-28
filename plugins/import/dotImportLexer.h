d9003660c4ff234ca8f578b557033a0113b0ba43
Antoine Lambert <antoine.lambert33@gmail.com>
2017-07-28 18:15:55 +0000

    fix deprecated register warnings
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12614 f5d4c5f9-c943-4916-8a57-bd8605bf961c

--- plugins/import/dotImportLexer.h
+++ plugins/import/dotImportLexer.h
@@ -766,9 +766,9 @@ extern int yylex (void);
 /** The main scanner function which does all the work.
  */
 YY_DECL {
-  register yy_state_type yy_current_state;
-  register char *yy_cp, *yy_bp;
-  register int yy_act;
+  yy_state_type yy_current_state;
+  char *yy_cp, *yy_bp;
+  int yy_act;
 
 #line 35 "dotImportLexer.l"
 
@@ -815,7 +815,7 @@ YY_DECL {
 yy_match:
 
     do {
-      register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
+      YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
 
       if ( yy_accept[yy_current_state] ) {
         (yy_last_accepting_state) = yy_current_state;
@@ -1203,9 +1203,9 @@ do_action:  /* This label is used only to access EOF actions. */
  *  EOB_ACT_END_OF_FILE - end of file
  */
 static int yy_get_next_buffer (void) {
-  register char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
-  register char *source = (yytext_ptr);
-  register int number_to_move, i;
+  char *dest = YY_CURRENT_BUFFER_LVALUE->yy_ch_buf;
+  char *source = (yytext_ptr);
+  int number_to_move, i;
   int ret_val;
 
   if ( (yy_c_buf_p) > &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[(yy_n_chars) + 1] )
@@ -1330,13 +1330,13 @@ static int yy_get_next_buffer (void) {
 /* yy_get_previous_state - get the state just before the EOB char was reached */
 
 static yy_state_type yy_get_previous_state (void) {
-  register yy_state_type yy_current_state;
-  register char *yy_cp;
+  yy_state_type yy_current_state;
+  char *yy_cp;
 
   yy_current_state = (yy_start);
 
   for ( yy_cp = (yytext_ptr) + YY_MORE_ADJ; yy_cp < (yy_c_buf_p); ++yy_cp ) {
-    register YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
+    YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
 
     if ( yy_accept[yy_current_state] ) {
       (yy_last_accepting_state) = yy_current_state;
@@ -1362,10 +1362,10 @@ static yy_state_type yy_get_previous_state (void) {
  *  next_state = yy_try_NUL_trans( current_state );
  */
 static yy_state_type yy_try_NUL_trans  (yy_state_type yy_current_state ) {
-  register int yy_is_jam;
-  register char *yy_cp = (yy_c_buf_p);
+  int yy_is_jam;
+  char *yy_cp = (yy_c_buf_p);
 
-  register YY_CHAR yy_c = 1;
+  YY_CHAR yy_c = 1;
 
   if ( yy_accept[yy_current_state] ) {
     (yy_last_accepting_state) = yy_current_state;
@@ -1385,9 +1385,9 @@ static yy_state_type yy_try_NUL_trans  (yy_state_type yy_current_state ) {
   return yy_is_jam ? 0 : yy_current_state;
 }
 
-//    static void yyunput (int c, register char * yy_bp )
+//    static void yyunput (int c, char * yy_bp )
 //{
-//  register char *yy_cp;
+//  char *yy_cp;
 //
 //    yy_cp = (yy_c_buf_p);
 //
@@ -1397,10 +1397,10 @@ static yy_state_type yy_try_NUL_trans  (yy_state_type yy_current_state ) {
 //  if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
 //    { /* need to shift things up to make room */
 //    /* +2 for EOB chars. */
-//    register int number_to_move = (yy_n_chars) + 2;
-//    register char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
+//    int number_to_move = (yy_n_chars) + 2;
+//    char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
 //          YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
-//    register char *source =
+//    char *source =
 //        &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move];
 //
 //    while ( source > YY_CURRENT_BUFFER_LVALUE->yy_ch_buf )
@@ -1993,7 +1993,7 @@ static void yy_fatal_error (yyconst char* msg ) {
 
 #ifndef yytext_ptr
 static void yy_flex_strncpy (char* s1, yyconst char * s2, int n ) {
-  register int i;
+  int i;
 
   for ( i = 0; i < n; ++i )
     s1[i] = s2[i];
@@ -2002,7 +2002,7 @@ static void yy_flex_strncpy (char* s1, yyconst char * s2, int n ) {
 
 #ifdef YY_NEED_STRLEN
 static int yy_flex_strlen (yyconst char * s ) {
-  register int n;
+  int n;
 
   for ( n = 0; s[n]; ++n )
     ;
--- plugins/import/dotImportParser.h
+++ plugins/import/dotImportParser.h
@@ -785,7 +785,7 @@ union yyalloc {
 #   define YYCOPY(To, From, Count)    \
       do          \
   {         \
-    register YYSIZE_T yyi;    \
+    YYSIZE_T yyi;    \
     for (yyi = 0; yyi < (Count); yyi++) \
       (To)[yyi] = (From)[yyi];    \
   }         \
@@ -1298,7 +1298,7 @@ yystrlen (yystr)
 const char *yystr;
 #   endif
 {
-  register const char *yys = yystr;
+  const char *yys = yystr;
 
   while (*yys++ != '\0')
     continue;
@@ -1323,8 +1323,8 @@ char *yydest;
 const char *yysrc;
 #   endif
 {
-  register char *yyd = yydest;
-  register const char *yys = yysrc;
+  char *yyd = yydest;
+  const char *yys = yysrc;
 
   while ((*yyd++ = *yys++) != '\0')
     continue;
@@ -1462,8 +1462,8 @@ yyparse ()
 #endif
 {
 
-  register int yystate;
-  register int yyn;
+  int yystate;
+  int yyn;
   int yyresult;
   /* Number of tokens to shift before error messages enabled.  */
   int yyerrstatus;
@@ -1481,12 +1481,12 @@ yyparse ()
   /* The state stack.  */
   short int yyssa[YYINITDEPTH];
   short int *yyss = yyssa;
-  register short int *yyssp;
+  short int *yyssp;
 
   /* The semantic value stack.  */
   YYSTYPE yyvsa[YYINITDEPTH];
   YYSTYPE *yyvs = yyvsa;
-  register YYSTYPE *yyvsp;
+  YYSTYPE *yyvsp;
 
 
 
