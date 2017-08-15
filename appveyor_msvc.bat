fe20d3fb0f2bc075376e652957cf1241b9f92b1a
Antoine Lambert <antoine.lambert33@gmail.com>
2017-08-15 15:06:54 +0000

    - towards moving to Github in a near future: add cross-platform continuous integration in the cloud setup for Tulip through TravisCI (Linux and MacOS) but also AppVeyor (Windows). When pushing changes to source code, Tulip will be built on every supported platforms and its tests will be run afterwards. - it enables contributors to develop and test new features safely by using the almighty git workflow (create branch, develop your feature, test that it will not break the build thanks to TravisCI and AppVeyor, merge to master) - for Linux and MacOS builds with Travis, Tulip is compiled using a legacy setup (default system compiler, C++98, Qt4, Python 2) but also a modern setup (recent compiler, C++11, Qt5, Python 3) - for Windows builds with AppVeyor, Tulip is compiled with GCC provided by the MSYS2 project but also with Microsoft Visual Studio 2015 (recent compilers, C++11, Qt5, Python 3) - all compilation jobs benefit from persistent build cache (generated with ccache or clcache) to speedup incremental builds - see it in action on the complete Github mirror of Tulip I am currently working on (https://github.com/anlambert/tulip)
    git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@12668 f5d4c5f9-c943-4916-8a57-bd8605bf961c

---
+++ appveyor_msvc.bat
@@ -0,0 +1,73 @@
+rem Matrix-driven Appveyor CI script for Tulip using Visual Studio compiler
+
+rem let's compile clcache in order to speedup incremental builds
+cd C:/
+git clone https://github.com/frerich/clcache.git
+set PATH=C:/Python35-x64;C:/Python35-x64/Scripts;%PATH%
+pip install pyinstaller --upgrade
+cd clcache
+pyinstaller clcache.py
+set PATH=C:/clcache/dist/clcache;%PATH%
+
+rem create a directory to store Tulip dependencies
+cd C:/ && md tulip_dependencies 
+
+rem get, compile and install freetype
+cd C:/tulip_dependencies
+curl -LO http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz
+7z x freetype-2.8.tar.gz -so | 7z x -aoa -si -ttar
+cd freetype-2.8
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
+msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+
+rem get, compile and install zlib
+cd C:/tulip_dependencies
+curl -LO https://zlib.net/zlib1211.zip
+7z x zlib1211.zip
+cd zlib-1.2.11
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
+msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+
+rem get, compile and install libpng
+cd C:/tulip_dependencies
+curl -LO http://prdownloads.sourceforge.net/libpng/libpng-1.6.30.tar.gz
+7z x libpng-1.6.30.tar.gz -so | 7z x -aoa -si -ttar
+cd libpng-1.6.30
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INCLUDE_PATH="C:/tulip_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/tulip_dependencies/lib" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
+msbuild INSTALL.vcxproj /clp:ErrorsOnly /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+
+rem get, compile and install glew
+cd C:/tulip_dependencies
+curl -LO https://downloads.sourceforge.net/project/glew/glew/2.1.0/glew-2.1.0.zip
+7z x glew-2.1.0.zip
+cd glew-2.1.0/build/cmake
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" .
+msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+
+rem get, compile and install libjpeg
+cd C:/tulip_dependencies
+curl -LO https://downloads.sourceforge.net/project/libjpeg-turbo/1.5.2/libjpeg-turbo-1.5.2.tar.gz
+7z x libjpeg-turbo-1.5.2.tar.gz -so | 7z x -aoa -si -ttar
+cd libjpeg-turbo-1.5.2
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DWITH_SIMD=OFF -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
+msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+
+rem get, compile and install cppunit
+cd C:/tulip_dependencies
+git clone git://anongit.freedesktop.org/git/libreoffice/cppunit/
+cd cppunit/src
+msbuild CppUnitLibraries2010.sln /m /clp:ErrorsOnly /p:Configuration=Release /p:Platform=%MSVC_PLATFORM% /p:PlatformToolset=%MSVC_PLATFORM_TOOLSET% /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+xcopy C:\tulip_dependencies\cppunit\include C:\tulip_dependencies\include /S /Y
+xcopy C:\tulip_dependencies\cppunit\lib C:\tulip_dependencies\lib /S /Y
+move C:\tulip_dependencies\lib\cppunit_dll.dll C:\tulip_dependencies\bin\
+
+rem we are good to go, let's compile Tulip now
+cd %APPVEYOR_BUILD_FOLDER%
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INCLUDE_PATH="C:/tulip_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/tulip_dependencies/lib;C:/tulip_dependencies/bin" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DTULIP_USE_QT5=ON -DPYTHON_EXECUTABLE="%PYTHON_EXECUTABLE%" -DTULIP_BUILD_TESTS=ON ..
+msbuild tulip.sln /m /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache
+msbuild runTests.vcxproj /p:Configuration=Release /p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\clcache\dist\clcache         
---
+++ appveyor_msys2.bat
@@ -0,0 +1,45 @@
+rem original script taken here: https://github.com/mypaint/libmypaint/blob/master/appveyor.bat
+rem Matrix-driven Appveyor CI script for Tulip using GCC compiler provided by MSYS2
+rem https://www.appveyor.com/docs/installed-software#mingw-msys-cygwin
+rem Needs the following vars:
+rem    MSYS2_ARCH:  x86_64 or i686
+rem    MSYSTEM:  MINGW64 or MINGW32
+
+rem Set the paths appropriately
+PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
+
+rem Upgrade the MSYS2 platform
+bash -lc "pacman --noconfirm --sync --refresh --refresh pacman"
+bash -lc "pacman --noconfirm --sync --refresh --refresh --sysupgrade --sysupgrade"
+
+rem Install required tools
+bash -lc "pacman --noconfirm -S --needed base-devel mingw-w64-%MSYS2_ARCH%-toolchain"
+
+rem Install the relevant native dependencies
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-yajl"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-qhull"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-freetype"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-glew"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-libpng"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-qt5"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-quazip"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-qtwebkit"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-python3"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-cppunit"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-cmake"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-ccache"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-ninja"
+
+rem Invoke subsequent bash in the build tree
+cd %APPVEYOR_BUILD_FOLDER%
+set CHERE_INVOKING=yes
+
+rem Build/test scripting
+bash -lc "set pwd"
+bash -lc "env"
+
+rem Build Tulip and run its unit tests
+bash -lc "mkdir build"
+bash -lc "cd build && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DTULIP_BUILD_TESTS=ON -DTULIP_USE_QT5=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=C:/msys64/%MSYSTEM%/bin/python3.exe .."
+bash -lc "cd build && ninja -j4"
+bash -lc "cd build && ninja runTests"
