922e7da8c69bbd4bc99e018e942937a8ae741baa
Antoine Lambert <antoine.lambert@inria.fr>
2017-11-02 22:46:18 +0100

    update Tulip CI on Travis and AppVeyor :
      send build status notifications (when build fails or succeeds after a failed one) to tulipdev@labri.fr
    
      now generate Linux AppImages, Windows installers and MacOS bundles
    
      add travis job for generating Tulip AppImage for 32 bits processor architecture
    
      fix mac bundle generation script in order to be able to use the Qt binaries provided by MacPorts
    
      generate mac bundles using qt4 but also qt5
    
      generate windows installers for Python 2.7 and Python 3.6
    
      last but not least: add automatic deployment of produced Tulip binaries to Bintray
      (see https://bintray.com/tulip-dev/tulip_binaries/Tulip), meaning Tulip installers
      are now generated and available for testing each time new commits are pushed to the
      master branch

--- appveyor_msys2.bat
+++ appveyor_msys2.bat
@@ -12,7 +12,7 @@ rem Upgrade the MSYS2 platform
 bash -lc "pacman --noconfirm --sync --refresh --sysupgrade"
 
 rem Install required tools
-bash -lc "pacman --noconfirm -S --needed base-devel mingw-w64-%MSYS2_ARCH%-toolchain"
+bash -lc "pacman --noconfirm -S --needed base-devel wget unzip mingw-w64-%MSYS2_ARCH%-toolchain"
 
 rem Install the relevant native dependencies
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-yajl"
@@ -24,11 +24,10 @@ bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-pcre2"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-qt5"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-quazip"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-qtwebkit"
-bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-python3"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-cppunit"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-cmake"
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-ccache"
-bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-ninja"
+bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-doxygen"
 
 rem Invoke subsequent bash in the build tree
 cd %APPVEYOR_BUILD_FOLDER%
@@ -38,8 +37,28 @@ rem Build/test scripting
 bash -lc "set pwd"
 bash -lc "env"
 
-rem Build Tulip and run its unit tests
+rem Install Inetc plugin for NSIS
+bash -lc "wget http://nsis.sourceforge.net/mediawiki/images/c/c9/Inetc.zip"
+bash -lc "unzip Inetc.zip -d \"C:/Program Files (x86)/NSIS\""
+
+rem Install sphinx for Python 3
+set PATH=%PYTHON3_HOME%;%PYTHON3_HOME%/Scripts;%PATH%
+pip install sphinx
+
+rem Build Tulip with Python 3, run its unit tests and package it
 bash -lc "mkdir build"
-bash -lc "cd build && cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=C:/msys64/%MSYSTEM% -DTULIP_BUILD_TESTS=ON -DTULIP_USE_QT5=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=C:/msys64/%MSYSTEM%/bin/python3.exe -DTULIP_FIXUP_BUNDLE=OFF -DTULIP_PYTHON_SITE_INSTALL=ON .."
-bash -lc "cd build && ninja -j4 install"
-bash -lc "cd build && ninja runTests"
+bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_TESTS=ON -DTULIP_USE_QT5=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON3_HOME%/python.exe .."
+bash -lc "cd build && make -j4 install"
+bash -lc "cd build && make runTests"
+bash -lc "cd build && make bundle"
+
+rem Install sphinx for Python 2
+set PATH=%PYTHON2_HOME%;%PYTHON2_HOME%/Scripts;%PATH%
+pip install sphinx
+
+rem Build Tulip with Python 2, run its unit tests and package it
+bash -lc "mkdir build"
+bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_TESTS=ON -DTULIP_USE_QT5=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON2_HOME%/python.exe .."
+bash -lc "cd build && make -j4 install"
+bash -lc "cd build && make runTests"
+bash -lc "cd build && make bundle"
--- bundlers/linux/tulip_appimage_centos6_build.sh
+++ bundlers/linux/tulip_appimage_centos6_build.sh
@@ -5,10 +5,11 @@ cd
 # clean packages cache
 yum -y clean all
 yum -y clean expire-cache
+yum -y update
 
 # install base build system
 yum -y install epel-release
-yum -y install rpm-build gcc gcc-c++ cmake git subversion tar gzip make wget ccache
+yum -y install xz gcc gcc-c++ cmake git subversion tar gzip make wget ccache
 
 # install tulip deps
 yum -y install zlib-devel qhull-devel
@@ -33,13 +34,15 @@ yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel r
 wget http://python.org/ftp/python/2.7.14/Python-2.7.14.tar.xz
 tar xf Python-2.7.14.tar.xz
 cd Python-2.7.14
-./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations
+./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations CC="ccache gcc" CXX="ccache g++"
 make -j4 && make altinstall
 
 # some extra setup for cmake et appimage build to succeed
-cp -r /usr/lib/libpython2.7* /usr/lib64/
-cp -r /usr/lib/python2.7 /usr/lib64/
-export PATH=/usr/lib64/qt48/bin/:$PATH
+if [ -d /usr/lib64 ]; then
+  cp -r /usr/lib/libpython2.7* /usr/lib64/
+  cp -r /usr/lib/python2.7 /usr/lib64/
+fi
+export PATH=/usr/lib64/qt48/bin/:/usr/lib/qt48/bin/:$PATH
 
 # install pip in order to grab the latest sphinx version
 wget https://bootstrap.pypa.io/get-pip.py
@@ -47,16 +50,17 @@ python2.7 get-pip.py
 pip2.7 install sphinx
 
 # build and install tulip
-cd && mkdir tulip_build && cd tulip_build
-cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_INSTALL_PREFIX=$PWD/install -DTULIP_USE_CCACHE=ON /tulip
+mkdir /tulip/build
+cd /tulip/build
+cmake -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_INSTALL_PREFIX=$PWD/install -DTULIP_USE_CCACHE=ON ..
 make -j4 install
 
 # build a bundle dir suitable for AppImageKit
 sh bundlers/linux/make_appimage_bundle.sh --appdir $PWD
 
 # get appimagetool
-wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
-chmod a+x appimagetool-x86_64.AppImage
+wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-$(uname -p).AppImage"
+chmod a+x appimagetool-$(uname -p).AppImage
 
 # finally build the portable app
-./appimagetool-x86_64.AppImage Tulip.AppDir Tulip-$(install/bin/tulip-config --version)
+./appimagetool-$(uname -p).AppImage Tulip.AppDir Tulip-$(install/bin/tulip-config --version)-$(uname -p).AppImage
