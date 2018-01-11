6b4c3e69b4b4a4e6fe90304be9d45e35f1de8c0f
p-mary <mary@labri.fr>
2018-01-11 14:12:52 +0100

    add gui tests of bubble layouts for non connected graphs

---
+++ tests/gui/run_layout_bubble_pack_not_connected_test.sh
@@ -0,0 +1,2 @@
+# force mono processor execution
+taskset -c 0 sh tulip_run_test.sh layout_bubble_pack_not_connected import_pajek_file.tlp
---
+++ tests/gui/run_layout_bubble_tree_not_connected_test.sh
@@ -0,0 +1,1 @@
+sh tulip_run_test.sh layout_bubble_tree_not_connected import_pajek_file.tlp
