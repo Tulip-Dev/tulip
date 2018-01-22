Tulip-Dev/tulip

commit da87cbbe2094569ce9bc753851d359c82db5728f

Some improvements to [Node|Edge]StaticProperty
  - add operators [] taking tlp::node/tlp::edge as argument and use them in
    the whole Tulip codebase (this should have been implemented this way as
    it enables to encapsulate the indices mapping between the nodes/edges
    and the underlying std::vector)

  - leverage the use of C++11 for range-based loops when filling these type
    of properties (except when OpenMP is used) as it makes the code more
    pleasant to read

plugins/layout/OGDF/OGDFFm3.cpp
@@ -498,11 +498,9 @@ void OGDFFm3::callOGDFLayoutAlgorithm(ogdf::GraphAttributes &gAttributes) {
 
   if (dataSet->get("Edge Length Property", length) && length) {
     EdgeArray<double> edgeLength(tlpToOGDF->getOGDFGraph());
-    const std::vector<tlp::edge> &edges = graph->edges();
-    unsigned int nbEdges = edges.size();
 
-    for (unsigned int i = 0; i < nbEdges; ++i) {
-      edgeLength[tlpToOGDF->getOGDFGraphEdge(i)] = length->getEdgeDoubleValue(edges[i]);
+    for (const tlp::edge &e : graph->edges()) {
+      edgeLength[tlpToOGDF->getOGDFGraphEdge(e)] = length->getEdgeDoubleValue(e);
     }
 
     fmmm->call(gAttributes, edgeLength);

library/tulip-core/include/tulip/GraphMeasure.h
@@ -122,7 +122,7 @@ TLP_SCOPE _DEPRECATED unsigned int maxDistance(const Graph *graph, const node n,
  * and INV_DIRECTED use reverse directed graph (ie. all edges are reversed)
  * all the edge's weight is set to 1. (it uses a bfs thus the complexity is o(m), m = |E|).
  */
-TLP_SCOPE unsigned int maxDistance(const Graph *graph, const unsigned int nPos,
+TLP_SCOPE unsigned int maxDistance(const Graph *graph, const node n,
                                    tlp::NodeStaticProperty<unsigned int> &distance,
                                    EDGE_TYPE direction = UNDIRECTED);
 /*

library/tulip-core/include/tulip/StaticProperty.h
@@ -22,6 +22,7 @@
 #ifdef _OPENMP
 #include <omp.h>
 #endif
+
 #include <tulip/Graph.h>
 
 namespace tlp {
@@ -39,22 +40,31 @@ public:
 
   // get the stored value of a node
   inline typename std::vector<TYPE>::const_reference getNodeValue(node n) const {
-    return (*this)[graph->nodePos(n)];
+    return std::vector<TYPE>::operator[](graph->nodePos(n));
   }
 
   // set the stored value of a node
-  inline typename std::vector<TYPE>::const_reference setNodeValue(node n, TYPE val) {
-    return (*this)[graph->nodePos(n)] = val;
+  inline void setNodeValue(node n, TYPE val) {
+    std::vector<TYPE>::operator[](graph->nodePos(n)) = val;
+  }
+
+  inline typename std::vector<TYPE>::const_reference operator[](node n) const {
+    return std::vector<TYPE>::operator[][graph->nodePos(n)];
+  }
+
+  inline typename std::vector<TYPE>::reference operator[](node n) {
+    return std::vector<TYPE>::operator[](graph->nodePos(n));
   }
 
   // set all to same values
   void setAll(const TYPE &val) {
+    const std::vector<node> &nodes = graph->nodes();
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-
-    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(this->size()); ++i)
-      (*this)[i] = val;
+    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(graph->numberOfNodes()); ++i) {
+      (*this)[nodes[i]] = val;
+    }
   }
 
   // add a value for a newly created node
@@ -71,23 +81,19 @@ public:
   template <typename PROP_PTR>
   void copyFromProperty(PROP_PTR prop) {
     const std::vector<node> &nodes = graph->nodes();
-    unsigned int nbNodes = nodes.size();
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-
-    for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-      (*this)[i] = prop->getNodeValue(nodes[i]);
+    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(graph->numberOfNodes()); ++i) {
+      (*this)[nodes[i]] = prop->getNodeValue(nodes[i]);
+    }
   }
 
   // copy values into a typed typed instance of PropertyInterface
   template <typename PROP_PTR>
   void copyToProperty(PROP_PTR prop) {
-    const std::vector<node> &nodes = graph->nodes();
-    unsigned int nbNodes = nodes.size();
-
-    for (unsigned int i = 0; i < nbNodes; ++i)
-      prop->setNodeValue(nodes[i], (*this)[i]);
+    for (const node &n : graph->nodes())
+      prop->setNodeValue(n, (*this)[n]);
   }
 };
 
@@ -96,11 +102,8 @@ public:
 template <>
 template <typename PROP_PTR>
 void NodeStaticProperty<bool>::copyFromProperty(PROP_PTR prop) {
-  const std::vector<node> &nodes = graph->nodes();
-  unsigned int nbNodes = nodes.size();
-
-  for (unsigned int i = 0; i < nbNodes; ++i)
-    (*this)[i] = prop->getNodeValue(nodes[i]);
+  for (const node &n : graph->nodes())
+    (*this)[n] = prop->getNodeValue(n);
 }
 
 template <typename TYPE>
@@ -117,21 +120,30 @@ public:
 
   // get the stored value of a edge
   inline typename std::vector<TYPE>::const_reference getEdgeValue(edge n) const {
-    return (*this)[graph->edgePos(n)];
+    return std::vector<TYPE>::operator[](graph->edgePos(n));
   }
 
   // set the stored value of a edge
-  inline typename std::vector<TYPE>::const_reference setEdgeValue(edge n, TYPE val) {
-    return (*this)[graph->edgePos(n)] = val;
+  inline void setEdgeValue(edge n, TYPE val) {
+    std::vector<TYPE>::operator[](graph->edgePos(n)) = val;
+  }
+
+  inline typename std::vector<TYPE>::const_reference operator[](edge e) const {
+    return std::vector<TYPE>::operator[](graph->edgePos(e));
+  }
+
+  inline typename std::vector<TYPE>::reference operator[](edge e) {
+    return std::vector<TYPE>::operator[](graph->edgePos(e));
   }
 
   void setAll(const TYPE &val) {
+    const std::vector<edge> &edges = graph->edges();
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-
-    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(this->size()); ++i)
-      (*this)[i] = val;
+    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(graph->numberOfEdges()); ++i) {
+      (*this)[edges[i]] = val;
+    }
   }
 
   // add a value for a newly created edge
@@ -148,23 +160,19 @@ public:
   template <typename PROP_PTR>
   void copyFromProperty(PROP_PTR prop) {
     const std::vector<edge> &edges = graph->edges();
-    unsigned int nbEdges = edges.size();
 #ifdef _OPENMP
 #pragma omp parallel for
 #endif
-
-    for (OMP_ITER_TYPE i = 0; i < nbEdges; ++i)
-      (*this)[i] = prop->getEdgeValue(edges[i]);
+    for (OMP_ITER_TYPE i = 0; i < static_cast<OMP_ITER_TYPE>(graph->numberOfEdges()); ++i) {
+      (*this)[edges[i]] = prop->getNodeValue(edges[i]);
+    }
   }
 
   // copy values into a typed typed instance of PropertyInterface
   template <typename PROP_PTR>
   void copyToProperty(PROP_PTR prop) {
-    const std::vector<edge> &edges = graph->edges();
-    unsigned int nbEdges = edges.size();
-
-    for (unsigned int i = 0; i < nbEdges; ++i)
-      prop->setEdgeValue(edges[i], (*this)[i]);
+    for (const edge &e : graph->edges())
+      prop->setEdgeValue(e, (*this)[e]);
   }
 };
 
@@ -173,11 +181,8 @@ public:
 template <>
 template <typename PROP_PTR>
 void EdgeStaticProperty<bool>::copyFromProperty(PROP_PTR prop) {
-  const std::vector<edge> &edges = graph->edges();
-  unsigned int nbEdges = edges.size();
-
-  for (unsigned int i = 0; i < nbEdges; ++i)
-    (*this)[i] = prop->getEdgeValue(edges[i]);
+  for (const edge &e : graph->edges())
+    (*this)[e] = prop->getEdgeValue(e);
 }
 }
 

library/tulip-core/src/CMakeLists.txt
@@ -73,6 +73,7 @@ TLPBExport.cpp
 TLPBImport.cpp
 TulipFontAwesome.cpp
 TulipMaterialDesignIcons.cpp
+StaticProperty.cpp
 )
 
 # disable GCC var tracking on that file as it eats too much compile time

library/tulip-core/src/ConnectedTest.cpp
@@ -76,39 +76,35 @@ void ConnectedTest::computeConnectedComponents(const tlp::Graph *graph,
                                                vector<vector<node>> &components) {
   NodeStaticProperty<bool> visited(graph);
   visited.setAll(false);
-  // do a bfs traversal for each node
-  const std::vector<node> &nodes = graph->nodes();
-  unsigned int nbNodes = nodes.size();
 
-  for (unsigned int i = 0; i < nbNodes; ++i) {
-    node curNode = nodes[i];
+  // do a bfs traversal for each node
+  for (const node &n : graph->nodes()) {
 
     // check if curNode has been already visited
-    if (!visited[i]) {
+    if (!visited[n]) {
       // add a new component
       components.push_back(std::vector<node>());
       std::vector<node> &component = components.back();
       // and initialize it with current node
-      component.push_back(curNode);
+      component.push_back(n);
       // do a bfs traversal this node
       list<node> nodesToVisit;
-      visited[i] = true;
-      nodesToVisit.push_front(curNode);
+      visited[n] = true;
+      nodesToVisit.push_front(n);
 
       while (!nodesToVisit.empty()) {
-        curNode = nodesToVisit.front();
+        node curNode = nodesToVisit.front();
         nodesToVisit.pop_front();
         // loop on all neighbours
         Iterator<node> *itn = graph->getInOutNodes(curNode);
 
         while (itn->hasNext()) {
           node neighbour = itn->next();
-          unsigned int nPos = graph->nodePos(neighbour);
 
           // check if neighbour has been visited
-          if (!visited[nPos]) {
+          if (!visited[neighbour]) {
             // mark neighbour as already visited
-            visited[nPos] = true;
+            visited[neighbour] = true;
             // insert it in current component
             component.push_back(neighbour);
             // push it for further deeper exploration
@@ -145,7 +141,7 @@ void ConnectedTest::computeConnectedComponents(const tlp::Graph *graph,
 static unsigned int connectedTest(const Graph *const graph, node n,
                                   NodeStaticProperty<bool> &visited) {
   list<node> nodesToVisit;
-  visited.setNodeValue(n, true);
+  visited[n] = true;
   nodesToVisit.push_front(n);
   unsigned int count = 1;
 
@@ -156,15 +152,14 @@ static unsigned int connectedTest(const Graph *const graph, node n,
     Iterator<node> *itn = graph->getInOutNodes(r);
 
     while (itn->hasNext()) {
-      n = itn->next();
-      unsigned int nPos = graph->nodePos(n);
+      node neighbour = itn->next();
 
       // check if neighbour has been visited
-      if (!visited[nPos]) {
+      if (!visited[neighbour]) {
         // mark neighbour as already visited
-        visited[nPos] = true;
+        visited[neighbour] = true;
         // push it for further deeper exploration
-        nodesToVisit.push_back(n);
+        nodesToVisit.push_back(neighbour);
         ++count;
       }
     }
@@ -199,19 +194,15 @@ void ConnectedTest::connect(const tlp::Graph *const graph, vector<node> &toLink)
       return;
   }
 
-  const std::vector<node> &nodes = graph->nodes();
-  unsigned int nbNodes = nodes.size();
-
-  if (nbNodes == 0)
+  if (graph->numberOfNodes() == 0)
     return;
 
   NodeStaticProperty<bool> visited(graph);
   visited.setAll(false);
 
-  for (unsigned int i = 0; i < nbNodes; ++i) {
-    node n = nodes[i];
+  for (const node &n : graph->nodes()) {
 
-    if (!visited[i]) {
+    if (!visited[n]) {
       toLink.push_back(n);
       connectedTest(graph, n, visited);
     }

library/tulip-core/src/GraphMeasure.cpp
@@ -53,42 +53,40 @@ inline Iterator<node> *getIt(const Graph *graph, node n, EDGE_TYPE direction) {
 unsigned int tlp::maxDistance(const Graph *graph, const node n,
                               MutableContainer<unsigned int> &distance, EDGE_TYPE direction) {
   distance.setAll(UINT_MAX);
-  unsigned int nbNodes = graph->numberOfNodes();
-  tlp::NodeStaticProperty<unsigned int> dist(graph);
-  unsigned int result = tlp::maxDistance(graph, graph->nodePos(n), dist, direction);
-  const std::vector<node> &nodes = graph->nodes();
 
-  for (unsigned int i = 0; i < nbNodes; ++i)
-    distance.set(nodes[i], dist[i]);
+  tlp::NodeStaticProperty<unsigned int> dist(graph);
+  unsigned int result = tlp::maxDistance(graph, n, dist, direction);
+
+  for (const node &n : graph->nodes())
+    distance.set(n, dist[n]);
 
   return result;
 }
 //================================================================
-unsigned int tlp::maxDistance(const Graph *graph, unsigned int nPos,
+unsigned int tlp::maxDistance(const Graph *graph, const node n,
                               tlp::NodeStaticProperty<unsigned int> &distance,
                               EDGE_TYPE direction) {
-  deque<unsigned int> fifo;
+  deque<node> fifo;
   distance.setAll(UINT_MAX);
-  fifo.push_back(nPos);
-  distance[nPos] = 0;
-  const std::vector<node> &nodes = graph->nodes();
+  fifo.push_back(n);
+  distance[n] = 0;
+
   unsigned int maxDist = 0;
 
   while (!fifo.empty()) {
-    unsigned int current = fifo.front();
+    node current = fifo.front();
     fifo.pop_front();
     unsigned int nDist = distance[current] + 1;
 
     Iterator<node> *itn;
-    itn = getIt(graph, nodes[current], direction);
+    itn = getIt(graph, current, direction);
 
     while (itn->hasNext()) {
-      unsigned int n = graph->nodePos(itn->next());
-
-      if (distance[n] == UINT_MAX) {
-        fifo.push_back(n);
-        distance[n] = nDist;
-        maxDist = std::max(maxDist, distance[n] = nDist);
+      node u = itn->next();
+      if (distance[u] == UINT_MAX) {
+        fifo.push_back(u);
+        distance[u] = nDist;
+        maxDist = std::max(maxDist, nDist);
       }
     }
 
@@ -102,6 +100,7 @@ unsigned int tlp::maxDistance(const Graph *graph, unsigned int nPos,
 double tlp::averagePathLength(const Graph *graph, PluginProgress *pluginProgress) {
   double result = 0;
 
+  const vector<node> &nodes = graph->nodes();
   size_t nbNodes = graph->numberOfNodes();
 
   if (nbNodes < 2)
@@ -139,14 +138,15 @@ double tlp::averagePathLength(const Graph *graph, PluginProgress *pluginProgress
     }
 
 #endif
+
     tlp::NodeStaticProperty<unsigned int> distance(graph);
-    maxDistance(graph, i, distance, UNDIRECTED);
+    maxDistance(graph, nodes[i], distance, UNDIRECTED);
 
     for (size_t j = 0; j < nbNodes; ++j) {
       if (j == i)
         continue;
 
-      unsigned int d = distance[j];
+      unsigned int d = distance[nodes[j]];
 
       if (d != UINT_MAX) {
 #ifdef _OPENMP
@@ -167,14 +167,13 @@ double tlp::averagePathLength(const Graph *graph, PluginProgress *pluginProgress
 //================================================================
 double tlp::averageClusteringCoefficient(const Graph *graph, PluginProgress *pluginProgress) {
   tlp::NodeStaticProperty<double> clusters(graph);
-  unsigned int nbNodes = graph->numberOfNodes();
   tlp::clusteringCoefficient(graph, clusters, UINT_MAX, pluginProgress);
   double sum = 0;
 
-  for (unsigned int i = 0; i < clusters.size(); ++i)
-    sum += clusters[i];
+  for (const node &n : graph->nodes())
+    sum += clusters[n];
 
-  return sum / nbNodes;
+  return sum / graph->numberOfNodes();
 }
 //================================================================
 unsigned int tlp::maxDegree(const Graph *graph) {
@@ -252,20 +251,16 @@ void tlp::clusteringCoefficient(const Graph *graph, MutableContainer<double> &cl
                                 unsigned int maxDepth, PluginProgress *pp) {
   tlp::NodeStaticProperty<double> vClusters(graph);
   tlp::clusteringCoefficient(graph, vClusters, maxDepth, pp);
-  const std::vector<node> &nodes = graph->nodes();
-
-  for (unsigned int i = 0; i < nodes.size(); ++i)
-    clusters.set(nodes[i].id, vClusters[i]);
+  for (const node &n : graph->nodes())
+    clusters.set(n, vClusters[n]);
 }
 //=================================================
 void tlp::clusteringCoefficient(const Graph *graph, tlp::NodeStaticProperty<double> &clusters,
                                 unsigned int maxDepth, PluginProgress *) {
-  const std::vector<node> &nodes = graph->nodes();
-  unsigned int nbNodes = nodes.size();
 
-  for (unsigned int i = 0; i < nbNodes; i++) {
+  for (const node &n : graph->nodes()) {
     TLP_HASH_MAP<node, bool> reachables;
-    markReachableNodes(graph, nodes[i], reachables, maxDepth);
+    markReachableNodes(graph, n, reachables, maxDepth);
     double nbEdge = 0; // e(N_v)*2$
 
     TLP_HASH_MAP<node, bool>::const_iterator itr = reachables.begin();
@@ -291,40 +286,34 @@ void tlp::clusteringCoefficient(const Graph *graph, tlp::NodeStaticProperty<doub
 
     if (reachables.size() > 1) {
       //$e(N_v)/(\frac{k*(k-1)}{2}}$
-      clusters[i] = nbEdge / (nNode * (nNode - 1));
+      clusters[n] = nbEdge / (nNode * (nNode - 1));
     } else
-      clusters[i] = 0;
+      clusters[n] = 0;
   }
 }
 //==================================================
 void tlp::dagLevel(const Graph *graph, MutableContainer<unsigned int> &level, PluginProgress *pp) {
   tlp::NodeStaticProperty<unsigned int> tmp(graph);
   dagLevel(graph, tmp, pp);
-  unsigned int nbNodes = graph->numberOfNodes();
-  const std::vector<node> &nodes = graph->nodes();
-
-  for (unsigned int i = 0; i < nbNodes; ++i)
-    level.set(nodes[i].id, tmp[i]);
+  for (const node &n : graph->nodes())
+    level.set(n, tmp[n]);
 }
 
 //==================================================
 void tlp::dagLevel(const Graph *graph, tlp::NodeStaticProperty<unsigned int> &level,
                    PluginProgress *) {
-  unsigned int nbNodes = graph->numberOfNodes();
-  const vector<node> &nodes = graph->nodes();
   tlp::NodeStaticProperty<unsigned int> totreat(graph);
   deque<node> fifo;
 
   //===============================================
-  for (unsigned int i = 0; i < nbNodes; ++i) {
-    node n = nodes[i];
+  for (const node &n : graph->nodes()) {
     unsigned int indegree = graph->indeg(n);
 
     if (indegree == 0) {
       fifo.push_back(n);
-      level[i] = 0;
+      level[n] = 0;
     } else
-      totreat[i] = indegree - 1;
+      totreat[n] = indegree - 1;
   }
 
   //==============================================
@@ -334,13 +323,12 @@ void tlp::dagLevel(const Graph *graph, tlp::NodeStaticProperty<unsigned int> &le
     node child;
     unsigned int curLevel = level.getNodeValue(current) + 1;
     forEach (child, graph->getOutNodes(current)) {
-      unsigned int childPos = graph->nodePos(child);
-      unsigned int childLevel = totreat[childPos];
+      unsigned int childLevel = totreat[child];
 
       if (childLevel > 0)
-        totreat[childPos] = childLevel - 1;
+        totreat[child] = childLevel - 1;
       else {
-        level[childPos] = curLevel;
+        level[child] = curLevel;
         fifo.push_back(child);
       }
     }
@@ -361,7 +349,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = graph->deg(nodes[i]);
+          deg[nodes[i]] = graph->deg(nodes[i]);
 
         break;
 
@@ -370,7 +358,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = graph->indeg(nodes[i]);
+          deg[nodes[i]] = graph->indeg(nodes[i]);
 
         break;
 
@@ -379,7 +367,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = graph->outdeg(nodes[i]);
+          deg[nodes[i]] = graph->outdeg(nodes[i]);
 
         break;
       }
@@ -395,7 +383,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = normalization * graph->deg(nodes[i]);
+          deg[nodes[i]] = normalization * graph->deg(nodes[i]);
 
         break;
 
@@ -404,7 +392,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = normalization * graph->indeg(nodes[i]);
+          deg[nodes[i]] = normalization * graph->indeg(nodes[i]);
 
         break;
 
@@ -413,7 +401,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
 #pragma omp parallel for
 #endif
         for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i)
-          deg[i] = normalization * graph->outdeg(nodes[i]);
+          deg[nodes[i]] = normalization * graph->outdeg(nodes[i]);
 
         break;
       }
@@ -429,7 +417,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInOutEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight;
+          deg[nodes[i]] = nWeight;
         }
 
         break;
@@ -442,7 +430,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight;
+          deg[nodes[i]] = nWeight;
         }
 
         break;
@@ -455,7 +443,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight;
+          deg[nodes[i]] = nWeight;
         }
 
         break;
@@ -488,7 +476,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInOutEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight * normalization;
+          deg[nodes[i]] = nWeight * normalization;
         }
 
         break;
@@ -501,7 +489,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight * normalization;
+          deg[nodes[i]] = nWeight * normalization;
         }
 
         break;
@@ -514,7 +502,7 @@ void tlp::degree(const Graph *graph, tlp::NodeStaticProperty<double> &deg, EDGE_
           edge e;
           double nWeight = 0.0;
           forEach (e, graph->getInEdges(nodes[i])) { nWeight += weights->getEdgeDoubleValue(e); }
-          deg[i] = nWeight * normalization;
+          deg[nodes[i]] = nWeight * normalization;
         }
 
         break;

library/tulip-core/src/GraphTools.cpp
@@ -149,7 +149,7 @@ std::vector<node> computeGraphCenters(Graph *graph) {
 
   for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i) {
     tlp::NodeStaticProperty<unsigned int> tmp(graph);
-    unsigned int maxD = maxDistance(graph, i, tmp, UNDIRECTED);
+    unsigned int maxD = maxDistance(graph, nodes[i], tmp, UNDIRECTED);
     dist[i] = maxD;
 #ifdef _OPENMP
 #pragma omp critical(COMPUTE_MIN)
@@ -169,20 +169,18 @@ std::vector<node> computeGraphCenters(Graph *graph) {
 //======================================================================
 node graphCenterHeuristic(Graph *graph, PluginProgress *pluginProgress) {
   assert(ConnectedTest::isConnected(graph));
-  unsigned int nbNodes = graph->numberOfNodes();
 
-  if (nbNodes == 0)
+  if (graph->numberOfNodes() == 0)
     return node();
 
-  const vector<node> &nodes = graph->nodes();
   tlp::NodeStaticProperty<bool> toTreat(graph);
   toTreat.setAll(true);
   tlp::NodeStaticProperty<unsigned int> dist(graph);
   unsigned int i = 0;
-  unsigned int n = 0;
-  unsigned int result = 0;
+  node n = graph->getOneNode();
+  node result;
   unsigned int cDist = UINT_MAX - 2;
-  unsigned int nbTry = 2 + sqrt(nbNodes);
+  unsigned int nbTry = 2 + sqrt(graph->numberOfNodes());
   unsigned int maxTries = nbTry;
 
   while (nbTry) {
@@ -206,16 +204,15 @@ node graphCenterHeuristic(Graph *graph, PluginProgress *pluginProgress) {
       } else {
         unsigned int delta = di - cDist;
 
-        for (unsigned int v = 0; v < nbNodes; v++)
+        for (const node &v : graph->nodes())
           if (dist[v] < delta)
             // all the nodes at distance less than delta can't be center
             toTreat[v] = false;
       }
 
       unsigned int nextMax = 0;
-      node v;
 
-      for (unsigned int v = 0; v < nbNodes; v++) {
+      for (const node &v : graph->nodes()) {
         if (dist[v] > (di / 2 + di % 2))
           toTreat[v] = false;
         else {
@@ -238,7 +235,7 @@ node graphCenterHeuristic(Graph *graph, PluginProgress *pluginProgress) {
     pluginProgress->progress(100, 100);
   }
 
-  return nodes[result];
+  return result;
 }
 //======================================================================
 void selectSpanningForest(Graph *graph, BooleanProperty *selectionProperty,
@@ -246,25 +243,21 @@ void selectSpanningForest(Graph *graph, BooleanProperty *selectionProperty,
   list<node> fifo;
 
   NodeStaticProperty<bool> nodeFlag(graph);
-  const std::vector<node> &nodes = graph->nodes();
 
-  unsigned int nbNodes = nodes.size();
   unsigned int nbSelectedNodes = selectionProperty->numberOfNonDefaultValuatedNodes();
 
   // get previously selected nodes
   if (nbSelectedNodes) {
-    for (unsigned int i = 0; i < nbNodes; ++i) {
-      node n = nodes[i];
-
+    for (const node &n : graph->nodes()) {
       if (selectionProperty->getNodeValue(n)) {
         fifo.push_back(n);
-        nodeFlag[i] = true;
+        nodeFlag[n] = true;
       }
     }
   } else {
     node n = graph->getOneNode();
     fifo.push_back(n);
-    nodeFlag.setNodeValue(n, true);
+    nodeFlag[n] = true;
     nbSelectedNodes = 1;
   }
 
@@ -272,8 +265,8 @@ void selectSpanningForest(Graph *graph, BooleanProperty *selectionProperty,
   edgeSel.setAll(true);
 
   // select all nodes
-  for (unsigned i = 0; i < nodes.size(); ++i)
-    selectionProperty->setNodeValue(nodes[i], true);
+  for (const node &n : graph->nodes())
+    selectionProperty->setNodeValue(n, true);
 
   bool ok = true;
   unsigned int edgeCount = 0;
@@ -287,21 +280,21 @@ void selectSpanningForest(Graph *graph, BooleanProperty *selectionProperty,
       for (; itE->hasNext();) {
         edge adjit = itE->next();
         node tgt = graph->target(adjit);
-        unsigned int tgtPos = graph->nodePos(tgt);
 
-        if (!nodeFlag[tgtPos]) {
-          nodeFlag[tgtPos] = true;
+        if (!nodeFlag[tgt]) {
+          nodeFlag[tgt] = true;
           ++nbSelectedNodes;
           fifo.push_back(tgt);
         } else
-          edgeSel[graph->edgePos(adjit)] = false;
+          edgeSel[adjit] = false;
 
         if (pluginProgress) {
           pluginProgress->setComment("Computing a spanning forest...");
           ++edgeCount;
 
           if (edgeCount == 200) {
-            if (pluginProgress->progress(nbSelectedNodes * 100 / nbNodes, 100) != TLP_CONTINUE) {
+            if (pluginProgress->progress(nbSelectedNodes * 100 / graph->numberOfNodes(), 100) !=
+                TLP_CONTINUE) {
               return;
             }
 
@@ -315,41 +308,39 @@ void selectSpanningForest(Graph *graph, BooleanProperty *selectionProperty,
 
     ok = false;
     bool degZ = false;
-    unsigned int goodNodePos = 0;
+    node goodNode = graph->getOneNode();
 
-    for (unsigned int i = 0; i < nbNodes; ++i) {
-      node n = nodes[i];
+    for (const node &n : graph->nodes()) {
 
-      if (!nodeFlag[i]) {
+      if (!nodeFlag[n]) {
         if (!ok) {
-          goodNodePos = i;
+          goodNode = n;
           ok = true;
         }
 
         if (graph->indeg(n) == 0) {
           fifo.push_back(n);
-          nodeFlag[i] = true;
+          nodeFlag[n] = true;
           ++nbSelectedNodes;
           degZ = true;
         }
 
         if (!degZ) {
-          node goodNode = nodes[goodNodePos];
 
           if (graph->indeg(n) < graph->indeg(goodNode))
-            goodNodePos = i;
+            goodNode = n;
           else {
             if ((graph->indeg(n) == graph->indeg(goodNode)) &&
                 (graph->outdeg(n) > graph->outdeg(goodNode)))
-              goodNodePos = i;
+              goodNode = n;
           }
         }
       }
     }
 
     if (ok && (!degZ)) {
-      fifo.push_back(nodes[goodNodePos]);
-      nodeFlag[goodNodePos] = true;
+      fifo.push_back(goodNode);
+      nodeFlag[goodNode] = true;
       ++nbSelectedNodes;
     }
   }
@@ -442,7 +433,7 @@ void selectMinimumSpanningTree(Graph *graph, BooleanProperty *selection,
 #endif
 
   for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i) {
-    classes[i] = i;
+    classes[nodes[i]] = i;
   }
 
   unsigned int maxCount = numClasses;
@@ -485,8 +476,8 @@ void selectMinimumSpanningTree(Graph *graph, BooleanProperty *selection,
 #endif
 
     for (OMP_ITER_TYPE i = 0; i < nbNodes; ++i) {
-      if (classes[i] == tgtClass)
-        classes[i] = srcClass;
+      if (classes[nodes[i]] == tgtClass)
+        classes[nodes[i]] = srcClass;
     }
 
     --numClasses;

commit c6fc77485bb264c21f31d42e7f6e2356265af9e3

Restore old code broken by commit da87cbbe2094569ce9bc753851d359c82db5728f (the use of this parameter lead to a segfault)

plugins/layout/OGDF/OGDFFm3.cpp
@@ -498,8 +498,11 @@ void OGDFFm3::callOGDFLayoutAlgorithm(ogdf::GraphAttributes &gAttributes) {
   if (dataSet->get("Edge Length Property", length) && length) {
     EdgeArray<double> edgeLength(tlpToOGDF->getOGDFGraph());
 
-    for (const tlp::edge &e : graph->edges()) {
-      edgeLength[tlpToOGDF->getOGDFGraphEdge(e)] = length->getEdgeDoubleValue(e);
+    const std::vector<tlp::edge> &edges = graph->edges();
+    unsigned int nbEdges = edges.size();
+
+    for (unsigned int i = 0; i < nbEdges; ++i) {
+      edgeLength[tlpToOGDF->getOGDFGraphEdge(i)] = length->getEdgeDoubleValue(edges[i]);
     }
 
     fmmm->call(gAttributes, edgeLength);