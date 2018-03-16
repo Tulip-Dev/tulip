379b6c628e37bc73a2b052c9be7702bf547c326f
p-mary <mary@labri.fr>
2018-03-16 16:00:37 +0100

    format and cleanup developer tutorial files

--- doc/developer/_documents/tutorial010.cpp
+++ doc/developer/_documents/tutorial010.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
 #include <tulip/TlpTools.h>
 #include <tulip/Graph.h>
-#include <String>
+#include <tulip/LayoutProperty.h>
 
 /**
  * Tutorial 010
  *
  * Create a graph and apply an algorithm on it
- * 
+ *
  */
 
 using namespace std;
 using namespace tlp;
 
 int main() {
-  //initialize the Tulip libs
-  initTulipLib(nullptr);
+  // initialize the Tulip libs
+  initTulipLib();
 
-  //create an empty graph
+  // create an empty graph
   Graph *graph = newGraph();
 
-  //add the nodes
+  // add the nodes
   node n0 = graph->addNode();
   node n1 = graph->addNode();
-  
-  //add the edge
-  edge e0 = graph->addEdge(n0,n1);
- 
-	//run the algorithm
-	String errMsg;
-	DataSet dataSet
-	PluginProgress pluginProgress;
-	bool result = graph->applyPropertyAlgorithm("Bubble Pack", _layout, errMsg, pluginProgress, &dataSet);
 
-	if (!result)
-		cout << errMsg << endl;
-	else
-		cout << "Algorithm successfully applied" << endl;
+  // add the edge
+  graph->addEdge(n0, n1);
+
+  // run the algorithm
+  string errMsg;
+  LayoutProperty *layout = graph->getProperty<LayoutProperty>("viewLayout");
+  bool result = graph->applyPropertyAlgorithm("Bubble Pack", layout, errMsg);
+
+  if (!result)
+    cout << errMsg << endl;
+  else
+    cout << "Algorithm successfully applied" << endl;
 
   delete graph;
   return EXIT_SUCCESS;
 }
-
--- doc/developer/_documents/tutorial106.cpp
+++ doc/developer/_documents/tutorial106.cpp
@@ -7,56 +7,56 @@
 #include <iostream>
 using namespace tlp;
 
-int main(int argc, char** argv) {
-	/*
-	A QApplication must always be declared at the beginning of the main function in order for Tulip to work.
-	This must be done before calling tlp::initTulipSoftware()
-	*/
-	QApplication app(argc,argv);
-	/*
-	Initialize the library, load plugins and set application runtime pathes accordingly to the host operating system
-	This method should always be called if you intend to use plugins in your application.
-	*/
-	tlp::initTulipSoftware(nullptr);
-	/*
-	Load the file passed as first argument into a graph.
-	This method will select the default Tulip algorithm plugin (TLP)
-	*/
-	//Graph* g = tlp::loadGraph(argv[1]);
-	Graph* g = newGraph();
+int main(int argc, char **argv) {
+  /*
+  A QApplication must always be declared at the beginning of the main function in order for Tulip to work.
+  This must be done before calling tlp::initTulipSoftware()
+  */
+  QApplication app(argc, argv);
+  /*
+  Initialize the library, load plugins and set application runtime pathes accordingly to the host
+  operating system
+  This method should always be called if you intend to use plugins in your application.
+  */
+  tlp::initTulipSoftware();
+  /*
+  Load the file passed as first argument into a graph.
+  This method will select the default Tulip algorithm plugin (TLP)
+  */
+  // Graph* g = tlp::loadGraph(argv[1]);
+  Graph *g = newGraph();
 
+  TulipProject *_project = TulipProject::openProject(QString::fromLatin1(argv[1]));
 
-	TulipProject* _project = TulipProject::openProject(QString::fromLatin1(argv[1]));
-
-	//std::cout << QString::fromLatin1(argv[1]) << std::endl;
+  // std::cout << QString::fromLatin1(argv[1]) << std::endl;
 
   if (_project->exists("/data/graphs/0/graph.tlp")) {
-std::cout << "pouet pouet" << std::endl;
+    std::cout << "pouet pouet" << std::endl;
     DataSet data;
 
-    data.set<std::string>("file::filename",QStringToTlpString(_project->toAbsolutePath("/data/graphs/0/graph.tlp")));
+    data.set<std::string>("file::filename",
+                          QStringToTlpString(_project->toAbsolutePath("/data/graphs/0/graph.tlp")));
 
-    g = tlp::importGraph("TLP Import",data);
-std::cout << g << std::endl;
-
+    g = tlp::importGraph("TLP Import", data);
+    std::cout << g << std::endl;
   }
-	
-// Creates the main widget that will display our graph
-	GlMainWidget* mainWidget = new GlMainWidget(nullptr);
-	// Adds a layer to the scene
-	GlLayer* mainLayer = mainWidget->getScene()->createLayer("Main");
-	// Adds the graph to this layer
-	mainLayer->addGraph(g,"graph");
 
-	// Display the widget
-	mainWidget->show();
-	// Flush event loop in order to let paint events pass through in order for the scene to be initialized.
-	QApplication::processEvents();
-	// Center the camera and draw the graph
-	mainWidget->centerScene();
-	mainWidget->draw();
-	// Adds Zoom and pan navigation to the widget
-	mainWidget->installEventFilter(new MouseNKeysNavigator);
-	return app.exec();
+  // Creates the main widget that will display our graph
+  GlMainWidget *mainWidget = new GlMainWidget();
+  // Adds a layer to the scene
+  GlLayer *mainLayer = mainWidget->getScene()->createLayer("Main");
+  // Adds the graph to this layer
+  mainLayer->addGraph(g, "graph");
+
+  // Display the widget
+  mainWidget->show();
+  // Flush event loop in order to let paint events pass through in order for the scene to be
+  // initialized.
+  QApplication::processEvents();
+  // Center the camera and draw the graph
+  mainWidget->centerScene();
+  mainWidget->draw();
+  // Adds Zoom and pan navigation to the widget
+  mainWidget->installEventFilter(new MouseNKeysNavigator);
+  return app.exec();
 }
-
