5e8190769d9e4707a13d1dc754efbf2fb157225f
p-mary <mary@labri.fr>
2018-05-29 15:16:07 +0200

    FindQtX fix: ensure QT_BINARY_DIR and QT_PLUGINS_DIR are set when building with Qt5 on linux platforms

--- cmake/FindQtX.cmake
+++ cmake/FindQtX.cmake
@@ -91,6 +91,12 @@ IF(${Qt5Widgets_FOUND} AND ${Qt5OpenGL_FOUND} AND ${Qt5Network_FOUND})
     GET_FILENAME_COMPONENT(QT_FRAMEWORKS_DIR ${QtCore_location} PATH)
     SET(QT_FRAMEWORKS_DIR ${QT_FRAMEWORKS_DIR}/..)
     SET(QT_BINARY_DIR ${QT_FRAMEWORKS_DIR}/../bin)
+  ELSE()
+    SET(QT_BINARY_DIR "${_qt5Gui_install_prefix}/bin")
+    # Standard Qt5 installation
+    IF(EXISTS ${QT_BINARY_DIR}/../plugins)
+      SET(QT_PLUGINS_DIR ${QT_BINARY_DIR}/../plugins)
+    ENDIF()
   ENDIF()
 
 
