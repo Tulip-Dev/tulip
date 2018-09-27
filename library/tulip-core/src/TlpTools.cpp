feac4cd791f8ed1fa99d0278cd7a01903249e851
p-mary <mary@labri.fr>
2018-09-27 13:09:12 +0200

    avoid tlp::initTulipLib error output when invoked from a python installed wheel

--- library/tulip-core/src/TlpTools.cpp
+++ library/tulip-core/src/TlpTools.cpp
@@ -145,7 +145,9 @@ static void checkDirectory(std::string dir, bool tlpDirSet, bool throwEx) {
       ess << std::endl << "Check your TLP_DIR environment variable";
     if (throwEx)
       throw TulipException(ess.str());
-    else
+    else if ((dir.find("python") == string::npos) &&
+	     (dir.find("site-packages") == string::npos))
+      // output only if not in a python installed wheel
       tlp::error() << ess.str();
   }
 }
