4e82acaecee9cb89978e99dbe39e89f7db27f1a8
p-mary <mary@labri.fr>
2018-10-26 15:07:04 +0200

    rework Tulip AppImage build on centos6 docker container: use python 2.7 and Qt 5.6 prebuilt packages

--- bundlers/linux/tulip_appimage_centos6_build.sh
+++ bundlers/linux/tulip_appimage_centos6_build.sh
@@ -26,37 +26,23 @@ yum -y install doxygen graphviz
 # needed for generating the appimage
 yum -y install fuse fuse-libs
 
-# install precompiled Qt 4.8.4 (could not find latest 4.8 version but it is still better than old Qt 4.6 bundled with CentOS 6)
-wget http://mirror.yandex.ru/fedora/russianfedora/russianfedora/free/el/releases/6/Everything/x86_64/os/puias-release-6-2.R.noarch.rpm
-wget http://mirror.yandex.ru/fedora/russianfedora/russianfedora/free/el/releases/6/Everything/x86_64/os/russianfedora-free-release-6-3.R.noarch.rpm
-rpm -i puias-release-6-2.R.noarch.rpm
-rpm -i russianfedora-free-release-6-3.R.noarch.rpm
-yum -y install qt48-devel
+# install python 2.7 and the corresponding sphinx version
+yum install -y centos-release-scl
+yum install -y python27-python-devel python27-python-sphinx
 
-# compile and install Python 2.7 (the Python version bundled with CentOS 6 is Python 2.6, which is quite old)
-yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
-wget http://python.org/ftp/python/2.7.14/Python-2.7.14.tar.xz
-tar xf Python-2.7.14.tar.xz
-cd Python-2.7.14
-./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations CC="ccache /opt/rh/devtoolset-2/root/usr/bin/gcc" CXX="ccache /opt/rh/devtoolset-2/root/usr/bin/g++"
-make -j4 && make altinstall
+# setup python
+source /opt/rh/python27/enable
 
-# some extra setup for cmake et appimage build to succeed
-if [ -d /usr/lib64 ]; then
-  cp -r /usr/lib/libpython2.7* /usr/lib64/
-  cp -r /usr/lib/python2.7 /usr/lib64/
-fi
-export PATH=/usr/lib64/qt48/bin/:/usr/lib/qt48/bin/:$PATH
-
-# install pip in order to grab the latest sphinx version
-wget https://bootstrap.pypa.io/get-pip.py
-python2.7 get-pip.py
-pip2.7 install sphinx
+# install qt5
+yum install -y qt5-qtbase-devel qt5-qtwebkit-devel
 
 # build and install tulip
+if [ -d /tulip/build ]; then
+  rm -rf /tulip/build
+fi
 mkdir /tulip/build
 cd /tulip/build
-cmake -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++ -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/usr/bin/python2.7 -DCMAKE_INSTALL_PREFIX=$PWD/install -DTULIP_USE_CCACHE=ON -DTULIP_BUILD_FOR_APPIMAGE=ON ..
+cmake -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_PREFIX_PATH=/usr/lib64/qt5 -DTULIP_USE_CCACHE=ON -DTULIP_USE_QT5=ON -DTULIP_BUILD_FOR_APPIMAGE=ON ..
 make -j4 install
 
 # build a bundle dir suitable for AppImageKit
@@ -67,4 +53,4 @@ wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appi
 chmod a+x appimagetool-$(uname -p).AppImage
 
 # finally build the portable app
-./appimagetool-$(uname -p).AppImage Tulip.AppDir Tulip-$(install/bin/tulip-config --version)-$(uname -p).AppImage
+./appimagetool-$(uname -p).AppImage Tulip.AppDir Tulip-$(sh tulip-config --version)-$(uname -p).AppImage
