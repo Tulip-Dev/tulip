Tulip-Dev/tulip

commit 3548bbb179bef60aa0a7aff9202f21b87ac59f3a

code factorization : use dedicated functions "tlpStringToQString" and "QStringToTlpString" from TlpQtTools to handle the conversion from a Tulip UTF-8 encoded std::string to a QString and vice-versa in the whole Tulip codebase + miscellaneous cleanups
git-svn-id: https://svn.code.sf.net/p/auber/code/tulip@11769 f5d4c5f9-c943-4916-8a57-bd8605bf961c

library/tulip-gui/src/TulipProject.cpp
@@ -209,7 +209,7 @@ bool TulipProject::copy(const QString &source, const QString &destination) {
 std::fstream *TulipProject::stdFileStream(const QString &path, std::ios_base::openmode mode) {
   QString filePath(toAbsolutePath(path));
   std::fstream *result = new std::fstream();
-  result->open(filePath.toUtf8().data(), mode);
+  result->open(QStringToTlpString(filePath), mode);
 
   if (!result->is_open()) {
     delete result;
@@ -309,19 +309,19 @@ bool TulipProject::readMetaInfo() {
 
   if (doc.hasError()) {
     in.close();
-    tlp::debug() << "Error opening xml meta information file: " << doc.errorString().toStdString() << std::endl;
+    tlp::debug() << "Error opening xml meta information file: " << QStringToTlpString(doc.errorString()) << std::endl;
     return false;
   }
 
   while (!doc.atEnd()) {
     if (doc.readNextStartElement()) {
       if(doc.hasError()) {
-        tlp::debug() << "Error reading xml meta information: " << doc.errorString().toStdString() << std::endl;
+        tlp::debug() << "Error reading xml meta information: " << QStringToTlpString(doc.errorString()) << std::endl;
         in.close();
         return false;
       }
 
-      const char* name(doc.name().toString().toStdString().data());
+      const char* name(QStringToTlpString(doc.name().toString()).c_str());
 
       if (property(name).isValid())
         setProperty(name,doc.readElementText());

commit cae2c9bd71715fd9d79954821f37f12cba02585e

tulip-gui/TulipProject: Fix not critical stack-use-after-scope issue
This memory management issue was introduced in commit 3548bbb179bef60aa0a7aff9202f21b87ac59f3a.

This is not critical but this makes memory debugging tool unhappy and
must be fixed anyway.

library/tulip-gui/src/TulipProject.cpp
@@ -332,10 +332,10 @@ bool TulipProject::readMetaInfo() {
         return false;
       }
 
-      const char *name(QStringToTlpString(doc.name().toString()).c_str());
+      std::string name = QStringToTlpString(doc.name().toString());
 
-      if (property(name).isValid())
-        setProperty(name, doc.readElementText());
+      if (property(name.c_str()).isValid())
+        setProperty(name.c_str(), doc.readElementText());
     }
   }
 