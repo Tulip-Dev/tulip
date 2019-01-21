95277adefa34a4e23e9c41c7d4eed8c77a688c47
Bruno Pinaud <bruno.pinaud@u-bordeaux.fr>
2019-01-21 21:50:09 +0100

    fix a crash when changing to an interactor withtout a widget

--- library/tulip-gui/src/InteractorConfigWidget.cpp
+++ library/tulip-gui/src/InteractorConfigWidget.cpp
@@ -25,37 +25,43 @@
 #include <tulip/TlpQtTools.h>
 #include <tulip/Interactor.h>
 
-
 using namespace tlp;
 
-InteractorConfigWidget::InteractorConfigWidget(QWidget *parent) : QDialog(parent),_ui(new Ui::InteractorConfigWidget), _interactor(nullptr)
-{
-    _ui->setupUi(this);
+InteractorConfigWidget::InteractorConfigWidget(QWidget *parent)
+    : QDialog(parent), _ui(new Ui::InteractorConfigWidget), _interactor(nullptr) {
+  _ui->setupUi(this);
 }
 
-InteractorConfigWidget::~InteractorConfigWidget(){
-    delete _ui;
+InteractorConfigWidget::~InteractorConfigWidget() {
+  delete _ui;
 }
 
-void InteractorConfigWidget::clearWidget(){
-    //removes widget from the layout to not delete the object and give back parenthood. It is up to the interactor developer to delete its config widget
-        if(_interactor!=nullptr) {
-            _interactor->configurationWidget()->hide();
-            _ui->scrollArea->takeWidget();
-            _interactor=nullptr;
-        }
+void InteractorConfigWidget::clearWidget() {
+  // removes widget from the layout to not delete the object and give back parenthood. It is up to
+  // the interactor developer to delete its config widget
+  if (_interactor != nullptr) {
+    _interactor->configurationWidget()->hide();
+    _ui->scrollArea->takeWidget();
+    _interactor = nullptr;
+  }
 }
 
 void InteractorConfigWidget::setWidget(Interactor *interactor) {
+  if (interactor->configurationWidget() == nullptr) {
+    clearWidget();
+    hide();
+  } else {
     setWindowTitle(tlpStringToQString(interactor->name()));
-    //removes widget from the layout to not delete the object and give back parenthood. It is up to the interactor developer to delete its config widget
-    if(_interactor!=nullptr) {
-        if(_interactor->configurationWidget()!=interactor->configurationWidget()) {
-            _ui->scrollArea->takeWidget();
-            _interactor->configurationWidget()->hide();
-        }
+    // removes widget from the layout to not delete the object and give back parenthood. It is up to
+    // the interactor developer to delete its config widget
+    if (_interactor != nullptr) {
+      if (_interactor->configurationWidget() != interactor->configurationWidget()) {
+        _ui->scrollArea->takeWidget();
+        _interactor->configurationWidget()->hide();
+      }
     }
 
     _ui->scrollArea->setWidget(interactor->configurationWidget());
     _interactor = interactor;
+  }
 }
