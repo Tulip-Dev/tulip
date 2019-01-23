523592a437f736db0eb48c632964ee3ba9602ba3
Antoine Lambert <antoine.lambert@inria.fr>
2019-01-23 17:15:36 +0100

    AppVeyor: Split build in two phases to avoid timeouts
    Compiling the whole Tulip source code with AppVeyor can often takes
    more than 1 hour, depending on the current CI server payload,
    and thus a numerous build jobs timeout and end up red.
    
    In order to avoid that annoying behavior, use the same strategy to
    build Tulip as the one used with Travis: perform a core Tulip build
    in a first job, cache generated object files then perform a complete
    Tulip build in a second job.
    
    As more time is now available for the complete MSYS2 builds, I reactivated
    the build of the Sphinx documentation. Tulip is also compiled against
    Python 2 and Python 3 and corresponding installers are generated.
    
    I have also deactivated the 32 bits builds to avoid too long overall
    builds times, they can be reactivated if needed.

--- appveyor_msvc.bat
+++ appveyor_msvc.bat
@@ -1,5 +1,15 @@
 rem Matrix-driven Appveyor CI script for Tulip using Visual Studio compiler
 
+rem Check which type of Tulip build to perform based on the job number
+rem   - odd number = core build
+rem   - even number = complete build
+rem We build Tulip in two phases (core build then complete build)
+rem to avoid AppVeyor build timeouts. Object files generated during
+rem the core build will be made available for the complete build
+rem thanks to the use of clcache.
+set /a TULIP_BUILD_CORE_ONLY = %APPVEYOR_JOB_NUMBER% %% 2
+echo TULIP_BUILD_CORE_ONLY=%TULIP_BUILD_CORE_ONLY%
+
 rem let's compile clcache in order to speedup incremental builds
 cd C:/
 set PATH=C:/Python35-x64;C:/Python35-x64/Scripts;%PATH%
@@ -7,19 +17,10 @@ pip install clcache
 set CLCACHE_MSBUILD_CONF=/p:TrackFileAccess=false /p:CLToolExe=clcache.exe /p:CLToolPath=C:\Python35-x64\Scripts
 
 rem create a directory to store Tulip dependencies
-cd C:/ && md tulip_dependencies 
-
-rem get, compile and install freetype
-cd C:/tulip_dependencies
-curl -LO http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz
-if %errorlevel% neq 0 exit /b %errorlevel%
-7z x freetype-2.8.tar.gz -so | 7z x -aoa -si -ttar
-cd freetype-2.8
-md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
-if %errorlevel% neq 0 exit /b %errorlevel%
-msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
+cd C:/ && md tulip_dependencies
+cd C:/tulip_dependencies && md bin
+cd C:/tulip_dependencies && md include
+cd C:/tulip_dependencies && md lib
 
 rem get, compile and install zlib
 cd C:/tulip_dependencies
@@ -33,6 +34,25 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+rem get, compile and install cppunit
+cd C:/tulip_dependencies
+git clone git://anongit.freedesktop.org/git/libreoffice/cppunit/
+if %errorlevel% neq 0 exit /b %errorlevel%
+cd cppunit/src
+msbuild CppUnitLibraries2010.sln /m /clp:ErrorsOnly /p:Configuration=Release /p:Platform=%MSVC_PLATFORM% /p:PlatformToolset=%MSVC_PLATFORM_TOOLSET% %CLCACHE_MSBUILD_CONF%
+if %errorlevel% neq 0 exit /b %errorlevel%
+xcopy C:\tulip_dependencies\cppunit\include C:\tulip_dependencies\include /S /Y
+xcopy C:\tulip_dependencies\cppunit\lib C:\tulip_dependencies\lib /S /Y
+move C:\tulip_dependencies\lib\cppunit_dll.dll C:\tulip_dependencies\bin\
+
+if "%TULIP_BUILD_CORE_ONLY%" == "0" (
+  goto install_complete_tulip_build_dependencies
+) else {
+  goto tulip_build
+}
+
+
+:install_complete_tulip_build_dependencies
 rem get, compile and install libpng
 cd C:/tulip_dependencies
 curl -LO https://sourceforge.mirrorservice.org/l/li/libpng/libpng16/1.6.35/libpng-1.6.35.tar.gz
@@ -43,6 +63,18 @@ md build && cd build
 cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INCLUDE_PATH="C:/tulip_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/tulip_dependencies/lib" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+rem get, compile and install freetype
+cd C:/tulip_dependencies
+curl -LO http://download.savannah.gnu.org/releases/freetype/freetype-2.8.tar.gz
+if %errorlevel% neq 0 exit /b %errorlevel%
+7z x freetype-2.8.tar.gz -so | 7z x -aoa -si -ttar
+cd freetype-2.8
+md build && cd build
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INSTALL_PREFIX="C:/tulip_dependencies" ..
+if %errorlevel% neq 0 exit /b %errorlevel%
+msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 rem get, compile and install glew
@@ -68,21 +100,14 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /m /clp:ErrorsOnly /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-rem get, compile and install cppunit
-cd C:/tulip_dependencies
-git clone git://anongit.freedesktop.org/git/libreoffice/cppunit/
-if %errorlevel% neq 0 exit /b %errorlevel%
-cd cppunit/src
-msbuild CppUnitLibraries2010.sln /m /clp:ErrorsOnly /p:Configuration=Release /p:Platform=%MSVC_PLATFORM% /p:PlatformToolset=%MSVC_PLATFORM_TOOLSET% %CLCACHE_MSBUILD_CONF%
-if %errorlevel% neq 0 exit /b %errorlevel%
-xcopy C:\tulip_dependencies\cppunit\include C:\tulip_dependencies\include /S /Y
-xcopy C:\tulip_dependencies\cppunit\lib C:\tulip_dependencies\lib /S /Y
-move C:\tulip_dependencies\lib\cppunit_dll.dll C:\tulip_dependencies\bin\
+goto tulip_build
 
+
+:tulip_build
 rem we are good to go, let's compile and install Tulip now
 cd %APPVEYOR_BUILD_FOLDER%
 md build && cd build
-cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INCLUDE_PATH="C:/tulip_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/tulip_dependencies/lib;C:/tulip_dependencies/bin" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DPYTHON_EXECUTABLE="%PYTHON_EXECUTABLE%" -DTULIP_BUILD_TESTS=ON ..
+cmake -G "%CMAKE_VS_GENERATOR%" -DCMAKE_INCLUDE_PATH="C:/tulip_dependencies/include" -DCMAKE_LIBRARY_PATH="C:/tulip_dependencies/lib;C:/tulip_dependencies/bin" -DCMAKE_PREFIX_PATH="%QT5_DIR%" -DPYTHON_EXECUTABLE="%PYTHON_EXECUTABLE%" -DTULIP_BUILD_CORE_ONLY=%TULIP_BUILD_CORE_ONLY% -DTULIP_BUILD_TESTS=ON ..
 if %errorlevel% neq 0 exit /b %errorlevel%
 msbuild INSTALL.vcxproj /m /p:Configuration=Release %CLCACHE_MSBUILD_CONF%
 if %errorlevel% neq 0 exit /b %errorlevel%
--- appveyor_msys2.bat
+++ appveyor_msys2.bat
@@ -5,6 +5,16 @@ rem Needs the following vars:
 rem    MSYS2_ARCH:  x86_64 or i686
 rem    MSYSTEM:  MINGW64 or MINGW32
 
+rem Check which type of Tulip build to perform based on the job number
+rem   - odd number = core build
+rem   - even number = complete build
+rem We build Tulip in two phases (core build then complete build)
+rem to avoid AppVeyor build timeouts. Object files generated during
+rem the core build will be made available for the complete build
+rem thanks to the use of ccache.
+set /a TULIP_BUILD_CORE_ONLY = %APPVEYOR_JOB_NUMBER% %% 2
+echo TULIP_BUILD_CORE_ONLY=%TULIP_BUILD_CORE_ONLY%
+
 rem Set the paths appropriately
 PATH C:\msys64\%MSYSTEM%\bin;C:\msys64\usr\bin;%PATH%
 
@@ -19,18 +29,31 @@ rem when its version evolves
 bash -lc "pacman --noconfirm -S --needed mingw-w64-%MSYS2_ARCH%-toolchain"
 
 rem Install the relevant native dependencies
+bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-ccache"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-yajl"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-qhull"
+bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-cppunit"
+
+set TULIP_BUILD_DOC=OFF
+
+if "%TULIP_BUILD_CORE_ONLY%" == "0" (
+  goto install_complete_tulip_build_dependencies
+) else (
+  goto tulip_build
+)
+
+:install_complete_tulip_build_dependencies
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-freetype"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-glew"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-libpng"
-bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-pcre2"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-qt5"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-quazip"
 bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-qtwebkit"
-bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-cppunit"
-bash -lc "pacman --noconfirm -S --needed --force mingw-w64-%MSYS2_ARCH%-ccache"
+set TULIP_BUILD_DOC=ON
+goto tulip_build
 
+
+:tulip_build
 rem Invoke subsequent bash in the build tree
 cd %APPVEYOR_BUILD_FOLDER%
 set CHERE_INVOKING=yes
@@ -42,28 +65,35 @@ bash -lc "env"
 rem Install Inetc plugin for NSIS
 bash -lc "unzip bundlers/win/Inetc.zip -d\"C:\Program Files (x86)\NSIS\""
 
+rem Install sphinx for Python 3
+set PATH=%PYTHON3_HOME%;%PYTHON3_HOME%/Scripts;%PATH%
+pip install sphinx
+
 rem Build Tulip with Python 3, run its unit tests and package it
 bash -lc "mkdir build"
-bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_DOC=OFF -DTULIP_BUILD_TESTS=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON3_HOME%/python.exe .."
+bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_CORE_ONLY=%TULIP_BUILD_CORE_ONLY% -DTULIP_BUILD_DOC=%TULIP_BUILD_DOC% -DTULIP_BUILD_TESTS=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON3_HOME%/python.exe .."
 if %errorlevel% neq 0 exit /b %errorlevel%
 bash -lc "cd build && make -j4 install"
 if %errorlevel% neq 0 exit /b %errorlevel%
 bash -lc "cd build && make runTests"
 if %errorlevel% neq 0 exit /b %errorlevel%
-bash -lc "cd build && make bundle"
 
-rem if %errorlevel% neq 0 exit /b %errorlevel%
+if "%TULIP_BUILD_CORE_ONLY%" == "0" (
+  bash -lc "cd build && make bundle"
+)
 
-rem rem Install sphinx for Python 2
-rem set PATH=%PYTHON2_HOME%;%PYTHON2_HOME%/Scripts;%PATH%
-rem pip install sphinx
+rem Install sphinx for Python 2
+set PATH=%PYTHON2_HOME%;%PYTHON2_HOME%/Scripts;%PATH%
+pip install sphinx
 
-rem rem Build Tulip with Python 2, run its unit tests and package it
-rem bash -lc "mkdir build"
-rem bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_DOC=OFF -DTULIP_BUILD_TESTS=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON2_HOME%/python.exe .."
-rem if %errorlevel% neq 0 exit /b %errorlevel%
-rem bash -lc "cd build && make -j4 install"
-rem if %errorlevel% neq 0 exit /b %errorlevel%
-rem bash -lc "cd build && make runTests"
-rem if %errorlevel% neq 0 exit /b %errorlevel%
-rem bash -lc "cd build && make bundle"
+rem Build Tulip with Python 2, run its unit tests and package it
+bash -lc "cd build && cmake -G \"MSYS Makefiles\" -DCMAKE_BUILD_TYPE=Release -DCMAKE_NEED_RESPONSE=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/build/install -DTULIP_BUILD_CORE_ONLY=%TULIP_BUILD_CORE_ONLY% -DTULIP_BUILD_DOC=%TULIP_BUILD_DOC% -DTULIP_BUILD_TESTS=ON -DTULIP_USE_CCACHE=ON -DPYTHON_EXECUTABLE=%PYTHON2_HOME%/python.exe .."
+if %errorlevel% neq 0 exit /b %errorlevel%
+bash -lc "cd build && make -j4 install"
+if %errorlevel% neq 0 exit /b %errorlevel%
+bash -lc "cd build && make runTests"
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+if "%TULIP_BUILD_CORE_ONLY%" == "0" (
+  bash -lc "cd build && make bundle"
+)
