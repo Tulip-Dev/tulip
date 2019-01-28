346a51b8b5913ede2861a012f071136e5ecaa15d
Antoine Lambert <antoine.lambert@inria.fr>
2019-01-28 19:31:08 +0100

    bundlers/linux: Use Python 3.5 to build Tulip AppImage

--- bundlers/linux/tulip_appimage_centos6_build.sh
+++ bundlers/linux/tulip_appimage_centos6_build.sh
@@ -20,42 +20,24 @@ yum -y install devtoolset-2-gcc devtoolset-2-binutils devtoolset-2-gcc-c++
 yum -y install zlib-devel qhull-devel
 yum -y install freetype-devel libpng-devel libjpeg-devel glew-devel
 
-# needed for generating the appimage
+# needed for generating the AppImage
 yum -y install fuse fuse-libs
 
+# set USR_LIB (needed to set CMAKE_PREFIX_PATH)
 if [ "$(uname -p)" = "x86_64" ]; then
-
-# install python 2.7 and the corresponding sphinx version
-yum install -y centos-release-scl
-yum install -y python27-python-devel python27-python-sphinx
-# setup python
-source /opt/rh/python27/enable
-# set USR_LIB (needed to set CMAKE_PREFIX_PATH)
-USR_LIB=/usr/lib64
-
+  USR_LIB=/usr/lib64
 else
-
-# download, compile and install Python 2.7 because there is
-# no 32 bit version of centos-release-scl
-yum install -y zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
-wget http://python.org/ftp/python/2.7.14/Python-2.7.14.tar.xz
-tar xf Python-2.7.14.tar.xz
-cd Python-2.7.14
-./configure --prefix=/usr --enable-unicode=ucs4 --enable-shared --enable-optimizations CC="ccache /opt/rh/devtoolset-2/root/usr/bin/gcc" CXX="ccache /opt/rh/devtoolset-2/root/usr/bin/g++"
-make -j4 && make altinstall
-
-# install pip in order to grab the latest sphinx version
-wget https://bootstrap.pypa.io/get-pip.py
-python2.7 get-pip.py
-pip2.7 install sphinx
-
-# set USR_LIB (needed to set CMAKE_PREFIX_PATH)
-USR_LIB=/usr/lib
-
+  USR_LIB=/usr/lib
 fi
 
 # install qt5
 yum install -y qt5-qtbase-devel qt5-qtwebkit-devel
+
+# install Python 3.5 from the IUS Community Project
+wget https://centos6.iuscommunity.org/ius-release.rpm
+rpm -Uvh ius-release*rpm
+yum -y install python35u-devel python35u-pip
+pip3.5 install sphinx
 
 # build and install tulip
 if [ -d /tulip/build ]; then
@@ -63,7 +45,7 @@ if [ -d /tulip/build ]; then
 fi
 mkdir /tulip/build
 cd /tulip/build
-cmake -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_PREFIX_PATH=$USR_LIB/qt5 -DTULIP_USE_CCACHE=ON -DTULIP_BUILD_FOR_APPIMAGE=ON ..
+cmake -DCMAKE_C_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/gcc -DCMAKE_CXX_COMPILER=/opt/rh/devtoolset-2/root/usr/bin/g++ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$PWD/install -DCMAKE_PREFIX_PATH=$USR_LIB/qt5 -DPYTHON_EXECUTABLE=/usr/bin/python3.5 -DTULIP_USE_CCACHE=ON -DTULIP_BUILD_FOR_APPIMAGE=ON ..
 make -j4 install
 
 # build a bundle dir suitable for AppImageKit
