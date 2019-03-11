Tulip-Dev/tulip

commit 1cf12ea48e81155a9a5d4c342a078ddf3aab978e

Get rid of deprecated QGLFramebufferObject: use QOpenGLFramebufferObject + cleanup

plugins/view/GeographicView/GeographicView.cpp
@@ -17,7 +17,6 @@
  *
  */
 
-#include <GL/glew.h>
 #include <tulip/GlDisplayListManager.h>
 #include <tulip/GlTextureManager.h>
 #include <tulip/GlMainWidget.h>
@@ -35,7 +34,8 @@
 #include <QComboBox>
 #include <QTimeLine>
 #include <QApplication>
-#include <QGLFramebufferObject>
+#include <QOpenGLFramebufferObject>
+#include <QOpenGLPaintDevice>
 #include <QMessageBox>
 #include <QTimer>
 
@@ -482,37 +482,38 @@ QPixmap GeographicView::snapshot(const QSize &size) const {
     }
   }
 
-  QGLFramebufferObjectFormat fboFormat;
-  fboFormat.setAttachment(QGLFramebufferObject::CombinedDepthStencil);
-  fboFormat.setSamples(OpenGlConfigManager::getInst().maxNumberOfSamples());
-
   int width = geoViewGraphicsView->width();
   int height = geoViewGraphicsView->height();
 
-  QGLFramebufferObject renderFbo(width, height, fboFormat);
-  QGLFramebufferObject renderFbo2(width, height);
+  QOpenGLFramebufferObjectFormat fboFormat;
+  fboFormat.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
+  fboFormat.setSamples(OpenGlConfigManager::getInst().maxNumberOfSamples());
 
-  QPainter fboPainter(&renderFbo);
+  QOpenGLFramebufferObject fbo(width, height, fboFormat);
+  QOpenGLFramebufferObject fbo2(width, height);
+
+  fbo.bind();
+  QOpenGLPaintDevice device(QSize(width, height));
+  QPainter fboPainter(&device);
   fboPainter.setRenderHint(QPainter::Antialiasing);
   fboPainter.setRenderHint(QPainter::HighQualityAntialiasing);
   geoViewGraphicsView->scene()->render(&fboPainter);
-  fboPainter.end();
-
-  QGLFramebufferObject::blitFramebuffer(&renderFbo2, QRect(0, 0, width, height), &renderFbo,
-                                        QRect(0, 0, width, height));
+  QRect fboRect(0, 0, width, height);
+  QOpenGLFramebufferObject::blitFramebuffer(&fbo2, fboRect, nullptr, fboRect);
+  fbo.release();
 
   // restore the graphics widgets previously hidden
   for (int i = 0; i < gWidgetsToRestore.size(); ++i) {
     gWidgetsToRestore.at(i)->show();
   }
 
-  QImage snapshotImage = renderFbo2.toImage();
-  snapshotImage = QImage(snapshotImage.bits(), snapshotImage.width(), snapshotImage.height(),
-                         QImage::Format_ARGB32)
+  QImage snapshotImage = fbo2.toImage();
+  snapshotImage = QImage(snapshotImage.bits(), snapshotImage.width(),
+			 snapshotImage.height(), QImage::Format_ARGB32)
                       .convertToFormat(QImage::Format_RGB32);
 
-  return QPixmap::fromImage(snapshotImage)
-      .scaled(size, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+  return QPixmap::fromImage(snapshotImage).scaled(size, Qt::KeepAspectRatio,
+						  Qt::SmoothTransformation);
 }
 
 PLUGIN(GeographicView)

library/tulip-gui/include/CMakeLists.txt
@@ -37,7 +37,6 @@ INSTALL(FILES
   tulip/Perspective.h
   tulip/PropertyCreationDialog.h
   tulip/QGlBufferManager.h
-  tulip/QGlPixelBufferManager.h
   tulip/QtGlSceneZoomAndPanAnimator.h
   tulip/RenamePropertyDialog.h
   tulip/SimplePluginProgressWidget.h

library/tulip-gui/include/tulip/GlMainWidget.h
@@ -27,7 +27,7 @@
 #include <tulip/GlScene.h>
 #include <tulip/Graph.h>
 
-class QGLFramebufferObject;
+class QOpenGLFramebufferObject;
 
 namespace tlp {
 
@@ -211,7 +211,7 @@ public:
    * You can use this texture with Tulip texture system
    * @see GlTextureManager
    */
-  QGLFramebufferObject *createTexture(const std::string &textureName, int width, int height);
+  QOpenGLFramebufferObject *createTexture(const std::string &textureName, int width, int height);
   /**
    * @brief Take a snapshot of the Widget and put it in a picture
    * @param width size
@@ -375,7 +375,7 @@ private:
   unsigned char *renderingStore;
   bool frameBufferStored;
   bool useFramebufferObject;
-  QGLFramebufferObject *glFrameBuf, *glFrameBuf2;
+  QOpenGLFramebufferObject *glFrameBuf, *glFrameBuf2;
   static bool inRendering;
   bool keepPointOfViewOnSubgraphChanging;
   bool advancedAntiAliasing;

library/tulip-gui/include/tulip/GlOffscreenRenderer.h
@@ -29,7 +29,7 @@
 
 #include <QImage>
 
-class QGLFramebufferObject;
+class QOpenGLFramebufferObject;
 
 namespace tlp {
 
@@ -131,7 +131,7 @@ private:
   static GlOffscreenRenderer instance;
 
   unsigned int vPWidth, vPHeight;
-  QGLFramebufferObject *glFrameBuf, *glFrameBuf2;
+  QOpenGLFramebufferObject *glFrameBuf, *glFrameBuf2;
   GlScene scene;
   GlLayer *mainLayer;
   unsigned int entitiesCpt;

library/tulip-gui/include/tulip/QGlBufferManager.h
@@ -24,69 +24,33 @@
 #include <map>
 
 #include <tulip/tulipconf.h>
-
-class QGLPixelBuffer;
-class QGLFramebufferObject;
+#include <QOpenGLFramebufferObject>
 
 namespace tlp {
 
-/** \brief Class to manage QGlPixelBuffer and QGlFramebufferObject
- * Singleton class to manager QGlPixelBuffer and QGlFramebufferObject
+/** \brief Class to manage QGlFramebufferObject
  */
 class TLP_QT_SCOPE QGlBufferManager {
 
 public:
-  /**
-   * Return the QGlBuffer manager singleton, il singleton doesn't exist this function create it
-   */
-  static QGlBufferManager &getInst() {
-    if (!inst)
-      inst = new QGlBufferManager();
-
-    return *inst;
-  }
 
   static void clearBuffers();
 
   /**
-   * Return if QGlPixelBuffer can be used
-   */
-  bool canUsePixelBuffer() {
-    return pixelBufferWork;
-  }
-
-  /**
    * Return if QGlFramebufferObject can be used
    */
-  bool canUseFramebufferObject() {
-    return framebufferObjectWork;
+  static bool canUseFramebufferObject() {
+    return QOpenGLFramebufferObject::hasOpenGLFramebufferObjects();
   }
 
   /**
-   * Return a QGlPixelBuffer with given size
+   * Return a QOpenGLFramebufferObject with given size
    */
-  QGLPixelBuffer *getPixelBuffer(int width, int height);
-
-  /**
-   * Return a QGLFramebufferObject with given size
-   */
-  QGLFramebufferObject *getFramebufferObject(int width, int height);
+  static QOpenGLFramebufferObject *getFramebufferObject(int width, int height);
 
 private:
-  /**
-   * empty private constructor for singleton
-   */
-  QGlBufferManager();
-
-  static QGlBufferManager *inst;
-
-  std::map<std::pair<int, int>, QGLPixelBuffer *> widthHeightToBuffer;
-  std::map<QGLPixelBuffer *, std::pair<int, int>> bufferToWidthHeight;
-  std::map<std::pair<int, int>, QGLFramebufferObject *> widthHeightToFramebuffer;
-  std::map<QGLFramebufferObject *, std::pair<int, int>> framebufferToWidthHeight;
-
-  bool pixelBufferWork;
-  bool framebufferObjectWork;
+  static std::map<std::pair<int, int>, QOpenGLFramebufferObject *> widthHeightToFramebuffer;
+  static std::map<QOpenGLFramebufferObject *, std::pair<int, int>> framebufferToWidthHeight;
 };
 } // namespace tlp
 

library/tulip-gui/include/tulip/QGlPixelBufferManager.h
@@ -1,31 +1,0 @@
-/*
- *
- * This file is part of Tulip (http://tulip.labri.fr)
- *
- * Authors: David Auber and the Tulip development Team
- * from LaBRI, University of Bordeaux
- *
- * Tulip is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * Tulip is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- * See the GNU General Public License for more details.
- *
- */
-///@cond DOXYGEN_HIDDEN
-
-#ifndef Tulip_QGLPIXELBUFFERMANAGER_H
-#define Tulip_QGLPIXELBUFFERMANAGER_H
-
-#include "tulip/QGlBufferManager.h"
-
-namespace tlp {
-
-typedef QGlBufferManager QGlPixelBufferManager;
-}
-#endif // Tulip_QGLPIXELBUFFERMANAGER_H
-///@endcond

library/tulip-gui/src/GlMainWidget.cpp
@@ -19,8 +19,7 @@
 
 #include <tulip/GlMainWidget.h>
 
-#include <QGLPixelBuffer>
-#include <QGLFramebufferObject>
+#include <QOpenGLFramebufferObject>
 #include <QGLFormat>
 #include <QWindow>
 #include <tulip/TulipSettings.h>
@@ -32,7 +31,7 @@
 #include <tulip/GlQuadTreeLODCalculator.h>
 #include <tulip/GLInteractor.h>
 #include <tulip/GlGraphComposite.h>
-#include <tulip/QGlPixelBufferManager.h>
+#include <tulip/QGlBufferManager.h>
 #include <tulip/Interactor.h>
 #include <tulip/GlCompositeHierarchyManager.h>
 #include <tulip/GlVertexArrayManager.h>
@@ -188,17 +187,17 @@ void GlMainWidget::setupOpenGlContext() {
 //==================================================
 void GlMainWidget::createRenderingStore(int width, int height) {
 
-  useFramebufferObject = advancedAntiAliasing && QGLFramebufferObject::hasOpenGLFramebufferBlit();
+  useFramebufferObject = advancedAntiAliasing && QOpenGLFramebufferObject::hasOpenGLFramebufferBlit();
 
   if (useFramebufferObject && (!glFrameBuf || glFrameBuf->size().width() != width ||
                                glFrameBuf->size().height() != height)) {
     makeCurrent();
     deleteRenderingStore();
-    QGLFramebufferObjectFormat fboFormat;
-    fboFormat.setAttachment(QGLFramebufferObject::CombinedDepthStencil);
+    QOpenGLFramebufferObjectFormat fboFormat;
+    fboFormat.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
     fboFormat.setSamples(OpenGlConfigManager::getInst().maxNumberOfSamples());
-    glFrameBuf = new QGLFramebufferObject(width, height, fboFormat);
-    glFrameBuf2 = new QGLFramebufferObject(width, height);
+    glFrameBuf = new QOpenGLFramebufferObject(width, height, fboFormat);
+    glFrameBuf2 = new QOpenGLFramebufferObject(width, height);
     useFramebufferObject = glFrameBuf->isValid();
     widthStored = width;
     heightStored = height;
@@ -258,8 +257,9 @@ void GlMainWidget::render(RenderingOptions options, bool checkVisibility) {
 
       if (useFramebufferObject) {
         glFrameBuf->release();
-        QGLFramebufferObject::blitFramebuffer(glFrameBuf2, QRect(0, 0, width, height), glFrameBuf,
-                                              QRect(0, 0, width, height));
+	QRect fbRect(0, 0, width, height);
+        QOpenGLFramebufferObject::blitFramebuffer(glFrameBuf2, fbRect,
+						  glFrameBuf, fbRect);
       }
     } else {
       scene.initGlParameters();
@@ -272,8 +272,9 @@ void GlMainWidget::render(RenderingOptions options, bool checkVisibility) {
     glDisable(GL_LIGHTING);
 
     if (useFramebufferObject) {
-      QGLFramebufferObject::blitFramebuffer(nullptr, QRect(0, 0, width, height), glFrameBuf2,
-                                            QRect(0, 0, width, height));
+      QRect fbRect(0, 0, width, height);
+      QOpenGLFramebufferObject::blitFramebuffer(nullptr, fbRect,
+						glFrameBuf2, fbRect);
     } else {
       if (options.testFlag(RenderScene)) {
         // Copy the back buffer (containing the graph render) in the rendering store to reuse it
@@ -476,15 +477,15 @@ void GlMainWidget::getTextureRealSize(int width, int height, int &textureRealWid
   }
 }
 //=====================================================
-QGLFramebufferObject *GlMainWidget::createTexture(const std::string &textureName, int width,
+QOpenGLFramebufferObject *GlMainWidget::createTexture(const std::string &textureName, int width,
                                                   int height) {
 
   makeCurrent();
   scene.setViewport(0, 0, width, height);
   scene.ajustSceneToSize(width, height);
 
-  QGLFramebufferObject *glFrameBuf =
-      QGlPixelBufferManager::getInst().getFramebufferObject(width, height);
+  QOpenGLFramebufferObject *glFrameBuf =
+      QGlBufferManager::getFramebufferObject(width, height);
   assert(glFrameBuf->size() == QSize(width, height));
 
   glFrameBuf->bind();
@@ -529,14 +530,14 @@ QImage GlMainWidget::createPicture(int width, int height, bool center) {
 
   GlMainWidget::getFirstQGLWidget()->makeCurrent();
 
-  QGLFramebufferObject *frameBuf = nullptr;
-  QGLFramebufferObject *frameBuf2 = nullptr;
+  QOpenGLFramebufferObject *frameBuf = nullptr;
+  QOpenGLFramebufferObject *frameBuf2 = nullptr;
 
-  QGLFramebufferObjectFormat fboFormat;
-  fboFormat.setAttachment(QGLFramebufferObject::CombinedDepthStencil);
+  QOpenGLFramebufferObjectFormat fboFormat;
+  fboFormat.setAttachment(QOpenGLFramebufferObject::CombinedDepthStencil);
   fboFormat.setSamples(OpenGlConfigManager::getInst().maxNumberOfSamples());
-  frameBuf = new QGLFramebufferObject(width, height, fboFormat);
-  frameBuf2 = new QGLFramebufferObject(width, height);
+  frameBuf = new QOpenGLFramebufferObject(width, height, fboFormat);
+  frameBuf2 = new QOpenGLFramebufferObject(width, height);
 
   if (frameBuf->isValid() && frameBuf2->isValid()) {
     frameBuf->bind();
@@ -564,7 +565,7 @@ QImage GlMainWidget::createPicture(int width, int height, bool center) {
     drawInteractors();
     frameBuf->release();
 
-    QGLFramebufferObject::blitFramebuffer(frameBuf2, QRect(0, 0, width, height), frameBuf,
+    QOpenGLFramebufferObject::blitFramebuffer(frameBuf2, QRect(0, 0, width, height), frameBuf,
                                           QRect(0, 0, width, height));
 
     resultImage = frameBuf2->toImage();
@@ -593,7 +594,7 @@ QImage GlMainWidget::createPicture(int width, int height, bool center) {
   delete frameBuf;
   delete frameBuf2;
 
-  // The QGLFramebufferObject returns the wrong image format QImage::Format_ARGB32_Premultiplied. We
+  // The QOpenGLFramebufferObject returns the wrong image format QImage::Format_ARGB32_Premultiplied. We
   // need to create an image from original data with the right format QImage::Format_ARGB32.
   // We need to clone the data as when the image var will be destroy at the end of the function it's
   // data will be destroyed too and the newly created image object will have invalid data pointer.

library/tulip-gui/src/GlMainWidgetGraphicsItem.cpp
@@ -19,7 +19,6 @@
 
 #include "tulip/GlMainWidgetGraphicsItem.h"
 
-#include <QGLFramebufferObject>
 #include <QGraphicsSceneMouseEvent>
 #include <QApplication>
 #include <QGraphicsProxyWidget>

commit 7e9060f3885c5ed7cee92749dacd2681ccd2aa1a

GeographicView: Fix snapshot feature broken in 1cf12ea

plugins/view/GeographicView/GeographicView.cpp
@@ -499,13 +499,14 @@ QPixmap GeographicView::snapshot(const QSize &size) const {
   fboPainter.setRenderHint(QPainter::HighQualityAntialiasing);
   geoViewGraphicsView->scene()->render(&fboPainter);
   QRect fboRect(0, 0, width, height);
-  QOpenGLFramebufferObject::blitFramebuffer(&fbo2, fboRect, nullptr, fboRect);
   fbo.release();
 
   // restore the graphics widgets previously hidden
   for (int i = 0; i < gWidgetsToRestore.size(); ++i) {
     gWidgetsToRestore.at(i)->show();
   }
+
+  QOpenGLFramebufferObject::blitFramebuffer(&fbo2, fboRect, &fbo, fboRect);
 
   QImage snapshotImage = fbo2.toImage();
   snapshotImage = QImage(snapshotImage.bits(), snapshotImage.width(), snapshotImage.height(),