ab219a6f1cc975216a8abbbb0a3c87ea8ba3f33c
Francois Queyroi <francois.queyroi@univ-nantes.fr>
2019-03-29 18:22:44 +0100

    Add boolean directed parameters to ERRandomGraph plugin

--- plugins/import/ERRandomGraph.cpp
+++ plugins/import/ERRandomGraph.cpp
@@ -33,7 +33,10 @@ const char *paramHelp[] = {
 
     // self loop
     "Generate self loops (an edge with source and target on the same node) with the same "
-    "probability"};
+    "probability",
+
+    // directed
+    "Generate a directed graph (arcs u->v and v->u have the same probability)"};
 }
 
 /** \addtogroup import */
@@ -54,6 +57,7 @@ public:
     addInParameter<unsigned int>("nodes", paramHelp[0], "50");
     addInParameter<double>("probability", paramHelp[1], "0.5");
     addInParameter<bool>("self loop", paramHelp[2], "false");
+    addInParameter<bool>("directed",paramHelp[3],"false");
   }
 
   bool importGraph() override {
@@ -63,11 +67,13 @@ public:
     unsigned int nbNodes = 50;
     double proba = 0.5;
     bool self_loop = false;
+    bool directed = false;
 
     if (dataSet != nullptr) {
       dataSet->get("nodes", nbNodes);
       dataSet->get("probability", proba);
       dataSet->get("self loop", self_loop);
+      dataSet->get("directed",directed);
     }
 
     if (nbNodes == 0) {
@@ -97,7 +103,11 @@ public:
       if (pluginProgress && pluginProgress->progress(i, nbNodes) != TLP_CONTINUE)
         return pluginProgress->state() != TLP_CANCEL;
 
-      for (unsigned int j = 0; j < nbNodes - i + 1; ++j) {
+      unsigned int max_index_j = nbNodes - i + 1;
+      if(directed)
+          max_index_j = nbNodes;
+
+      for (unsigned int j = 0; j < max_index_j; ++j) {
         node v = nodes[j];
 
         if ((u == v) && (!self_loop))
