f3ca655912fc8f091ea30b1606f9a77bfb6af6fc
p-mary <mary@labri.fr>
2019-04-04 10:30:43 +0200

    fix for truncation of SnapshotDialog width/height spin boxes on MacOSX

--- library/tulip-gui/src/SnapshotDialog.cpp
+++ library/tulip-gui/src/SnapshotDialog.cpp
@@ -104,8 +104,8 @@ SnapshotDialog::SnapshotDialog(const View *v, QWidget *parent)
           SLOT(clicked(QAbstractButton *)));
 
   lockLabel = new LockLabel();
-  ui->horizontalLayout_2->addWidget(lockLabel);
-  ui->horizontalLayout_2->setAlignment(lockLabel, Qt::AlignLeft | Qt::AlignVCenter);
+  ui->horizontalLayout_5->insertWidget(2, lockLabel);
+  ui->horizontalLayout_5->setAlignment(lockLabel, Qt::AlignLeft | Qt::AlignVCenter);
 }
 
 void SnapshotDialog::clicked(QAbstractButton *b) {
