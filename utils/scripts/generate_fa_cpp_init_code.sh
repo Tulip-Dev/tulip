0d0c7751f0ab531df86289e078c5d05d4fa41b15
p-mary <mary@labri.fr>
2019-04-11 15:52:25 +0200

    Font awesome icons 5.6.3 upgrade

--- utils/scripts/generate_fa_cpp_init_code.sh
+++ utils/scripts/generate_fa_cpp_init_code.sh
@@ -5,6 +5,10 @@
 # the first parameter is the _variable.scss file
 # found in the scss sub directory
 # after extracting files from a fontawesome downladable zip archive
+# the second is the cpp file which must be copied as
+# library/tulip-ogl/src/TulipInitFontAwesome.cpp
+# The files fa-[brands-400|regular-400|solid-900].[ttf|woff|woff2] from
+# the webfonts sub directory must be copied in library/tulip-ogl/bitmaps
 
 FA_VARIABLES_FILE=$1
 CPP_FILE=$2
@@ -22,7 +26,7 @@ FA_VERSION=$(grep fa-version ${FA_VARIABLES_FILE} | awk -F '"' '{print $2}')
  # grep 'fa-var-' ${FA_VARIABLES_FILE} | awk -F ';' '{print $1}' | awk -F '$' '{print $2}' | awk -F 'var-' '{print $1 $2}' | awk -F ': \' '{printf "  addIconCodePoint(\"%s\", 0x%s);\n", $1, $2}';
  grep 'fa-var-' ${FA_VARIABLES_FILE} | awk -F ';' '{print $1}' | awk -F '$' '{print $2}' | awk -F 'var-' '{print $1 $2}' | awk -F ': ' '{print $1 $2}' | awk -F '\' '{printf "  addIconCodePoint(\"%s\", 0x%s);\n", $1, $2}';
  echo;
- echo "#error fa-regular-400.ttf needs to be updated (see library/tulip-ogl/src/TulipInitFontAwesome.cpp)"
+ echo "#error fa-regular-400.ttf needs to be patched because the defined font has the same name that the one in fa-solid-900.ttf (see library/tulip-ogl/src/TulipFontAwesome.cpp)"
  echo;
  echo "  clearFtFaces();";
  echo;
