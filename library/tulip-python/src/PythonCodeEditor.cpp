Tulip-Dev/tulip

commit 7a398f0f843891af2eaa18944a4afa0757d138af

tulip-python/PythonCodeEditor: Fix heap-use-after-free memory issue
Spotted with clang asan, also delete static resources when exiting Tulip

library/tulip-python/src/PythonCodeEditor.cpp
@@ -79,8 +79,8 @@ private:
 };
 } // namespace tlp
 
-AutoCompletionList::AutoCompletionList(PythonCodeEditor *parent)
-    : QListWidget(), _codeEditor(parent) {
+AutoCompletionList::AutoCompletionList()
+    : QListWidget(), _codeEditor(nullptr) {
 #if defined(__APPLE__)
   setWindowFlags(static_cast<Qt::WindowFlags>(Qt::Popup | Qt::FramelessWindowHint));
 #else
@@ -89,7 +89,6 @@ AutoCompletionList::AutoCompletionList(PythonCodeEditor *parent)
   setAttribute(Qt::WA_StaticContents);
   setFrameShape(StyledPanel);
   setFrameShadow(Plain);
-  setFocusProxy(parent);
   installEventFilter(&keyboardFocusEventFilter);
   _activated = false;
   _wasActivated = false;
@@ -116,8 +115,8 @@ void AutoCompletionList::keyPressEvent(QKeyEvent *e) {
     }
   } else if (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) {
     e->accept();
-    close();
     insertSelectedItem();
+    close();
   } else {
     QCoreApplication::sendEvent(_codeEditor, e);
   }
@@ -216,6 +215,7 @@ void AutoCompletionList::hideEvent(QHideEvent *event) {
   releaseKeyboard();
   _codeEditor->setFocus();
   _activated = false;
+  _codeEditor = nullptr;
 }
 
 void AutoCompletionList::setCodeEditor(PythonCodeEditor *parent) {
@@ -225,7 +225,7 @@ void AutoCompletionList::setCodeEditor(PythonCodeEditor *parent) {
 }
 
 bool AutoCompletionList::eventFilter(QObject *obj, QEvent *event) {
-  if (obj != _codeEditor && obj != _codeEditor->mainWindow()) {
+  if (_codeEditor == nullptr || (obj != _codeEditor && obj != _codeEditor->mainWindow())) {
     return false;
   }
 
@@ -473,31 +473,31 @@ PythonCodeEditor::PythonCodeEditor(QWidget *parent)
   _parenHighlighter = new ParenMatcherHighlighter(document());
   _highlighter = new PythonCodeHighlighter(document());
 
-  if (_autoCompletionList == nullptr)
-    _autoCompletionList = new AutoCompletionList(this);
-  if (_autoCompletionDb == nullptr)
+  if (_autoCompletionList == nullptr) {
+    _autoCompletionList = new AutoCompletionList();
     _autoCompletionDb = new AutoCompletionDataBase(APIDataBase::getInstance());
 
-  // Hack to get a pointer on the main window
-  // in order for the autocompletion dialog to catch
-  // window activate/desactivate events
-  if (Perspective::instance()) {
-    _mainWindow = Perspective::instance()->mainWindow();
-  } else {
-    QWidget *parente = dynamic_cast<QWidget *>(this->parent());
+    // Hack to get a pointer on the main window
+    // in order for the autocompletion dialog to catch
+    // window activate/desactivate events
+    if (Perspective::instance()) {
+      _mainWindow = Perspective::instance()->mainWindow();
+    } else {
+      QWidget *parente = dynamic_cast<QWidget *>(this->parent());
 
-    while (parente) {
-      _mainWindow = dynamic_cast<QMainWindow *>(parente);
+      while (parente) {
+        _mainWindow = dynamic_cast<QMainWindow *>(parente);
 
-      if (_mainWindow)
-        break;
+        if (_mainWindow)
+          break;
 
-      parente = dynamic_cast<QWidget *>(parente->parent());
+        parente = dynamic_cast<QWidget *>(parente->parent());
+      }
     }
-  }
 
-  if (_mainWindow) {
-    _mainWindow->installEventFilter(_autoCompletionList);
+    if (_mainWindow) {
+      _mainWindow->installEventFilter(_autoCompletionList);
+    }
   }
 
   _findReplaceDialog = new FindReplaceDialog(this);

library/tulip-python/include/tulip/PythonCodeEditor.h
@@ -48,7 +48,7 @@ class TLP_PYTHON_SCOPE AutoCompletionList : public QListWidget {
   bool _wasActivated;
 
 public:
-  explicit AutoCompletionList(tlp::PythonCodeEditor *parent = nullptr);
+  explicit AutoCompletionList();
 
   void setCodeEditor(tlp::PythonCodeEditor *parent);
 
@@ -217,6 +217,11 @@ public:
 
   QMainWindow *mainWindow() const {
     return _mainWindow;
+  }
+
+  static void deleteStaticResources() {
+    delete _autoCompletionDb;
+    delete _autoCompletionList;
   }
 
 protected:

plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -22,6 +22,7 @@
 #include <tulip/PythonInterpreter.h>
 #include <tulip/APIDataBase.h>
 #include <tulip/PythonIDE.h>
+#include <tulip/PythonCodeEditor.h>
 #include "PythonPanel.h"
 #endif
 
@@ -281,6 +282,7 @@ GraphPerspective::~GraphPerspective() {
 
 #ifdef TULIP_BUILD_PYTHON_COMPONENTS
   delete _pythonIDEDialog;
+  PythonCodeEditor::deleteStaticResources();
 #endif
 
   delete _ui;

commit a23fe5a8ef92b67b98e75ab0e810563fcd3dbdca

tulip-python/PythonCodeEditor: Fix autocompletion list feature broken in 7a398f0
The selected string was not the right one to be inserted in the code editor.

library/tulip-python/src/PythonCodeEditor.cpp
@@ -114,8 +114,8 @@ void AutoCompletionList::keyPressEvent(QKeyEvent *e) {
     }
   } else if (e->key() == Qt::Key_Enter || e->key() == Qt::Key_Return) {
     e->accept();
-    insertSelectedItem();
     close();
+    insertSelectedItem();
   } else {
     QCoreApplication::sendEvent(_codeEditor, e);
   }
@@ -204,6 +204,7 @@ void AutoCompletionList::insertSelectedItem() {
 }
 
 void AutoCompletionList::showEvent(QShowEvent *event) {
+  setFocusProxy(_codeEditor);
   QListWidget::showEvent(event);
   grabKeyboard();
   _activated = true;
@@ -214,13 +215,11 @@ void AutoCompletionList::hideEvent(QHideEvent *event) {
   releaseKeyboard();
   _codeEditor->setFocus();
   _activated = false;
-  _codeEditor = nullptr;
 }
 
 void AutoCompletionList::setCodeEditor(PythonCodeEditor *parent) {
   _codeEditor = parent;
   parent->installEventFilter(this);
-  setFocusProxy(parent);
 }
 
 bool AutoCompletionList::eventFilter(QObject *obj, QEvent *event) {