1b811eb17e9d057776ba076271051759a206cff2
Antoine Lambert <antoine.lambert@inria.fr>
2019-05-30 20:02:25 +0200

    tulip-core/SimpleTest: Ensure no duplicated edges in removed vector (#117)
    The removed edges vector filled by method tlp::SimpleTest::makeSimple could
    contain duplicated edges for the corner case where an edge is a loop but also
    a multiple one.
    
    So ensure to remove duplicates to avoid processing errors in client code.

--- library/tulip-core/src/SimpleTest.cpp
+++ library/tulip-core/src/SimpleTest.cpp
@@ -61,6 +61,12 @@ void SimpleTest::makeSimple(Graph *graph, vector<edge> &removed, const bool dire
   SimpleTest::simpleTest(graph, &removed, &removed, directed);
   vector<edge>::const_iterator it;
 
+  // multiple edges and loops were pushed back into the removed vector
+  // but an edge can be a loop and a multiple one at the same time
+  // so ensure no duplicate edges in the removed vector
+  sort(removed.begin(), removed.end());
+  removed.erase(unique(removed.begin(), removed.end()), removed.end());
+
   for (it = removed.begin(); it != removed.end(); ++it) {
     graph->delEdge(*it);
   }
