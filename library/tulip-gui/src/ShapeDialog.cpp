40293a53a546fea1764d3eec07565ad3463340db
p-mary <mary@labri.fr>
2019-06-14 12:23:15 +0200

    add missing Tulip License header

--- library/tulip-gui/src/ShapeDialog.cpp
+++ library/tulip-gui/src/ShapeDialog.cpp
@@ -1,3 +1,21 @@
+/**
+ *
+ * This file is part of Tulip (http://tulip.labri.fr)
+ *
+ * Authors: David Auber and the Tulip development Team
+ * from LaBRI, University of Bordeaux
+ *
+ * Tulip is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * Tulip is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ */
 #include <tulip/ShapeDialog.h>
 
 #include "ui_ShapeDialog.h"
