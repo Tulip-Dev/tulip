Tulip-Dev/tulip

commit 212228bc568630bf9fa8b5f6a47033b2d75eac51

tulip-python: Improve formatting and cleanup Python code
Format Tulip-Python code according to the recommended coding style detailed
in PEP-8 (thanks to the flake8 tool).

Also cleanup code in favor of more Python idioms and recommended syntax.

library/tulip-python/bindings/tulip-gui/__init__.py
@@ -19,7 +19,6 @@ import os
 import os.path
 import sys
 import platform
-from datetime import date
 
 from tulip import tlp
 
@@ -28,70 +27,80 @@ from tulip import tlp
 # it ends up loading the mesa one instead of the nvidia one.
 # The use of the tulipgui module through the classical python intrepreter
 # was quite unusable because of that ...
-# Apply the workaround proposed here : https://bugs.launchpad.net/ubuntu/+source/python-qt4/+bug/941826
+# Apply the workaround proposed here :
+# https://bugs.launchpad.net/ubuntu/+source/python-qt4/+bug/941826
 if hasattr(os, 'uname'):
-  if os.uname()[3].lower().find('ubuntu') != -1:
-    import ctypes.util
-    ctypes.CDLL(ctypes.util.find_library('GL'), ctypes.RTLD_GLOBAL)
+    if os.uname()[3].lower().find('ubuntu') != -1:
+        import ctypes.util
+        ctypes.CDLL(ctypes.util.find_library('GL'), ctypes.RTLD_GLOBAL)
 
-_tulipGuiNativeLibsPath = os.path.dirname(__file__) + '/native/'
-_tulipGuiNativePluginsPath = _tulipGuiNativeLibsPath + 'plugins'
-_tulipGuiPluginsPath = os.path.dirname(__file__) + '/plugins/'
+_tulipGuiNativeLibsPath = '%s/native/' % os.path.dirname(__file__)
+_tulipGuiNativePluginsPath = '%splugins' % _tulipGuiNativeLibsPath
+_tulipGuiPluginsPath = '%s/plugins/' % os.path.dirname(__file__)
 
 sys.path.append(_tulipGuiNativeLibsPath)
 
 if platform.system() == 'Windows':
-  os.environ['PATH'] = _tulipGuiNativeLibsPath + ';' + _tulipGuiNativeLibsPath + '../../../;' + os.environ['PATH']
+    os.environ['PATH'] = '%s;%s../../../;%s' % (
+        _tulipGuiNativeLibsPath, _tulipGuiNativeLibsPath, os.environ['PATH'])
 
 if not sys.argv[0] == 'tulip':
-  # when the tulipgui module is installed from the Python Packaging Index
-  # modify the TulipBitmapDir variable as it is different from the default one
-  bitmapDir = os.path.dirname(__file__) + '/share/bitmaps/'
-  if os.path.isdir(bitmapDir):
-    tlp.TulipBitmapDir = bitmapDir
-    tlp.TulipPluginsPath = _tulipGuiNativePluginsPath
-    tlp.TulipViewSettings.instance().setDefaultFontFile(tlp.TulipBitmapDir + 'font.ttf')
+    # when the tulipgui module is installed from the Python Packaging Index
+    # modify the TulipBitmapDir variable as it is different from
+    # the default one
+    bitmapDir = '%s/share/bitmaps/' % os.path.dirname(__file__)
+    if os.path.isdir(bitmapDir):
+        tlp.TulipBitmapDir = bitmapDir
+        tlp.TulipPluginsPath = _tulipGuiNativePluginsPath
+        tlp.TulipViewSettings.instance().setDefaultFontFile(
+            '%sfont.ttf' % tlp.TulipBitmapDir)
 
-import _tulipgui
+
+import _tulipgui # noqa
 
 sys.path.pop()
 
+
 class tlpgui(_tulipgui.tlpgui):
-  pass
+    pass
+
 
 def tulipguiExitFunc():
-  import tulipgui
-  tulipgui.tlpgui.runQtMainLoop()
+    import tulipgui
+    tulipgui.tlpgui.runQtMainLoop()
 
-# fix loading of Tulip plugins when the tulipgui module has been installed through pip
+
+# fix loading of Tulip plugins when the tulipgui module has been installed
+# with the pip tool
 if platform.system() == 'Linux' and os.path.exists(_tulipGuiNativePluginsPath):
-  dlOpenFlagsBackup = sys.getdlopenflags()
-  if sys.version_info < (3, 6):
-    import DLFCN
-    dlOpenFlags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
-  else:
-    dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
-  sys.setdlopenflags(dlOpenFlags)
+    dlOpenFlagsBackup = sys.getdlopenflags()
+    if sys.version_info < (3, 6):
+        import DLFCN
+        dlOpenFlags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
+    else:
+        dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
+    sys.setdlopenflags(dlOpenFlags)
 
-tlp.loadTulipPluginsFromDir(_tulipGuiPluginsPath)
+    tlp.loadTulipPluginsFromDir(_tulipGuiPluginsPath)
 
-if platform.system() == 'Linux' and os.path.exists(_tulipGuiNativePluginsPath):
-  sys.setdlopenflags(dlOpenFlagsBackup)
+    if (platform.system() == 'Linux'
+            and os.path.exists(_tulipGuiNativePluginsPath)):
+        sys.setdlopenflags(dlOpenFlagsBackup)
 
-# Check if we are in script execution mode (sys.ps1 is not defined in that case)
-# If so, register an exit callback that will run the Qt event loop if some widgets are
-# still in a visible state
-if not hasattr(sys, 'ps1') and not sys.flags.interactive:
-  import atexit
-  atexit.register(tulipguiExitFunc)
+    # Check if we are in script execution mode
+    # (sys.ps1 is not defined in that case)
+    # If so, register an exit callback that will run the Qt event loop
+    # if some widgets are still in a visible state
+    if not hasattr(sys, 'ps1') and not sys.flags.interactive:
+        import atexit
+        atexit.register(tulipguiExitFunc)
 
-import signal
-signal.signal(signal.SIGINT, signal.SIG_DFL)
+    import signal
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 __all__ = ['tlpgui']
-__author__ = "David Auber, and the Tulip development Team"
-__copyright__ = "Copyright " + str(date.today().year) + ", LaBRI and University of Bordeaux"
-__license__ = "LGPLv3"
+__author__ = 'David Auber, and the Tulip development Team'
+__license__ = 'LGPLv3'
 __version__ = tlp.getTulipRelease()
-__email__ = "tulipdev@labri.fr"
-__status__ = "Production"
+__email__ = 'tulipdev@labri.fr'
+__status__ = 'Production'

library/tulip-python/bindings/tulip-core/__init__.py
@@ -20,146 +20,164 @@ import os.path
 import sys
 import traceback
 import platform
-from datetime import date
 
 _tulipNativeLibsPath = os.path.dirname(__file__) + '/native/'
 sys.path.append(_tulipNativeLibsPath)
 
 if platform.system() == 'Windows':
-  os.environ['PATH'] = _tulipNativeLibsPath + ';' + _tulipNativeLibsPath + '../../../;' + os.environ['PATH']
+    os.environ['PATH'] = '%s;%s../../../;%s' % (_tulipNativeLibsPath,
+                                                _tulipNativeLibsPath,
+                                                os.environ['PATH'])
 
-import _tulip
+import _tulip # noqa
 
 sys.path.pop()
 
+
 class tlpType(_tulip.tlp.__class__):
 
-  def __getattr__(cls, name):
-    if hasattr(_tulip.tlp, name):
-      return _tulip.tlp.getTulipGlobalVar(name)
-    else:
-      raise AttributeError(name)
+    def __getattr__(cls, name):
+        if hasattr(_tulip.tlp, name):
+            return _tulip.tlp.getTulipGlobalVar(name)
+        else:
+            raise AttributeError(name)
 
-  def __setattr__(cls, name, value):
-    if hasattr(_tulip.tlp, name):
-      _tulip.tlp.setTulipGlobalVar(name, value)
-    else:
-      super(tlpType, cls).__setattr__(name, value)
+    def __setattr__(cls, name, value):
+        if hasattr(_tulip.tlp, name):
+            _tulip.tlp.setTulipGlobalVar(name, value)
+        else:
+            super(tlpType, cls).__setattr__(name, value)
+
 
 # utility function from the 'six' module
 def with_metaclass(meta, *bases):
-  """Create a base class with a metaclass."""
-  # This requires a bit of explanation: the basic idea is to make a dummy
-  # metaclass for one level of class instantiation that replaces itself with
-  # the actual metaclass.
-  class metaclass(meta):
-    def __new__(cls, name, this_bases, d):
-      return meta(name, bases, d)
-    
-  return type.__new__(metaclass, 'temporary_class', (), {})
+    """Create a base class with a metaclass."""
+    # This requires a bit of explanation: the basic idea is to make a dummy
+    # metaclass for one level of class instantiation that replaces itself with
+    # the actual metaclass.
+    class metaclass(meta):
+        def __new__(cls, name, this_bases, d):
+            return meta(name, bases, d)
+
+    return type.__new__(metaclass, 'temporary_class', (), {})
+
 
 class tlp(with_metaclass(tlpType, _tulip.tlp)):
-  @staticmethod
-  def loadTulipPythonPlugin(pluginFilePath):
-    if not os.path.isfile(pluginFilePath) or not pluginFilePath.endswith('.py'):
-      return False
 
-    try:
-      pluginFile = open(pluginFilePath)
-      pluginFileContent = pluginFile.read()
-    except:
-      return False
+    @staticmethod
+    def loadTulipPythonPlugin(pluginFilePath):
+        if (not os.path.isfile(pluginFilePath) or
+                not pluginFilePath.endswith('.py')):
+            return False
 
-    if not 'tulipplugins.register' in pluginFileContent:
-      return False
+        try:
+            pluginFile = open(pluginFilePath)
+            pluginFileContent = pluginFile.read()
+        except Exception:
+            return False
 
-    modulePath = os.path.dirname(pluginFilePath)
-    moduleName = os.path.basename(pluginFilePath)[:-3]
+        if 'tulipplugins.register' not in pluginFileContent:
+            return False
 
-    if not modulePath in sys.path:
-      sys.path.append(modulePath)
+        modulePath = os.path.dirname(pluginFilePath)
+        moduleName = os.path.basename(pluginFilePath)[:-3]
 
-    try:
-      mod = __import__(moduleName)
-    except ImportError:
-      sys.stdout.write('There was an error when trying to load the Tulip Python plugin from the file ' + pluginFilePath + '. See stack trace below.\\n')
-      traceback.print_exc()
-      return False
+        if modulePath not in sys.path:
+            sys.path.append(modulePath)
 
-    return True
+        try:
+            __import__(moduleName)
+        except ImportError:
+            sys.stdout.write(('There was an error when trying to load the'
+                              ' Tulip Python plugin from the file %s. See '
+                              'stack trace below.\\n') % pluginFilePath)
+            traceback.print_exc()
+            return False
 
-  @staticmethod
-  def loadTulipPluginsFromDir(pluginsDirPath, loadCppPlugin=True, pluginLoader=None):
-    if not os.path.exists(pluginsDirPath):
-      return False
+        return True
 
-    if loadCppPlugin:
-      tlp.loadPluginsFromDir(pluginsDirPath, pluginLoader, False)
+    @staticmethod
+    def loadTulipPluginsFromDir(pluginsDirPath, loadCppPlugin=True,
+                                pluginLoader=None):
+        if not os.path.exists(pluginsDirPath):
+            return False
 
-    files = os.listdir(pluginsDirPath)
+        if loadCppPlugin:
+            tlp.loadPluginsFromDir(pluginsDirPath, pluginLoader, False)
 
-    for file in files:
-      filePath = pluginsDirPath+'/'+file
-      if not os.path.isdir(filePath):
-        tlp.loadTulipPythonPlugin(filePath)
+        files = os.listdir(pluginsDirPath)
 
-    for file in files:
-      filePath = pluginsDirPath+'/'+file
-      if os.path.isdir(filePath):
-        tlp.loadTulipPluginsFromDir(filePath, loadCppPlugin, pluginLoader)
+        for file in files:
+            filePath = pluginsDirPath+'/'+file
+            if not os.path.isdir(filePath):
+                tlp.loadTulipPythonPlugin(filePath)
 
-    return True
+        for file in files:
+            filePath = pluginsDirPath+'/'+file
+            if os.path.isdir(filePath):
+                tlp.loadTulipPluginsFromDir(filePath, loadCppPlugin,
+                                            pluginLoader)
+
+        return True
+
 
 tulipVersion = tlp.getTulipRelease()
 tulipVersion = tulipVersion[:tulipVersion.rfind('.')]
 
-startupScriptsPath = tlp.TulipLibDir + '/tulip/python/startup'
-startupScriptsHomePath = os.path.expanduser('~') + '/.Tulip-' + tulipVersion + '/python/startup'
+startupScriptsPath = '%s/tulip/python/startup' % tlp.TulipLibDir
+startupScriptsHomePath = '%s/.Tulip-%s/python/startup' % (
+    os.path.expanduser('~'), tulipVersion)
+
 
 def runStartupScripts(scriptsPath):
-  if not os.path.exists(scriptsPath):
-    return
+    if not os.path.exists(scriptsPath):
+        return
 
-  files = os.listdir(scriptsPath)
+    files = os.listdir(scriptsPath)
 
-  for file in files:
-    filePath = scriptsPath+'/'+file
-    if os.path.isfile(filePath) and filePath.endswith('.py'):
-      exec(compile(open(filePath).read(), filePath, 'exec'), globals(), locals())
+    for file in files:
+        filePath = scriptsPath+'/'+file
+        if os.path.isfile(filePath) and filePath.endswith('.py'):
+            exec(compile(open(filePath).read(), filePath, 'exec'),
+                 globals(), locals())
+
 
 runStartupScripts(startupScriptsPath)
 runStartupScripts(startupScriptsHomePath)
 
-tlpPythonPluginsPath = tlp.TulipLibDir + '/tulip/python/tulip/plugins'
-tlpPythonPluginsHomePath = os.path.expanduser('~') + '/.Tulip-' + tulipVersion + '/plugins/python'
+tlpPythonPluginsPath = '%s/tulip/python/tulip/plugins' % tlp.TulipLibDir
+tlpPythonPluginsHomePath = '%s/.Tulip-%s/plugins/python' % (
+    os.path.expanduser('~'), tulipVersion)
 
 tlp.loadTulipPluginsFromDir(tlpPythonPluginsPath, False)
 tlp.loadTulipPluginsFromDir(tlpPythonPluginsHomePath, False)
 
 _tulipNativePluginsPath = _tulipNativeLibsPath + 'plugins'
 
-# fix loading of Tulip plugins when the tulip module has been installed through pip
+# fix loading of Tulip plugins when the tulip module has been
+# installed with the pip tool
 if platform.system() == 'Linux' and os.path.exists(_tulipNativePluginsPath):
-  dlOpenFlagsBackup = sys.getdlopenflags()
-  if sys.version_info < (3, 6):
-    import DLFCN
-    dlOpenFlags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
-  else:
-    dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
-  sys.setdlopenflags(dlOpenFlags)
+    dlOpenFlagsBackup = sys.getdlopenflags()
+    if sys.version_info < (3, 6):
+        import DLFCN
+        dlOpenFlags = DLFCN.RTLD_NOW | DLFCN.RTLD_GLOBAL
+    else:
+        dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
+    sys.setdlopenflags(dlOpenFlags)
 
 tlp.loadTulipPluginsFromDir(_tulipNativePluginsPath)
 
-# load bundled Tulip Python plugins when the tulip module has been installed with pip
+# load bundled Tulip Python plugins when the tulip module has been
+# installed with the pip tool
 if not sys.argv[0] == 'tulip':
-  tlp.loadTulipPluginsFromDir(os.path.dirname(__file__) + '/plugins/')
+    tlp.loadTulipPluginsFromDir('%s/plugins/' % os.path.dirname(__file__))
 
 if platform.system() == 'Linux' and os.path.exists(_tulipNativePluginsPath):
-  sys.setdlopenflags(dlOpenFlagsBackup)
+    sys.setdlopenflags(dlOpenFlagsBackup)
 
 __all__ = ['tlp']
-__author__ = "David Auber and the Tulip development Team"
-__license__ = "LGPLv3"
+__author__ = 'David Auber and the Tulip development Team'
+__license__ = 'LGPLv3'
 __version__ = tlp.getTulipRelease()
-__email__ = "tulipdev@labri.fr"
-__status__ = "Production"
+__email__ = 'tulipdev@labri.fr'
+__status__ = 'Production'

doc/developer/conf.py.in
@@ -3,46 +3,36 @@
 # userHandbook documentation build configuration file, created by
 # sphinx-quickstart on Mon Jul  1 10:58:47 2013.
 #
-# This file is execfile()d with the current directory set to its containing dir.
-#
 # Note that not all possible configuration values are present in this
 # autogenerated file.
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, sphinx
+import sphinx
 
 sphinxVersionStr = sphinx.__version__
 sphinxVersion = float(sphinxVersionStr[:sphinxVersionStr.rfind('.')])
 
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# -- General configuration ----------------------------------------------------
 
-# -- General configuration -----------------------------------------------------
-
-# If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
-
-# Add any Sphinx extension module names here, as strings. They can be extensions
-# coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest',
+              'sphinx.ext.intersphinx', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.ifconfig',
+              'sphinx.ext.viewcode']
 if sphinxVersion < 1.4:
-  extensions.append('sphinx.ext.pngmath')
-  extensions.append('sphinx.ext.mathjax')
+    extensions.append('sphinx.ext.pngmath')
+    extensions.append('sphinx.ext.mathjax')
 else:
-  extensions.append('sphinx.ext.imgmath')
+    extensions.append('sphinx.ext.imgmath')
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['@CMAKE_CURRENT_SOURCE_DIR@/../common/_templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
-
-# The encoding of source files.
-#source_encoding = 'utf-8-sig'
 
 # The master toctree document.
 master_doc = 'index'
@@ -51,76 +41,25 @@ master_doc = 'index'
 project = u'Tulip Developer Handbook'
 copyright = u'@CurrentYear@, Tulip Team'
 
-# The version info for the project you're documenting, acts as replacement for
-# |version| and |release|, also used in various other places throughout the
-# built documents.
-#
 # The short X.Y version.
 version = '@TulipMMVersion@'
 # The full version, including alpha/beta/rc tags.
 release = '@TulipVersion@'
 
-# The language for content autogenerated by Sphinx. Refer to documentation
-# for a list of supported languages.
-#language = None
-
-# There are two options for replacing |today|: either, you set today to some
-# non-false value, then it is used:
-#today = ''
-# Else, today_fmt is used as the format for a strftime call.
-#today_fmt = '%B %d, %Y'
-
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 exclude_patterns = []
-
-# The reST default role (used for this markup: `text`) to use for all documents.
-#default_role = None
-
-# If true, '()' will be appended to :func: etc. cross-reference text.
-#add_function_parentheses = True
-
-# If true, the current module name will be prepended to all description
-# unit titles (such as .. function::).
-#add_module_names = True
-
-# If true, sectionauthor and moduleauthor directives will be shown in the
-# output. They are ignored by default.
-#show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
 pygments_style = 'sphinx'
 highlight_language = 'c'
 
-# A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
-
-
-# -- Options for HTML output ---------------------------------------------------
+# -- Options for HTML output --------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'agogo'
 html_style = 'customized.css'
-
-# Theme options are theme-specific and customize the look and feel of a theme
-# further.  For a list of options available for each theme, see the
-# documentation.
-#html_theme_options = {}
-
-# Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
-
-# The name for this set of Sphinx documents.  If None, it defaults to
-# "<project> v<release> documentation".
-#html_title = None
-
-# A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = None
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
-#html_logo = './_static/logo.bmp'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -132,29 +71,8 @@ html_favicon = '@CMAKE_CURRENT_SOURCE_DIR@/../../textures/logo32x32.ico'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['@CMAKE_CURRENT_SOURCE_DIR@/../common/_static']
 
-# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
-# using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
-
-# If true, SmartyPants will be used to convert quotes and dashes to
-# typographically correct entities.
-#html_use_smartypants = True
-
-# Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
-
-# Additional templates that should be rendered to pages, maps page names to
-# template names.
-#html_additional_pages = {}
-
-# If false, no module index is generated.
-#html_domain_indices = True
-
 # If false, no index is generated.
 html_use_index = False
-
-# If true, the index is split into individual pages for each letter.
-#html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
 html_show_sourcelink = False
@@ -162,63 +80,19 @@ html_show_sourcelink = False
 # If true, "Created using Sphinx" is shown in the HTML footer. Default is True.
 html_show_sphinx = False
 
-# If true, "(C) Copyright ..." is shown in the HTML footer. Default is True.
-#html_show_copyright = True
-
-# If true, an OpenSearch description file will be output, and all pages will
-# contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished HTML is served.
-#html_use_opensearch = ''
-
-# This is the file name suffix for HTML files (e.g. ".xhtml").
-#html_file_suffix = None
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'TulipDeveloperHandbookdoc'
 
-
-# -- Options for LaTeX output --------------------------------------------------
-
-latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
-
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
-
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
-}
+# -- Options for LaTeX output -------------------------------------------------
 
 # Grouping the document tree into LaTeX files. List of tuples
-# (source start file, target name, title, author, documentclass [howto/manual]).
+# (source start file, target name, title, author, documentclass).
 latex_documents = [
   ('index', 'TulipDeveloperrHandbook.tex', u'Tulip Developer Documentation',
    u'Tulip Team', 'manual'),
 ]
 
-# The name of an image file (relative to this directory) to place at the top of
-# the title page.
-#latex_logo = None
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
-
-# If true, show page references after internal links.
-#latex_show_pagerefs = False
-
-# If true, show URL addresses after external links.
-#latex_show_urls = False
-
-# Documents to append as an appendix to all manuals.
-#latex_appendices = []
-
-# If false, no module index is generated.
-#latex_domain_indices = True
-
-
-# -- Options for manual page output --------------------------------------------
+# -- Options for manual page output -------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
@@ -227,11 +101,7 @@ man_pages = [
      [u'Tulip Team'], 1)
 ]
 
-# If true, show URL addresses after external links.
-#man_show_urls = False
-
-
-# -- Options for Texinfo output ------------------------------------------------
+# -- Options for Texinfo output -----------------------------------------------
 
 # Grouping the document tree into Texinfo files. List of tuples
 # (source start file, target name, title, author,
@@ -242,58 +112,13 @@ texinfo_documents = [
    'Miscellaneous'),
 ]
 
-# Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
-
-# If false, no module index is generated.
-#texinfo_domain_indices = True
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
-
-
-# -- Options for Epub output ---------------------------------------------------
+# -- Options for Epub output --------------------------------------------------
 
 # Bibliographic Dublin Core info.
 epub_title = u'TulipDeveloperHandbook'
 epub_author = u'Tulip Team'
 epub_publisher = u'Tulip Team'
 epub_copyright = u'@CurrentYear@, Tulip Team'
-
-# The language of the text. It defaults to the language option
-# or en if the language is not set.
-#epub_language = ''
-
-# The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#epub_identifier = ''
-
-# A unique identification for the text.
-#epub_uid = ''
-
-# A tuple containing the cover image and cover page html template filenames.
-#epub_cover = ()
-
-# HTML files that should be inserted before the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_pre_files = []
-
-# HTML files shat should be inserted after the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_post_files = []
-
-# A list of files that should not be packed into the epub file.
-#epub_exclude_files = []
-
-# The depth of the table of contents in toc.ncx.
-#epub_tocdepth = 3
-
-# Allow duplicate toc entries.
-#epub_tocdup = True
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'http://docs.python.org/': None}

commit d558c7e6685485151a3a55a11ee5377c3f33a0de

tulip-python/tulipgui: Fix regression introduced in 212228b
Wrong indentation level was introduced when formatting the code according to PEP-8

library/tulip-python/bindings/tulip-gui/__init__.py
@@ -81,22 +81,22 @@ if platform.system() == 'Linux' and os.path.exists(_tulipGuiNativePluginsPath):
         dlOpenFlags = os.RTLD_NOW | os.RTLD_GLOBAL
     sys.setdlopenflags(dlOpenFlags)
 
-    tlp.loadTulipPluginsFromDir(_tulipGuiPluginsPath)
+tlp.loadTulipPluginsFromDir(_tulipGuiPluginsPath)
 
-    if (platform.system() == 'Linux'
-            and os.path.exists(_tulipGuiNativePluginsPath)):
-        sys.setdlopenflags(dlOpenFlagsBackup)
+if (platform.system() == 'Linux'
+        and os.path.exists(_tulipGuiNativePluginsPath)):
+    sys.setdlopenflags(dlOpenFlagsBackup)
 
-    # Check if we are in script execution mode
-    # (sys.ps1 is not defined in that case)
-    # If so, register an exit callback that will run the Qt event loop
-    # if some widgets are still in a visible state
-    if not hasattr(sys, 'ps1') and not sys.flags.interactive:
-        import atexit
-        atexit.register(tulipguiExitFunc)
+# Check if we are in script execution mode
+# (sys.ps1 is not defined in that case)
+# If so, register an exit callback that will run the Qt event loop
+# if some widgets are still in a visible state
+if not hasattr(sys, 'ps1') and not sys.flags.interactive:
+    import atexit
+    atexit.register(tulipguiExitFunc)
 
-    import signal
-    signal.signal(signal.SIGINT, signal.SIG_DFL)
+import signal # noqa
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 __all__ = ['tlpgui']
 __author__ = 'David Auber, and the Tulip development Team'