2b509c143e92a2d8f9aacb7e256803f36575f259
janos-varga <real.janos@gmail.com>
2019-11-10 12:02:17 +0100

    VS2019 build fixes, Python 3.8 api added

--- library/tulip-gui/src/PythonVersionChecker.cpp
+++ library/tulip-gui/src/PythonVersionChecker.cpp
@@ -30,8 +30,9 @@ using namespace tlp;
 using namespace std;
 
 // Current Python versions
-static const char *pythonVersion[] = {"3.7", "3.6", "3.5", "3.4", "3.3", "3.2",
-                                      "3.1", "3.0", "2.7", "2.6", "2.5", nullptr};
+static const char *pythonVersion[] = {"3.8", "3.7", "3.6", "3.5", "3.4", "3.3", 
+                                      "3.2", "3.1", "3.0", "2.7", "2.6", "2.5", 
+                                      nullptr};
 
 // Windows specific functions
 #ifdef WIN32
