dd1bf602f09c6601e01754e539d60b611a4fe581
p-mary <mary@labri.fr>
2020-02-13 16:29:14 +0100

    GeographicView improvements: delay the display of geolocation failures until the end of geocoding loop + show them as log warnings to allow copy/paste

--- plugins/view/GeographicView/GeographicViewGraphicsView.cpp
+++ plugins/view/GeographicView/GeographicViewGraphicsView.cpp
@@ -766,6 +766,7 @@ void GeographicViewGraphicsView::createLayoutWithAddresses(const string &address
 
     Iterator<node> *nodesIt = graph->getNodes();
     node n;
+    unsigned int failures = 0;
 
     while (nodesIt->hasNext() && !progressWidget->cancelRequested() && !cancelGeocoding) {
 
@@ -802,7 +803,7 @@ void GeographicViewGraphicsView::createLayoutWithAddresses(const string &address
 
           unsigned int idx = 0;
           vector<NominatimGeocoderResult> &&geocodingResults =
-              nominatimGeocoder.getLatLngForAddress(addr);
+	    nominatimGeocoder.getLatLngForAddress(addr);
 
           if (geocodingResults.size() > 1) {
             bool showProgressWidget = false;
@@ -826,11 +827,8 @@ void GeographicViewGraphicsView::createLayoutWithAddresses(const string &address
               progressWidget->show();
             }
           } else if (geocodingResults.empty()) {
-            progressWidget->hide();
-            QMessageBox::warning(Perspective::instance()->mainWindow(), "Geolocation failed",
-                                 "No results were found for address : \n" +
-                                     tlpStringToQString(addr));
-            progressWidget->show();
+	    qWarning() << "No geolocation found for" << tlpStringToQString(addr);
+	    failures++;
           }
 
           if (geocodingResults.size() > 0) {
@@ -851,6 +849,19 @@ void GeographicViewGraphicsView::createLayoutWithAddresses(const string &address
 
     delete nodesIt;
     progressWidget->hide();
+    if (failures) {
+      QString msg =
+	QString("%1 %2 have not been geolocated.\nDo you want to see %3 ?")
+	.arg(failures > 1 ? QString::number(failures) : QString("One"))
+	.arg(failures > 1 ? "addresses" : "address")
+	.arg(failures > 1 ? "them" : "it");
+
+      if (QMessageBox::warning(Perspective::instance()->mainWindow(),
+			       "Geolocation failed", msg,
+			       QMessageBox::Yes | QMessageBox::No,
+			       QMessageBox::Yes) == QMessageBox::Yes)
+	Perspective::showLogMessages();
+    }
   }
 
   Observable::unholdObservers();
