Tulip-Dev/tulip

commit 933aad903df44344ccff8d040932278d8e612257

more tulip-gui build cleanup

library/tulip-gui/src/PythonVersionChecker.cpp
@@ -250,7 +250,7 @@ QStringList PythonVersionChecker::installedVersions() {
 }
 
 QString PythonVersionChecker::compiledVersion() {
-#ifdef TULIP_BUILD_PYTHON_COMPONENTS
+#ifdef TLP_PYTHON
   return TLP_PYTHON;
 #else
   return QString();

library/tulip-gui/src/TlpQtTools.cpp
@@ -351,16 +351,14 @@ void initTulipSoftware(tlp::PluginLoader *loader, bool removeDiscardedPlugins) {
   QApplication::addLibraryPath(QApplication::applicationDirPath() + "/../lib/");
 #endif
 
-#if defined(__MINGW32__) && defined(TULIP_BUILD_PYTHON_COMPONENTS)
+#if defined(TULIP_BUILD_PYTHON_COMPONENTS) && defined(WIN32)
+#if defined(__MINGW32__)
   // When using MSYS2 platform to compile Tulip, force the dynamic loading of
   // OpenSSL libraries Qt was compiled against before Python initialization to
   // avoid a DLL Hell on windows
   QSslSocket::supportsSsl();
-#endif
-
-#ifdef TULIP_BUILD_PYTHON_COMPONENTS
-// MS stated that SetDllDirectory only exists since WinXP SP1
-#if defined(WIN32) && (_WIN32_WINNT >= 0x0502)
+#elif (_WIN32_WINNT >= 0x0502)
+  // MS stated that SetDllDirectory only exists since WinXP SP1
 
   // Python on windows can be installed for current user only.
   // In that case, the Python dll is not located in system path but in the Python home directory.
@@ -369,7 +367,6 @@ void initTulipSoftware(tlp::PluginLoader *loader, bool removeDiscardedPlugins) {
   if (tlp::PythonVersionChecker::isPythonVersionMatching()) {
     SetDllDirectory(tlp::QStringToTlpString(tlp::PythonVersionChecker::getPythonHome()).c_str());
   }
-
 #endif
 #endif
 

library/tulip-gui/src/AboutTulipPage.cpp
@@ -41,11 +41,13 @@
 #include <QNetworkReply>
 #include <QXmlStreamReader>
 
-#ifdef TULIP_BUILD_PYTHON_COMPONENTS
 static QString getSipVersion() {
+#ifdef SIP_VERSION
   return SIP_VERSION;
-}
+#else
+  return QString();
 #endif
+}
 
 static QString getTulipGitRevision() {
   QFile gitCommitFile(tlp::tlpStringToQString(tlp::TulipShareDir + "GIT_COMMIT"));
@@ -109,19 +111,17 @@ AboutTulipPage::AboutTulipPage(QWidget *parent)
       "<li><b>OGDF</b> v" +
       OGDF_VERSION +
       ": <a "
-      "href=\"http://ogdf.net/\"><span style=\"color: #0d47f1;\">ogdf.net</span></a> </li>"
-#ifdef TULIP_BUILD_PYTHON_COMPONENTS
-      "  <li> <b> Python </b> " +
+      "href=\"http://ogdf.net/\"><span style=\"color: #0d47f1;\">ogdf.net</span></a> </li>" +
+      (!PythonVersionChecker::compiledVersion().isNull() ?
+      QString("  <li> <b> Python </b> ") +
       PythonVersionChecker::compiledVersion() +
       ": <a href=\"https://www.python.org\"><span style=\"color: "
       "#0d47f1;\">www.python.org</span></a> </li>"
-      "  <li> <b> SIP </b> " +
-      getSipVersion() +
+      "  <li> <b> SIP </b> " + getSipVersion() +
       ": <a "
       "href=\"https://www.riverbankcomputing.com/software/sip\"><span style=\"color: "
-      "#0d47f1;\">www.riverbankcomputing.com/software/sip</span></a></li>"
-#endif
-      "<li><b>Font Awesome</b> " +
+											       "#0d47f1;\">www.riverbankcomputing.com/software/sip</span></a></li>" : "")
+      + "<li><b>Font Awesome</b> " +
       TulipFontAwesome::getVersion().c_str() +
       ": <a href=\"http://fontawesome.com\"><span style=\"color: #0d47f1;\">"
       "fontawesome.com</span></a></li>"

commit 201379ba1e58758c7ed15c68e59a3f5152eb96cf

fix regression bug introduced in commit 933aad9 (Python installation path cannot be found) + cleanup

library/tulip-gui/src/PythonVersionChecker.cpp
@@ -39,27 +39,6 @@ static const char *pythonVersion[] = {"3.9", "3.8", "3.7", "3.6", nullptr};
 
 #include <windows.h>
 
-#ifndef X86_64
-// function to check if a 32 bits program run on a 64 bits system
-static bool isWow64() {
-  BOOL bIsWow64 = FALSE;
-
-  typedef BOOL(APIENTRY * LPFN_ISWOW64PROCESS)(HANDLE, PBOOL);
-
-  LPFN_ISWOW64PROCESS fnIsWow64Process;
-
-  HMODULE module = GetModuleHandle(TEXT("kernel32"));
-  const char funcName[] = "IsWow64Process";
-  fnIsWow64Process = LPFN_ISWOW64PROCESS(GetProcAddress(module, funcName));
-
-  if (fnIsWow64Process != nullptr) {
-    fnIsWow64Process(GetCurrentProcess(), &bIsWow64);
-  }
-
-  return bIsWow64 != FALSE;
-}
-#endif
-
 #ifndef MSYS2_PYTHON
 // Check if a path is a valid Python Home, meaning it is not empty and contains the python
 // executable
@@ -86,34 +65,10 @@ static QString pythonHome(const QString &pythonVersion) {
 
   QString winRegKeyAllUsers, winRegKeyCurrentUser;
 
-// 32 bit Python
-#ifndef X86_64
-
-  // on windows 64 bit
-  if (isWow64()) {
-    winRegKeyAllUsers = QString("HKEY_LOCAL_MACHINE\\SOFTWARE\\Wow6432Node\\Python\\PythonCore\\") +
-                        pythonVersion + QString("\\InstallPath");
-    winRegKeyCurrentUser =
-        QString("HKEY_CURRENT_USER\\SOFTWARE\\Wow6432Node\\Python\\PythonCore\\") + pythonVersion +
-        QString("\\InstallPath");
-  }
-  // on windows 32 bit
-  else {
-    winRegKeyAllUsers = QString("HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\") +
-                        pythonVersion + QString("\\InstallPath");
-    winRegKeyCurrentUser = QString("HKEY_CURRENT_USER\\SOFTWARE\\Python\\PythonCore\\") +
-                           pythonVersion + QString("\\InstallPath");
-  }
-
-// 64 bit Python
-#else
-
   winRegKeyAllUsers = QString("HKEY_LOCAL_MACHINE\\SOFTWARE\\Python\\PythonCore\\") +
                       pythonVersion + QString("\\InstallPath");
   winRegKeyCurrentUser = QString("HKEY_CURRENT_USER\\SOFTWARE\\Python\\PythonCore\\") +
                          pythonVersion + QString("\\InstallPath");
-
-#endif
 
   QSettings winSettingsAllUsers(winRegKeyAllUsers, QSettings::NativeFormat);
   QSettings winSettingsCurrentUser(winRegKeyCurrentUser, QSettings::NativeFormat);
@@ -177,21 +132,8 @@ static QString getDefaultPythonVersionIfAny() {
               << "-c"
               << "import struct;import sys;sys.stdout.write(str(struct.calcsize('P')*8))");
       pythonProcess.waitForFinished(-1);
-      QString arch = pythonProcess.readAll();
-
-#ifdef IS_64BIT
-
-      if (arch != "64") {
+      if (pythonProcess.readAll() != "64")
         defaultPythonVersion = "";
-      }
-
-#else
-
-      if (arch != "32") {
-        defaultPythonVersion = "";
-      }
-
-#endif
     }
   }
 
@@ -263,8 +205,9 @@ bool PythonVersionChecker::isPythonVersionMatching() {
 
 #ifdef WIN32
 QString PythonVersionChecker::getPythonHome() {
+  QString pythonHomeDir;
   if (isPythonVersionMatching()) {
-    QString pythonHomeDir = pythonHome(compiledVersion());
+    pythonHomeDir = pythonHome(compiledVersion());
 // This is a hack for MinGW to allow the debugging of Tulip through GDB when compiled with Python
 // 3.X installed in a non standard way.
 #ifdef __MINGW32__
@@ -278,9 +221,7 @@ QString PythonVersionChecker::getPythonHome() {
     }
 
 #endif
-    return pythonHomeDir;
   }
-
-  return "";
+  return pythonHomeDir;
 }
 #endif

library/tulip-gui/src/TlpQtTools.cpp
@@ -369,24 +369,25 @@ void initTulipSoftware(tlp::PluginLoader *loader, bool removeDiscardedPlugins) {
 #if defined(__APPLE__)
   QApplication::addLibraryPath(QApplication::applicationDirPath() + "/../");
   QApplication::addLibraryPath(QApplication::applicationDirPath() + "/../lib/");
-#endif
-
-#if defined(TULIP_BUILD_PYTHON_COMPONENTS) && defined(WIN32)
+#elif defined(WIN32)
+#if defined(TULIP_BUILD_PYTHON_COMPONENTS)
 #if defined(__MINGW32__)
   // When using MSYS2 platform to compile Tulip, force the dynamic loading of
   // OpenSSL libraries Qt was compiled against before Python initialization to
   // avoid a DLL Hell on windows
   QSslSocket::supportsSsl();
-#elif (_WIN32_WINNT >= 0x0502)
+#endif
+#if (_WIN32_WINNT >= 0x0502)
   // MS stated that SetDllDirectory only exists since WinXP SP1
 
   // Python on windows can be installed for current user only.
   // In that case, the Python dll is not located in system path but in the Python home directory.
   // So add the Python home directory in the Dll search paths in order to be able to load plugins
   // depending on Python.
-  if (tlp::PythonVersionChecker::isPythonVersionMatching()) {
-    SetDllDirectory(tlp::QStringToTlpString(tlp::PythonVersionChecker::getPythonHome()).c_str());
-  }
+  auto pythonHome = tlp::PythonVersionChecker::getPythonHome();
+  if (!pythonHome.isEmpty())
+    SetDllDirectory(pythonHome.toUtf8().data());
+#endif
 #endif
 #endif
 