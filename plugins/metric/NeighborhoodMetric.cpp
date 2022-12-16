975d5fadbeb1544fa8c9dfa1eae469761731246e
pmary <patrick.mary@u-bordeaux.fr>
2022-12-16 14:32:46 +0100

    rework neighborhood metric plugin: give it a more explicit name + rename "consider loops" parameter + remove check() method + speedup the computation + doc enhancements

--- plugins/metric/NeighborhoodMetric.cpp
+++ plugins/metric/NeighborhoodMetric.cpp
@@ -18,78 +18,80 @@
  */
 #include <tulip/StringCollection.h>
 #include <tulip/StaticProperty.h>
-#include <tulip/GraphMeasure.h>
 #include <tulip/DoubleProperty.h>
-#include <tulip/SimpleTest.h>
+#include <tulip/GraphMeasure.h>
+#include <tulip/TreeTest.h>
+#include <unordered_set>
 
 using namespace tlp;
 
 static const char *paramHelp[] = {
     // type
-    "Type of edges to follow (in/out/inout).",
+    "Type of edges to consider (in/out/inout).",
 
-    // loops
-    "if true, reflexive edges (loops) are considered. By default, loops are not considered."};
+    // self loops
+    "if true, self loops are considered, i.e. a node is considered as its neighbor. By default, they are ignored."};
 
 #define NEIGH_TYPE "type"
 #define NEIGH_TYPES "InOut;In;Out"
-#define NEIGH_INOUT 0
-#define NEIGH_IN 1
-#define NEIGH_OUT 2
+#define NEIGH_INOUT UNDIRECTED
+#define NEIGH_IN INV_DIRECTED
+#define NEIGH_OUT DIRECTED
 
 //==============================================================================
 
 class NeighborhoodMetric : public tlp::DoubleAlgorithm {
 public:
 
-  PLUGININFORMATION("Neighborhood", "Bruno Pinaud", "15/12/2022", "Compute the number of unique neighboor for each node "
-                                                                  "in a graph with multipe edges between two nodes. "
-                                                                  "If the graph is simple, the Degree metric should be used instead.", "1.0", "Graph")
+  PLUGININFORMATION("Unique Neighbors", "Bruno Pinaud", "15/12/2022", "Compute the number of unique neighbors of each node; \"unique\" means that if multiple edges exist between two nodes, they count as one. When the graph is simple, it is equivalent to the degree computation.", "1.0", "Graph")
   NeighborhoodMetric(const tlp::PluginContext *context) : DoubleAlgorithm(context) {
     addInParameter<StringCollection>(NEIGH_TYPE, paramHelp[0], NEIGH_TYPES, true,
                                      "InOut <br> In <br> Out");
-    addInParameter<bool>("consider loops", paramHelp[1], "false");
+    addInParameter<bool>("self loops", paramHelp[1], "false");
   }
   //==================================================================
   bool run() override {
-    StringCollection neighborTypes(NEIGH_TYPES);
-    neighborTypes.setCurrent(0);
+    StringCollection neighTypes(NEIGH_TYPES);
+    neighTypes.setCurrent(0);
     bool loops(false);
+    NodeStaticProperty<double> num(graph);
 
     if (dataSet != nullptr) {
-      dataSet->get(NEIGH_TYPE, neighborTypes);
-      dataSet->get("consider loops", loops);
+      dataSet->get(NEIGH_TYPE, neighTypes);
+      dataSet->get("self loops", loops);
     }
 
-    NodeStaticProperty<double> num(graph);
-    switch (neighborTypes.getCurrent()) {
-    case NEIGH_INOUT:
-      TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
-        std::set<node> nd;
-        for (auto n2 : graph->getInOutNodes(n)) {
-          if (!loops && (n2 == n)) {
-            continue;
+    if (TreeTest::isFreeTree(graph))
+      tlp::degree(graph, num, static_cast<EDGE_TYPE>(neighTypes.getCurrent()));
+    else {
+      switch (neighTypes.getCurrent()) {
+      case NEIGH_INOUT:
+        TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
+          std::unordered_set<node> nd;
+          for (auto n2 : graph->getInOutNodes(n)) {
+            if (!loops && (n2 == n)) {
+              continue;
+            }
+            nd.insert(n2);
           }
-          nd.insert(n2);
-        }
-        num[i] = nd.size();
-      });
-      break;
-    case NEIGH_IN:
-      TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
-        std::set<node> nd;
-        for (auto n2 : graph->getInNodes(n)) {
-          if (!loops && (n2 == n)) {
-            continue;
+          num[i] = nd.size();
+        });
+        break;
+      case NEIGH_IN:
+        TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
+          std::unordered_set<node> nd;
+          for (auto n2 : graph->getInNodes(n)) {
+            if (!loops && (n2 == n)) {
+              continue;
+            }
+            nd.insert(n2);
           }
-          nd.insert(n2);
-        }
-        num[i] = nd.size();
-      });
-      break;
-    case NEIGH_OUT:
-      TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
-        std::set<node> nd;
+          num[i] = nd.size();
+        });
+        break;
+      case NEIGH_OUT:
+        TLP_PARALLEL_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
+        std::unordered_set<node> nd;
         for (auto n2 : graph->getOutNodes(n)) {
           if (!loops && (n2 == n)) {
             continue;
@@ -97,22 +99,13 @@ public:
           nd.insert(n2);
         }
         num[i] = nd.size();
-      });
-      break;
-    default:
-      break;
+        });
+        break;
+      default:
+        break;
+      }
     }
     num.copyToProperty(result);
-
-    return true;
-  }
-  //==================================================================
-  bool check(std::string &errorMsg) override {
-
-    if (SimpleTest::isSimple(graph, false)) {
-      errorMsg = "The graph is simple. Use the Degree metric plugin instead";
-      return false;
-    }
 
     return true;
   }
