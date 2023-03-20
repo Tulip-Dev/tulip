5061bd2a1be817239dcf60740f9f0ce3782328fc
pmary <patrick.mary@u-bordeaux.fr>
2023-03-20 15:32:53 +0100

    rework latest macports release installer

--- bundlers/mac/macports_init.sh
+++ bundlers/mac/macports_init.sh
@@ -7,24 +7,51 @@ MACOS_VERSION=$(sw_vers -productVersion | cut -f 1-2 -d .)
 # get major version
 MAJOR_VERSION=$(echo $MACOS_VERSION | cut -f 1 -d .)
 
-# for macOS 11 & 12 (Big Sur and Monterey) only major is needed
+# for macOS > 10 only major is needed
 if [ $MAJOR_VERSION -gt 10 ]
 then
     MACOS_VERSION=$MAJOR_VERSION
 fi
 
+# set MACOS_NAME
+case $MACOS_VERSION in
+    10.14)
+	MACOS_NAME=Mojave
+	;;
+    10.15)
+	MACOS_NAME=Catalina
+	;;
+    11)
+	MACOS_NAME=BigSur
+	;;
+    12)
+	MACOS_NAME=Monterey
+	;;
+    13)
+	MACOS_NAME=Ventura
+	;;
+    *)
+	echo "Unknown MACOSX version $MACOS_VERSION"
+        exit 1
+	;;
+esac
+
 # get macport latest release page
 curl -LO https://github.com/macports/macports-base/releases/latest
-# extract install url corresponding to MACOS_VERSION
-MACPORTS_URL=https://github.com$(grep 'href="/macports/macports-base/releases/download/' latest | grep '.pkg"' | grep "\-$MACOS_VERSION\-" | awk -F '\"' '{print $2}')
+# extract MACPORTS_VERSION
+MACPORTS_VERSION=$(grep '<title>' latest | awk '{print $3}')
+
+# set macports installer and url
+MACPORTS_INSTALLER=MacPorts-${MACPORTS_VERSION}-${MACOS_VERSION}-${MACOS_NAME}.pkg
+MACPORTS_INSTALLER_URL=https://github.com/macports/macports-base/releases/download/v${MACPORTS_VERSION}/${MACPORTS_INSTALLER}
 
 # download installer
-curl -LO $MACPORTS_URL
+curl -LO ${MACPORTS_INSTALLER_URL}
 # install MacPorts
-sudo installer -store -pkg MacPorts-*.pkg -target /
+sudo installer -store -pkg ${MACPORTS_INSTALLER} -target /
 # update it
 export PATH=/opt/local/bin:$PATH
 sudo port -d -N selfupdate
 
-# remove donwloaded files
-rm latest MacPorts-*.pkg
+# remove downloaded files
+rm latest ${MACPORTS_INSTALLER}
