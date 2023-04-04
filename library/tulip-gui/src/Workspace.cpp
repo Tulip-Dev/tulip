Tulip-Dev/tulip

commit 21a116a8e98a734ad9bd710a2974832c97f949b3

more tulip-gui cleanup for Qt6 compatibility

library/tulip-gui/src/Workspace.cpp
@@ -55,28 +55,15 @@ Workspace::Workspace(QWidget *parent)
 
   // This map allows us to know how much slots we have for each mode and which widget corresponds to
   // those slots
-  _modeToSlots[_ui->startupPage] = QVector<PlaceHolderWidget *>();
-  _modeToSlots[_ui->singlePage] = QVector<PlaceHolderWidget *>() << _ui->singlePage;
-  _modeToSlots[_ui->splitPage] = QVector<PlaceHolderWidget *>()
-                                 << _ui->splitPagePanel1 << _ui->splitPagePanel2;
-  _modeToSlots[_ui->splitPageHorizontal] = QVector<PlaceHolderWidget *>()
-                                           << _ui->splitPageHorizontalPanel1
-                                           << _ui->splitPageHorizontalPanel2;
-  _modeToSlots[_ui->split3Page] = QVector<PlaceHolderWidget *>()
-                                  << _ui->split3PagePanel1 << _ui->split3PagePanel2
-                                  << _ui->split3PagePanel3;
-  _modeToSlots[_ui->split32Page] = QVector<PlaceHolderWidget *>()
-                                   << _ui->split32Panel1 << _ui->split32Panel2
-                                   << _ui->split32Panel3;
-  _modeToSlots[_ui->split33Page] = QVector<PlaceHolderWidget *>()
-                                   << _ui->split33Panel1 << _ui->split33Panel2
-                                   << _ui->split33Panel3;
-  _modeToSlots[_ui->gridPage] = QVector<PlaceHolderWidget *>()
-                                << _ui->gridPagePanel1 << _ui->gridPagePanel2 << _ui->gridPagePanel3
-                                << _ui->gridPagePanel4;
-  _modeToSlots[_ui->sixPage] = QVector<PlaceHolderWidget *>()
-                               << _ui->sixMode1 << _ui->sixMode2 << _ui->sixMode3 << _ui->sixMode4
-                               << _ui->sixMode5 << _ui->sixMode6;
+  _modeToSlots[_ui->startupPage] = std::vector<PlaceHolderWidget *>();
+  _modeToSlots[_ui->singlePage] = std::vector<PlaceHolderWidget *> {_ui->singlePage};
+  _modeToSlots[_ui->splitPage] = std::vector<PlaceHolderWidget *> {_ui->splitPagePanel1 , _ui->splitPagePanel2};
+  _modeToSlots[_ui->splitPageHorizontal] = std::vector<PlaceHolderWidget *> {_ui->splitPageHorizontalPanel1, _ui->splitPageHorizontalPanel2};
+  _modeToSlots[_ui->split3Page] = std::vector<PlaceHolderWidget *> {_ui->split3PagePanel1, _ui->split3PagePanel2, _ui->split3PagePanel3};
+  _modeToSlots[_ui->split32Page] = std::vector<PlaceHolderWidget *> {_ui->split32Panel1, _ui->split32Panel2, _ui->split32Panel3};
+  _modeToSlots[_ui->split33Page] = std::vector<PlaceHolderWidget *> {_ui->split33Panel1, _ui->split33Panel2, _ui->split33Panel3};
+  _modeToSlots[_ui->gridPage] = std::vector<PlaceHolderWidget *> {_ui->gridPagePanel1, _ui->gridPagePanel2, _ui->gridPagePanel3, _ui->gridPagePanel4};
+  _modeToSlots[_ui->sixPage] = std::vector<PlaceHolderWidget *> {_ui->sixMode1, _ui->sixMode2, _ui->sixMode3, _ui->sixMode4, _ui->sixMode5, _ui->sixMode6};
 
   // This map allows us to know which widget can toggle a mode
   _modeSwitches[_ui->singlePage] = _ui->singleModeButton;
@@ -122,7 +109,7 @@ void Workspace::closeAll() {
   // if expose mode activated, close it before closing views to prevent a crash
   hideExposeMode();
 
-  QList<WorkspacePanel *> panels(_panels);
+  std::vector<WorkspacePanel *> panels(_panels);
   // iterate on a _panels clone to allow successive calls to panelDestroyed
   // which will do the cleanup job
   for (auto p : panels) {
@@ -131,8 +118,9 @@ void Workspace::closeAll() {
   emit panelsEmpty();
 }
 
-QList<tlp::View *> Workspace::panels() const {
-  QList<tlp::View *> result;
+std::vector<tlp::View *> Workspace::panels() const {
+  std::vector<tlp::View *> result;
+  result.reserve(_panels.size());
 
   for (auto panel : _panels) {
     result.push_back(panel->view());
@@ -202,15 +190,17 @@ int Workspace::addPanel(tlp::View *view) {
 }
 
 void Workspace::delView(tlp::View *view) {
+  unsigned int i = 0;
   for (auto it : _panels) {
     if (it->view() == view) {
       delete it;
-      _panels.removeOne(it);
+      _panels.erase(_panels.begin() + i);
       if (_panels.empty()) {
         emit panelsEmpty();
       }
       return;
     }
+    ++i;
   }
 }
 
@@ -220,13 +210,20 @@ void Workspace::panelDestroyed(QObject *obj) {
   if (panel == _focusedPanel)
     _focusedPanel = nullptr;
 
-  int removeCount = _panels.removeAll(panel);
+  unsigned int i = 0;
+  for (auto it : _panels) {
+    if (it == panel) {
+      _panels.erase(_panels.begin() + i);
+      break;
+    }
+    ++i;
+  }
 
   if (_panels.empty()) {
     emit panelsEmpty();
   }
 
-  if (removeCount == 0)
+  if (i > _panels.size())
     return;
 
   // To prevent segfaults due to Qt's event queue handling when deleting views, we reset the
@@ -341,11 +338,11 @@ QWidget *Workspace::currentModeWidget() const {
   return _ui->workspaceContents->currentWidget();
 }
 
-QVector<PlaceHolderWidget *> Workspace::currentModeSlots() const {
+std::vector<PlaceHolderWidget *> Workspace::currentModeSlots() const {
   return _modeToSlots[currentModeWidget()];
 }
 
-int Workspace::currentSlotsCount() const {
+unsigned int Workspace::currentSlotsCount() const {
   return currentModeSlots().size();
 }
 
@@ -367,7 +364,7 @@ void Workspace::updatePanels() {
     if (mode == currentModeWidget())
       continue;
 
-    QVector<PlaceHolderWidget *> panelSlots = _modeToSlots[mode];
+    std::vector<PlaceHolderWidget *> panelSlots = _modeToSlots[mode];
 
     for (auto panel : panelSlots) {
       panel->setWidget(nullptr);
@@ -376,12 +373,11 @@ void Workspace::updatePanels() {
 
   if (_currentPanelIndex > _panels.size() - currentSlotsCount())
     _currentPanelIndex = _panels.size() - currentSlotsCount();
-
-  if (_currentPanelIndex < 0)
+  else
     _currentPanelIndex = 0;
 
   //   Fill up slots according to the current index until there is no panel to show
-  int i = _currentPanelIndex;
+  unsigned int i = _currentPanelIndex;
 
   for (auto slt : currentModeSlots()) {
     if (i >= _panels.size()) {
@@ -411,7 +407,7 @@ void Workspace::updatePanels() {
   }
 
   if (!_modeSwitches[currentModeWidget()]->isEnabled()) {
-    int maxSize = 0;
+    unsigned int maxSize = 0;
     QWidget *fallbackMode = _ui->startupPage;
 
     // Current mode is not available, fallback to the largest available mode
@@ -447,8 +443,12 @@ void Workspace::setExposeModeSwitch(QAbstractButton *b) {
 }
 
 void Workspace::setActivePanel(tlp::View *view) {
-  int newIndex = panels().indexOf(view);
-  _currentPanelIndex = newIndex;
+  for (unsigned int i = 0; i < _panels.size(); ++i) {
+    if (_panels[i]->view() == view) {
+      _currentPanelIndex = i;
+      break;
+    }
+  }
   updatePanels();
 }
 
@@ -540,13 +540,7 @@ void Workspace::showExposeMode() {
   _ui->nextPageButton->setEnabled(false);
   _ui->previousPageButton->setEnabled(false);
 
-  QVector<WorkspacePanel *> panels;
-
-  for (auto p : _panels) {
-    panels << p;
-  }
-
-  _ui->exposeMode->setData(panels, _currentPanelIndex);
+  _ui->exposeMode->setData(_panels, _currentPanelIndex);
   _ui->workspaceContents->setCurrentWidget(_ui->exposePage);
   _exposeButton->setChecked(true);
 }
@@ -560,7 +554,7 @@ void Workspace::hideExposeMode() {
     return;
 
   _exposeButton->setChecked(false);
-  QVector<WorkspacePanel *> newPanels = _ui->exposeMode->panels();
+  std::vector<WorkspacePanel *> newPanels = _ui->exposeMode->panels();
   _panels.clear();
 
   for (auto p : newPanels)
@@ -584,11 +578,11 @@ QWidget *Workspace::suitableMode(QWidget *oldMode) {
   if (_modeSwitches.contains(oldMode) && _modeSwitches[oldMode]->isEnabled())
     return oldMode;
 
-  int maxSlots = 0;
+  unsigned int maxSlots = 0;
   QWidget *result = _ui->startupPage;
 
   for (auto mode : _modeToSlots.keys()) {
-    int slotCount = _modeToSlots[mode].size();
+    unsigned int slotCount = _modeToSlots[mode].size();
 
     if (slotCount <= _panels.size() && slotCount > maxSlots) {
       maxSlots = slotCount;
@@ -707,8 +701,8 @@ void Workspace::readProject(TulipProject *project, QMap<QString, Graph *> rootId
 
   if (doc.readNextStartElement()) {
     if (!doc.hasError()) {
-      int current = doc.attributes().value("current").toString().toInt();
-      int mode = doc.attributes().value("mode").toString().toInt();
+      unsigned int current = doc.attributes().value("current").toString().toUInt();
+      unsigned int mode = doc.attributes().value("mode").toString().toUInt();
 
       for (auto modeWidget : _modeToSlots.keys()) {
         if (_modeToSlots[modeWidget].size() == mode) {
@@ -762,8 +756,13 @@ void Workspace::swapPanelsRequested(WorkspacePanel *panel) {
   WorkspacePanel *sourcePanel = static_cast<WorkspacePanel *>(sender());
 
   if (sourcePanel) {
-    auto pb = _panels.begin();
-    std::iter_swap(pb + _panels.indexOf(sourcePanel), pb + _panels.indexOf(panel));
+    int i = 0;
+    for (auto it : _panels) {
+      if (it == panel)
+	_panels[i] = sourcePanel;
+      else if (it == sourcePanel)
+	_panels[i] = panel;
+    }
     updatePanels();
   }
 }

library/tulip-gui/include/tulip/GraphElementModel.h
@@ -28,8 +28,6 @@
 #include <tulip/TulipModel.h>
 #include <tulip/GraphModel.h>
 
-#include <QVector>
-
 namespace tlp {
 
 class TLP_QT_SCOPE GraphElementModel : public TulipModel {
@@ -59,7 +57,7 @@ public:
   }
 
 protected:
-  QVector<PropertyInterface *> getGraphProperties() const;
+  std::vector<PropertyInterface *> getGraphProperties() const;
 
   Graph *_graph;
   unsigned int _id;

library/tulip-gui/include/tulip/Workspace.h
@@ -23,7 +23,6 @@
 
 #include <QWidget>
 #include <QMap>
-#include <QVector>
 
 #include <tulip/tulipconf.h>
 
@@ -49,13 +48,13 @@ class TLP_QT_SCOPE Workspace : public QWidget {
   Q_OBJECT
 
   Ui::Workspace *_ui;
-  QList<WorkspacePanel *> _panels;
-  int _currentPanelIndex;
+  std::vector<WorkspacePanel *> _panels;
+  unsigned int _currentPanelIndex;
   QWidget *_oldWorkspaceMode;
   WorkspacePanel *_focusedPanel;
   bool _focusedPanelHighlighting;
 
-  QMap<QWidget *, QVector<PlaceHolderWidget *>> _modeToSlots;
+  QMap<QWidget *, std::vector<PlaceHolderWidget *>> _modeToSlots;
   QMap<QWidget *, QWidget *> _modeSwitches;
   GraphHierarchiesModel *_model;
   QAbstractButton *_exposeButton;
@@ -72,7 +71,7 @@ public:
   inline bool empty() const {
     return _panels.empty();
   }
-  QList<tlp::View *> panels() const;
+  std::vector<tlp::View *> panels() const;
 
 public slots:
   void delView(tlp::View *view);
@@ -149,8 +148,8 @@ protected:
   bool eventFilter(QObject *, QEvent *) override;
 
   QWidget *currentModeWidget() const;
-  QVector<PlaceHolderWidget *> currentModeSlots() const;
-  int currentSlotsCount() const;
+  std::vector<PlaceHolderWidget *> currentModeSlots() const;
+  unsigned int currentSlotsCount() const;
 
   void setFocusedPanel(WorkspacePanel *panel);
 };

library/tulip-gui/include/tulip/WorkspaceExposeWidget.h
@@ -65,7 +65,7 @@ class TLP_QT_SCOPE WorkspaceExposeWidget : public QGraphicsView {
   Q_OBJECT
 
   QAbstractAnimation *_positionAnimation;
-  QList<PreviewItem *> _items;
+  std::vector<PreviewItem *> _items;
   PreviewItem *_selectedItem;
   QGraphicsRectItem *_placeholderItem;
   bool _switchToSingleMode;
@@ -82,13 +82,13 @@ public:
 
   explicit WorkspaceExposeWidget(QWidget *parent = nullptr);
   ~WorkspaceExposeWidget() override;
-  int currentPanelIndex() const;
-  QVector<WorkspacePanel *> panels() const;
+  unsigned int currentPanelIndex() const;
+  std::vector<WorkspacePanel *> panels() const;
 
   bool isSwitchToSingleMode() const;
 
 public slots:
-  void setData(const QVector<WorkspacePanel *> &panels, int currentPanelIndex);
+  void setData(const std::vector<WorkspacePanel *> &panels, unsigned int currentPanelIndex);
 
 signals:
   void exposeFinished();

library/tulip-gui/src/AboutTulipPage.cpp
@@ -141,13 +141,13 @@ AboutTulipPage::AboutTulipPage(QWidget *parent)
 
   if (authorsFile.open(QFile::ReadOnly | QFile::Text)) {
     QTextStream in(&authorsFile);
-    in.setCodec("UTF-8");
+    in.setAutoDetectUnicode(true);
     _ui->authorsTextEdit->setText(in.readAll());
   }
 
   if (licenseFile.open(QFile::ReadOnly | QFile::Text)) {
     QTextStream in(&licenseFile);
-    in.setCodec("UTF-8");
+    in.setAutoDetectUnicode(true);
     _ui->licenseTextEdit->setText(in.readAll());
   }
 }

library/tulip-gui/src/GraphElementModel.cpp
@@ -88,8 +88,8 @@ Qt::ItemFlags GraphElementModel::flags(const QModelIndex &index) const {
 #endif
 }
 
-QVector<PropertyInterface *> GraphElementModel::getGraphProperties() const {
-  QVector<PropertyInterface *> properties;
+std::vector<PropertyInterface *> GraphElementModel::getGraphProperties() const {
+  std::vector<PropertyInterface *> properties;
   for (PropertyInterface *prop : _graph->getObjectProperties()) {
     auto propName = prop->getName();
     // an empty set indicates that all the properties are visible
@@ -99,7 +99,7 @@ QVector<PropertyInterface *> GraphElementModel::getGraphProperties() const {
     if (propName == "viewMetaGraph")
       continue;
 #endif
-    properties.append(prop);
+    properties.push_back(prop);
   }
   return properties;
 }

library/tulip-gui/src/WorkspaceExposeWidget.cpp
@@ -134,15 +134,16 @@ WorkspaceExposeWidget::~WorkspaceExposeWidget() {
   delete scene();
 }
 
-int WorkspaceExposeWidget::currentPanelIndex() const {
+unsigned int WorkspaceExposeWidget::currentPanelIndex() const {
   return _currentPanelIndex;
 }
 
-QVector<WorkspacePanel *> WorkspaceExposeWidget::panels() const {
-  QVector<WorkspacePanel *> result;
+std::vector<WorkspacePanel *> WorkspaceExposeWidget::panels() const {
+  std::vector<WorkspacePanel *> result;
+  result.reserve(_items.size());
 
   for (auto item : _items)
-    result << item->panel();
+    result.push_back(item->panel());
 
   return result;
 }
@@ -151,10 +152,11 @@ bool WorkspaceExposeWidget::isSwitchToSingleMode() const {
   return _switchToSingleMode;
 }
 
-void WorkspaceExposeWidget::setData(const QVector<WorkspacePanel *> &panels,
-                                    int currentPanelIndex) {
+void WorkspaceExposeWidget::setData(const std::vector<WorkspacePanel *> &panels,
+                                    unsigned int currentPanelIndex) {
   scene()->clear();
   _items.clear();
+  _items.reserve(panels.size());
 
   for (auto p : panels) {
     QPixmap pixmap = p->view()
@@ -162,7 +164,7 @@ void WorkspaceExposeWidget::setData(const QVector<WorkspacePanel *> &panels,
                          .scaled(previewSize(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     PreviewItem *item = new PreviewItem(pixmap, p);
     scene()->addItem(item);
-    _items << item;
+    _items.push_back(item);
     item->installEventFilter(this);
     connect(item, SIGNAL(opened()), this, SLOT(itemOpened()));
   }
@@ -246,7 +248,12 @@ bool WorkspaceExposeWidget::eventFilter(QObject *obj, QEvent *ev) {
 
   if (ev->type() == QEvent::GraphicsSceneMousePress) {
     if (item->shouldClose(static_cast<QGraphicsSceneMouseEvent *>(ev)->pos())) {
-      _items.removeAll(item);
+      // remove from _items
+      for (unsigned int i = 0; i < _items.size(); ++i)
+	if (item == _items[i]) {
+	  _items.erase(_items.begin() + i);
+	  break;
+	}
       item->panel()->close();
       item->deleteLater();
 
@@ -266,23 +273,25 @@ bool WorkspaceExposeWidget::eventFilter(QObject *obj, QEvent *ev) {
     if (ev->type() == QEvent::GraphicsSceneMouseMove) {
       QGraphicsSceneMouseEvent *mouseEv = static_cast<QGraphicsSceneMouseEvent *>(ev);
       QPointF itemPos = mouseEv->scenePos();
-      int itemPerLine = floor(width() / (previewSize().width() + MARGIN));
-      int nbLines = _items.size() / itemPerLine;
-      int line = itemPos.y() / (previewSize().height() + MARGIN);
+      unsigned int itemPerLine =
+	floor(width() / (previewSize().width() + MARGIN));
+      unsigned int nbLines = _items.size() / itemPerLine;
+      unsigned int line = itemPos.y() / (previewSize().height() + MARGIN);
       line = std::min<int>(nbLines, line);
-      int col = itemPos.x() / (previewSize().width() + MARGIN);
-      int index = line * itemPerLine + col;
+      unsigned int col = itemPos.x() / (previewSize().width() + MARGIN);
+      unsigned int index = line * itemPerLine + col;
 
-      if (index != _items.indexOf(item)) {
-        _items.removeOne(item);
-
-        if (index < 0)
-          index = 0;
+      unsigned int i = 0;
+      for(; i < _items.size(); ++i)
+	if (item == _items[i])
+	  break;
+      if (index != i) {
+        _items.erase(_items.begin() + i);
 
         if (index > _items.size())
           index = _items.size();
 
-        _items.insert(index, item);
+        _items.insert(_items.begin() + index, item);
         updatePositions(false);
       }
     } else if (ev->type() == QEvent::GraphicsSceneMouseRelease) {
@@ -308,7 +317,11 @@ bool WorkspaceExposeWidget::event(QEvent *event) {
 
 void WorkspaceExposeWidget::itemOpened() {
   PreviewItem *item = static_cast<PreviewItem *>(sender());
-  _currentPanelIndex = _items.indexOf(item);
+  for(unsigned int i = 0; i < _items.size(); ++i)
+    if (item == _items[i]) {
+      _currentPanelIndex = i;
+      break;
+    }
   _switchToSingleMode = true;
   finish();
 }

library/tulip-python/bindings/tulip-gui/TulipViewsUtils.cpp
@@ -68,8 +68,7 @@ std::vector<tlp::View *> TulipViewsManager::getOpenedViews() {
   tlp::Workspace *workspace = tlpWorkspace();
 
   if (workspace) {
-    QList<tlp::View *> views = workspace->panels();
-    return views.toVector().toStdVector();
+    return workspace->panels();
   } else {
     return openedViews;
   }
@@ -148,11 +147,11 @@ std::vector<tlp::View *> TulipViewsManager::getViewsOfGraph(tlp::Graph *graph) {
   std::vector<tlp::View *> ret;
 
   if (workspace) {
-    QList<tlp::View *> views = workspace->panels();
+    std::vector<tlp::View *> views = workspace->panels();
 
-    for (int i = 0; i < views.count(); ++i) {
-      if (views.at(i)->graph() == graph) {
-        ret.push_back(views.at(i));
+    for (unsigned int i = 0; i < views.size(); ++i) {
+      if (views[i]->graph() == graph) {
+        ret.push_back(views[i]);
       }
     }
   } else {
@@ -170,11 +169,11 @@ void TulipViewsManager::closeAllViews() {
   tlp::Workspace *workspace = tlpWorkspace();
 
   if (workspace) {
-    QList<tlp::View *> views = workspace->panels();
+    std::vector<tlp::View *> views = workspace->panels();
 
     for (int i = 0; i < views.size(); ++i) {
-      if (views.at(i)->name() != "Python Script view") {
-        workspace->delView(views.at(i));
+      if (views[i]->name() != "Python Script view") {
+        workspace->delView(views[i]);
       }
     }
   } else {
@@ -190,11 +189,11 @@ void TulipViewsManager::closeViewsRelatedToGraph(tlp::Graph *graph) {
   tlp::Workspace *workspace = tlpWorkspace();
 
   if (workspace) {
-    QList<tlp::View *> views = workspace->panels();
+    std::vector<tlp::View *> views = workspace->panels();
 
-    for (int i = 0; i < views.count(); ++i) {
-      if (views.at(i)->graph() == graph) {
-        workspace->delView(views.at(i));
+    for (int i = 0; i < views.size(); ++i) {
+      if (views[i]->graph() == graph) {
+        workspace->delView(views[i]);
       }
     }
   } else {

commit 527ab676e87bdec0f83646bd47dea96f013c503f

fix for workspace panels swapping failing after since commit 21a116a8

library/tulip-gui/src/Workspace.cpp
@@ -763,6 +763,7 @@ void Workspace::swapPanelsRequested(WorkspacePanel *panel) {
         _panels[i] = sourcePanel;
       else if (it == sourcePanel)
         _panels[i] = panel;
+      ++i;
     }
     updatePanels();
   }