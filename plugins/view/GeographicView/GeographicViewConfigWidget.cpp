111d76a1caa14e9279ac39cfa16d23b087b9e6f4
tulipdev <tulipdev@labri.fr>
2023-06-26 23:30:08 +0200

    [Jenkins] clang-format commit (26/06/23)

--- plugins/view/GeographicView/GeographicViewConfigWidget.cpp
+++ plugins/view/GeographicView/GeographicViewConfigWidget.cpp
@@ -60,12 +60,13 @@ void GeographicViewConfigWidget::openCsvHelp() {
 }
 
 void GeographicViewConfigWidget::openPolyHelp() {
-  QMessageBox msgB(QMessageBox::NoIcon, "Map poly files",
-                   ".poly files format are an open street map "
-                   "format.\nYou can download .poly files from :<br/>"
-                   "<a href=\"https://github.com/jameschevalier/cities\" style=\"color:" HTML_LINK_COLOR ";\">https://github.com/jameschevalier/cities</a>",
-                   QMessageBox::Ok,
-                   Perspective::instance()->mainWindow()->centralWidget());
+  QMessageBox msgB(
+      QMessageBox::NoIcon, "Map poly files",
+      ".poly files format are an open street map "
+      "format.\nYou can download .poly files from :<br/>"
+      "<a href=\"https://github.com/jameschevalier/cities\" style=\"color:" HTML_LINK_COLOR
+      ";\">https://github.com/jameschevalier/cities</a>",
+      QMessageBox::Ok, Perspective::instance()->mainWindow()->centralWidget());
   msgB.setTextFormat(Qt::RichText);
   msgB.exec();
 }
