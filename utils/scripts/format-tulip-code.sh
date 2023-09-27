dd995463cadccc4472821666616046cefa46fec5
Bruno Pinaud <bruno.pinaud@u-bordeaux.fr>
2023-09-27 16:15:37 +0200

    remove all references to the old demos folder

--- utils/scripts/format-tulip-code.sh
+++ utils/scripts/format-tulip-code.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-find ./demos/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
 find ./library/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
 find ./plugins/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
 find ./software/ -iname *.h -o -iname *.cpp -o -iname *.cxx | xargs clang-format -i
