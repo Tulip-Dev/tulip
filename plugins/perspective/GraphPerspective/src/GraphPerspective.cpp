Tulip-Dev/tulip

commit daf22d2625678523e0614bf5bd7ac388a9af9253

TulipMainWindow window title enhacements: no need for perspective name if it "Tulip" + add current graph name to improve discrimination between running perspectives

plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -1162,6 +1162,10 @@ tlp::GraphHierarchiesModel *GraphPerspective::model() const {
   return _graphs;
 }
 
+Graph *GraphPerspective::currentGraph() {
+  return _graphs->currentGraph();
+}
+
 void GraphPerspective::refreshDockExpandControls() {
   QList<HeaderFrame *> expandedHeaders, collapsedHeaders;
 

software/tulip_perspective/src/TulipPerspectiveMainWindow.cpp
@@ -21,6 +21,7 @@
 #include <QCloseEvent>
 #include <QShortcut>
 #include <QAction>
+#include <tulip/Graph.h>
 #include <tulip/Perspective.h>
 #include <tulip/TulipProject.h>
 
@@ -45,7 +46,10 @@ void TulipPerspectiveProcessMainWindow::setProject(tlp::TulipProject *project) {
 void TulipPerspectiveProcessMainWindow::projectFileChanged(const QString &projectFile) {
   QString wTitle(_title);
 
-  wTitle += QString(" [") + _project->perspective() + "]";
+  auto perspective = _project->perspective();
+  // add perspective name if is not a GraphPerspective
+  if (perspective != "Tulip")
+    wTitle += QString(" [") + _project->perspective() + "]";
 
   if (!_project->name().isEmpty())
     wTitle += QString(" - ") + _project->name();
@@ -53,7 +57,11 @@ void TulipPerspectiveProcessMainWindow::projectFileChanged(const QString &projec
     wTitle += QString(" - ") + projectFile;
   else { // all graphs are deleted. Clear project. Useful?
     _project->clearProject();
-    wTitle += QString(" - unsaved project");
+    wTitle += QString(" - ");
+    tlp::Graph *cg = tlp::Perspective::instance() ? tlp::Perspective::instance()->currentGraph() : nullptr;
+    if (cg)
+      wTitle += QString(cg->getName().c_str()) + ' ';
+    wTitle += QString("(unsaved project)");
   }
 
   wTitle += "[*]"; // placeholder for window modification

library/tulip-gui/include/tulip/Perspective.h
@@ -34,6 +34,7 @@ class QTcpSocket;
 
 namespace tlp {
 
+class Graph;
 class PluginProgress;
 class TulipProject;
 
@@ -205,6 +206,13 @@ public:
    */
   virtual void usage(std::string &usage_str) const {
     usage_str = "No options for this perspective.";
+  }
+
+  /**
+   * @return The Perspective's current graph.
+   */
+  virtual Graph *currentGraph() {
+    return nullptr;
   }
 
   /**

plugins/perspective/GraphPerspective/include/GraphHierarchiesEditor.h
@@ -68,6 +68,7 @@ class GraphHierarchiesEditor : public QWidget, tlp::Observable {
 public:
   explicit GraphHierarchiesEditor(QWidget *parent = nullptr);
   ~GraphHierarchiesEditor() override;
+  tlp::Graph *currentGraph();
   void setModel(tlp::GraphHierarchiesModel *model);
   bool synchronized() const;
   void treatEvents(const std::vector<tlp::Event> &) override;

plugins/perspective/GraphPerspective/include/GraphPerspective.h
@@ -32,6 +32,7 @@ class GraphHierarchiesEditor;
 class GraphPerspectiveLogger;
 
 namespace tlp {
+class Graph;
 class GraphHierarchiesModel;
 class View;
 class BooleanProperty;
@@ -81,6 +82,7 @@ public:
   ~GraphPerspective() override;
   void start(tlp::PluginProgress *) override;
   tlp::GraphHierarchiesModel *model() const;
+  tlp::Graph *currentGraph() override;
   void copy(tlp::Graph *, bool deleteAfter = false);
   tlp::Graph *createSubGraph(tlp::Graph *);
   QAction *createPanelAction();

plugins/perspective/GraphPerspective/src/GraphHierarchiesEditor.cpp
@@ -153,6 +153,10 @@ void GraphHierarchiesEditor::setModel(tlp::GraphHierarchiesModel *model) {
           SLOT(currentGraphChanged(tlp::Graph *)));
 }
 
+Graph *GraphHierarchiesEditor::currentGraph() {
+  return _model->currentGraph();
+}
+
 GraphHierarchiesEditor::~GraphHierarchiesEditor() {
   delete _ui;
 }
@@ -283,6 +287,7 @@ void GraphHierarchiesEditor::currentGraphChanged(Graph *graph) {
       selection->addObserver(this);
     _currentSelection = selection;
   }
+  GraphPerspective::typedInstance<GraphPerspective>()->resetTitle();
   updateSelectionInfos();
 }
 

commit d51295b86fcfc4e8b4509731275ecab2d000d6a1

fix for views creation failure when loading tlpx files (introduced in commit daf22d26)

plugins/perspective/GraphPerspective/src/GraphPerspective.cpp
@@ -1405,6 +1405,9 @@ bool GraphPerspective::saveAs(const QString &path) {
     _project->setProjectFile(path);
   }
 
+  // cleanup previous loading or saving infos
+  _project->clearProject();
+
   SimplePluginProgressDialog progress(_mainWindow);
   progress.showPreview(false);
   progress.show();

software/tulip_perspective/src/TulipPerspectiveMainWindow.cpp
@@ -64,8 +64,7 @@ void TulipPerspectiveProcessMainWindow::projectFileChanged(const QString &projec
     wTitle += QString(" - ") + _project->name();
   else if (!projectFile.isEmpty())
     wTitle += QString(" - ") + projectFile;
-  else { // all graphs are deleted. Clear project. Useful?
-    _project->clearProject();
+  else {
     wTitle += QString(" - ");
     tlp::Graph *cg =
         tlp::Perspective::instance() ? tlp::Perspective::instance()->currentGraph() : nullptr;