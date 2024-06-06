199fb555864a69250715a7f00507fe87125a5b4e
pmary <patrick.mary@u-bordeaux.fr>
2024-06-06 09:33:08 +0200

    add file missing from my previous commit

--- library/tulip-gui/include/tulip/MouseShowElementInfo.h
+++ library/tulip-gui/include/tulip/MouseShowElementInfo.h
@@ -94,6 +94,8 @@ protected:
    */
   virtual QString elementName(ElementType elementType, unsigned int elementId) const;
 
+  void buildAndShowInfos(const SelectedEntity &selectedEntity, QPoint position);                            
+
 private slots:
 
   void showVisualProp(bool show);
