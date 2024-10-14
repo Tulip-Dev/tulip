5e864600f11903f4fc48defc1aa1ac2fd7d295f4
pmary <patrick.mary@u-bordeaux.fr>
2024-10-14 17:40:52 +0200

    MCLClustering fix and performance improvements

--- plugins/metric/MCLClustering.cpp
+++ plugins/metric/MCLClustering.cpp
@@ -85,15 +85,19 @@ const double epsilon = 1E-9;
 void MCLClustering::power(node n) {
   std::unordered_map<node, double> newTargets;
 
-  for (auto e1 : g.getOutEdges(n)) {
-    double v1 = inW[e1];
+  for (auto &adjn : g.adj(n)) {
+    if (!adjn.isOut())
+      continue;
+    double vn = inW[adjn.link()];
 
-    if (v1 > epsilon) {
-      for (auto e2 : g.getOutEdges(g.target(e1))) {
-        double v2 = inW[e2] * v1;
+    if (vn > epsilon) {
+      for (auto &adjo : g.adj(adjn.opposite())) {
+        if (!adjo.isOut())
+          continue;
+        double v2 = inW[adjo.link()] * vn;
 
         if (v2 > epsilon) {
-          node tgt = g.target(e2);
+          node tgt = adjo.opposite();
           edge ne = g.existEdge(n, tgt, true);
 
           if (ne.isValid())
@@ -140,7 +144,10 @@ void MCLClustering::prune(node n) {
   // - avoid a costly stable iteration when deleting edges
   std::vector<pair<double, edge>> pvect;
   pvect.reserve(outdeg);
-  for (auto e : g.getOutEdges(n)) {
+  for (auto &adj : g.adj(n)) {
+    if (!adj.isOut())
+      continue;
+    auto e = adj.link();
     pvect.push_back(pair<double, edge>(outW[e], e));
   }
 
@@ -167,7 +174,10 @@ bool MCLClustering::inflate(double r, unsigned int k, node n, bool equal
   pvect.reserve(sz);
 
   double sum = 0.;
-  for (auto e : g.getOutEdges(n)) {
+  for (auto &adj : g.adj(n)) {
+    if (!adj.isOut())
+      continue;
+    auto e = adj.link();
     double outVal = outW[e];
     sum += pow(outVal, r);
     pvect.push_back(pair<double, edge>(outVal, e));
@@ -290,9 +300,6 @@ struct DegreeSort {
 //==============================================================================
 bool MCLClustering::run() {
 
-  g.alloc(inW);
-  g.alloc(outW);
-
   weights = nullptr;
   _r = 2.;
   _k = 5;
@@ -306,17 +313,19 @@ bool MCLClustering::run() {
   NodeStaticProperty<node> nodeMapping(graph);
   const std::vector<node> &tlpNodes = graph->nodes();
   unsigned int nbNodes = tlpNodes.size();
-  g.reserveNodes(nbNodes);
+  g.addNodes(nbNodes);
+  g.alloc(inW);
+  g.alloc(outW);
 
   // add nodes to g
   TLP_MAP_NODES_AND_INDICES(graph, [&](const node n, unsigned int i) {
-    g.reserveAdj(nodeMapping[i] = g.addNode(), 2 * graph->deg(n) + 1);
+    g.reserveAdj(nodeMapping[i] = node(i), 2 * graph->deg(n) + 1);
   });
 
   for (auto e : graph->edges()) {
     auto eEnds = graph->ends(e);
-    node src = nodeMapping[eEnds.first];
-    node tgt = nodeMapping[eEnds.second];
+    node src = nodeMapping[graph->nodePos(eEnds.first)];
+    node tgt = nodeMapping[graph->nodePos(eEnds.second)];
     edge tmp = g.addEdge(src, tgt);
 
     double weight = (weights != nullptr) ? weights->getEdgeDoubleValue(e) : 1.0;
@@ -337,7 +346,10 @@ bool MCLClustering::run() {
 
     if (weights != nullptr) {
       double tmpVal = inW[tmp] = 0.;
-      for (auto e : g.getOutEdges(n)) {
+      for (auto &adj : g.adj(n)) {
+        if (!adj.isOut())
+          continue;
+        auto e = adj.link();
         double eVal = inW[e];
         sum += eVal;
 
@@ -351,8 +363,11 @@ bool MCLClustering::run() {
     }
 
     double oos = 1. / sum;
-    for (auto e : g.getOutEdges(n))
-      inW[e] *= oos;
+    for (auto &adj : g.adj(n)) {
+      if (!adj.isOut())
+        continue;
+      inW[adj.link()] *= oos;
+    }
   }
 
   // output for mcl
